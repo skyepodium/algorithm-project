075eaa45cb4492aac20227f643f429d22aca3973
김정윤 <skyepodium@gimjeong-yun-ui-MacBook-Pro.local>
2017-06-13 21:56:58 +0900

    06.13

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -7,15 +7,24 @@ int main(void){
     int t;
     cin >> t;
     
-    for (int a = 0; a <= t; a++){
-        for(int c= 0; c< a; c++){
+    for (int a = 1; a < t; a++){
+        for(int c= 1; c<=t-a; c++){
             cout<<" ";
         }
-
-        for(int b = 0; b < (t-a);b++){
+        
+        for(int k=1; k < 2*a; k++){
             cout<<"*";
         }
-        
+        cout<<"\n";
+    }
+    
+    for (int a=0; a < t; a++){
+        for(int b=0; b < a; b++){
+            cout<< " ";
+        }
+        for(int c=0; c <= 2*(t-1)-a*2; c++){
+            cout<< "*";
+        }
         cout<<"\n";
     }
 }
