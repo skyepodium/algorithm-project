f9beb28612797bc0da65e35b4dd9d2d7498bfb69
김정윤 <skyepodium@gmail.com>
2017-06-15 23:39:39 +0900

    06.15

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -4,12 +4,39 @@ using namespace std;
 int main(void){
     ios::sync_with_stdio(false);
     
-    //int t;
-    //cin >> t;
-    char a[100];
+    int t;
+    cin >> t;
     
-    while(fgets(a, 100, stdin)){
-        cout<<a;
+    
+    for (int a=0; a < t; a++){
+        
+        if(t%2 == 0){
+            for(int b=a; b < t-1; b++){
+                cout<<" ";
+            }
+            for(int c=t; c < t+2*(a)+1; c++){
+                if(c%2 == 0 ){
+                    cout<<"*";
+                }else{
+                    cout<<" ";
+                }
+            }
+            cout<<"\n";
+        }else{
+            
+            for(int b=a; b< t-1; b++){
+                cout<<" ";
+            }
+            for(int c=t; c< t+2*(a)+1; c++){
+                if(c%2 == 1 ){
+                    cout<<"*";
+                }else{
+                    cout<<" ";
+                }
+                
+            }
+            cout<<"\n";
+        }
     }
 }
 
