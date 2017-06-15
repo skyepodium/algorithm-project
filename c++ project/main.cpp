d074137bf4814c7c8ee2336039a37fbac7bb882d
김정윤 <skyepodium@gimjeong-yun-ui-MacBook-Pro.local>
2017-06-15 23:36:31 +0900

    06.15

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -4,31 +4,13 @@ using namespace std;
 int main(void){
     ios::sync_with_stdio(false);
     
-    int t;
-    cin >> t;
+    //int t;
+    //cin >> t;
+    char a[100];
     
-    
-    for (int a=0; a < t; a++){
-        for(int b=0; b < a; b++){
-            cout<< " ";
-        }
-        for(int c=0; c <= 2*(t-1)-a*2; c++){
-            cout<< "*";
-        }
-        cout<<"\n";
+    while(fgets(a, 100, stdin)){
+        cout<<a;
     }
-    
-    for (int a = 2; a <= t; a++){
-        for(int c= 1; c<=t-a; c++){
-            cout<<" ";
-        }
-        
-        for(int k=1; k < 2*a; k++){
-            cout<<"*";
-        }
-        cout<<"\n";
-    }
-
 }
 
 
