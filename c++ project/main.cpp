64f2348980f9902c1a21deedaa567eb6a3ec1d94
김정윤 <skyepodium@gmail.com>
2017-07-04 23:59:05 +0900

    07.04

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,35 +1,57 @@
 #include <iostream>
-#include <queue>
+#include <deque>
 #include <string>
 using namespace std;
 int main() {
     int n;
-    int m;
-    
     int num = 0;
-    
     cin>>n;
-    cin>>m;
-    queue<int> s;
     
-    for(int i=1; i<=n; i++){
-        s.push(i);
-    }
-
-    cout<<"<";
+    deque<int> s;
+    
     while(n--){
-        for(int i=0; i<m; i++){
-        num = s.front();
-        s.pop();
-            
-            if(i<m-1){
-            s.push(num);
+        string cmd;
+        cin>>cmd;
+        
+        if(cmd == "push_back"){
+            cin>>num;
+            s.push_back(num);
+        } else if (cmd == "push_front"){
+            cin>>num;
+            s.push_front(num);
+        } else if (cmd == "size"){
+            cout<<s.size()<<endl;
+        } else if (cmd == "empty"){
+            cout<<s.empty()<<endl;
+        } else if (cmd == "front"){
+            if(s.size() == 0){
+                cout<<-1<<endl;
+            }else{
+                cout<<s.front()<<endl;
+            }
+        } else if (cmd == "back"){
+            if(s.size() == 0){
+                cout<<-1<<endl;
+            }else{
+                cout<<s.back()<<endl;
+            }
+        } else if (cmd == "pop_front"){
+            if(s.size() == 0){
+                cout<<-1<<endl;
+            }else{
+                cout<<s.front()<<endl;
+                s.pop_front();
+            }
+        } else{
+            if(s.size() == 0){
+                cout<<-1<<endl;
+            }else{
+                cout<<s.back()<<endl;
+                s.pop_back();
             }
         }
-        cout<<num;
-        if(n!=0){
-            cout<<", ";
-        }
+        
+        
+        
     }
-    cout<<">"<<endl;
 }
