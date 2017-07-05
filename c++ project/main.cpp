e0e04661cd8feedbec5a7c3fb6bc2ab0621553ac
김정윤 <skyepodium@gmail.com>
2017-07-05 19:45:53 +0900

    07.05

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -2,56 +2,11 @@
 #include <deque>
 #include <string>
 using namespace std;
+
 int main() {
-    int n;
-    int num = 0;
-    cin>>n;
+
+    int a, b;
+    cin>>a>>b;
     
-    deque<int> s;
-    
-    while(n--){
-        string cmd;
-        cin>>cmd;
-        
-        if(cmd == "push_back"){
-            cin>>num;
-            s.push_back(num);
-        } else if (cmd == "push_front"){
-            cin>>num;
-            s.push_front(num);
-        } else if (cmd == "size"){
-            cout<<s.size()<<endl;
-        } else if (cmd == "empty"){
-            cout<<s.empty()<<endl;
-        } else if (cmd == "front"){
-            if(s.size() == 0){
-                cout<<-1<<endl;
-            }else{
-                cout<<s.front()<<endl;
-            }
-        } else if (cmd == "back"){
-            if(s.size() == 0){
-                cout<<-1<<endl;
-            }else{
-                cout<<s.back()<<endl;
-            }
-        } else if (cmd == "pop_front"){
-            if(s.size() == 0){
-                cout<<-1<<endl;
-            }else{
-                cout<<s.front()<<endl;
-                s.pop_front();
-            }
-        } else{
-            if(s.size() == 0){
-                cout<<-1<<endl;
-            }else{
-                cout<<s.back()<<endl;
-                s.pop_back();
-            }
-        }
-        
-        
-        
-    }
+    cout<<a*b;
 }
