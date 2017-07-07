ce471f416f31b6f80a58784d1d0601ccb74fc98c
김정윤 <skyepodium@gmail.com>
2017-07-07 18:30:04 +0900

    07.07

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,23 +1,39 @@
 #include <iostream>
 #include <queue>
+#include <stack>
 #include <string>
 #include <algorithm>
 using namespace std;
 int main() {
     
-    int a = 0;
+    int a;
     cin>>a;
-    while(a--){
-        int b;
-        string c;
-        cin>>b;
-        cin>>c;
-        
-        for(int i=0; i<c.size(); i++){
-            for(int j=0; j<b; j++){
-                cout<<c[i];
-            }
-        }
-        cout<<" ";
+    
+    long total =1;
+    for(int i=1; i<=a; i++){
+        total=total*i;
     }
+    string word =to_string(total);
+    
+    cout<<total<<endl;
+    for(int i=0; i<word.size(); i++){
+
+        if(a ==0){
+            cout<<1;
+            break;
+        }
+        
+        if(word[word.size()-1]!=48){
+            cout<<0;
+            break;
+        }
+
+        total = total/10;
+        
+        if(total%10 != 0){
+            cout<<i+1<<endl;
+            break;
+        }
+    }
+    
 }
