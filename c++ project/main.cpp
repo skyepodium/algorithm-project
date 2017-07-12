d03fd8ea475de7b4a3eca3ab8aa8c0952a114f5a
김정윤 <skyepodium@gmail.com>
2017-07-12 23:26:53 +0900

    07.12

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -6,22 +6,39 @@
 #include <vector>
 using namespace std;
 
+int gcd(int a, int b){
+    
+    if(b==0){
+        return a;
+    }else{
+        return gcd(b, a%b);
+    }
+}
+
 int main(){
+    
+    
     int n;
     cin>>n;
     
-    int p[10001];
-    p[0] = 0;
-    for(int i=1; i<=n; i++){
-        cin>>p[i];
-    }
-    
-    int d[1001];
-    d[0] = 0;
-    for(int i=1; i<=n; i++){
-        for(int j=1; j<=i; j++){
-            d[i] = max(d[i], d[i-j]+p[j]);
+    while(n--){
+
+        int num;
+        cin>>num;
+        
+        int ar[num];
+        for(int i=0; i<num; i++){
+            cin>>ar[i];
         }
+        
+        
+        int total = 0;
+        for(int i=0; i<num-1; i++){
+            for(int j=i+1; j<num; j++){
+                total+=gcd(ar[i], ar[j]);
+            }
+        }
+        cout<<total<<endl;
+
     }
-    cout<<d[n]<<endl;
 }
