0908babdd237b8f399c0fbd4d14689b97127f293
김정윤 <skyepodium@gmail.com>
2017-09-03 14:09:02 +0900

    09.03 카카오 블라인드 테스트

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -6,18 +6,40 @@
 #include <vector>
 using namespace std;
 
-int main(){
 
-//    vector<int> arr = {{1,4},{3,4},{3,10}};
-//    vector<vector<int>> = {{1,4},{3,4},{3,10}};
-    int arr[3][2] = {1,4,3,4,3,10};
-/*
-    arr[0][0] = 1;
-    arr[0][1] = 4;
-    arr[1][0] = 3;
-    arr[1][1] = 4;
-    arr[2][0] = 3;
-    arr[2][1] = 10;
- */
-    cout<<arr[0][1]<<endl;
+int main(){
+    vector<vector<int>> v(3, vector<int>(2,0));
+    
+    v[0][0] = 1;
+    v[0][1] = 4;
+    v[1][0] = 3;
+    v[1][1] = 4;
+    v[2][0] = 3;
+    v[2][1] = 10;
+
+    vector<int> ans;
+
+    
+    for(int i=0; i<2; i++){
+        int sum=0;
+        for(int j=0; j<2; j++){
+            if(v[j][i] != v[j+1][i]){
+                sum=sum+1;
+            }
+        }
+        if(sum == 2){
+            ans.push_back(v[1][i]);
+        }else{
+            if(v[0][i] == v[1][i]){
+                ans.push_back(v[2][i]);
+            }else{
+                ans.push_back(v[0][i]);
+            }
+        }
+    }
+
+    
+    for(int i=0; i<ans.size(); i++){
+        cout<<ans[i]<<endl;
+    }
 }
