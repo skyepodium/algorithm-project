d2687366947008d682b4efce1332e1b30fe130f7
김정윤 <skyepodium@gmail.com>
2017-09-04 14:19:52 +0900

    09.04 카카오 블라인드 테스트

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -8,38 +8,40 @@ using namespace std;
 
 
 int main(){
-    vector<vector<int>> v(3, vector<int>(2,0));
+    vector<vector<int>> board(4, vector<int>(4,0));
     
-    v[0][0] = 1;
-    v[0][1] = 4;
-    v[1][0] = 3;
-    v[1][1] = 4;
-    v[2][0] = 3;
-    v[2][1] = 10;
+    board[0][0] = 0;
+    board[0][1] = 1;
+    board[0][2] = 1;
+    board[0][3] = 1;
+    board[1][0] = 1;
+    board[1][1] = 1;
+    board[1][2] = 1;
+    board[1][3] = 1;
+    board[2][0] = 1;
+    board[2][1] = 1;
+    board[2][2] = 1;
+    board[2][3] = 1;
+    board[3][0] = 0;
+    board[3][1] = 0;
+    board[3][2] = 1;
+    board[3][3] = 0;
 
-    vector<int> ans;
-
-    
-    for(int i=0; i<2; i++){
-        int sum=0;
-        for(int j=0; j<2; j++){
-            if(v[j][i] != v[j+1][i]){
-                sum=sum+1;
+    int max = 0;
+    for(int a=1; a<board.size(); a++){
+        for(int b=1; b<board[0].size(); b++){
+            if(board[a][b] == 1){
+                board[a][b] = min(board[a][b-1], min(board[a-1][b-1], board[a-1][b])) + 1;
             }
-        }
-        if(sum == 2){
-            ans.push_back(v[1][i]);
-        }else{
-            if(v[0][i] == v[1][i]){
-                ans.push_back(v[2][i]);
-            }else{
-                ans.push_back(v[0][i]);
+            if(board[a][b] > max){
+                max = board[a][b];
             }
         }
     }
-
-    
-    for(int i=0; i<ans.size(); i++){
-        cout<<ans[i]<<endl;
+    if(max == 0){
+        
     }
+    
+    cout<<max*max<<endl;
 }
+
