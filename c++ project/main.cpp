2d7ca3ab090274e15ca52ef17359eaf1ad1132bb
김정윤 <skyepodium@gmail.com>
2017-09-13 16:39:27 +0900

    09.13

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -4,39 +4,43 @@
 #include <string>
 #include <algorithm>
 #include <vector>
+#include <cstdio>
 using namespace std;
-
-
-int main() {
-
-    string day;
-    int num = 0;
-    
-    cout<<"오늘은 무슨 요일 입니까?";
-    cin>>day;
-    
-    if(day == "sunday"){
-        num = 1;
-    }else if(day == "monday"){
-        num = 2;
-    }else if(day == "tuesday"){
-        num = 3;
-    }else if(day == "wednesday"){
-        num = 4;
-    }else if(day == "thursday"){
-        num = 5;
-    }else if(day == "friday"){
-        num = 6;
-    }else if(day == "토요일"){
-        num = 7;
-    }else{
-        num = 8;
-    }
-    if(num < 8){
-        cout<<"오늘은"<<day<<"이고 순서는"<<num<<"번째 입니다."<<endl;
-    }else{
-        cout<<"요일을 잘못 입력하셨습니다."<<endl;
+int main(){
+    int w[20][20];
+    int n;
+    cin>>n;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cin>>w[i][j];
+        }
     }
     
+    vector<int> d(n);
+    for(int i=0; i<n; i++){
+        d[i] = i;
+    }
+    
+    int ans=2147483647;
+    
+    do{
+        bool ok = true;
+        int sum = 0;
+        
+        for(int i=0; i<n-1; i++){
+            if(w[d[i]][d[i+1]] == 0){
+                ok = false;
+            }else{
+                sum = sum + w[d[i]][d[i+1]];
+            }
+        }
+            if(w[d[n-1]][d[0]] != 0 && ok){
+                sum = sum + w[d[n-1]][d[0]];
+                if(ans > sum){
+                    ans = sum;
+                }
+            }
+    }while(next_permutation(d.begin(), d.end()));
+    
+    cout<<ans<<endl;
 }
-
