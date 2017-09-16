c830b5bfa8ac915ce7b4e1e65f96e522bcf54eec
김정윤 <skyepodium@gmail.com>
2017-09-16 23:37:57 +0900

    09.16

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -5,21 +5,39 @@
 #include <algorithm>
 #include <vector>
 #include <cstdio>
+
+
+
 using namespace std;
-int main(){
-    int a[7] = {22, 21, 24, 20, 18, 25, 23};
-    
-    int max = a[0];
-    int index=0;
-    for(int i=1; i<7; i++){
-        
-        if(a[i]%2 == 0){
-            if(max<a[i]){
-                max = a[i];
-                index = i;
-            }
+string tentotwo(int num){
+    string result;
+    for(int i=1; num>0; i=i*10){
+        int binary = num % 2;
+        if(binary == 0){
+            result.append(" ");
+        }else{
+            result.append("#");
         }
+        num = num/2;
     }
-    cout<<max<<endl;
-    cout<<index<<endl;
+
+    char temp;
+    for(int i=0; i<result.size()/2; i++){
+        temp = result[i];
+        result[i] = result[result.size()-(i+1)];
+        result[result.size()-(i+1)] = temp;
+    }
+
+    return result;
+}
+int main(){
+    
+    
+    int a = 28;
+   
+    int b=28;
+    int c=21;
+    int d = b | c;
+    cout<<d<<endl;
+    cout<<tentotwo(d)<<endl;
 }
