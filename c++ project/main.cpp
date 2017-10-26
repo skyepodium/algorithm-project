b07b92bfbbde12d39966826f4ba090152a28ed31
김정윤 <skyepodium@gmail.com>
2017-10-26 21:01:56 +0900

    10.26

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -8,23 +8,29 @@
 #include <math.h>
 using namespace std;
 
+
+
 class CMoney {
-
-    public:
-        CMoney();
-        ~CMoney();
-        int GetDollar() const;
-        int GetCent() const;
-        CMoney(int nDollar, int nCent);
     
-    private:
-        int m_nDollar;
-        int m_nCent;
-
+public:
+    CMoney();
+    ~CMoney();
+    int GetDollar() const;
+    int GetCent() const;
+    CMoney(int nDollar, int nCent);
+    //const CMoney operator +(const CMoney& bMoney);
+    friend const CMoney operator +(const CMoney &aMoney, const CMoney &bMoney);
+    friend  ostream& operator <<(ostream& outStream, const CMoney& aMoney);
+    
+private:
+    int m_nDollar;
+    int m_nCent;
+    
 };
 
+
 //CMoney Add(CMoney aMoney, CMoney bMoney);
-const CMoney operator +(const CMoney &aMoney, const CMoney &bMoney);
+//const CMoney operator +(const CMoney &aMoney, const CMoney &bMoney);
 
 
 void ShowMoney(CMoney aMoney);
@@ -32,23 +38,27 @@ void ShowMoney(CMoney aMoney);
 int main(){
     CMoney aMoney(3, 60), bMoney(9, 45), addedMoney;
     addedMoney = aMoney + bMoney;
-//    addedMoney = Add(aMoney, bMoney);
-    ShowMoney(aMoney);
+    //addedMoney = Add(aMoney, bMoney);
+    //ShowMoney(aMoney);
+    cout << aMoney << " + " << bMoney << " = " << addedMoney << endl;
+    /*
     cout << " + ";
     ShowMoney(bMoney);
     cout << " + ";
     cout << " = ";
     ShowMoney(addedMoney);
     cout<<endl;
+     */
 }
 
 //CMoney Add(CMoney aMoney, CMoney bMoney){
-const CMoney operator +(const CMoney &aMoney, const CMoney &bMoney){
 
-    int nCent = aMoney.GetCent() + bMoney.GetCent();
+const CMoney operator +(const CMoney &aMoney, const CMoney &bMoney){
+    
+    int nCent = aMoney.m_nCent + bMoney.m_nCent;
     int nCarry = nCent / 100;
     nCent = nCent % 100;
-    int nDollar = aMoney.GetDollar() + bMoney.GetDollar() + nCarry;
+    int nDollar = aMoney.m_nDollar + bMoney.m_nDollar + nCarry;
     
     return CMoney(nDollar, nCent);
 }
@@ -80,5 +90,26 @@ CMoney::CMoney(int nDollar, int nCent){
 
 void ShowMoney(CMoney aMoney){
     cout << "$" << aMoney.GetDollar()<<"."<<aMoney.GetCent();
+}
+//멤버 함수 쓰는 이유 변수를 직접 엑세스 해서 속도 높이기 위함
+/*
+const CMoney CMoney::operator +(const CMoney &bMoney)
+{
+    int nCent = m_nCent + bMoney.m_nCent;
+    int nCarry = nCent / 100;
+    nCent = nCent % 100;
+    int nDollar = m_nDollar + bMoney.m_nDollar + nCarry;
+    
+    return CMoney(nDollar, nCent);
+}
+*/
+
+//cout이라는 포트의 주소를 받는 것,
+ ostream& operator <<(ostream& outStream, const CMoney& aMoney)
+{
+    
+   outStream << "$" << aMoney.m_nDollar <<"."<<aMoney.m_nCent;
+
+    return outStream;
 }
 
