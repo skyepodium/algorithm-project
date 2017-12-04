071a8649961f717a39dc08303b748c7d100d8aca
김정윤 <skyepodium@gmail.com>
2017-12-04 20:00:59 +0900

    12.04

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -2,33 +2,42 @@
 
 using namespace std;
 
+int sum = 0;
+int min_number = 0;
+
+
 int main() {
 
-    int a;
-    scanf("%d", &a);
-    int *p;
-    p = new int[a];
-    
-    for(int i=0; i<a; i++){
-        scanf("%d", &p[i]);
+    int a, b;
+    cin >> a >> b;
+
+    bool *check;
+    check = new bool[b+1];
+    check[1] = false;
+    for(int i=2; i<=b; i++){
+        check[i] = true;
     }
     
-    int b;
-    cin >> b;
+    for(int i=2; i*i<=b; i++){
+        for(int j=i*i; j<=b; j=j+i){
+            check[j] = false;
+        }
+    }
+
+    for(int i=a; i<=b; i++){
+        if(check[i] == true && sum == 0){
+            min_number = i;
+        }
+        
+        if(check[i]){
+            sum = sum + i;
+        }
+    }
     
-    for(int i=0; i<b; i++){
-        int c;
-        scanf("%d", &c);
-        bool check = false;
-        for(int j=0; j<a; j++){
-            if(c==p[j]){
-                check = true;
-            }
-        }
-        if(check == true){
-            printf("%d\n", 1);
-        }else{
-            printf("%d\n", 0);
-        }
+    if(sum == 0){
+        cout<<-1<<endl;
+    }else{
+        cout<<sum<<endl;
+        cout<<min_number<<endl;
     }
 }
