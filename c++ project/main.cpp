c472ffb6f50a4fe853b5abf84b38c46b57ae3218
김정윤 <skyepodium@gmail.com>
2017-12-06 23:32:57 +0900

    12.06

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -4,11 +4,7 @@ using namespace std;
 
 int main() {
 
-    int t;
-    scanf("%d", &t);
-    
-    while(--t){
-        printf("%d\n", t);
+    for(int i=0; i<2; i++){
+        cout << "강한친구 대한육군" <<endl;
     }
-    
 }
