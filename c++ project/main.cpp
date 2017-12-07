f7fd54bf4fb33af49b139182a8e46f811b5f2c8f
김정윤 <skyepodium@gmail.com>
2017-12-07 16:01:35 +0900

    12.07

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,47 +1,45 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
+#include <string>
 
 using namespace std;
 
-int cnt = 0;
-bool check[1001];
-
 void eratos(int a){
+  
+    bool *check = new bool[a+1];
+    for(int i=1; i<=a; i++){
+        check[i] = true;
+    }
+    
     for(int i=2; i*i<=a; i++){
         for(int j=i*i; j<=a; j=j+i){
             check[j] = false;
         }
     }
+    
+    if(check[a]){
+        cout<<"It is a prime word."<<endl;
+    }else{
+        cout<<"It is not a prime word."<<endl;
+    }
 }
+
 
 int main() {
-
-    check[1] = false;
-    for(int i=2; i<=1001; i++){
-        check[i] = true;
-    }
     
-    int n;
-    scanf("%d", &n);
-
-    vector<int> v;
-    for(int i=0; i<n; i++){
-        int a;
-        scanf("%d", &a);
-        v.push_back(a);
-    }
-
-    sort(v.begin(), v.end());
-
-    eratos(1001);
-
-    for(int i=0; i<n; i++){
-        if(check[v[i]]){
-            cnt++;
+    string word;
+    cin >> word;
+    
+    int sum =0 ;
+    
+    for(int i=0; i<word.size(); i++){
+        if((int)word[i] >= 97){
+            sum = sum + (int)word[i] - 96;
+        }else{
+            sum = sum + (int)word[i] - 38;
         }
     }
-    
-    cout<<cnt<<endl;
+    eratos(sum);
 }
+
+
 
