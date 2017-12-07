2fd02da16a8a909218fbcbf36b01c4c698cde8b8
김정윤 <skyepodium@gmail.com>
2017-12-07 14:27:11 +0900

    12.07

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -12,17 +12,19 @@ int gcd(int a, int b){
 
 int main() {
 
-    int a, b;
-    cin >> a >> b;
-    
-    if(b>a){
-        int tmp;
-        tmp = a;
-        a = b;
-        b = tmp;
+    int n;
+    cin >> n;
+    while(n--){
+        int a, b;
+        cin >> a >> b;
+        
+        if(b>a){
+            int tmp;
+            tmp = a;
+            a = b;
+            b = tmp;
+        }
+        cout << (a*b)/gcd(a, b) <<endl;
     }
-    
-    cout << gcd(a, b) << endl;
-    cout << (a*b)/gcd(a, b) <<endl;
 }
 
