34f77274f624d9c1e21bb43bf1ba135664690375
김정윤 <skyepodium@gmail.com>
2017-12-08 16:08:40 +0900

    12.08

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,13 +1,41 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
 int main() {
 
-    int a, b;
-    cin >> a >> b;
-    cout << a+b <<endl;
+    int n, han = 0;
+    cin >> n;
+    
+    vector<int> v;
+    
+    if(n < 100){
+        
+        han = n;
+        
+    }else{
+        han = 99;
+        
+        if(n == 1000){
+            n = n-1;
+        }
+        
+        for(int i=100; i<=n; i++){
+            int t = i;
+            while(t > 0){
+                v.push_back(t%10);
+                t = t/10;
+            }
+            if(v[0] - v[1] == v[1] - v[2]){
+                han++;
+            }
+            v.clear();
+        }
+    }
+    
+    cout << han << endl;
 }
 
 
