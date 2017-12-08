8ea3d9d7b4f7b36853e615094b2ca8f3dff277da
김정윤 <skyepodium@gmail.com>
2017-12-08 15:32:11 +0900

    12.08

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -3,35 +3,9 @@
 
 using namespace std;
 
-int n, k;
-
-void eratos(int a){
-    bool *check = new bool[a+1];
-    check[1] = false;
-    for(int i=2; i<=a; i++){
-        check[i] = true;
-    }
-    
-    int cnt = 0;
-    
-    for(int i=2; i<=a; i++){
-        for(int j=i; j<=a; j=j+i){
-            if(check[j]){
-                check[j] = false;
-                cnt++;
-                if(cnt == k){
-                    cout<< j <<endl;
-                }
-            }
-        }
-    }
-}
-
 int main() {
-    
-    cin >> n >> k;
-    
-    eratos(n);
+
+    cout << "Hello World!" << endl;
 }
 
 
