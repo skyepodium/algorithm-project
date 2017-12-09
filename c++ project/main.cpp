b3333c19bb910d343de1731908b6b4064e157581
김정윤 <skyepodium@gmail.com>
2017-12-09 15:44:36 +0900

    12.09

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,20 +1,48 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
-int main() {
+int cnt = 0;
 
-    int t;
-    cin >> t;
+bool* eratos(int n, bool *check){
     
-    int n = 0;
-    for(int i=0; i<t; i++){
-        int a;
-        cin >> a;
-        n = n + a;
+    for(int i=2; i*i<=n; i++){
+        for(int j=i*i; j<=n; j=j+i){
+            check[j] = false;
+        }
     }
-
-    cout << n - t + 1<<endl;
+    
+    return check;
 }
 
+int main() {
+    
+    int n;
+    cin >> n;
+    
+    const int max_size = 10000000;
+    bool *check = new bool[max_size+1];
+    check[1] = false;
+    for(int i=0; i<=max_size; i++){
+        check[i] = true;
+    }
+    
+    check = eratos(max_size+1, check);
+    
+    vector<int> v;
+    for(int i=2; i<=n; i++){
+        if(check[i] && n%i == 0){
+            v.push_back(i);
+        }
+    }
+    for(int i=0; i<v.size(); i++){
 
+        while(n%v[i] == 0){
+            n = n/v[i];
+            cout << v[i] <<endl;
+        }
+        
+    }
+}
