1303328b42278c066c3f6bb54e55054b1b52e379
김정윤 <skyepodium@gmail.com>
2017-12-09 16:20:06 +0900

    12.09

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,20 +1,25 @@
 #include <iostream>
-#include <string>
+#include <vector>
 
 using namespace std;
 
 int main() {
 
-    string word;
-    cin >> word;
+    int n;
+    cin >> n;
+    vector<int> v;
     
-    for(int i=0; i<word.size(); i++){
-        if(word[i] >= 97){
-            word[i] = word[i] - 32;
-        }else{
-            word[i] = word[i] + 32;
-        }
+    while(n>0){
+        v.push_back(n%2);
+        n = n/2;
     }
     
-    cout << word <<endl;
+    int result = 0;
+    int mul_num = 1;
+    int size = (int)v.size();
+    for(int i= size-1; i>=0; i--){
+        result = result + v[i]*mul_num;
+        mul_num = mul_num*2;
+    }
+    cout << result << endl;
 }
