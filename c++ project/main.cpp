800a26a49fa1cb09c77a9eca50ba6419c3d10005
김정윤 <skyepodium@gmail.com>
2017-12-09 13:21:55 +0900

    12.09

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -2,18 +2,11 @@
 #include <string>
 using namespace std;
 
-int gcd(int a, int b){
-    if(b == 0){
-        return a;
-    }else{
-        return gcd(b, a%b);
-    }
-}
-
-
 int main() {
-
-    cout<<gcd(124, 196) <<endl;
+    int a, b;
+    cin >> a >> b;
+    
+    cout << a-b <<endl;
 }
 
 
