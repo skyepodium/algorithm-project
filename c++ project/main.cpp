d4b935275059523325dbf717a92a120dcde7e1e6
김정윤 <skyepodium@gmail.com>
2017-12-10 23:29:38 +0900

    12.10

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,40 +1,10 @@
 #include <iostream>
-#include <string>
-#include <vector>
 
 using namespace std;
 
 int main() {
-    
-    string number;
-    cin >> number;
-    
-    int size = (int)number.size();
-    vector<int> v;
-    for(int i=0; i<size; i++){
-        v.push_back(number[i]-48);
-    }
 
-    vector<int> result;
-    int mid_sum = 0;
-    int mul_num = 1;
-    int cnt = 0;
-    
-    for(int i=size-1; i>=0; i--){
-        if(cnt == 3){
-            result.push_back(mid_sum);
-            mul_num = 1;
-            mid_sum = 0;
-            cnt = 0;
-        }
-        mid_sum = mid_sum + v[i]*mul_num;
-        mul_num = mul_num*2;
-        cnt++;
-    }
-    result.push_back(mid_sum);
-    
-    int result_size = (int)result.size();
-    for(int i = result_size-1; i >= 0; i--){
-        cout << result[i];
-    }
+    int a, b;
+    cin >> a >> b;
+    cout <<a*b<<endl;
 }
