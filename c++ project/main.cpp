7218c3306f92c2630a1965e90c3dae9438baea3a
김정윤 <skyepodium@gmail.com>
2017-12-11 15:30:06 +0900

    12.11

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-long long int gcd(long long int a, long long int b){
+int gcd(int a, int b){
     
     if(b==0){
         return a;
@@ -12,15 +13,43 @@ long long int gcd(long long int a, long long int b){
     
 }
 
+
 int main() {
     
-    long long int a, b, result;
-    cin >> a >> b;
-    result = gcd(a, b);
+    int n;
+    scanf("%d", &n);
     
-    while(result--){
-        cout << 1;
+    vector<int> dif;
+
+    int first = 0;
+    while(n--){
+        int a;
+        scanf("%d", &a);
+        if(first == 0){
+            first = a;
+        }else{
+            dif.push_back(abs(a-first));
+            first = a;
+        }
     }
-    cout << endl;
+    
+    
+    int result = dif[0];
+    
+    if(dif.size() > 1){
+        result = dif[0];
+        for(int i=1; i<dif.size(); i++){
+            result = gcd(result, dif[i]);
+        }
+
+    }
+    
+    
+    for(int i=2; i<=result/2; i++){
+        if(result%i == 0){
+            printf("%d ", i);
+        }
+    }
+    printf("%d\n", result);
 }
 
