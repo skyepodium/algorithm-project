0ec1fe5517b4feff1f2b5e6092b549b3649ceed4
김정윤 <skyepodium@gmail.com>
2017-12-13 18:17:49 +0900

    12.13

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -2,36 +2,9 @@
 
 using namespace std;
 
-int gcd(int a, int b){
-    
-    if(b==0){
-        return a;
-    }else{
-        return gcd(b, a%b);
-    }
-    
-}
-
-
 int main() {
-
-    int n;
-    cin >> n;
-    
-    int base;
-    cin >> base;
-    n--;
-    
-    while(n--){
-        int top;
-        cin >> top;
-        
-        int gcd_num;
-        gcd_num = gcd(base, top);
-        
-        cout << base/gcd_num << '/' << top/gcd_num <<endl;
-        
-    }
- 
+    int a, b;
+    cin >> a >> b;
+    cout << a+b <<endl;
 }
 
