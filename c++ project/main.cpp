eee04fc42321be9e36c26739b10104da68f92ee9
김정윤 <skyepodium@gmail.com>
2017-12-14 10:46:20 +0900

    12.14

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -3,8 +3,14 @@
 using namespace std;
 
 int main() {
+    
     int a, b;
-    cin >> a >> b;
-    cout << a+b <<endl;
+    cin>>a>>b;
+    
+    cout<<a+b<<endl;
+    cout<<a-b<<endl;
+    cout<<a*b<<endl;
+    cout<<a/b<<endl;
+    cout<<a%b<<endl;
+    
 }
-
