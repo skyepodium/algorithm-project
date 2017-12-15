ea1c4338ea35a2e1369ffec5ecdc1d22db950fbb
김정윤 <skyepodium@gmail.com>
2017-12-15 23:26:35 +0900

    12.15

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,13 +1,8 @@
 #include <iostream>
-#include <fstream>
-#include <iomanip>
 #include <string>
+
 using namespace std;
 
 int main(){
-    
-    cout.setf(ios::fixed | ios::showpoint);
-    
-    cout << "$" << setprecision(2) <<10.3 <<" " <<"$" <<20.5<<endl;
-    
+    cout << "비와이" <<endl;
 }
