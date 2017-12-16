d3fb1de7aaa49d7803adbe7046c5ab01192248ef
김정윤 <skyepodium@gmail.com>
2017-12-17 02:18:13 +0900

    12.17

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,18 +1,29 @@
 #include <iostream>
+#include <vector>
 #include <string>
 
 using namespace std;
 
 int main(){
 
-    int n;
-    cin >> n;
+    vector<int> v;
     
-    while(n--){
-        string word;
-        cin >> word;
-        
-        int size = (int)word.size();
-        cout<<word[0]<<word[size-1]<<endl;
+    int a, b, c, d;
+    int sum = 0;
+    for(int i=0; i<5; i++){
+        cin >> a >> b >> c >> d;
+        sum = a+b+c+d;
+        v.push_back(sum);
     }
+    
+    int max = v[0];
+    int index = 0;
+    for(int i=1; i<v.size(); i++){
+        if(max<v[i]){
+            max = v[i];
+            index = i;
+        }
+    }
+    cout<<index+1<<" "<<max<<endl;
+    
 }
