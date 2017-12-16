3ce57fc3e24990cf8def5ebf802d04bdcc207f4f
김정윤 <skyepodium@gmail.com>
2017-12-17 00:07:12 +0900

    12.17

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -3,20 +3,13 @@
 using namespace std;
 
 int main(){
-    int chang = 100, sang = 100;
-    int n;
     int a, b;
-    cin >> n;
-    while(n--){
-        cin >> a >> b;
-        if(a>b){
-            sang = sang - a;
-        }else if(a<b){
-            chang = chang - b;
+    while(cin>>a>>b){
+        if(a==0 && b==0){
+            break;
+        }else{
+            cout<<a+b<<endl;
         }
     }
-    
-    cout<<chang<<endl;
-    cout<<sang<<endl;
 }
 
