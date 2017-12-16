abb91084957e54ce478e3870e3659d6730c4a58d
김정윤 <skyepodium@gmail.com>
2017-12-17 00:54:18 +0900

    12.17

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main(){
-    int a;
-    cin >> a;
-    
-    if(a%4 == 0 && a%100 != 0){
-        cout<<1<<endl;
-    }else if(a%400 == 0){
+    string word;
+    cin >> word;
+
+    bool check = true;
+    int size = (int)word.size();
+    for(int i=0; i<size/2; i++){
+        if(word[i] != word[size-1-i]){
+            check = false;
+        }
+    }
+    if(check){
         cout<<1<<endl;
     }else{
         cout<<0<<endl;
