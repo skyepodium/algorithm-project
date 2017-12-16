1d6364e01725a3411625df7e97823ad778f6da04
김정윤 <skyepodium@gmail.com>
2017-12-17 01:45:17 +0900

    12.17

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -6,19 +6,16 @@ using namespace std;
 int main(){
 
     string word;
-
-    while(getline(cin, word)){
-        
-        if(word != "END"){
-        int size = (int)word.size();
-
-        for(int i=size-1; i>=0; i--){
-            cout<<word[i];
+    cin >> word;
+    
+    int cnt = 0;
+    int size = (int)word.size();
+    for(int i=0; i<size; i++){
+        if(word[i] == 'a' || word[i] == 'e' || word[i] == 'i' || word[i] == 'o' || word[i] == 'u'){
+            cnt++;
         }
-        cout<<endl;
-        }else{
-            break;
-        }
-
     }
+    
+    cout << cnt << endl;
+
 }
