e3efc5ceef54f5708468429bb57bcdf571f33a19
김정윤 <skyepodium@gmail.com>
2017-12-17 00:03:29 +0900

    12.17

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,43 +1,22 @@
 #include <iostream>
-#include <string>
 
 using namespace std;
 
 int main(){
-    cout.setf(ios::fixed);
-    cout.setf(ios::showpoint);
-    cout.precision(1);
-    
-    string grade;
-    cin >> grade;
-    
-    if(grade == "A+"){
-        cout<<4.3<<endl;
-    }else if(grade == "A0"){
-        cout<<4.0<<endl;
-    }else if(grade == "A-"){
-        cout<<3.7<<endl;
-    }else if(grade == "B+"){
-        cout<<3.3<<endl;
-    }else if(grade == "B0"){
-        cout<<3.0<<endl;
-    }else if(grade == "B-"){
-        cout<<2.7<<endl;
-    }else if(grade == "C+"){
-        cout<<2.3<<endl;
-    }else if(grade == "C0"){
-        cout<<2.0<<endl;
-    }else if(grade == "C-"){
-        cout<<1.7<<endl;
-    }else if(grade == "D+"){
-        cout<<1.3<<endl;
-    }else if(grade == "D0"){
-        cout<<1.0<<endl;
-    }else if(grade == "D-"){
-        cout<<0.7<<endl;
-    }else{
-        cout<<0.0<<endl;
+    int chang = 100, sang = 100;
+    int n;
+    int a, b;
+    cin >> n;
+    while(n--){
+        cin >> a >> b;
+        if(a>b){
+            sang = sang - a;
+        }else if(a<b){
+            chang = chang - b;
+        }
     }
-
+    
+    cout<<chang<<endl;
+    cout<<sang<<endl;
 }
 
