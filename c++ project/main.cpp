a91285e2734ccf01cdc4e0099a2dfeb50cc380f7
김정윤 <skyepodium@gmail.com>
2017-12-17 02:25:23 +0900

    12.17

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,29 +1,23 @@
 #include <iostream>
-#include <vector>
-#include <string>
 
 using namespace std;
 
 int main(){
 
-    vector<int> v;
+    int n;
+    cin >> n;
     
-    int a, b, c, d;
-    int sum = 0;
-    for(int i=0; i<5; i++){
-        cin >> a >> b >> c >> d;
-        sum = a+b+c+d;
-        v.push_back(sum);
-    }
-    
-    int max = v[0];
-    int index = 0;
-    for(int i=1; i<v.size(); i++){
-        if(max<v[i]){
-            max = v[i];
-            index = i;
+    while(n--){
+        int a;
+        cin >> a;
+        
+        int sum = 0 ;
+        for(int i=0; i<a; i++){
+            int b;
+            cin >> b;
+            sum = sum + b;
         }
+        cout << sum << endl;
     }
-    cout<<index+1<<" "<<max<<endl;
-    
+
 }
