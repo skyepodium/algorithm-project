c7449f7c9d435bbcf42f679f85467aa8baca0328
김정윤 <skyepodium@gmail.com>
2017-12-17 15:54:36 +0900

    12.17

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -4,28 +4,33 @@
 using namespace std;
 
 int main(){
-
+    
     int n;
     cin >> n;
     
+    vector<int> v;
     while(n--){
         int a;
         cin >> a;
+        v.push_back(a);
+    }
+    
+    int size = (int)v.size();
+    int sum = 0;
+    
+    if(v[0] == 1){
+        sum = sum + 1;
+    }
+    for(int i=1; i<size; i++){
         
-        vector<int> v;
-        
-        while(a>0){
-            v.push_back(a%2);
-            a = a/2;
+        if(v[i] > 0 && v[i-1] > 0){
+            v[i] = v[i-1] + 1;
         }
-        
-        int size = (int)v.size();
-        
-        for(int i=0; i<size; i++){
-            if(v[i] == 1){
-                cout<<i<<" ";
-            }
+
+        if(v[i] > 0){
+            sum = sum + v[i];
         }
 
     }
+    cout << sum << endl;
 }
