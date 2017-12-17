f6daac7f1ecc3d62fc992a21378b324d223455ca
김정윤 <skyepodium@gmail.com>
2017-12-17 19:19:12 +0900

    12.17

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,23 +1,18 @@
 #include <iostream>
-#include <string>
 
 using namespace std;
 
-char first[8][4] = {"", "1", "10", "11", "100", "101", "110", "111"};
-char other[8][4] = {"000", "001", "010", "011", "100", "101", "110", "111"};
-
 int main(){
-    string number;
-    cin >> number;
+        
+    int t;
+    cin >> t;
+
     
-    cout << first[number[0] - 48];
-    
-    for(int i=1; i < number.size(); i++){
-        cout << other[number[i] - 48];
+    int a, b;
+    while(t--){
+        cin >> a >> b;
+        
+        printf("You get %d piece(s) and your dad gets %d piece(s).\n", a/b, a%b );
     }
-    
-    if(number =="0"){
-        cout<<0<<endl;
-    }
-    
 }
+
