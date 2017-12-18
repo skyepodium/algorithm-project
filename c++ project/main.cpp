7818725462d858a301628c6c342bd6475a6359a0
김정윤 <skyepodium@gmail.com>
2017-12-18 13:02:26 +0900

    12.18

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,28 +1,42 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main(){
+    
+    string num1, num2;
+    char op;
+    
+    cin >> num1 >> op >> num2;
 
-    int t;
-    cin >> t;
+    int num1_size = (int)num1.size();
+    int num2_size = (int)num2.size();
     
-    while(t--){
+    
+    if(op == '*'){
+  
+        num1.append(num2.begin()+1, num2.end());
+        cout << num1 <<endl;
         
-        int base;
-        cin >> base;
-        
-        int n;
-        cin >> n;
-        
-        int num, price;
-        for(int i=0; i<n; i++){
-            cin >> num >> price;
-            base = base + num * price;
+    }else{
+
+        if(num1_size == num2_size){
+            
+            num1[0] = '2';
+            cout<<num1<<endl;
+            
+        }else if(num1_size < num2_size){
+
+            num2[num2_size - num1_size] = '1';
+            cout << num2 <<endl;
+
+        }else{
+
+            num1[num1_size - num2_size] = '1';
+            cout << num1 <<endl;
+
         }
-        
-        cout << base <<endl;
     }
-    
 }
 
