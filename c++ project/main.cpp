635423fe1cc474f6da93ccde213ed5c7fb13c941
김정윤 <skyepodium@gmail.com>
2017-12-19 12:04:57 +0900

    12.19

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,31 +1,12 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 
 using namespace std;
 
-int main(){
-
-    vector<pair<int, int> > v;
-    int num;
+int main() {
     
-    for(int i=0; i<8; i++){
-        cin >> num;
-        v.push_back(pair<int, int>(num, i+1));
-    }
-    sort(v.begin(), v.end());
-    
-    int sum = 0;
-    vector<int> result;
-    for(int i=3; i<8; i++){
-        sum = sum + v[i].first;
-        result.push_back(v[i].second);
-    }
-    cout << sum <<endl;
-
-    sort(result.begin(), result.end());
-    for(int i=0; i<result.size(); i++){
-        cout << result[i] <<" ";
-    }
+    cout<<"|"<<"\\"<<"_/|"<<endl;
+    cout<<"|q p|   /}"<<endl;
+    cout<<"( 0 )"<<'"'<<'"'<<'"'<<"\\"<<endl;
+    cout<<"|"<<'"'<<"^"<<'"'<<'`'<<"    |"<<endl;
+    cout<<"||_/="<<"\\"<<"\\"<<"__|";
 }
-
