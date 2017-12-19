d8f17580a35b5fd203c064a7de51f6dbdb321eb9
김정윤 <skyepodium@gmail.com>
2017-12-19 18:51:51 +0900

    12.19

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -4,9 +4,32 @@ using namespace std;
 
 int main() {
     
-    cout<<"|"<<"\\"<<"_/|"<<endl;
-    cout<<"|q p|   /}"<<endl;
-    cout<<"( 0 )"<<'"'<<'"'<<'"'<<"\\"<<endl;
-    cout<<"|"<<'"'<<"^"<<'"'<<'`'<<"    |"<<endl;
-    cout<<"||_/="<<"\\"<<"\\"<<"__|";
+    int t;
+    cin >> t;
+    
+    int n;
+    char problem_kind;
+    while(t--){
+
+        cin >> n >> problem_kind;
+        
+        if(problem_kind == 'C'){
+            
+            char ch;
+            for(int i=0; i<n; i++){
+                cin >> ch;
+                printf("%d ", ch-64);
+            }
+            
+        }else{
+            
+            int num;
+            for(int i=0; i<n; i++){
+                cin >> num;
+                printf("%c ", num+64);
+            }
+            
+        }
+        cout<<endl;
+    }
 }
