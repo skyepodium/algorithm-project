4377fe5c4d4dee886564b78c3532b7d1dccbd3ef
김정윤 <skyepodium@gmail.com>
2017-12-20 21:47:55 +0900

    12.20

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -5,25 +5,8 @@ using namespace std;
 
 int main() {
 
-    string base, word;
-    cin >> base >> word;
-    
-    int cnt = 0;
-    for(int i=0; i<base.size(); i++){
-        if(base[i] == word[0]){
-            bool check = true;
-            for(int j=0; j<word.size(); j++){
-                if(base[i+j] != word[j]){
-                    check = false;
-                    break;
-                }
-            }
-            if(check){
-                cnt++;
-            }
-        }
+    string word;
+    while(getline(cin , word)){
+        cout << word <<endl;
     }
-    
-    cout << cnt <<endl;
-    
 }
