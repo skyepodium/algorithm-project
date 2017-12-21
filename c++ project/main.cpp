993be07019d0925ed407343413a7399a40896b22
김정윤 <skyepodium@gmail.com>
2017-12-21 16:00:55 +0900

    12.21

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -5,30 +5,23 @@ using namespace std;
 
 int main() {
 
+    int cnt = 0;
     string word;
-    cin >> word;
-    
-    int time = 0;
-    for(int i=0; i<word.size(); i++){
+    for(int i=0; i<8; i++){
+        cin >> word;
         
-        if(word[i] == 'A' || word[i] == 'B' || word[i] =='C'){
-            time = time + 3;
-        }else if(word[i] == 'D' || word[i] == 'E' || word[i] =='F'){
-            time = time + 4;
-        }else if(word[i] == 'G' || word[i] == 'H' || word[i] =='I'){
-            time = time + 5;
-        }else if(word[i] == 'J' || word[i] == 'K' || word[i] =='L'){
-            time = time + 6;
-        }else if(word[i] == 'M' || word[i] == 'N' || word[i] =='O'){
-            time = time + 7;
-        }else if(word[i] == 'P' || word[i] == 'Q' || word[i] =='R' || word[i] == 'S'){
-            time = time + 8;
-        }else if(word[i] == 'T' || word[i] == 'U' || word[i] =='V'){
-            time = time + 9;
-        }else if(word[i] == 'W' || word[i] == 'X' || word[i] =='Y' || word[i] == 'Z'){
-            time = time + 10;
+        for(int j=0; j<word.size(); j++){
+            
+            if(i%2 == 0 && j%2 == 0){
+                if(word[j] == 'F'){
+                    cnt++;
+                }
+            }else if(i%2 == 1 && j%2 == 1){
+                if(word[j] == 'F'){
+                    cnt++;
+                }
+            }
         }
-        
     }
-    cout << time << endl;
+    cout << cnt << endl;
 }
