39d1b78dab000af31a8330179d2d67668b74e248
김정윤 <skyepodium@gmail.com>
2017-12-21 15:48:26 +0900

    12.21

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,10 +1,34 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main() {
 
-    int a, b;
-    cin >> a >> b;
-    cout << a+b <<endl;
+    string word;
+    cin >> word;
+    
+    int time = 0;
+    for(int i=0; i<word.size(); i++){
+        
+        if(word[i] == 'A' || word[i] == 'B' || word[i] =='C'){
+            time = time + 3;
+        }else if(word[i] == 'D' || word[i] == 'E' || word[i] =='F'){
+            time = time + 4;
+        }else if(word[i] == 'G' || word[i] == 'H' || word[i] =='I'){
+            time = time + 5;
+        }else if(word[i] == 'J' || word[i] == 'K' || word[i] =='L'){
+            time = time + 6;
+        }else if(word[i] == 'M' || word[i] == 'N' || word[i] =='O'){
+            time = time + 7;
+        }else if(word[i] == 'P' || word[i] == 'Q' || word[i] =='R' || word[i] == 'S'){
+            time = time + 8;
+        }else if(word[i] == 'T' || word[i] == 'U' || word[i] =='V'){
+            time = time + 9;
+        }else if(word[i] == 'W' || word[i] == 'X' || word[i] =='Y' || word[i] == 'Z'){
+            time = time + 10;
+        }
+        
+    }
+    cout << time << endl;
 }
