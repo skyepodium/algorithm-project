bc5c6e62e151b701922633ff681639329831e551
김정윤 <skyepodium@gmail.com>
2017-12-21 15:08:55 +0900

    12.21

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
-#include <string>
 
 using namespace std;
 
 int main() {
 
-    string word;
-    while(getline(cin , word)){
-        cout << word <<endl;
-    }
+    int a, b;
+    cin >> a >> b;
+    cout << a+b <<endl;
 }
