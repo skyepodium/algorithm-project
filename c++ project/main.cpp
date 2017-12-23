c47509084d0d090e5aa6c7f9c593656e03e4f370
김정윤 <skyepodium@gmail.com>
2017-12-23 17:14:52 +0900

    12.23

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,48 +1,23 @@
 #include <iostream>
-#include <string>
 
 using namespace std;
 
+int Rev(int num){
+    
+    int result = 0;
+    while(num > 0){
+        result = result * 10;
+        result = result + num%10;
+        num = num/10;
+    }
+    
+    return result;
+}
 
 int main() {
     
-    string num1, num2;
-    cin >> num1 >> num2;
+    int X, Y;
+    cin >> X >> Y;
     
-    int num1_size = (int)num1.size();
-    int num2_size = (int)num2.size();
-    
-    for(int i=0; i<num1_size/2; i++){
-        
-        char temp;
-        temp = num1[i];
-        num1[i] = num1[num1_size-1-i];
-        num1[num1_size-1-i] = temp;
-        
-    }
-
-    for(int i=0; i<num2_size/2; i++){
-        
-        char temp;
-        temp = num2[i];
-        num2[i] = num2[num2_size-1-i];
-        num2[num2_size-1-i] = temp;
-        
-    }
-
-    int result_mid = stoi(num1) + stoi(num2);
-    string result = to_string(result_mid);
-
-    int result_size = (int)result.size();
-
-    for(int i=0; i<result_size/2; i++){
-        
-        char temp;
-        temp = result[i];
-        result[i] = result[result_size-1-i];
-        result[result_size-1-i] = temp;
-        
-    }
-    
-    cout<<stoi(result)<<endl;
+    cout << Rev(Rev(X) + Rev(Y)) << endl;
 }
