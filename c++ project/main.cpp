8bbfb251df312c25631e694b389f22f6c455ee35
김정윤 <skyepodium@gmail.com>
2017-12-23 16:53:39 +0900

    12.23

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -3,24 +3,46 @@
 
 using namespace std;
 
-char ar[11][11];
 
 int main() {
     
-    int a,b;
-    cin >> a >> b;
+    string num1, num2;
+    cin >> num1 >> num2;
     
-    for(int i=0; i<a; i++){
-        for(int j=0; j<b; j++){
-            cin >> ar[i][j];
-        }
+    int num1_size = (int)num1.size();
+    int num2_size = (int)num2.size();
+    
+    for(int i=0; i<num1_size/2; i++){
+        
+        char temp;
+        temp = num1[i];
+        num1[i] = num1[num1_size-1-i];
+        num1[num1_size-1-i] = temp;
+        
     }
 
-    for(int i=0; i<a; i++){
-        for(int j=b-1; j>=0; j--){
-            cout<< ar[i][j];
-        }
-        cout << endl;
+    for(int i=0; i<num2_size/2; i++){
+        
+        char temp;
+        temp = num2[i];
+        num2[i] = num2[num2_size-1-i];
+        num2[num2_size-1-i] = temp;
+        
     }
 
+    int result_mid = stoi(num1) + stoi(num2);
+    string result = to_string(result_mid);
+
+    int result_size = (int)result.size();
+
+    for(int i=0; i<result_size/2; i++){
+        
+        char temp;
+        temp = result[i];
+        result[i] = result[result_size-1-i];
+        result[result_size-1-i] = temp;
+        
+    }
+    
+    cout<<stoi(result)<<endl;
 }
