4fc08b31ffd4ce9b9bbc5263290f1888d54a3329
김정윤 <skyepodium@gmail.com>
2017-12-23 22:45:38 +0900

    12.23

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,32 +1,18 @@
 #include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
 using namespace std;
 
 int main() {
     
-    int t;
+    int t, num;
     cin >> t;
     
-    string name;
-    int day, month, year;
-    
-    vector<pair<int, string>> v;
-    
     while(t--){
-        cin >> name >> day >> month >> year;
         
-        int day_sum = day + month*31 + year*12*31;
+        cin >> num;
         
-        v.push_back(pair<int, string>(day_sum, name));
-        
+        for(int i=0; i<num; i++){
+            cout<<'=';
+        }
+        cout<<endl;
     }
-    
-    sort(v.begin(), v.end());
-    
-    int size = (int)v.size();
-    
-    cout << v[size - 1].second << endl;
-    cout << v[0].second << endl;
 }
