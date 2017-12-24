cd68812940fdde604229e1816e6ea4ba7a5d4785
김정윤 <skyepodium@gmail.com>
2017-12-24 19:06:06 +0900

    12.24

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -7,12 +7,16 @@ int main() {
     cin >> t;
     
     for(int i=0; i<t; i++){
-        
-        for(int k=0; k<i; k++){
-            cout<<' ';
+
+        for(int j=0; j<i+1; j++){
+            cout<<'*';
         }
+        cout<<endl;
+    }
+    
+    for(int i=0; i<t-1; i++){
         
-        for(int j=i; j<2*t-1-i; j++){
+        for(int j=0; j<t-i-1; j++){
             cout<<'*';
         }
         cout<<endl;
