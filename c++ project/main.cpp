e4e070a3f1d6b4a77ac8f10e6513412e9691acbd
김정윤 <skyepodium@gmail.com>
2017-12-25 13:53:17 +0900

    12.25

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -3,26 +3,25 @@ using namespace std;
 
 int main() {
     
-    int n;
-    cin >> n;
+    int a, b, c;
+    cin >> a >> b >> c;
     
-    for(int k=0; k<2*n; k++){
-        if(k%2 == 1){
-            cout<<' ';
-        }
-        
-        for(int i=0; i<n; i++){
-            if(i%2 == 0){
-                if((n-k)%2 == 0 && i == n-1){
-                    continue;
-                }else{
-                    cout<<'*';
-                }
-            }else{
-                cout<<' ';
-            }
-        }
-        cout<<endl;
+    if(a+b == c){
+        cout<<a<<'+'<<b<<'='<<c<<endl;
+    }else if(a-b == c){
+        cout<<a<<'-'<<b<<'='<<c<<endl;
+    }else if(a*b == c){
+        cout<<a<<'*'<<b<<'='<<c<<endl;
+    }else if(a/b == c){
+        cout<<a<<'/'<<b<<'='<<c<<endl;
+    }else if(a == b+c){
+        cout<<a<<'='<<b<<'+'<<c<<endl;
+    }else if(a == b-c){
+        cout<<a<<'='<<b<<'-'<<c<<endl;
+    }else if(a == b*c){
+        cout<<a<<'='<<b<<'*'<<c<<endl;
+    }else{
+        cout<<a<<'='<<b<<'/'<<c<<endl;
     }
 }
 
