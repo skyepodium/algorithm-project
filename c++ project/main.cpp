7fc1b092683fb65173505ed9b02dc1e1f4913785
김정윤 <skyepodium@gmail.com>
2017-12-29 13:42:52 +0900

    12.29 Top down

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -4,33 +4,54 @@ using namespace std;
 
 //시간 복잡도: O(n)
 //공간 복잡도: O(n)
-//사용한 알고리즘: 동적 계획법 - Bottom Up
+//사용한 알고리즘: 동적 계획법 - Top Down(memoization)
 //사용한 자료구조: 배열
 
-long long int d[101][11] = {0};
+long long int d[101][10] = {0};
 long long int mod = 1000000000;
 long long int result = 0;
 
+long long int go(int n, int j){
+    
+    if(n == 1){
+        return d[1][j];
+    }
+    
+    if(d[n][j] > 0){
+        return d[n][j];
+    }
+    
+    if(j == 0){
+        d[n][j] = go(n-1, 1);
+    }else if(j==9){
+        d[n][j] = go(n-1, 8);
+    }else{
+        d[n][j] = go(n-1, j-1) + go(n-1, j+1);
+    }
+
+    d[n][j] %= mod;
+    
+    return d[n][j];
+}
+
+
 int main(){
 
-    long long int n;
+    int n;
     cin >> n;
 
     for(int i=1; i < 10; i++){
         d[1][i] = 1;
     }
     
-    for(int i=2; i<=n; i++){
-        d[i][0] = d[i-1][1];
-        for(int j=1; j<10; j++){
-            d[i][j] = (d[i-1][j-1] + d[i-1][j+1])%mod;
-        }
+    for(int j=0; j<10; j++){
+        go(n, j);
     }
 
     for(int i=0; i<10; i++){
         result = (result + d[n][i])%mod;
     }
-    
+
     cout << result << endl;
     
 }
