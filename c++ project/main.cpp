a58bdd4b4b8db5e7f0c176168cb3bbeaae1a88f7
김정윤 <skyepodium@gmail.com>
2017-12-29 20:20:40 +0900

    12.29

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,53 +1,71 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
 //시간 복잡도: O(n)
 //공간 복잡도: O(n)
-//사용한 알고리즘: 동적 계획법 - Top Down
+//사용한 알고리즘: Brute Force Search
 //사용한 자료구조: 배열
 
-int d[1001][10] = {0};
-int mod = 10007;
-int result = 0;
+bool check[10];
+int base = 100;
+int last = 0;
 
-int go(int n, int j){
+int check_number(int n){
     
-    if(n == 1){
-        return d[1][j];
+    if(n == 0){
+        if(check[0]){
+            return 1;
+        }else{
+            return -1;
+        }
     }
     
-    if(d[n][j] > 0){
-        return d[n][j];
-        
-    }
-
-    for(int k=0; k<=j; k++){
-        d[n][j] += go(n-1, k);
-        d[n][j] %= mod;
+    if(n == 100){
+        return 0;
     }
     
+    int cnt = 0;
+    while(n>0){
+        if(!check[n%10]){
+            return -1;
+        }else{
+            n /= 10;
+            cnt++;
+        }
+    }
     
-    return d[n][j];
+    return cnt;
 }
 
 
 int main(){
-    
+
     int n;
-    cin >> n;
+    cin >> last >> n;
     
     for(int i=0; i<10; i++){
-        d[1][i] = 1;
-    }
-
-    for(int j=0; j<10; j++){
-        go(n, j);
-    }
-
-    for(int i=0; i<10; i++){
-        result = (result + d[n][i])%mod;
+        check[i] = true;
     }
     
-    cout << result << endl;
+    for(int i=0; i<n; i++){
+        int a;
+        cin >> a;
+        check[a] = false;
+    }
+    
+    int pressed = 0, result = 0, min_value = 1000000;
+    
+    for(int i=0; i<=1000000; i++){
+        pressed = check_number(i);
+        if(pressed > -1){
+            result = abs(last-i) + pressed;
+            min_value = min(result, min_value);
+        }
+    }
+
+    cout << min_value << endl;
 }
+
+
