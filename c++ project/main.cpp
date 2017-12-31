d6fa99bf001f2d20fd0e76474d8292e2e5b293c2
김정윤 <skyepodium@gmail.com>
2017-12-31 18:50:00 +0900

    12.31

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -8,24 +8,29 @@ using namespace std;
 //사용한 알고리즘: 동적 계획법 Top down
 //사용한 자료구조: 배열
 
-int d[10001];
+int d[10001][3];
 int a[10001];
 
-int go(int n){
-    
-    if(n<3){
-        return d[n];
-    }
-    
-    if(d[n] > 0){
-        return d[n];
-    }
-    
-    d[n] = max(go(n-1), max(go(n-2) + a[n], go(n-3) + a[n-1] + a[n]));
-    
-    return d[n];
-}
+int go(int n, int j){
 
+    if(n<2){
+        if(j==1){
+            return a[1];
+        }else{
+            return 0;
+        }
+    }
+    
+    if(d[n][j] > 0){
+        return d[n][j];
+    }
+    
+    d[n][0] = max(go(n-1, 0), max(go(n-1, 1), go(n-1, 2)));
+    d[n][1] = go(n-1, 0) + a[n];
+    d[n][2] = go(n-1, 1) + a[n];
+    
+    return d[n][j];
+}
 
 int main(){
     
@@ -36,8 +41,15 @@ int main(){
         cin >> a[i];
     }
     
-    d[1] = a[1];
-    d[2] = a[1] + a[2];
+    d[1][0] = 0;
+    d[1][1] = a[1];
+    d[1][2] = 0;
     
-    cout << go(n) << endl;
+
+    int result = 0;
+    for(int i=0; i<3; i++){
+        result = max(result, go(n, i));
+    }
+    
+    cout << result << endl;
 }
