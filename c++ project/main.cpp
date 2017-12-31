a389aa8a66d273f7982793651e72f6cbfd228a7e
김정윤 <skyepodium@gmail.com>
2017-12-31 17:36:01 +0900

    12.31

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
-#include <string>
 
 using namespace std;
+
+//시간 복잡도: O(n)
+//공간 복잡도: O(n)
+//사용한 알고리즘: 동적 계획법
+//사용한 자료구조: 배열
 
 int d[10001];
 int a[101];
@@ -11,31 +15,17 @@ int main(){
     int n, k;
     cin >> n >> k;
     
-    for(int i=0; i<=10000; i++){
-        d[i] = 0;
-    }
-    
     for(int i=1; i<=n; i++){
         cin >> a[i];
     }
     
     d[0] = 1;
-    
     for(int i=1; i<=n; i++){
         for(int j=a[i]; j<=k; j++){
-            d[j] = d[j] + d[j-a[i]];
+            d[j] += d[j-a[i]];
         }
     }
-    
-/*
-    for(int i=1; i<=k; i++){
-        for(int j=1; j<=n; j++){
-            if(i-a[j] > 0){
-                d[i] = d[i] + d[i - a[j]];
-                cout <<"i "<<i<<" d[i] "<<d[i] <<endl;
-            }
-        }
-    }
- */
+
     cout << d[k] << endl;
+    
 }
