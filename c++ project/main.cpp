9e7d8528248a4386f7fad3707aa3356385a6dfd0
김정윤 <skyepodium@gmail.com>
2017-12-31 19:43:21 +0900

    12.31

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -5,51 +5,41 @@ using namespace std;
 
 //시간 복잡도: O(n)
 //공간 복잡도: O(n)
-//사용한 알고리즘: 동적 계획법 Top down
+//사용한 알고리즘: 동적 계획법 Bottom up
 //사용한 자료구조: 배열
 
-int d[10001][3];
-int a[10001];
-
-int go(int n, int j){
-
-    if(n<2){
-        if(j==1){
-            return a[1];
-        }else{
-            return 0;
-        }
-    }
-    
-    if(d[n][j] > 0){
-        return d[n][j];
-    }
-    
-    d[n][0] = max(go(n-1, 0), max(go(n-1, 1), go(n-1, 2)));
-    d[n][1] = go(n-1, 0) + a[n];
-    d[n][2] = go(n-1, 1) + a[n];
-    
-    return d[n][j];
-}
+int d[100000][3];
+int a[100000][3];
 
 int main(){
     
-    int n;
-    cin >> n;
-    
-    for(int i=1; i<=n; i++){
-        cin >> a[i];
-    }
-    
-    d[1][0] = 0;
-    d[1][1] = a[1];
-    d[1][2] = 0;
-    
+    int t, n;
+    cin >> t;
 
-    int result = 0;
-    for(int i=0; i<3; i++){
-        result = max(result, go(n, i));
+    while(t--){
+        cin >> n;
+
+        for(int i=1; i<=2; i++){
+            for(int j=1; j<=n; j++){
+                cin >> a[j][i];
+            }
+        }
+
+        d[1][0] = 0;
+        d[1][1] = a[1][1];
+        d[1][2] = a[1][2];
+        
+        for(int j=2; j<=n; j++){
+
+            d[j][0] = max(d[j-1][0], max(d[j-1][1], d[j-1][2]));
+            d[j][1] = max(d[j-1][0], d[j-1][2]) + a[j][1];
+            d[j][2] = max(d[j-1][0], d[j-1][1]) + a[j][2];
+                
+        }
+
+        int result = 0;
+        result = max(d[n][0], max(d[n][1], d[n][2]));
+        cout << result << endl;
     }
     
-    cout << result << endl;
 }
