f450d67f168a81b7b5a9ff17b804dc9679f67cac
김정윤 <skyepodium@gmail.com>
2018-01-02 15:32:05 +0900

    01.02

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,36 +1,29 @@
 #include <iostream>
-#include <string>
-#include <stack>
+#include <queue>
 
 using namespace std;
 
-//시간 복잡도: O(n)
+//시간 복잡도: O(nm)
 //공간 복잡도: O(n)
 //사용한 알고리즘: 없음
-//사용한 자료구조: string class, STL 스택
+//사용한 자료구조: STL 큐
 
 int main(){
-    string word;
-    cin >> word;
-    int size = (int)word.size();
     
-    stack<int> s;
-    
-    int result = 0;
-    for(int i=0; i<size; i++){
-        if(word[i] == '('){
-            s.push(i);
-        }else{
-            if(i-1 == s.top()){
-                s.pop();
-                result += (int)s.size();
-            }else{
-                s.pop();
-                result += 1;
-            }
+    int n, m;
+    cin >> n >> m;
+
+    int *array = new int[n];
+    int size = n;
+
+    for(int i=0; i<n; i++){
+
+        int begin = i;
+        
+        for(int j=0; j<3; j++){
+            
         }
     }
     
-    cout << result << endl;
-    
+
 }
