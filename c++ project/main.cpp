7682fcbe5a461dc68659470d7e5b867c96397410
김정윤 <skyepodium@gmail.com>
2018-01-03 19:06:09 +0900

    01.03

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,29 +1,12 @@
 #include <iostream>
-#include <queue>
 
 using namespace std;
 
-//시간 복잡도: O(nm)
-//공간 복잡도: O(n)
-//사용한 알고리즘: 없음
-//사용한 자료구조: STL 큐
-
 int main(){
+
+    int n;
+    cin >> n;
     
-    int n, m;
-    cin >> n >> m;
-
-    int *array = new int[n];
-    int size = n;
-
-    for(int i=0; i<n; i++){
-
-        int begin = i;
-        
-        for(int j=0; j<3; j++){
-            
-        }
-    }
-    
-
+    cout << std::hex << n << endl;
 }
+
