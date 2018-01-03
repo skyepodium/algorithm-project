0df79b08d8d3f12c1c1baa9f798124829ffcc5d4
김정윤 <skyepodium@gmail.com>
2018-01-03 22:17:20 +0900

    01.03

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,49 +1,23 @@
 #include <iostream>
-#include <algorithm>
+#include <string>
 
 using namespace std;
 
-//시간 복잡도: O(n)
-//공간 복잡도: O(n)
-//사용한 알고리즘: 동적 계획법 Top Down
-//사용한 자료구조: 2차원 배열
-
-int d[1001][4];
-int a[1001][4];
-
-int go(int n, int j){
-    
-    if(n<2){//재귀 탈출을 위한 기저사례
-        return d[n][j];
-    }
-    
-    if(d[n][j] > 0){//메모이제이션
-        return d[n][j];
-    }
-    
-    d[n][1] = min(go(n-1, 2), go(n-1, 3)) + a[n][1];
-    d[n][2] = min(go(n-1, 1), go(n-1, 3)) + a[n][2];
-    d[n][3] = min(go(n-1, 1), go(n-1, 2)) + a[n][3];
-    
-    return d[n][j];
-}
-
 int main(){
 
-    int n, result = 0;
-    cin >> n;
+    int t , n;
+    string word;
+    cin >> t;
     
-    for(int i=1; i<=n; i++){
-        for(int j=1; j<=3; j++){
-            cin >> a[i][j];
-        }
+
+    for(int i=1; i<=t; i++){
+        cin >> n >> word;
+        cout<<i<<" ";
+            for(int i=0; i<word.size(); i++){
+                if((i+1)!=n){
+                    cout << word[i];
+                }
+            }
+        cout << endl;
     }
-
-    d[1][1] = a[1][1];
-    d[1][2] = a[1][2];
-    d[1][3] = a[1][3];
-    
-    result = min(go(n, 1), min(go(n, 2), go(n, 3)));
-    cout << result << endl;
 }
-
