4cb63fc568f373e552ee9a3b7b57bea05a982dd9
김정윤 <skyepodium@gmail.com>
2018-01-04 00:12:40 +0900

    01.04

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,23 +1,63 @@
 #include <iostream>
 #include <string>
+#include <stack>
 
 using namespace std;
 
+string check(string word){
+    
+    stack<int> s;
+    for(int i=0; i<word.size(); i++){
+
+        if(word[i] == 40 || word[i] == 91 || word[i] == 123){
+
+            s.push(word[i]);
+
+        }else{
+            
+            if(s.size()!=0){
+                if(word[i] > 41){
+                    if(word[i] != s.top()+2){
+                        return "NO";
+                    }else{
+                        s.pop();
+                    }
+                }else{
+                    if(word[i] != s.top()+1){
+                        return "NO";
+                    }else{
+                        s.pop();
+                    }
+                }
+            }else{
+                return "NO";
+            }
+            
+        }
+        
+    }
+    
+    
+    if(s.size() == 0){
+        return "YES";
+    }else{
+        return "NO";
+    }
+}
+
+
 int main(){
 
-    int t , n;
-    string word;
+    int t;
     cin >> t;
     
-
-    for(int i=1; i<=t; i++){
-        cin >> n >> word;
-        cout<<i<<" ";
-            for(int i=0; i<word.size(); i++){
-                if((i+1)!=n){
-                    cout << word[i];
-                }
-            }
-        cout << endl;
+    while(t--){
+        
+        string word;
+        cin >> word;
+        
+        cout << check(word) << endl;
+        
     }
+
 }
