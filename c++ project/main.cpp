1c023607e8dd2f5a94edabb1931da5236a30e3d3
김정윤 <skyepodium@gmail.com>
2018-01-04 12:05:50 +0900

    01.04

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,1 +1,71 @@
-열
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+//시간 복잡도: O(n)
+//공간 복잡도: O(n)
+//사용한 알고리즘: 없음
+//사용한 자료구조: string class, 1차원 배열
+
+int stack[10000];
+
+string check(string word){
+    
+    int size = 0;
+    for(int i=0; i<word.size(); i++){
+        
+        stack[size] = word[i];
+        
+        if(word[i] == 40 || word[i] == 91 || word[i] == 123){
+            size++;
+        }else{
+            
+            if(size!=0){
+                if(word[i] > 41){
+                    if(word[i] != stack[size-1]+2){
+                        return "NO";
+                    }else{
+                        stack[size-1] = 0;
+                        size--;
+                    }
+                }else{
+                    if(word[i] != stack[size-1]+1){
+                        return "NO";
+                    }else{
+                        stack[size-1] = 0;
+                        size--;
+                    }
+                }
+            }else{
+                return "NO";
+            }
+            
+        }
+        
+    }
+
+    if(size == 0){
+        return "YES";
+    }else{
+        return "NO";
+    }
+}
+
+
+int main(){
+    
+    int t;
+    cin >> t;
+    
+    while(t--){
+        
+        string word;
+        cin >> word;
+        
+        cout << check(word) << endl;
+        
+    }
+    
+}
+
