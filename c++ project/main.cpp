dc286ab2247088140265422c17845d19a1f50ce0
김정윤 <skyepodium@gmail.com>
2018-01-04 18:32:01 +0900

    01.04

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,23 +1,59 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
-int go(int n, int k){
+int arr[101];
+
+long long int result = 0;
+
+int gcd(int a, int b){
     
-    if(n==k || k==0){
-        return 1;
+    if(b==0){
+        return a;
+    }else{
+        return gcd(b, a%b);
+    }
+}
+
+void pick(int n, vector<int>& picked, int to_pick){
+
+    if(to_pick == 0){
+        result += gcd(arr[picked[0]], arr[picked[1]]);
+        return;
     }
     
-    return go(n-1, k-1) + go(n-1, k);
+    int smallest = picked.empty() ? 0 : picked.back() + 1;
+
+    for(int next = smallest; next < n; ++next){
+        picked.push_back(next);
+        pick(n, picked, to_pick - 1);
+        picked.pop_back();
+    }
+    
 }
 
 
 int main(){
+
+    int t;
+    cin >> t;
     
-    int a, b;
-    cin >> a >> b;
-    cout << go(a,b) << endl;
-    
-    
+    while(t--){
+        
+        
+        int n;
+        cin >> n;
+        
+        for(int i=0; i<n; i++){
+            cin >> arr[i];
+        }
+        
+        vector<int> picked;
+
+        pick(n, picked, 2);
+        cout << result << endl;
+        result = 0;
+    }
+
 }
 
