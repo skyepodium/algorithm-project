9a9280570f43b3ef44ba932106c6bc10afb70223
김정윤 <skyepodium@gmail.com>
2018-01-04 15:21:57 +0900

    01.04

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -2,40 +2,22 @@
 
 using namespace std;
 
-int a[101];
-
-int gcd(int a, int b){
+int go(int n, int k){
     
-    if(b==0){
-        return a;
-    }else{
-        return gcd(b, a%b);
+    if(n==k || k==0){
+        return 1;
     }
     
+    return go(n-1, k-1) + go(n-1, k);
 }
 
-int main(){
- 
-    int t, n;
-    cin >> t;
-    
-    while(t--){
 
-        cin >> n;
-        
-        for(int i=0; i<n; i++){
-            cin >> a[i];
-        }
-        
-        long long int result = 0;
-        for(int i=0; i<n-1; i++){
-            for(int j=i+1; j<n; j++){
-                result += gcd(a[i],a[j]);
-            }
-        }
-        
-        cout << result << endl;
-    }
+int main(){
+    
+    int a, b;
+    cin >> a >> b;
+    cout << go(a,b) << endl;
+    
     
 }
 
