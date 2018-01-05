bdba04e6df1fcc7a34b6215f267c6487c7473763
김정윤 <skyepodium@gmail.com>
2018-01-05 20:38:07 +0900

    01.05

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -3,38 +3,24 @@
 
 using namespace std;
 
-int d[1001];
-int a[1001];
-
 int main(){
     
     int n;
     cin >> n;
     
-    int is_continue = false;
+    int before = 1001;
+    int current = 0;
+    int diff = 0;
     int result = 0;
     
-    d[0] = 0;
-    a[0] = 1001;
-
     for(int i=1; i<=n; i++){
-
-        cin >> a[i];
         
-        if(a[i] > a[i-1]){
-            d[i] = a[i] - a[i-1];
-
-            if(is_continue){
-                d[i] += d[i-1];
-            }
-
-            result = max(d[i], result);
-            is_continue = true;
-            
-        }else{
-            is_continue = false;
-        }
+        cin >> current;
         
+        diff = before < current ? diff + current - before : 0;
+        
+        result = max(result, diff);
+        before = current;
     }
     
     cout << result << endl;
