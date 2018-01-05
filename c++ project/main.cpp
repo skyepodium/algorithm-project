9ee59022e10879c3f87df98db294419ae4cb270e
김정윤 <skyepodium@gmail.com>
2018-01-05 16:36:17 +0900

    01.05

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,59 +1,32 @@
 #include <iostream>
-#include <vector>
+#include <algorithm>
+
 using namespace std;
 
-int arr[101];
-
-long long int result = 0;
-
-int gcd(int a, int b){
-    
-    if(b==0){
-        return a;
-    }else{
-        return gcd(b, a%b);
-    }
-}
-
-void pick(int n, vector<int>& picked, int to_pick){
-
-    if(to_pick == 0){
-        result += gcd(arr[picked[0]], arr[picked[1]]);
-        return;
-    }
-    
-    int smallest = picked.empty() ? 0 : picked.back() + 1;
-
-    for(int next = smallest; next < n; ++next){
-        picked.push_back(next);
-        pick(n, picked, to_pick - 1);
-        picked.pop_back();
-    }
-    
-}
-
+int d[1001];
+int a[1001];
 
 int main(){
-
-    int t;
-    cin >> t;
     
-    while(t--){
-        
-        
-        int n;
-        cin >> n;
-        
-        for(int i=0; i<n; i++){
-            cin >> arr[i];
-        }
-        
-        vector<int> picked;
-
-        pick(n, picked, 2);
-        cout << result << endl;
-        result = 0;
+    int n;
+    cin >> n;
+    
+    for(int i=1; i<=n; i++){
+        cin >> a[i];
+        d[i] = a[i];
     }
-
+    
+    for(int i=1; i<=n; i++){
+        int max_val = 0;
+        for(int j=1; j<i; j++){
+            if(a[j] < a[i]){
+                max_val = max(max_val, d[j]);
+            }
+        }
+        d[i] = max_val + a[i];
+    }
+    
+    cout << *max_element(d+1, d+n+1) << endl;
 }
+
 
