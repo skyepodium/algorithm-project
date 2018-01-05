ac13e4205522f553142b51e21642d26b09f9de19
김정윤 <skyepodium@gmail.com>
2018-01-06 01:37:38 +0900

    01.06

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -6,21 +6,4 @@ using namespace std;
 
 int main(){
     
-    string word;
-    while(cin >> word){
-        
-        if(word!="0"){
-            
-            while(word.size()>1){
-                int sum = 0;
-                for(int i=0; i<word.size(); i++){
-                    sum += word[i] - 48;
-                }
-                word = to_string(sum);
-            }
-            cout << word << endl;
-        }else{
-            break;
-        }
-    }
 }
