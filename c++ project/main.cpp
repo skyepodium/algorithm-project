d18ace99abca24447758b0768ac46225230c2096
김정윤 <skyepodium@gmail.com>
2018-01-09 18:44:34 +0900

    01.09

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,61 +1,20 @@
 #include <iostream>
-#include <vector>
-#include <queue>
-#include <algorithm>
+#include <string>
 
 using namespace std;
 
-vector<int> a[1001];
-bool check[1001];
-
-void dfs(int node){
-    check[node] = true;
-    cout << node <<" ";
-    for(int i=0; i<a[node].size(); i++){
-        int next = a[node][i];
-        if(check[next] == false){
-            dfs(next);
-        }
-    }
-}
-
-void bfs(int start){
-    queue<int> q;
-    memset(check, false, sizeof(check));
-    check[start] = true;
-    q.push(start);
-    while(!q.empty()){
-        int node = q.front();
-        q.pop();
-        cout <<node<<" ";
-        for(int i=0; i<a[node].size(); i++){
-            int next = a[node][i];
-            if(check[next] == false){
-                check[next] = true;
-                q.push(next);
-            }
-        }
-    }
-}
-
-
 int main(){
-    int n, m, start;
-    cin >> n >> m >> start;
     
-    for(int i=0; i<m; i++){
-        int u, v;
-        cin >> u >> v;
-        a[u].push_back(v);
-        a[v].push_back(u);
+    string word;
+    cin >> word;
+    
+    for(int i=0; i<word.size(); i++){
+        if(word[i] != 45 && word[i] <= 90){
+            cout << word[i];
+        }
     }
-    
-    for(int i=1; i<=n; i++){
-        sort(a[i].begin(), a[i].end());
-    }
-    
-    dfs(start);
     cout << endl;
-    bfs(start);
-    cout << endl;
+    
 }
+
+
