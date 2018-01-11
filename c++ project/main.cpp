e29e31525bb6de32cd8aade7e7ba8839b408ccff
김정윤 <skyepodium@gmail.com>
2018-01-12 01:04:56 +0900

    01.12

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,43 +1,44 @@
 #include <iostream>
-#include <algorithm>
 
 using namespace std;
 
-int d[1001];
-int a[1001];
+//시간 복잡도: O(n√n)
+//공간 복잡도: O(n)
+//사용한 알고리즘: 동적 계획법 Top Down
+//사용한 자료구조: 1차원 배열
+
+int d[100001];
 
 int go(int n){
+
+    if(n < 2){
+        return n;
+    }
+
+    if(d[n] < n){
+        return d[n];
+    }
     
+    for(int j=1; j*j<=n; j++){
+        if(d[n] > go(n-j*j) + 1){
+            d[n] = go(n-j*j) + 1;
+        }
+    }
     
-    
-    return 1;
+    return d[n];
 }
 
 
 int main(){
-    
+
     int n;
     cin >> n;
     
-    for(int i=1; i<=n; i++){
-        cin >> a[i];
-        d[i] = 1;
+    for(int i=0; i<=n; i++){
+        d[i] = i;
     }
     
-    for(int i=1; i<=n; i++){
-        
-        int max_length = 0;
-        for(int j=1; j<i; j++){
-            
-            if(a[i] < a[j]){
-                max_length = max(max_length, d[j]);
-            }
-        }
-        d[i] = max_length + 1;
-    }
-    
-    int result = *max_element(d+1, d+n+1);
-    
-    cout << result << endl;
-    
+    cout << go(n) << endl;
+
 }
+
