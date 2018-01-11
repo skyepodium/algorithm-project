11b940b5ba85dc8e736282a560d94a796552ff0d
김정윤 <skyepodium@gmail.com>
2018-01-11 18:42:43 +0900

    01.11

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,20 +1,43 @@
 #include <iostream>
-#include <string>
+#include <algorithm>
 
 using namespace std;
 
-int main(){
+int d[1001];
+int a[1001];
+
+int go(int n){
     
-    string word;
-    cin >> word;
     
-    for(int i=0; i<word.size(); i++){
-        if(word[i] != 45 && word[i] <= 90){
-            cout << word[i];
-        }
-    }
-    cout << endl;
     
+    return 1;
 }
 
 
+int main(){
+    
+    int n;
+    cin >> n;
+    
+    for(int i=1; i<=n; i++){
+        cin >> a[i];
+        d[i] = 1;
+    }
+    
+    for(int i=1; i<=n; i++){
+        
+        int max_length = 0;
+        for(int j=1; j<i; j++){
+            
+            if(a[i] < a[j]){
+                max_length = max(max_length, d[j]);
+            }
+        }
+        d[i] = max_length + 1;
+    }
+    
+    int result = *max_element(d+1, d+n+1);
+    
+    cout << result << endl;
+    
+}
