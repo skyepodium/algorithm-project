e5261c45f76faeee7bc09733b027a919a4c22508
김정윤 <skyepodium@gmail.com>
2018-01-12 19:09:32 +0900

    01.12

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,49 +1,54 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
-//시간 복잡도: O(n)
+//시간 복잡도: O(n^2)
 //공간 복잡도: O(n)
-//사용한 알고리즘: 동적 계획법 Top Down
-//사용한 자료구조: 1차원 배열
+//사용한 알고리즘: 동적 계획법 Bottom up
+//사용한 자료구조: 배열
 
-long long int d[101];
-
-long long int go(int n){
-    
-    if(n < 5){
-        return d[n];
-    }
-    
-    if(d[n] > 0){
-        return d[n];
-    }
-    
-    d[n] = go(n-1) + go(n-5);
-    
-    return d[n];
-}
-
-
+int d[1001];
+int d2[1001];
+int a[1001];
 
 int main(){
     
-    int t;
-    cin >> t;
+    int n;
+    cin >> n;
     
-    d[0] = 0;
-    d[1] = 1;
-    d[2] = 1;
-    d[3] = 1;
-    d[4] = 2;
-
-    go(100);
-    
-    while(t--){
-        int a;
-        cin >> a;
-        cout << d[a] << endl;
+    for(int i=1; i<=n; i++){
+        cin >> a[i];
     }
     
-}
+    for(int i=1; i<=n; i++){
+        d[i] = 1;
+        int max_length = 0;
+        for(int j=1; j<i; j++){
+            if(a[j] < a[i]){
+                max_length = max(max_length, d[j]);
+            }
+        }
+        d[i] = max_length + 1;
+    }
 
+    for(int i=n; i>0; i--){
+        d2[i] = 1;
+        int max_length = 0;
+        for(int j=n; j>i; j--){
+            if(a[i] > a[j]){
+                max_length = max(max_length, d2[j]);
+            }
+        }
+        d2[i] = max_length + 1;
+    }
+    
+    int result = 0;
+    for(int i=1; i<=n; i++){
+        if(result < d[i] + d2[i] -1){
+            result = d[i] + d2[i] -1;
+        }
+    }
+    
+    cout << result << endl;
+}
