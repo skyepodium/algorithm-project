ed4d2a6c7e410b507809c0dd1ce718548ae831e0
김정윤 <skyepodium@gmail.com>
2018-01-16 19:27:15 +0900

    01.16

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,40 +1,49 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
 
 using namespace std;
 
-int d1[100001];
-int d2[100001];
-int a[100001];
+int arr[101];
+
+long long int result = 0;
+
+void pick(int n, vector<int>& picked, int to_pick){
+    
+    if(to_pick == 0){
+        result = 0;
+        for(int i=0; i<7; i++){
+            result = result + arr[picked[i]];
+        }
+        if(result == 100){
+            for(int i=0; i<7; i++){
+                cout << arr[picked[i]]<<endl;
+            }
+        }
+        
+        return;
+    }
+    
+    int smallest = picked.empty() ? 0 : picked.back() + 1;
+    
+    for(int next = smallest; next < n; ++next){
+        picked.push_back(next);
+        pick(n, picked, to_pick - 1);
+        picked.pop_back();
+    }
+    
+}
+
 
 int main(){
     
-    int n;
-    cin >> n;
-    
-    int result = -1001;
-    
-    for(int i=1; i<=n; i++){
-        cin >> a[i];
-        d1[i] = a[i];
-        d2[i] = a[i];
+    for(int i=0; i<9; i++){
+        cin >> arr[i];
     }
-    for(int i=1; i<=n; i++){
-        d1[i] = max(d1[i-1] + a[i], a[i]);
-    }
-    
-    for(int i=n; i>=1; i--){
-        d2[i] = max(d2[i+1] + a[i], a[i]);
-    }
-    
-    for(int i=1; i<=n; i++){
-        if(d1[i] < d1[i-1] + d2[i+1]){
-            result = max(d1[i-1] + d2[i+1], result);
-        }else{
-            result = max(d1[i], result);
-        }
-    }
-    
-    
-    cout << result << endl;
+        
+    sort(arr, arr+9);
+        
+    vector<int> picked;
+        
+    pick(9, picked, 7);
 }
