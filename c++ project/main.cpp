f4695f14ee63c28df95990747598232e563f7c8d
김정윤 <skyepodium@gmail.com>
2018-01-16 21:30:54 +0900

    01.16

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,44 +1,27 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-//시간 복잡도: O(ESM)
-//공간 복잡도: 0
-//사용한 알고리즘: Brute Force Search
-//사용한 자료구조: 없음
 
-int main() {
+int main(){
     
-    int E, S, M;
-    cin >> E >> S >> M;
+    int n, m;
+    cin >> n >> m;
     
-    int e=1, s=1, m=1;
-    
-    for(int i=1; ; i++){
-        
-        if(e == E && s == S && m == M){
-            cout << i <<endl;
-            break;
-        }
-        
-        e++;
-        s++;
-        m++;
-        
-        if(e == 16){
-            e = 1;
-        }
-        
-        if(s == 29){
-            s = 1;
-        }
-        
-        if(m == 20){
-            m = 1;
-        }
-        
+    vector<int> v;
+    for(int i=0; i<n+m; i++){
+        int a;
+        cin >> a;
+        v.push_back(a);
     }
+    
+    sort(v.begin(), v.end());
+    
+    for(int i=0; i<v.size(); i++){
+        cout << v[i] <<" ";
+    }
+    
 }
-
-
 
