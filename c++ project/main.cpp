00b19181daa957f1b579e8ff0fb6367c4b6fadb6
김정윤 <skyepodium@gmail.com>
2018-01-17 16:05:07 +0900

    01.17

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,27 +1,38 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
+
+int arr[9];
 
 using namespace std;
 
+void print_arr(int a, int b){
+    
+    for(int i=0; i<9; i++){
+        if(i == a || i == b){
+            continue;
+        }else{
+            cout << arr[i] << endl;
+        }
+    }
+}
 
 int main(){
     
-    int n, m;
-    cin >> n >> m;
-    
-    vector<int> v;
-    for(int i=0; i<n+m; i++){
-        int a;
-        cin >> a;
-        v.push_back(a);
+    int sum = 0 ;
+    for(int i=0; i<9; i++){
+        cin >> arr[i];
+        sum = sum + arr[i];
     }
     
-    sort(v.begin(), v.end());
+    sort(arr, arr+9);
     
-    for(int i=0; i<v.size(); i++){
-        cout << v[i] <<" ";
+    for(int i=0; i<9; i++){
+        for(int j=i+1; j<9; j++){
+            if(sum - arr[i] - arr[j] == 100){
+                print_arr(i, j);
+                break;
+            }
+        }
     }
-    
 }
 
