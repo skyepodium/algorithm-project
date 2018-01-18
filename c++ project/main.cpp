962a41a048928855ab8a02ce4218c38928c83272
김정윤 <skyepodium@gmail.com>
2018-01-18 15:06:02 +0900

    01.18

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,38 +1,25 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
-
-int arr[9];
 
 using namespace std;
 
-void print_arr(int a, int b){
+int main() {
+    int n;
+    cin >> n;
     
-    for(int i=0; i<9; i++){
-        if(i == a || i == b){
-            continue;
-        }else{
-            cout << arr[i] << endl;
+    vector<int> v(n);
+    for(int i=0; i<n; i++){
+        cin >> v[i];
+    }
+    
+    if(next_permutation(v.begin(), v.end())){
+        for(int i=0; i<n; i++){
+            cout << v[i] <<" ";
         }
+    }else{
+        cout << -1;
     }
+    
+    cout << endl;
 }
-
-int main(){
-    
-    int sum = 0 ;
-    for(int i=0; i<9; i++){
-        cin >> arr[i];
-        sum = sum + arr[i];
-    }
-    
-    sort(arr, arr+9);
-    
-    for(int i=0; i<9; i++){
-        for(int j=i+1; j<9; j++){
-            if(sum - arr[i] - arr[j] == 100){
-                print_arr(i, j);
-                break;
-            }
-        }
-    }
-}
-
