b0fad125843a89a8d525d69c3dbf477f5069225a
김정윤 <skyepodium@gmail.com>
2018-01-19 15:56:08 +0900

    01.19

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,40 +1,18 @@
 #include <iostream>
-#include <algorithm>
-#include <vector>
 
 using namespace std;
 
-int main() {
+int main(){
     
-    while(true){
-        int n;
-        cin >> n;
-        
-        if(n == 0){
-            break;
-        }
-        
-        vector<int> v(n);
-        for(int i=0; i<n; i++){
-            cin >> v[i];
-        }
-        
-        vector<int> d;
-        for(int i=0; i<6; i++){
-            d.push_back(1);
-        }
-        for(int i=0; i<n-6; i++){
-            d.push_back(0);
-        }
-        
-        do{
-            for(int i=0; i<n; i++){
-                if(d[i] == 1){
-                    cout << v[i] <<" ";
-                }
-            }
-            cout << endl;
-        }while(prev_permutation(d.begin(), d.end()));
-        cout << endl;
+    long long int s;
+    cin >> s;
+    
+    long long int cnt = 0;
+    while(s>cnt){
+        cnt++;
+        s = s-cnt;
     }
+    cout << cnt << endl;
 }
+
+
