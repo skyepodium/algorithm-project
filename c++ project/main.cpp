854d1441885c5d67b79bc3fd10bfe109b55b730c
김정윤 <skyepodium@gmail.com>
2018-01-21 16:11:48 +0900

    01.21

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -5,24 +5,25 @@ using namespace std;
 
 //시간 복잡도: O(n^2)
 //공간 복잡도: O(n)
-//사용한 알고리즘: 동적 계획법 Top Down
+//사용한 알고리즘: 동적 계획법, Top Down
 //사용한 자료구조: 1차원 배열
 
 int d[1001];
 int a[1001];
 
 int go(int i){
+
+    if(d[i] > 0){
+        return d[i];
+    }
     
-    if(d[i] > 0) return d[i];
-    
-    int max_length = 0;
+    int max_val = 0;
     for(int j=1; j<i; j++){
-        
-        if(a[i] < a[j]){
-            max_length = max(max_length, d[j]);
+        if(a[j] < a[i]){
+            max_val = max(max_val, d[j]);
         }
     }
-    d[i] = max_length + 1;
+    d[i] = max_val + a[i];
     
     return d[i];
 }
@@ -39,10 +40,10 @@ int main(){
     
     int result = 0;
     for(int i=1; i<=n; i++){
-        if (result < go(i)){
+        if(result < go(i)){
             result = go(i);
         }
     }
-    
+
     cout << result << endl;
 }
