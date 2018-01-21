424eab2ef3006882f0732e6b7d19ea1fb15d168e
김정윤 <skyepodium@gmail.com>
2018-01-21 16:03:16 +0900

    01.21

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,30 +1,48 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
-
-//시간 복잡도: O(n)
-//공간 복잡도: O(n)
-//사용한 알고리즘: 완전탐색, next_permutation
-//사용한 자료구조: 1차원 벡터
 
 using namespace std;
 
-int main() {
+//시간 복잡도: O(n^2)
+//공간 복잡도: O(n)
+//사용한 알고리즘: 동적 계획법 Top Down
+//사용한 자료구조: 1차원 배열
+
+int d[1001];
+int a[1001];
+
+int go(int i){
+    
+    if(d[i] > 0) return d[i];
+    
+    int max_length = 0;
+    for(int j=1; j<i; j++){
+        
+        if(a[i] < a[j]){
+            max_length = max(max_length, d[j]);
+        }
+    }
+    d[i] = max_length + 1;
+    
+    return d[i];
+}
+
+
+int main(){
+    
     int n;
     cin >> n;
     
-    vector<int> v(n);
-    for(int i=0; i<n; i++){
-        cin >> v[i];
+    for(int i=1; i<=n; i++){
+        cin >> a[i];
     }
     
-    if(next_permutation(v.begin(), v.end())){
-        for(int i=0; i<n; i++){
-            cout << v[i] <<" ";
+    int result = 0;
+    for(int i=1; i<=n; i++){
+        if (result < go(i)){
+            result = go(i);
         }
-    }else{
-        cout << -1;
     }
     
-    cout << endl;
+    cout << result << endl;
 }
