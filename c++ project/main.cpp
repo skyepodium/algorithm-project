175e73103199e637fba5f46c4d76ed8286c44e00
김정윤 <skyepodium@gmail.com>
2018-01-22 18:53:06 +0900

    01.22

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -2,33 +2,21 @@
 
 using namespace std;
 
-int d[15][15];
-
 int main(){
     
-    for(int i=1; i<=14; i++){
-        d[0][i] = i;
-        d[i][1] = 1;
-    }
-    
-    for(int i=1; i<=14; i++){
-        for(int j=2; j<=14; j++){
-            
-            for(int k=1; k<=j; k++){
-                d[i][j] = d[i][j] + d[i-1][k];
-            }
-            
+    int a;
+    char b;
+    int result = 0;
+    while(cin >> a >> b){
+        
+        result = result + a;
+        
+        if(b!=','){
+            break;
         }
+        
     }
-    
-    int t, a, b;
-    cin >> t;
-    
-    while(t--){
-        cin >> a >> b;
-        cout << d[a][b] << endl;
-    }
-    
+    cout << result << endl;
 }
 
 
