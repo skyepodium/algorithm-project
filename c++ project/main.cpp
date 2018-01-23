b7273fa720a30be2acbfea7af2dc70a3b1309d60
김정윤 <skyepodium@gmail.com>
2018-01-23 16:15:55 +0900

    01.23

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -3,15 +3,11 @@
 
 using namespace std;
 
-//시간 복잡도: O(E)
-//공간 복잡도: O(E)
-//사용한 알고리즘: DFS
-//사용한 자료구조: 2차원 벡터, 1차원 배열
-
 vector<int> a[1001];
-bool check[1001];
+int check[1001];
 
 void dfs(int node){
+    
     check[node] = true;
     
     for(int i=0; i<a[node].size(); i++){
@@ -24,23 +20,36 @@ void dfs(int node){
 
 int main(){
     
-    int n, m;
-    cin >> n >> m;
+    int t;
+    cin >> t;
     
-    for(int i=0; i<m; i++){
-        int u, v;
-        cin >> u >> v;
-        a[u].push_back(v);
-        a[v].push_back(u);
-    }
-
-    int cnt = 0;
-    for(int i=1; i<=n; i++){
-        if(check[i] == false){
-            dfs(i);
-            cnt++;
+    while(t--){
+        
+        int n;
+        cin >> n;
+        
+        for(int i=0; i<=n; i++){
+            a[i].clear();
+            check[i] = 0;
         }
-    }
-    cout << cnt << endl;
-}
+        
+        for(int i=1; i<=n; i++){
+            int v;
+            cin >> v;
+            a[i].push_back(v);
+            a[v].push_back(i);
+        }
 
+        int cnt = 0;
+        for(int i=1; i<=n; i++){
+            if(check[i] == false){
+                dfs(i);
+                cnt++;
+            }
+        }
+        
+        cout << cnt << endl;
+    }
+    
+    
+}
