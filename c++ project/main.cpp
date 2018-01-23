3904a2683bf7bdeeb566c4e845adec3a61968f11
김정윤 <skyepodium@gmail.com>
2018-01-23 13:55:22 +0900

    01.23

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,22 +1,18 @@
 #include <iostream>
-#include <queue>
 #include <vector>
-#include <cstring>
-#include <algorithm>
 
 using namespace std;
 
 //시간 복잡도: O(E)
 //공간 복잡도: O(E)
-//사용한 알고리즘: BFS, DFS, sort
+//사용한 알고리즘: DFS
 //사용한 자료구조: 2차원 벡터, 1차원 배열
 
-vector<int> a[10001];
-bool check[10001];
+vector<int> a[1001];
+bool check[1001];
 
 void dfs(int node){
     check[node] = true;
-    cout << node << " ";
     
     for(int i=0; i<a[node].size(); i++){
         int next = a[node][i];
@@ -26,32 +22,10 @@ void dfs(int node){
     }
 }
 
-void bfs(int start){
-    queue<int> q;
-    memset(check, false, sizeof(check));
-    check[start] = true;
-    q.push(start);
-    
-    while(!q.empty()){
-        int node = q.front();
-        q.pop();
-        cout << node << " ";
-
-        for(int i=0; i<a[node].size(); i++){
-            int next = a[node][i];
-            if(check[next] == false){
-                check[next] = true;
-                q.push(next);
-            }
-        }
-    }
-}
-
-
 int main(){
     
-    int n, m, start;
-    cin >> n >> m >> start;
+    int n, m;
+    cin >> n >> m;
     
     for(int i=0; i<m; i++){
         int u, v;
@@ -59,15 +33,14 @@ int main(){
         a[u].push_back(v);
         a[v].push_back(u);
     }
-    
+
+    int cnt = 0;
     for(int i=1; i<=n; i++){
-        sort(a[i].begin(), a[i].end());
+        if(check[i] == false){
+            dfs(i);
+            cnt++;
+        }
     }
-    
-    dfs(start);
-    cout << endl;
-    bfs(start);
-    cout << endl;
-    
+    cout << cnt << endl;
 }
 
