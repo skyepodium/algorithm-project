20b19c8498b3077850f9e69471c0d9eaef32e26e
김정윤 <skyepodium@gmail.com>
2018-01-24 16:55:14 +0900

    01.24

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,54 +1,42 @@
 #include <iostream>
-#include <algorithm>
 
 using namespace std;
 
+int a[101][101];
+long long d[101][101];
 
-//시간 복잡도: O(nm)
-//공간 복잡도: O(nm)
+//시간 복잡도: O(n^3)
+//공간 복잡도: O(n^2)
 //사용한 알고리즘: 동적 계획법, Bottom up
 //사용한 자료구조: 2차원 배열
 
-int a[101][101];
-long long int d[101][101];
-
-long long int go(int i, int j){
-    
-    if(i==1 && j==1){
-        return 1;
-    }
-    
-    if(d[i][j] > 0){
-        return d[i][j];
-    }
-    
-    for(int k=1; k<j; k++){
-        if (k+a[i][k] == j) {
-            d[i][j] += go(i, k);
-        }
-    }
-    
-    for (int k=1; k<i; k++) {
-        if (k+a[k][j] == i) {
-            d[i][j] += go(k, j);
-        }
-    }
-    
-    return d[i][j];
-}
-
-int main(){
-    
+int main() {
     int n;
     cin >> n;
-    
-    for(int i=1; i<=n; i++){
-        for(int j=1; j<=n; j++){
+    for (int i=1; i<=n; i++) {
+        for (int j=1; j<=n; j++) {
             cin >> a[i][j];
         }
     }
     
-    cout << go(n, n) << endl;
+    d[1][1] = 1;
+    for (int i=1; i<=n; i++) {
+        for (int j=1; j<=n; j++) {
+            if (i == 1 && j == 1) continue;
+            
+            for (int k=1; k<j; k++) {
+                if (k+a[i][k] == j) {
+                    d[i][j] += d[i][k];
+                }
+            }
+
+            for (int k=1; k<i; k++) {
+                if (k+a[k][j] == i) {
+                    d[i][j] += d[k][j];
+                }
+            }
+        }
+    }
+    
+    cout << d[n][n] << endl;
 }
-
-
