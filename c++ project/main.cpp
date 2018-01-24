c860297e920cfce9082f6b02828f3621a3e1a89e
김정윤 <skyepodium@gmail.com>
2018-01-24 19:34:14 +0900

    01.24

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,42 +1,54 @@
 #include <iostream>
+#include <vector>
+#include <queue>
 
 using namespace std;
 
-int a[101][101];
-long long d[101][101];
+//시간 복잡도: O(E)
+//공간 복잡도: O(E)
+//사용한 알고리즘: BFS
+//사용한 자료구조: 2차원 벡터, 1차원 배열
 
-//시간 복잡도: O(n^3)
-//공간 복잡도: O(n^2)
-//사용한 알고리즘: 동적 계획법, Bottom up
-//사용한 자료구조: 2차원 배열
+vector<int> a[1001];
+bool check[1001];
 
-int main() {
-    int n;
-    cin >> n;
-    for (int i=1; i<=n; i++) {
-        for (int j=1; j<=n; j++) {
-            cin >> a[i][j];
-        }
-    }
+void bfs(int start){
+    queue<int> q;
+    check[start] = true;
+    q.push(start);
     
-    d[1][1] = 1;
-    for (int i=1; i<=n; i++) {
-        for (int j=1; j<=n; j++) {
-            if (i == 1 && j == 1) continue;
-            
-            for (int k=1; k<j; k++) {
-                if (k+a[i][k] == j) {
-                    d[i][j] += d[i][k];
-                }
-            }
-
-            for (int k=1; k<i; k++) {
-                if (k+a[k][j] == i) {
-                    d[i][j] += d[k][j];
-                }
+    while(!q.empty()){
+        int node = q.front();
+        q.pop();
+        
+        for(int i=0; i<a[node].size(); i++){
+            int next= a[node][i];
+            if(check[next] == false){
+                check[next] = true;
+                q.push(next);
             }
         }
     }
+}
+
+int main(){
     
-    cout << d[n][n] << endl;
+    int n, m;
+    cin >> n >> m;
+    
+    for(int i=0; i<m; i++){
+        int u, v;
+        cin >> u >> v;
+        a[u].push_back(v);
+        a[v].push_back(u);
+    }
+    
+    int cnt = 0;
+    for(int i=1; i<=n; i++){
+        if(check[i] == false){
+            bfs(i);
+            cnt++;
+        }
+    }
+    cout << cnt << endl;
 }
