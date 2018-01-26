87770c6cd223df31e3470206453bdff58bdf8b6b
김정윤 <skyepodium@gmail.com>
2018-01-26 20:05:21 +0900

    01.26

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,29 +1,12 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-int d[6] = {500, 100, 50, 10, 5, 1};
+int d[100001];
+int a[100001];
 
 int main(){
-    
-    int n;
-    cin >> n;
-    
-    int money = 1000-n;
-    
-    int cnt = 0;
-    int i=0;
-    while(money>0){
 
-        if(money >= d[i]){
-            money = money - d[i];
-            cnt++ ;
-        }else{
-            i++;
-        }
 
-    }
-    
-    cout << cnt << endl;
-    
-}
+};
