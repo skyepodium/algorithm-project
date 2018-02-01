e6e803afd492081e66d29431e5569412564c2a8f
김정윤 <skyepodium@gmail.com>
2018-02-01 20:15:23 +0900

    02.01

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -2,8 +2,27 @@
 
 using namespace std;
 
+long long int a, b, c;
+
+
+long long int calc (long long int m, long long int n){
+    
+    if(n == 0){
+        return 1%c;
+    }else if(n == 1){
+        return m%c;
+    }else if(n%2 == 0){
+        long long int temp = calc(m, n/2)%c;
+        return (temp * temp)%c;
+    }else{
+        return (m * calc(m, n-1))%c;
+    }
+}
+
 int main(){
     
-    cout << "hi" << endl;
+    cin >> a >> b >> c;
+    
+    cout << calc(a, b)%c << endl;
 
 }
