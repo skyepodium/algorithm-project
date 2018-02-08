e0f5b0f368907eb9e444ca3426134c6a881fa23c
김정윤 <skyepodium@gmail.com>
2018-02-08 16:11:15 +0900

    02.08

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,59 +1,57 @@
 #include <iostream>
-#include <algorithm>
-#include <cstring>
+#include <queue>
 
 using namespace std;
 
 //시간 복잡도: O(n^2)
 //공간 복잡도: O(n^2)
-//사용한 알고리즘: DFS
-//사용한 자료구조: 2차원 배열
+//사용한 알고리즘: BFS
+//사용한 자료구조: 2차원 배열, queue
 
-int w, h;
-int a[50][50];
-bool check[50][50];
-int dx[] = {1, -1, 0, 0, 1, 1, -1, -1};
-int dy[] = {0, 0, 1, -1, 1, -1, 1, -1};
+int n, m;
+int a[100][100];
+bool check[100][100];
+int dx[] = {0, 0, 1, -1};
+int dy[] = {1, -1, 0, 0};
 
-void dfs(int x, int y){
+void bfs(int x, int y){
     check[x][y] = true;
     
-    for(int i=0; i<8; i++){
-        int nx = x+dx[i];
-        int ny = y+dy[i];
-        if(0<=nx && nx<w && 0<=ny && ny<h){
-            if(a[nx][ny] == 1 && check[nx][ny] == false){
-                dfs(nx, ny);
+    queue<pair<int, int>> q;
+    q.push(make_pair(x, y));
+    
+    while(!q.empty()){
+        x = q.front().first;
+        y = q.front().second;
+        q.pop();
+        
+        for(int i=0; i<4; i++){
+            int nx = x+dx[i];
+            int ny = y+dy[i];
+            if(0<=nx && nx<m && 0<=ny && ny<n){
+                if(a[nx][ny] == 1 && check[nx][ny] == false){
+                    check[nx][ny] = true;
+                    q.push(make_pair(nx, ny));
+                    a[nx][ny] = a[x][y]+1;
+
+                }
             }
         }
+
     }
 }
 
 int main(){
     
-    while(cin >> w >> h){
-        if(w == 0 && h == 0){
-            break;
+    cin >> n >> m;
+    
+    for(int j=0; j<n; j++){
+        for(int i=0; i<m; i++){
+            scanf("%1d", &a[i][j]);
         }
-        
-        for(int j=0; j<h; j++){
-            for(int i=0; i<w; i++){
-                cin >> a[i][j];
-            }
-        }
-        
-        memset(check, false, sizeof(check));
-        
-        int cnt = 0;
-        for(int j=0; j<h; j++){
-            for(int i=0; i<w; i++){
-                if(a[i][j] == 1 && check[i][j] == false){
-                    dfs(i, j);
-                    cnt++;
-                }
-            }
-        }
-        
-        cout << cnt << endl;
     }
+        
+    bfs(0, 0);
+
+    cout << a[m-1][n-1] << endl;
 }
