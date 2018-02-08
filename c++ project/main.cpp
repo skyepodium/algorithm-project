61bde36472f47a066a58c364c06b80219d0842b2
김정윤 <skyepodium@gmail.com>
2018-02-08 14:55:32 +0900

    02.08

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -2,28 +2,59 @@
 #include <algorithm>
 #include <vector>
 #include <queue>
+#include <cstring>
 
 using namespace std;
 
-vector<int> v(10);
+int a[30][30];
+int d[30][30];
+int n;
+int ans[25*25];
+
+int dx[] = {0, 0, 1, -1};
+int dy[] = {1, -1, 0, 0};
+
+void dfs(int x, int y, int cnt){
+    d[x][y] = cnt;
+    
+    for(int i=0; i<4; i++){
+        int nx = x + dx[i];
+        int ny = y + dy[i];
+        if(0<= nx && nx<n && 0<= ny && ny <n){
+            if(a[nx][ny] == 1 && d[nx][ny] == 0){
+                dfs(nx, ny, cnt);
+            }
+        }
+    }
+}
 
 int main(){
-
-    int n, k, cnt = 0;
-    cin >> n >> k;
     
+    cin >> n;
     for(int i=0; i<n; i++){
-        cin >> v[i];
-    }
-    sort(v.begin(), v.end(), greater<int>());
-    
-    for(int i=0; i<v.size(); i++){
-        while(k>=v[i]){
-            k = k-v[i];
-            cnt++;
+        for(int j=0; j<n; j++){
+            scanf("%1d", &a[i][j]);
         }
     }
     
+    int cnt = 0;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            if(a[i][j] == 1 && d[i][j] == 0){
+                dfs(i, j, ++cnt);
+            }
+        }
+    }
     cout << cnt << endl;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            ans[d[i][j]] +=1;
+        }
+    }
+    sort(ans+1, ans+1+cnt);
+    
+    for(int i=1; i<=cnt; i++){
+        cout << ans[i] << endl;
+    }
     
 }
