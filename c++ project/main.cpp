87547ad8ffaac34b97f5c4f40fb63abed03c6171
김정윤 <skyepodium@gmail.com>
2018-02-08 15:51:09 +0900

    02.08

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,28 +1,29 @@
 #include <iostream>
 #include <algorithm>
-#include <vector>
-#include <queue>
 #include <cstring>
 
 using namespace std;
 
-int a[30][30];
-int d[30][30];
-int n;
-int ans[25*25];
+//시간 복잡도: O(n^2)
+//공간 복잡도: O(n^2)
+//사용한 알고리즘: DFS
+//사용한 자료구조: 2차원 배열
 
-int dx[] = {0, 0, 1, -1};
-int dy[] = {1, -1, 0, 0};
+int w, h;
+int a[50][50];
+bool check[50][50];
+int dx[] = {1, -1, 0, 0, 1, 1, -1, -1};
+int dy[] = {0, 0, 1, -1, 1, -1, 1, -1};
 
-void dfs(int x, int y, int cnt){
-    d[x][y] = cnt;
+void dfs(int x, int y){
+    check[x][y] = true;
     
-    for(int i=0; i<4; i++){
-        int nx = x + dx[i];
-        int ny = y + dy[i];
-        if(0<= nx && nx<n && 0<= ny && ny <n){
-            if(a[nx][ny] == 1 && d[nx][ny] == 0){
-                dfs(nx, ny, cnt);
+    for(int i=0; i<8; i++){
+        int nx = x+dx[i];
+        int ny = y+dy[i];
+        if(0<=nx && nx<w && 0<=ny && ny<h){
+            if(a[nx][ny] == 1 && check[nx][ny] == false){
+                dfs(nx, ny);
             }
         }
     }
@@ -30,31 +31,29 @@ void dfs(int x, int y, int cnt){
 
 int main(){
     
-    cin >> n;
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            scanf("%1d", &a[i][j]);
+    while(cin >> w >> h){
+        if(w == 0 && h == 0){
+            break;
         }
-    }
-    
-    int cnt = 0;
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            if(a[i][j] == 1 && d[i][j] == 0){
-                dfs(i, j, ++cnt);
+        
+        for(int j=0; j<h; j++){
+            for(int i=0; i<w; i++){
+                cin >> a[i][j];
             }
         }
-    }
-    cout << cnt << endl;
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            ans[d[i][j]] +=1;
+        
+        memset(check, false, sizeof(check));
+        
+        int cnt = 0;
+        for(int j=0; j<h; j++){
+            for(int i=0; i<w; i++){
+                if(a[i][j] == 1 && check[i][j] == false){
+                    dfs(i, j);
+                    cnt++;
+                }
+            }
         }
+        
+        cout << cnt << endl;
     }
-    sort(ans+1, ans+1+cnt);
-    
-    for(int i=1; i<=cnt; i++){
-        cout << ans[i] << endl;
-    }
-    
 }
