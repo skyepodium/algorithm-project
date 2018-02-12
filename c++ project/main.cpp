d8bec0f936dfb0ad391eed64bd9d303585498a23
김정윤 <skyepodium@gmail.com>
2018-02-12 18:08:29 +0900

    02.12

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,12 +1,32 @@
 #include <iostream>
 #include <vector>
+#include <cstring>
+#include <math.h>
 
 using namespace std;
 
-vector<int> a[101];
-bool check[101];
-
 int cnt = 0;
+
+vector<pair<int, int>> v;
+vector<int> radius;
+vector<int> a[3001];
+bool check[3001];
+int n;
+
+void make_graph(){
+    
+    for(int i=0; i<n; i++){
+        for(int j=i+1; j<n; j++){
+            int dx = v[i].first - v[j].first;
+            int dy = v[i].second - v[j].second;
+
+            if(sqrt(dx*dx+dy*dy) <= radius[i] + radius[j]){
+                a[i].push_back(j);
+                a[j].push_back(i);
+            }
+        }
+    }
+}
 
 void dfs(int node){
     check[node] = true;
@@ -14,27 +34,42 @@ void dfs(int node){
     for(int i=0; i<a[node].size(); i++){
         int next = a[node][i];
         if(check[next] == false){
-            check[next] = true;
-            cnt++;
             dfs(next);
         }
     }
-    
 }
 
 int main(){
-    
-    int n, m;
-    cin >> n >> m;
-    
-    for(int i=0; i<m; i++){
-        int u, v;
-        cin >> u >> v;
-        a[u].push_back(v);
-        a[v].push_back(u);
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        v.clear();
+        radius.clear();
+        memset(check, false, sizeof(check));
+        int cnt = 0;
+        for(int i=0; i<3000; i++){
+            a[i].clear();
+        }
+        
+        
+        int x, y, r;
+        cin >> n;
+        for(int i=0; i<n; i++){
+            cin >> x >> y >> r;
+            v.push_back(make_pair(x, y));
+            radius.push_back(r);
+        }
+        make_graph();
+        
+        for(int i=0; i<n; i++){
+            if(check[i] == false){
+                dfs(i);
+                cnt++;
+            }
+        }
+        
+        cout << cnt << endl;
     }
-    
-    dfs(1);
-    
-    cout << cnt << endl;
 }
