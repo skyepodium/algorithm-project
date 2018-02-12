ad3c9a4fe453c4910e302386c93141cf009666fb
김정윤 <skyepodium@gmail.com>
2018-02-12 15:04:36 +0900

    02.12

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,14 +1,40 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
 
 using namespace std;
 
-//시간 복잡도: O(t)
-//공간 복잡도: O(t)
-//사용한 알고리즘: 정렬
-//사용한 자료구조: 1차원 벡터
+vector<int> a[101];
+bool check[101];
+
+int cnt = 0;
+
+void dfs(int node){
+    check[node] = true;
+    
+    for(int i=0; i<a[node].size(); i++){
+        int next = a[node][i];
+        if(check[next] == false){
+            check[next] = true;
+            cnt++;
+            dfs(next);
+        }
+    }
+    
+}
 
 int main(){
-
+    
+    int n, m;
+    cin >> n >> m;
+    
+    for(int i=0; i<m; i++){
+        int u, v;
+        cin >> u >> v;
+        a[u].push_back(v);
+        a[v].push_back(u);
+    }
+    
+    dfs(1);
+    
+    cout << cnt << endl;
 }
