e659a81400bd4ca9a7eb5e04f40d6acc0639ab80
김정윤 <skyepodium@gmail.com>
2018-02-14 20:35:47 +0900

    02.14

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,16 +1,11 @@
 #include <iostream>
-#include <math.h>
+#include <algorithm>
+#include <vector>
 
 using namespace std;
 
 int main(){
-    
-    int a, b, v, day = 1;
-    cin >> a >> b >> v;
-    day += (v-a)/(a-b);
-    if((v-a)%(a-b) != 0){
-        day++;
-    }
 
-    cout << day << endl;
+    
+
 }
