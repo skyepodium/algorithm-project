ed4d2b520a9e4e513b272f04e0ee2259d0ff5205
김정윤 <skyepodium@gmail.com>
2018-02-15 17:48:27 +0900

    02.15 baekjoon 9095 Top-Down

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -5,10 +5,26 @@ using namespace std;
 
 //시간 복잡도: O(n)
 //공간 복잡도: O(n)
-//사용한 알고리즘: 동적 계획법, Bottom-Up
+//사용한 알고리즘: 동적 계획법, Top-Down
 //사용한 자료구조: 1차원 배열
 
 int d[11];
+
+int go(int n){
+    
+    if(n < 4){
+        return d[n];
+    }
+    
+    if(d[n] > 0){
+        return d[n];
+    }
+    
+    d[n] = go(n-1) + go(n-2) + go(n-3);
+    
+    return d[n];
+}
+
 
 int main(){
     
@@ -24,10 +40,8 @@ int main(){
     d[1] = 1;
     d[2] = 2;
     d[3] = 4;
-    
-    for(int i=4; i<=11; i++){
-        d[i] = d[i-1] + d[i-2] + d[i-3];
-    }
+
+    go(10);
     
     for(int i=0; i<v.size(); i++){
         cout << d[v[i]] << endl;
