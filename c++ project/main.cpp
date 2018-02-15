ab738b00e2133dee1b83307463be82fd92c9058c
김정윤 <skyepodium@gmail.com>
2018-02-15 21:15:24 +0900

    02.15 baekjoon 11057 Bottom-Up

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,50 +1,32 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
-//시간 복잡도: O(n)
-//공간 복잡도: O(n)
-//사용한 알고리즘: 동적 계획법, Top-Down
-//사용한 자료구조: 1차원 배열
-
-int d[11];
-
-int go(int n){
-    
-    if(n < 4){
-        return d[n];
-    }
-    
-    if(d[n] > 0){
-        return d[n];
-    }
-    
-    d[n] = go(n-1) + go(n-2) + go(n-3);
-    
-    return d[n];
-}
-
+int d[1000][10];
+int mod = 10007;
 
 int main(){
     
-    int t;
-    cin >> t;
+    int n;
+    cin >> n;
     
-    vector<int> v(t);
-    
-    for(int i=0; i<t; i++){
-        cin >> v[i];
+    for(int i=0; i<10; i++){
+        d[1][i] = 1;
     }
     
-    d[1] = 1;
-    d[2] = 2;
-    d[3] = 4;
-
-    go(10);
-    
-    for(int i=0; i<v.size(); i++){
-        cout << d[v[i]] << endl;
+    for(int i=2; i<=n; i++){
+        for(int j=0; j<10; j++){
+            for(int k=0; k<=j; k++){
+                d[i][j] += d[i-1][k]%mod;
+                d[i][j] %= mod;
+            }
+        }
     }
     
+    int result = 0;
+    for(int i=0; i<10; i++){
+        result += d[n][i]%mod;
+        result %= mod;
+    }
+    cout << result%mod << endl;
 }
