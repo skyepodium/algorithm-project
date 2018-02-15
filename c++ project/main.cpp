e47c2ec972bce9abd37a1ea9d9dfc9980e82d591
김정윤 <skyepodium@gmail.com>
2018-02-15 16:35:12 +0900

    02.15 baekjoon 11726 re-solve

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,11 +1,35 @@
 #include <iostream>
-#include <algorithm>
-#include <vector>
 
 using namespace std;
 
+//시간 복잡도: O(n)
+//공간 복잡도: O(n)
+//사용한 알고리즘: 동적 계획법, Top-Down
+//사용한 자료구조: 1차원 배열
+
+int d[1001];
+int mod = 10007;
+
+int go(int n){
+    
+    if(n < 2){
+        return 1;
+    }
+    
+    if(d[n] > 0){
+        return d[n]%mod;
+    }
+    
+    d[n] = go(n-1)%mod + go(n-2)%mod;
+    return d[n]%mod;
+}
+
 int main(){
 
+    int n;
+    cin >> n;
     
+    cout << go(n)%mod << endl;
 
+    
 }
