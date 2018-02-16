854e33ee015259a113a44b0f97f44c839be6ca53
김정윤 <skyepodium@gmail.com>
2018-02-16 16:29:46 +0900

    02.16 baekjoon 2156 bottom-up

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -8,55 +8,28 @@ using namespace std;
 //사용한 알고리즘: 동적 계획법 Bottom-Up
 //사용한 자료구조: 2차원 배열
 
-int d[100001][3];
-int a[100001][3];
-
-int go(int n, int i){
-    
-    if(n<2){
-        return a[n][i];
-    }
-    
-    if(d[n][i] > 0){
-        return d[n][i];
-    }
-    
-    if(i==0) d[n][i] = max(go(n-1, 0), max(go(n-1, 1), go(n-1, 2)));
-    else if(i==1) d[n][i] = max(go(n-1, 0), go(n-1, 2)) + a[n][1];
-    else d[n][i] = max(go(n-1, 0), go(n-1, 1)) + a[n][2];
-    
-    return d[n][i];
-}
-
+int d[10001][3];
+int a[10001];
 
 int main(){
     
-    int t;
-    cin >> t;
+    int n;
+    cin >> n;
     
-    while(t--){
-        
-        int n;
-        cin >> n;
-        
-        for(int i=1; i<=2; i++){
-            for(int j=1; j<=n; j++){
-                cin >> a[j][i];
-            }
-        }
-        
+    for(int i=1; i<=n; i++){
+        cin >> a[i];
+    }
+    
+    d[1][0] = 0;
+    d[1][1] = a[1];
+    
+    for(int i=2; i<=n; i++){
 
-        d[1][0] = 0;
-        d[1][1] = a[1][1];
-        d[1][2] = a[1][2];
-        cout << max(go(n, 0), max(go(n, 1), go(n,2))) << endl;
-        
-        for(int i=0; i<=2; i++){
-            for(int j=1; j<=n; j++){
-                d[j][i] = 0;
-            }
-        }
+        d[i][0] = max(d[i-1][0], max(d[i-1][1], d[i-1][2]));
+        d[i][1] = d[i-1][0] + a[i];
+        d[i][2] = d[i-1][1] + a[i];
         
     }
-
+    
+    cout << max(d[n][0], max(d[n][1], d[n][2])) << endl;
 }
