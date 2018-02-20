7ecddc2778f0cb5575c665e3f00f55b989ff1670
김정윤 <skyepodium@gmail.com>
2018-02-20 12:24:43 +0900

    02.20 counting num of nine

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,43 +1,35 @@
 #include <iostream>
-#include <algorithm>
 
 using namespace std;
 
 //시간 복잡도: O(n)
 //공간 복잡도: O(n)
-//사용한 알고리즘: 동적 계획법 Top-Down
+//사용한 알고리즘: 동적 계획법 Bottom-Up
 //사용한 자료구조: 1차원 배열
 
-int a[100001];
-int d[100001];
+int cnt = 0;
 
-int go(int n){
-    
-    if(n<2){
-        d[1] = a[1];
-        return d[1];
+void count_nine(int a){
+
+    while(a>0){
+        
+        if(a%10 == 9) cnt++;
+        a /= 10;
     }
     
-    if(d[n] > 0){
-        return d[n];
-    }
-    
-    d[n] = max(go(n-1)+a[n], a[n]);
-    
-    return d[n];
 }
 
 
 int main(){
     
-    int n;
-    cin >> n;
-    
-    for(int i=1; i<=n; i++){
-        cin >> a[i];
+    for(int i=1; i<=500; i++){
+        
+        count_nine(i);
+        
     }
-
-    go(n);
     
-    cout << *max_element(d+1, d+1+n) << endl;
+    cout << cnt << endl;
+    
+    
 }
+
