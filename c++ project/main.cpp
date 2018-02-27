45a2da61a6d257d18acea9468b4aafc10e60894e
김정윤 <skyepodium@gmail.com>
2018-02-27 22:32:24 +0900

    02.27

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,40 +1,57 @@
 #include <iostream>
-#include <algorithm>
-#include <vector>
 #include <queue>
-#include <cstring>
-#include <math.h>
 
 using namespace std;
 
-//시간 복잡도: O(n)
-//공간 복잡도: O(n)
-//사용한 알고리즘: 동적 계획법 Bottom-Up
-//사용한 자료구조: 1차원 배열
+//시간 복잡도: O(n^2)
+//공간 복잡도: O(n^2)
+//사용한 알고리즘: BFS
+//사용한 자료구조: 2차원 배열, queue
 
-int cnt = 0;
+int n, m;
+int a[100][100];
+bool check[100][100];
+int dx[] = {0, 0, 1, -1};
+int dy[] = {1, -1, 0, 0};
 
-void count_nine(int a){
-
-    while(a>0){
-        
-        if(a%10 == 9) cnt++;
-        a /= 10;
-    }
+void bfs(int x, int y){
+    check[x][y] = true;
     
+    queue<pair<int, int>> q;
+    q.push(make_pair(x, y));
+    
+    while(!q.empty()){
+        x = q.front().first;
+        y = q.front().second;
+        q.pop();
+        
+        for(int i=0; i<4; i++){
+            int nx = x+dx[i];
+            int ny = y+dy[i];
+            if(0<=nx && nx<m && 0<=ny && ny<n){
+                if(a[nx][ny] == 1 && check[nx][ny] == false){
+                    check[nx][ny] = true;
+                    q.push(make_pair(nx, ny));
+                    a[nx][ny] = a[x][y]+1;
+                    
+                }
+            }
+        }
+        
+    }
 }
-
 
 int main(){
     
-    for(int i=1; i<=500; i++){
-        
-        count_nine(i);
-        
+    cin >> n >> m;
+    
+    for(int j=0; j<n; j++){
+        for(int i=0; i<m; i++){
+            scanf("%1d", &a[i][j]);
+        }
     }
     
-    cout << cnt << endl;
+    bfs(0, 0);
     
-    
+    cout << a[m-1][n-1] << endl;
 }
-
