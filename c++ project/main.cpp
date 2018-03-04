ed76f723082655527aef0e725acb330765ad9360
김정윤 <skyepodium@gmail.com>
2018-03-04 23:25:06 +0900

    03.04

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,60 +1,40 @@
 #include <iostream>
 #include <queue>
-#include <vector>
-#include <cstring>
-#include <cmath>
 
 using namespace std;
 
-//시간 복잡도: O(n^2)
-//공간 복잡도: O(n^2)
-//사용한 알고리즘: BFS
-//사용한 자료구조: 2차원 배열, queue
-
-int n, m;
-int a[100][100];
-bool check[100][100];
-int dx[] = {0, 0, 1, -1};
-int dy[] = {1, -1, 0, 0};
-
-void bfs(int x, int y){
-    check[x][y] = true;
-    
-    queue<pair<int, int>> q;
-    q.push(make_pair(x, y));
-    
-    while(!q.empty()){
-        x = q.front().first;
-        y = q.front().second;
-        q.pop();
-        
-        for(int i=0; i<4; i++){
-            int nx = x+dx[i];
-            int ny = y+dy[i];
-            if(0<=nx && nx<m && 0<=ny && ny<n){
-                if(a[nx][ny] == 1 && check[nx][ny] == false){
-                    check[nx][ny] = true;
-                    q.push(make_pair(nx, ny));
-                    a[nx][ny] = a[x][y]+1;
-                    
-                }
-            }
-        }
-        
-    }
-}
+//시간 복잡도: O(nm)
+//공간 복잡도: O(n)
+//사용한 알고리즘: 없음
+//사용한 자료구조: STL 큐
 
 int main(){
     
+    int n, m;
     cin >> n >> m;
     
-    for(int j=0; j<n; j++){
-        for(int i=0; i<m; i++){
-            scanf("%1d", &a[i][j]);
-        }
+    queue<int> q;
+    
+    for(int i=1; i<=n; i++){
+        q.push(i);
     }
     
-    bfs(0, 0);
+    cout <<'<';
     
-    cout << a[m-1][n-1] << endl;
+    while(!q.empty()){
+        
+        for(int i=0; i<m-1; i++){
+            q.push(q.front());
+            q.pop();
+        }
+        
+        cout << q.front();
+        if(q.size() != 1){
+            cout << ", ";
+        }
+        q.pop();
+    }
+    
+    cout <<'>'<<endl;
+    
 }
