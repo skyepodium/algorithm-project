773efa6e4099154d1fd50d9934491977cdb1407d
김정윤 <skyepodium@gmail.com>
2018-03-07 20:06:25 +0900

    03.07

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,69 +1,70 @@
 #include <iostream>
-#include <string>
-#include <stack>
+#include <vector>
 #include <queue>
+#include <cstring>
+#include <algorithm>
 
 using namespace std;
 
-//시간 복잡도: O(n)
-//공간 복잡도: O(n)
-//사용한 알고리즘: 없음
-//사용한 자료구조: string class, STL 스택
+vector<int> a[1001];
+bool check[1001];
 
-string check(string word){
+void dfs(int node){
+
+    check[node] = true;
+    cout << node <<" ";
     
-    stack<int> s;
-    for(int i=0; i<word.size(); i++){
+    for(int i=0; i<a[node].size(); i++){
+        int next = a[node][i];
         
-        if(word[i] == 40 || word[i] == 91 || word[i] == 123){
-            
-            s.push(word[i]);
-            
-        }else{
-            
-            if(s.size()!=0){//닫는 괄호는 스택의 크기가 1이상일때 들어와야한다. 열고 닫아야 하니까
-                if(word[i] > 41){
-                    if(word[i] != s.top()+2){//스택의 top과 짝이 맞지 않으면 NO 리턴, 맞으면 POP
-                        return "NO";
-                    }else{
-                        s.pop();
-                    }
-                }else{
-                    if(word[i] != s.top()+1){//스택의 top과 짝이 맞지 않으면 NO 리턴, 맞으면 POP
-                        return "NO";
-                    }else{
-                        s.pop();
-                    }
-                }
-            }else{//스택이 비어있는데 닫는 괄호가 들어왔다, 틀린 괄호 문자열이다.
-                return "NO";
-            }
-            
+        if(check[next] == false){
+            check[next] = true;
+            dfs(next);
         }
-        
     }
+}
+
+void bfs(int start){
+    memset(check, false, sizeof(check));
+    check[start] = true;
+    queue<int> q;
+    q.push(start);
     
-    
-    if(s.size() == 0){//문자열을 다 검사했는데, 스택의 크기가 0이면 옳은 문자열이다.
-        return "YES";
-    }else{
-        return "NO";
+    while(!q.empty()){
+        int node = q.front();
+        cout << node <<" ";
+        q.pop();
+        
+        for(int i=0; i<a[node].size(); i++){
+            int next = a[node][i];
+            
+            if(check[next] == false){
+                check[next] = true;
+                q.push(next);
+            }
+        }
     }
 }
 
 
 int main(){
     
-    int t;
-    cin >> t;
+    int n, m, start;
+    cin >> n >> m >> start;
     
-    while(t--){
-        
-        string word;
-        cin >> word;
-        
-        cout << check(word) << endl;
-        
+    int u, v;
+    for(int i=0; i<m; i++){
+        cin >> u >> v;
+        a[u].push_back(v);
+        a[v].push_back(u);
     }
     
+    for(int i=0; i<n; i++){
+        sort(a[i].begin(), a[i].end());
+    }
+    
+    dfs(start);
+    cout << endl;
+    bfs(start);
+    cout << endl;
 }
