00b461eb923866dc4f772e81c4e2e45059f52660
김정윤 <skyepodium@gmail.com>
2018-03-09 18:07:03 +0900

    baekjoon 1697

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -8,9 +8,11 @@ using namespace std;
 //사용한 알고리즘: BFS
 //사용한 자료구조: 2차원 벡터, 1차원 배열
 
-int check[100001];
+bool check[100001];
+int dist[100001];
 
 void bfs(int start){
+    check[start] = true;
     queue<int> q;
     q.push(start);
     
@@ -19,17 +21,20 @@ void bfs(int start){
         q.pop();
         
         if(node-1 >= 0 && check[node-1] == 0){
-            check[node-1] = check[node] + 1;
+            check[node-1] = true;
+            dist[node-1] = dist[node] + 1;
             q.push(node-1);
         }
         
         if(node+1 <= 100000 && check[node+1] == 0){
-            check[node+1] = check[node] + 1;
+            check[node+1] = true;
+            dist[node+1] = dist[node] + 1;
             q.push(node+1);
         }
         
         if(node*2 <= 100000 && check[node*2] == 0){
-            check[node*2] = check[node] + 1;
+            check[node*2] = true;
+            dist[node*2] = dist[node] + 1;
             q.push(node*2);
         }
         
@@ -42,9 +47,7 @@ int main(){
     cin >> start >> end;
     
     bfs(start);
-    if(start != end){
-        cout << check[end] << endl;
-    }else{
-        cout << 0 << endl;
-    }
+    cout << dist[end] << endl;
+    
 }
+
