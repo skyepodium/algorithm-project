be4d87dbcb9fb90112d95c6e39f2dd7c91670638
김정윤 <skyepodium@gmail.com>
2018-03-11 21:54:46 +0900

    03.11

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,65 +1,50 @@
 #include <iostream>
-#include <queue>
-#include <algorithm>
+#include <cstring>
 
 using namespace std;
 
-//시간 복잡도: O(n^2)
-//공간 복잡도: O(n^2)
-//사용한 알고리즘: BFS
-//사용한 자료구조: 2차원배열, queue
+//시간 복잡도: O(n^3)
+//공간 복잡도: O(n^3)
+//사용한 알고리즘: DFS
+//사용한 자료구조: 2차원 배열, 인접행렬
 
-int dist[1001][1001];
-bool check[1001][1001];
-int s;
+int d[101][101];
+bool check[101];
+int result[101][101];
+int n;
 
-void bfs(int s, int c){
-    check[s][c] = true;
-    queue<pair<int, int>> q;
-    q.push(make_pair(s, c));
+void dfs(int node){
     
-    while(!q.empty()){
-        s = q.front().first;
-        c = q.front().second;
-        q.pop();
-        
-        if(check[s][s] == false && s>0){
-            check[s][s] = true;
-            dist[s][s] = dist[s][c] + 1;
-            q.push(make_pair(s, s));
+    for(int i=0; i<n; i++){
+        int next = i;
+        if(d[node][next] == 1 && check[next] == false){
+            check[next] = true;
+            dfs(next);
         }
-        
-        if(s+c<=1000 && check[s+c][c] == false && c>0){
-            check[s+c][c] = true;
-            dist[s+c][c] = dist[s][c] + 1;
-            q.push(make_pair(s+c, c));
-        }
-
-        if(s-1 >= 0 && check[s-1][c] == false){
-            check[s-1][c] = true;
-            dist[s-1][c] = dist[s][c] + 1;
-            q.push(make_pair(s-1, c));
-        }
-
-
     }
-    
-    
 }
-
 
 int main(){
     
-    cin >> s;
-    bfs(1, 0);
-
-    int min_day = 1000;
+    cin >> n;
     
-    for(int i=0; i<1000; i++){
-        if(dist[s][i] > 0){
-            min_day = min(min_day, dist[s][i]);
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cin >> d[i][j];
         }
     }
     
-    cout << min_day << endl;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            dfs(i);
+            if(check[j] == true){
+                result[i][j] = 1;
+            }
+            memset(check, false, sizeof(check));
+            cout << result[i][j] <<" ";
+        }
+        cout << endl;
+    }
+    
 }
+
