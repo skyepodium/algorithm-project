7765b7332f964d4f6df9b876f5cbb1fe94e93abf
김정윤 <skyepodium@gmail.com>
2018-03-11 17:18:05 +0900

    baekjoon 7569

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -7,35 +7,44 @@ using namespace std;
 //시간 복잡도: O(n^2)
 //공간 복잡도: O(n^2)
 //사용한 알고리즘: BFS
-//사용한 자료구조: 배열, queue
+//사용한 자료구조: 3차원배열, queue, 구조체
 
-int d[1001][1001];
-bool check[1001][1001];
+int d[101][101][101];
+bool check[101][101][101];
 
-int dx[] = {0, 0, 1, -1};
-int dy[] = {-1, 1, 0, 0};
+int dir[6][3] = { {1, 0, 0}, {0, 1, 0}, {0, 0, 1}, {-1, 0, 0}, {0, -1, 0}, {0, 0, -1} };
 
 int tomato_cnt = 0;
-int n, m;
+int n, m, h;
 
-queue<pair<int, int>> q;
+struct s{
+    int height, length, width;
+};
+
+queue<s> q;
 
 void bfs(){
     
     while(!q.empty()){
-        int x = q.front().first;
-        int y = q.front().second;
-        tomato_cnt--;
+        int height = q.front().height;
+        int length = q.front().length;
+        int width = q.front().width;
         q.pop();
         
-        for(int i=0; i<4; i++){
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            if(nx>=0 && nx<n && ny >= 0 && ny < m){
-                if(check[nx][ny] == false && d[nx][ny] == 0){
-                    check[nx][ny] = true;
-                    d[nx][ny] = d[x][y] + 1;
-                    q.push(make_pair(nx, ny));
+        for(int i=0; i<6; i++){
+            
+            int n_height = height+dir[i][0];
+            int n_length = length+dir[i][1];
+            int n_width = width+dir[i][2];
+            
+            if(n_height>=0 && n_height<h && n_width >= 0 && n_width < m && n_length>=0 && n_length<n){
+                if(check[n_height][n_length][n_width] == false && d[n_height][n_length][n_width] == 0){
+
+                    check[n_height][n_length][n_width] = true;
+                    tomato_cnt--;
+                    d[n_height][n_length][n_width] = d[height][length][width] + 1;
+                    q.push({n_height, n_length, n_width});
+
                 }
             }
         }
@@ -46,29 +55,34 @@ void bfs(){
 
 int main(){
 
-    cin >> m >> n;
+    cin >> m >> n >> h;
     
-    for(int i=0; i<n; i++){
-        for(int j=0; j<m; j++){
-            cin >> d[i][j];
-            if(d[i][j] == 1 || d[i][j] == 0){
-                tomato_cnt++;
+    for(int i=0; i<h; i++){
+        for(int j=0; j<n; j++){
+            for(int k=0; k<m; k++){
+                cin >> d[i][j][k];
+                
+                if(d[i][j][k] == 0){
+                    tomato_cnt++;
+                }
+                
+                if(d[i][j][k] == 1){
+                    q.push({i, j, k});
+                    check[i][j][k] = true;
+                }
+                
             }
-            
-            if(d[i][j] == 1){
-                q.push(make_pair(i, j));
-                check[i][j] = true;
-            }
-            
         }
     }
-
+    
     bfs();
 
     int day = 0;
-    for(int i=0; i<n; i++){
-        for(int j=0; j<m; j++){
-            day = max(day, d[i][j]);
+    for(int i=0; i<h; i++){
+        for(int j=0; j<n; j++){
+            for(int k=0; k<m; k++){
+                day = max(d[i][j][k], day);
+            }
         }
     }
     
@@ -77,6 +91,5 @@ int main(){
     }else{
         cout << -1 << endl;
     }
-    
     
 }
