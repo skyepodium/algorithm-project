50a824fe6354fef2337596393d1ede93c057d310
김정윤 <skyepodium@gmail.com>
2018-03-11 18:21:20 +0900

    baekjoon 14226

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -7,89 +7,59 @@ using namespace std;
 //시간 복잡도: O(n^2)
 //공간 복잡도: O(n^2)
 //사용한 알고리즘: BFS
-//사용한 자료구조: 3차원배열, queue, 구조체
+//사용한 자료구조: 2차원배열, queue
 
-int d[101][101][101];
-bool check[101][101][101];
+int dist[1001][1001];
+bool check[1001][1001];
+int s;
 
-int dir[6][3] = { {1, 0, 0}, {0, 1, 0}, {0, 0, 1}, {-1, 0, 0}, {0, -1, 0}, {0, 0, -1} };
-
-int tomato_cnt = 0;
-int n, m, h;
-
-struct s{
-    int height, length, width;
-};
-
-queue<s> q;
-
-void bfs(){
+void bfs(int s, int c){
+    check[s][c] = true;
+    queue<pair<int, int>> q;
+    q.push(make_pair(s, c));
     
     while(!q.empty()){
-        int height = q.front().height;
-        int length = q.front().length;
-        int width = q.front().width;
+        s = q.front().first;
+        c = q.front().second;
         q.pop();
         
-        for(int i=0; i<6; i++){
-            
-            int n_height = height+dir[i][0];
-            int n_length = length+dir[i][1];
-            int n_width = width+dir[i][2];
-            
-            if(n_height>=0 && n_height<h && n_width >= 0 && n_width < m && n_length>=0 && n_length<n){
-                if(check[n_height][n_length][n_width] == false && d[n_height][n_length][n_width] == 0){
-
-                    check[n_height][n_length][n_width] = true;
-                    tomato_cnt--;
-                    d[n_height][n_length][n_width] = d[height][length][width] + 1;
-                    q.push({n_height, n_length, n_width});
-
-                }
-            }
+        if(check[s][s] == false && s>0){
+            check[s][s] = true;
+            dist[s][s] = dist[s][c] + 1;
+            q.push(make_pair(s, s));
         }
+        
+        if(s+c<=1000 && check[s+c][c] == false && c>0){
+            check[s+c][c] = true;
+            dist[s+c][c] = dist[s][c] + 1;
+            q.push(make_pair(s+c, c));
+        }
+
+        if(s-1 >= 0 && check[s-1][c] == false){
+            check[s-1][c] = true;
+            dist[s-1][c] = dist[s][c] + 1;
+            q.push(make_pair(s-1, c));
+        }
+
+
     }
+    
+    
 }
 
 
-
 int main(){
-
-    cin >> m >> n >> h;
     
-    for(int i=0; i<h; i++){
-        for(int j=0; j<n; j++){
-            for(int k=0; k<m; k++){
-                cin >> d[i][j][k];
-                
-                if(d[i][j][k] == 0){
-                    tomato_cnt++;
-                }
-                
-                if(d[i][j][k] == 1){
-                    q.push({i, j, k});
-                    check[i][j][k] = true;
-                }
-                
-            }
+    cin >> s;
+    bfs(1, 0);
+
+    int min_day = 1000;
+    
+    for(int i=0; i<1000; i++){
+        if(dist[s][i] > 0){
+            min_day = min(min_day, dist[s][i]);
         }
     }
     
-    bfs();
-
-    int day = 0;
-    for(int i=0; i<h; i++){
-        for(int j=0; j<n; j++){
-            for(int k=0; k<m; k++){
-                day = max(d[i][j][k], day);
-            }
-        }
-    }
-    
-    if(tomato_cnt == 0){
-        cout << day-1 << endl;
-    }else{
-        cout << -1 << endl;
-    }
-    
+    cout << min_day << endl;
 }
