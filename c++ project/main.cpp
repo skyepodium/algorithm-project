51dc9d9b214ec2f3314a6ed169fed37e323834bd
김정윤 <skyepodium@gmail.com>
2018-03-12 17:37:34 +0900

    03.12 baekjoon 2331 DFS

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <queue>
 #include <cstring>
+#include <math.h>
 
 using namespace std;
 
@@ -10,59 +11,58 @@ using namespace std;
 //사용한 알고리즘: BFS
 //사용한 자료구조: 1차원 배열, 2차원 벡터, 큐
 
-vector<int> a[1001];
-bool check[1001];
+vector<int> a[1000001];
+bool exist[1000001];
+int check[1000001];
+int n, p;
 
-void bfs(int start){
-    check[start] = true;
-    queue<int> q;
-    q.push(start);
+int cal_next(int num){
     
-    while(!q.empty()){
-        int node = q.front();
-        q.pop();
-        
-        for(int i=0; i<a[node].size(); i++){
-            int next = a[node][i];
-            if(check[next] == false){
-                check[next] = true;
-                q.push(next);
-            }
+    int result = 0;
+    while(num>0){
+        result = result + pow(num%10, p);
+        num = num/10;
+    }
+    return result;
+}
+
+void dfs(int node, int cnt){
+    check[node] = cnt+1;
+    
+    for(int i=0; i<a[node].size(); i++){
+        int next = a[node][i];
+        if(check[next] == 0){
+            dfs(next, cnt+1);
+        }else{
+            cout << check[next] - 1 << endl;
+            break;
         }
     }
+    
 }
 
 
 int main(){
-    
-    int t;
-    cin >> t;
-    
-    while(t--){
-        int n;
-        cin >> n;
-        
-        for(int i=1; i<=n; i++){
-            int v;
-            cin >> v;
-            a[i].push_back(v);
-        }
-        
-        int cnt = 0;
-        
-        for(int i=1; i<=n; i++){
-            if(check[i] == false){
-                bfs(i);
-                cnt++;
-            }
-        }
-        cout << cnt << endl;
-        memset(check, false, sizeof(check));
-        for(int i=0; i<1001; i++){
-            a[i].clear();
-        }
 
+    cin >> n >> p;
+    
+    exist[n] = true;
+    int start = n;
+    
+    while(true){
+        
+        int next = cal_next(n);
+        if(exist[next] == false){
+            exist[next] = true;
+            a[n].push_back(next);
+        }else{
+            a[n].push_back(next);
+            break;
+        }
+        
+        n = next;
     }
     
+    dfs(start, 0);
+    
 }
-
