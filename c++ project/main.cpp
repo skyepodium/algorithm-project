e059a81806d67f87a17937bfcd15021641e2e4ec
김정윤 <skyepodium@gmail.com>
2018-03-13 17:22:16 +0900

    baekjoon 2468 DFS

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,68 +1,72 @@
 #include <iostream>
-#include <vector>
-#include <queue>
-#include <cstring>
-#include <math.h>
+#include <algorithm>
 
 using namespace std;
 
-//시간 복잡도: O(n)
-//공간 복잡도: O(n)
-//사용한 알고리즘: BFS
-//사용한 자료구조: 1차원 배열, 2차원 벡터, 큐
+//시간 복잡도: O(n^2)
+//공간 복잡도: O(n^2)
+//사용한 알고리즘: DFS, 플러드 필
+//사용한 자료구조: 2차원 배열
 
-vector<int> a[1000001];
-bool exist[1000001];
-int check[1000001];
-int n, p;
+int d[101][101];
+bool check[101][101];
+int dx[] = {0, 0, 1, -1};
+int dy[] = {-1, 1, 0, 0};
+int n;
+int rain=1;
 
-int cal_next(int num){
+void dfs(int x, int y){
+    check[x][y] = true;
     
-    int result = 0;
-    while(num>0){
-        result = result + pow(num%10, p);
-        num = num/10;
-    }
-    return result;
-}
-
-void dfs(int node, int cnt){
-    check[node] = cnt+1;
-    
-    for(int i=0; i<a[node].size(); i++){
-        int next = a[node][i];
-        if(check[next] == 0){
-            dfs(next, cnt+1);
-        }else{
-            cout << check[next] - 1 << endl;
-            break;
+    for(int i=0; i<4; i++){
+        int nx = x + dx[i];
+        int ny = y + dy[i];
+        
+        if(nx>=0 && nx<n && ny>=0 && ny<n){
+            if(check[nx][ny] == false && d[nx][ny] > rain){
+                dfs(nx, ny);
+            }
         }
     }
     
 }
-
 
 int main(){
-
-    cin >> n >> p;
     
-    exist[n] = true;
-    int start = n;
-    
-    while(true){
-        
-        int next = cal_next(n);
-        if(exist[next] == false){
-            exist[next] = true;
-            a[n].push_back(next);
-        }else{
-            a[n].push_back(next);
-            break;
+    cin >> n;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cin >> d[i][j];
         }
-        
-        n = next;
     }
     
-    dfs(start, 0);
+    
+    int result = 1;
+
+    while(rain<101){
+
+        int cnt = 0;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(check[i][j] == false && d[i][j] > rain){
+                    dfs(i, j);
+                    cnt++;
+                }
+            }
+        }
+        
+        result = max(result, cnt);
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                check[i][j] = false;
+            }
+        }
+        rain++;
+
+    }
+    
+    cout << result << endl;
+
     
 }
+
