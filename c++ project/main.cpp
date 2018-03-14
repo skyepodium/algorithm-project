689fab62ef005cb16d2078948081452d12489cbe
김정윤 <skyepodium@gmail.com>
2018-03-14 18:57:28 +0900

    baekjoon 2589

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,62 +1,98 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-#include <algorithm>
+#include <string>
 
 using namespace std;
 
-//시간 복잡도: O(n^4)
-//공간 복잡도: O(n^2)
+//시간 복잡도: O(E)
+//공간 복잡도: O(E)
 //사용한 알고리즘: BFS
-//사용한 자료구조: 2차원 배열
+//사용한 자료구조: 2차원 벡터, 1차원 배열
 
-vector<int> a[101];
-int check[101];
-int n;
-int start_node, end_node;
+int d[51][51];
+int check[51][51];
+int dx[] = {0, 0, 1, -1};
+int dy[] = {-1, 1, 0, 0};
+int n, m;
 
-int bfs(int start){
-    check[start] = 0;
-    queue<int> q;
-    q.push(start);
-    int result = -1;
+void bfs(int x, int y){
+ 
+    check[x][y] = true;
+    queue<pair<int, int>> q;
+    q.push(make_pair(x, y));
     
     while(!q.empty()){
-        int node = q.front();
+        x = q.front().first;
+        y = q.front().second;
         q.pop();
         
-        for(int i=0; i<a[node].size(); i++){
-            int next = a[node][i];
-            if(check[next] == 0){
-                check[next] = check[node] + 1;
-                q.push(next);
-            }
+        for(int i=0; i<4; i++){
+            int nx = x + dx[i];
+            int ny = y + dy[i];
             
-            if(next == end_node){
-                result = check[next];
+            if(nx>=0 & nx<n && ny>=0 && ny<m){
+                if(check[nx][ny] == false && d[nx][ny] == 1){
+                    check[nx][ny] = true;
+                    d[nx][ny] = d[x][y] + 1;
+                    q.push(make_pair(nx, ny));
+                }
             }
-            
         }
     }
     
-    return result;
+    
+}
+
+void clear_check(){
+    
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            check[i][j] = false;
+            if(d[i][j] > 0){
+                d[i][j] = 1;
+            }
+        }
+    }
+    
+}
+
+int check_max(){
+    int max_cnt = 0;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            max_cnt = max(max_cnt, d[i][j]);
+        }
+    }
+    return max_cnt - 1;
 }
 
 int main(){
+    cin >> n >> m;
     
-    cin >> n;
-    cin >> start_node >> end_node;
-    
-    int m;
-    cin >> m;
-    
-    for(int i=0; i<m; i++){
-        int u, v;
-        cin >> u >> v;
-        a[u].push_back(v);
-        a[v].push_back(u);
+    for(int i=0; i<n; i++){
+        string word;
+        cin >> word;
+        for(int j=0; j<m; j++){
+            if(word[j] == 'W'){
+                d[i][j] = 0;
+            }else{
+                d[i][j] = 1;
+            }
+        }
     }
     
-    cout << bfs(start_node) << endl;
+    int result = 0;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            if(d[i][j] == 1){
+                bfs(i, j);
+                result = max(check_max(), result);
+                clear_check();
+            }
+        }
+    }
+    
+    cout << result << endl;
     
 }
