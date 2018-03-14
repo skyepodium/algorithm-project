c6e50ef69568cdc702c1f25d424294ff799c6c97
김정윤 <skyepodium@gmail.com>
2018-03-14 16:27:36 +0900

    baekjoon 2146

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -5,18 +5,23 @@
 
 using namespace std;
 
-//시간 복잡도: O(n^2)
+//시간 복잡도: O(n^4)
 //공간 복잡도: O(n^2)
 //사용한 알고리즘: BFS
 //사용한 자료구조: 2차원 배열
 
+int d[101][101];
 bool check[101][101];
+int color[101][101];
 int dx[] = {0, 0, 1, -1};
 int dy[] = {-1, 1, 0, 0};
-int m, n, k;
-int cnt = 0;
+int n = 0;
+int number = 1;
+vector<pair<int, int>> list;
 
-void bfs(int x, int y){
+
+//플러드 필로 섬 넘버링
+void flood_fill(int x, int y){
     check[x][y] = true;
     queue<pair<int, int>> q;
     q.push(make_pair(x, y));
@@ -24,15 +29,16 @@ void bfs(int x, int y){
     while(!q.empty()){
         x = q.front().first;
         y = q.front().second;
+        d[x][y] = number;
+        color[x][y] = number;
         q.pop();
-        cnt++;
         
         for(int i=0; i<4; i++){
             int nx = x + dx[i];
             int ny = y + dy[i];
             
-            if(nx>=0 && nx<m && ny>=0 && ny<n){
-                if(check[nx][ny] == false){
+            if(nx>=0 && nx<n && ny>=0 && ny<n){
+                if(check[nx][ny] == false && d[nx][ny] == 1){
                     check[nx][ny] = true;
                     q.push(make_pair(nx, ny));
                 }
@@ -42,39 +48,89 @@ void bfs(int x, int y){
 }
 
 
-int main(){
-    cin >> m >> n >> k;
+int bfs(int x, int y){
     
-    for(int i=0; i<k; i++){
-        int y1, x1, y2, x2;
-        cin >> y1 >> x1 >> y2 >> x2;
+    int min_cnt = 10000;
+    int origin_color = color[x][y];
+    check[x][y] = true;
+    queue<pair<int, int>> q;
+    q.push(make_pair(x, y));
+    
+    while(!q.empty()){
+        x = q.front().first;
+        y = q.front().second;
+        q.pop();
         
-        y2 = y2 - 1;
-        x2 = x2 - 1;
-        for(int i=x1; i<=x2; i++){
-            for(int j=y1; j<=y2; j++){
-                check[i][j] = true;
+        for(int i=0; i<4; i++){
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            
+            if(nx>=0 && nx<n && ny>=0 && ny<n){
+                if(check[nx][ny] == false && d[nx][ny] == 0){
+                    d[nx][ny] = d[x][y] + 1;
+                    
+                    check[nx][ny] = true;
+                    q.push(make_pair(nx, ny));
+                }
+                
+                //다리가 만들어진것
+                if(color[nx][ny] > 0 && color[nx][ny] != origin_color){
+                    min_cnt = min(min_cnt, d[x][y] - 1);
+                }
             }
         }
-        
     }
+    return min_cnt;
+}
+
+void clear_check(){
     
-    vector<int> cnt_array;
-    for(int i=0; i<m; i++){
+    for(int i=0; i<n; i++){
         for(int j=0; j<n; j++){
-            if(check[i][j] == false){
-                bfs(i, j);
-                cnt_array.push_back(cnt);
-                cnt = 0;
+            if(color[i][j] < 1){
+                check[i][j] = false;
+                d[i][j] = 0;
+            }else{
+                d[i][j] = 1;
             }
         }
     }
     
-    sort(cnt_array.begin(), cnt_array.end());
+}
+
+
+int main(){
+
+    cin >> n;
     
-    cout << cnt_array.size() << endl;
-    for(int i=0; i<cnt_array.size(); i++){
-        cout << cnt_array[i] <<" ";
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cin >> d[i][j];
+            if(d[i][j] == 1){
+                list.push_back(make_pair(i, j));
+            }
+        }
     }
-    cout << endl;
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            if(check[i][j] == false && d[i][j] == 1){
+                flood_fill(i, j);
+                number++;
+            }
+        }
+    }
+    
+    clear_check();
+
+    int result = 10000;
+    for(int i=0; i<list.size(); i++){
+
+        result = min(result, bfs(list[i].first, list[i].second));
+  
+        clear_check();
+    }
+
+    cout << result << endl;
+    
 }
