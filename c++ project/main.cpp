04759cab5b02792c65ee9e7070c3c005f807851a
김정윤 <skyepodium@gmail.com>
2018-03-14 14:39:43 +0900

    baekjoon 2583 DFS

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,72 +1,71 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
 
 using namespace std;
 
 //시간 복잡도: O(n^2)
 //공간 복잡도: O(n^2)
-//사용한 알고리즘: DFS, 플러드 필
+//사용한 알고리즘: BFS, 플러드 필
 //사용한 자료구조: 2차원 배열
 
-int d[101][101];
 bool check[101][101];
 int dx[] = {0, 0, 1, -1};
 int dy[] = {-1, 1, 0, 0};
-int n;
-int rain=1;
+int m, n, k;
+int cnt = 0;
 
 void dfs(int x, int y){
     check[x][y] = true;
+    cnt++;
     
     for(int i=0; i<4; i++){
         int nx = x + dx[i];
         int ny = y + dy[i];
-        
-        if(nx>=0 && nx<n && ny>=0 && ny<n){
-            if(check[nx][ny] == false && d[nx][ny] > rain){
+     
+        if(nx>=0 && nx<m && ny>=0 && ny<n){
+            if(check[nx][ny] == false){
                 dfs(nx, ny);
             }
         }
     }
     
+    
 }
 
 int main(){
+    cin >> m >> n >> k;
     
-    cin >> n;
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            cin >> d[i][j];
-        }
-    }
-    
-    
-    int result = 1;
-
-    while(rain<101){
-
-        int cnt = 0;
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                if(check[i][j] == false && d[i][j] > rain){
-                    dfs(i, j);
-                    cnt++;
-                }
+    for(int i=0; i<k; i++){
+        int y1, x1, y2, x2;
+        cin >> y1 >> x1 >> y2 >> x2;
+        
+        y2 = y2 - 1;
+        x2 = x2 - 1;
+        for(int i=x1; i<=x2; i++){
+            for(int j=y1; j<=y2; j++){
+                check[i][j] = true;
             }
         }
         
-        result = max(result, cnt);
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                check[i][j] = false;
-            }
-        }
-        rain++;
-
     }
     
-    cout << result << endl;
-
+    vector<int> cnt_array;
+    for(int i=0; i<m; i++){
+        for(int j=0; j<n; j++){
+            if(check[i][j] == false){
+                dfs(i, j);
+                cnt_array.push_back(cnt);
+                cnt = 0;
+            }
+        }
+    }
     
+    sort(cnt_array.begin(), cnt_array.end());
+    
+    cout << cnt_array.size() << endl;
+    for(int i=0; i<cnt_array.size(); i++){
+        cout << cnt_array[i] <<" ";
+    }
+    cout << endl;
 }
-
