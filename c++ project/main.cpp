60fbf593c5dfbae76911eb17fef9995f9113ae9f
김정윤 <skyepodium@gmail.com>
2018-03-16 01:43:56 +0900

    03.16 baekjoon 2420

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,54 +1,12 @@
 #include <iostream>
-#include <queue>
+#include <cmath>
 
 using namespace std;
 
-//시간 복잡도: O(E)
-//공간 복잡도: O(V)
-//사용한 알고리즘: BFS
-//사용한 자료구조: 2차원 벡터, 1차원 배열
-
-bool check[100001];
-int dist[100001];
-
-void bfs(int start){
-    check[start] = true;
-    queue<int> q;
-    q.push(start);
-    
-    while(!q.empty()){
-        int node = q.front();
-        q.pop();
-        
-        if(node*2 <= 100000 && check[node*2] == 0){
-            check[node*2] = true;
-            dist[node*2] = dist[node];
-            q.push(node*2);
-        }
-        
-        if(node-1 >= 0 && check[node-1] == 0){
-            check[node-1] = true;
-            dist[node-1] = dist[node] + 1;
-            q.push(node-1);
-        }
-        
-        if(node+1 <= 100000 && check[node+1] == 0){
-            check[node+1] = true;
-            dist[node+1] = dist[node] + 1;
-            q.push(node+1);
-        }
-        
-    }
-}
-
 int main(){
-    
-    int start, end;
-    cin >> start >> end;
-    
-    bfs(start);
-    cout << dist[end] << endl;
+    long long int n, m;
+    cin >> n >> m;
+    cout << abs(n-m) << endl;
     
 }
-
 
