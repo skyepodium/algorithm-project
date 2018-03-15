1c39a63db52a9004cb72be57a7bbb7f06f5ebf0a
김정윤 <skyepodium@gmail.com>
2018-03-16 01:34:38 +0900

    baekjoon 13549

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
-#include <vector>
 #include <queue>
-#include <algorithm>
 
 using namespace std;
 
-vector<int> a[101];
-int check[101];
-int n, m;
+//시간 복잡도: O(E)
+//공간 복잡도: O(V)
+//사용한 알고리즘: BFS
+//사용한 자료구조: 2차원 벡터, 1차원 배열
+
+bool check[100001];
+int dist[100001];
 
 void bfs(int start){
-    check[start] = 0;
+    check[start] = true;
     queue<int> q;
     q.push(start);
     
@@ -18,57 +20,35 @@ void bfs(int start){
         int node = q.front();
         q.pop();
         
-        for(int i=0; i<a[node].size(); i++){
-            int next = a[node][i];
-            
-            if(check[next] == 0){
-                check[next] = check[node] + 1;
-                q.push(next);
-            }
-            
+        if(node*2 <= 100000 && check[node*2] == 0){
+            check[node*2] = true;
+            dist[node*2] = dist[node];
+            q.push(node*2);
         }
-    }
-}
-
-int check_dist(int num){
-    
-    int total_dist = 0;
-    for(int i=1; i<=n; i++){
-        if(i == num){
-            continue;
+        
+        if(node-1 >= 0 && check[node-1] == 0){
+            check[node-1] = true;
+            dist[node-1] = dist[node] + 1;
+            q.push(node-1);
         }
-        total_dist += check[i];
-    }
-    
-    return total_dist;
-}
-
-void clear_check(){
-    for(int i=0; i<101; i++){
-        check[i] = 0;
+        
+        if(node+1 <= 100000 && check[node+1] == 0){
+            check[node+1] = true;
+            dist[node+1] = dist[node] + 1;
+            q.push(node+1);
+        }
+        
     }
 }
 
 int main(){
-
-    cin >> n >> m;
     
-    for(int i=0; i<m; i++){
-        int u, v;
-        cin >> u >> v;
-        
-        a[u].push_back(v);
-        a[v].push_back(u);
-    }
-
-    vector<pair<int, int>> result;
-    for(int i=1; i<=n; i++){
-        bfs(i);
-        result.push_back(make_pair(check_dist(i), i));
-        clear_check();
-    }
-
-    sort(result.begin(), result.end());
+    int start, end;
+    cin >> start >> end;
     
-    cout << result[0].second << endl;
+    bfs(start);
+    cout << dist[end] << endl;
+    
 }
+
+
