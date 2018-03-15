e21737401c05b1cf95d4569b740204124fd5b77a
김정윤 <skyepodium@gmail.com>
2018-03-15 19:29:25 +0900

    baekjoon 7562

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,116 +1,76 @@
 #include <iostream>
+#include <queue>
 
 using namespace std;
 
 //시간 복잡도: O(n^2)
 //공간 복잡도: O(n^2)
-//사용한 알고리즘: DFS
+//사용한 알고리즘: BFS
 //사용한 자료구조: 2차원 배열
 
 int d[301][301];
-int melt_height[301][301];
 bool check[301][301];
-int dx[] = {0, 0, 1, -1};
-int dy[] = {-1, 1, 0, 0};
-int n, m;
+int dx[] = {-2, -1, 1, 2, 2, 1, -1, -2};
+int dy[] = {1, 2, 2, 1, -1, -2, -2, -1};
+int n;
 
-void minus_height(){
-    
-    for(int i=0; i<n; i++){
-        for(int j=0; j<m; j++){
-            if(d[i][j] > 0){
-            
-                int m_height = 0;
-                for(int k=0; k<4; k++){
-                    int nx = i+dx[k];
-                    int ny = j+dy[k];
-                    
-                    if(nx >= 0 && nx<n && ny>=0 && ny<m){
-                        if(d[nx][ny] == 0){
-                            m_height++;
-                        }
-                    }
-                    
-                }
-                melt_height[i][j] = m_height;
-            }
-        }
-    }
-    
-    for(int i=0; i<n; i++){
-        for(int j=0; j<m; j++){
-            if(d[i][j] - melt_height[i][j] >= 0){
-                d[i][j] = d[i][j] - melt_height[i][j];
-            }else{
-                d[i][j] = 0;
-            }
-        }
-    }
-    
-}
-
-
-void dfs(int x, int y){
+void bfs(int x, int y){
     check[x][y] = true;
+    queue<pair<int, int>> q;
+    q.push(make_pair(x, y));
     
-    for(int i=0; i<4; i++){
-        int nx = x + dx[i];
-        int ny = y + dy[i];
+    while(!q.empty()){
+        x = q.front().first;
+        y = q.front().second;
+        q.pop();
         
-        if(nx>= 0 && nx<n && ny>=0 && ny<m){
-            if(check[nx][ny] == false && d[nx][ny] > 0){
-                dfs(nx, ny);
+        for(int i=0; i<8; i++){
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            
+            if(nx>=0 && nx<n && ny>=0 && ny<n){
+            
+                if(check[nx][ny] == false){
+                    check[nx][ny] = true;
+                    d[nx][ny] = d[x][y] + 1;
+                    q.push(make_pair(nx, ny));
+                }
+                
             }
         }
     }
 }
 
-void clear_check(){
-    for(int i=0; i<n; i++){
-        for(int j=0; j<m; j++){
+void clear_map(){
+    
+    for(int i=0; i<301; i++){
+        for(int j=0; j<301; j++){
+            d[i][j] = 0;
             check[i][j] = false;
         }
     }
-    
 }
 
 int main(){
-
-    cin >> n >> m;
     
-    for(int i=0; i<n; i++){
-        for(int j=0; j<m; j++){
-            cin >> d[i][j];
-        }
-    }
+    int t;
+    cin >> t;
 
-    int year = 0;
-    int cnt = 1;
+    while(t--){
     
-    while(cnt == 1){
-        minus_height();
-        year++;
-        cnt = 0;
-
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(check[i][j] == false && d[i][j] > 0){
-                    dfs(i, j);
-                    cnt++;
-                }
-            }
-        }
+        cin >> n;
+    
+        int start_x, start_y;
+        int end_x, end_y;
         
-        clear_check();
-            
-    }
-
-    if(cnt>1){ //빙하기 두 덩어리 이상으로 분리됨
-        cout << year << endl;
-    }else{ //빙하가 두 덩어리 이상으로 분리되지 않음
-        cout << 0 << endl;
-    }
+        cin >> start_x >> start_y;
+        cin >> end_x >> end_y;
     
+        bfs(start_x, start_y);
+        cout << d[end_x][end_y] << endl;
+        
+        clear_map();
+        
+    }
     
 }
-
