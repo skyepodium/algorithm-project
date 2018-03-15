4ea953b8f7468e856793b4c770bfacea5e9808ae
김정윤 <skyepodium@gmail.com>
2018-03-15 20:42:26 +0900

    baekjoon 1520

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,76 +1,53 @@
 #include <iostream>
-#include <queue>
 
 using namespace std;
 
 //시간 복잡도: O(n^2)
 //공간 복잡도: O(n^2)
-//사용한 알고리즘: BFS
+//사용한 알고리즘: 동적 계획법, Top-down
 //사용한 자료구조: 2차원 배열
 
-int d[301][301];
-bool check[301][301];
-int dx[] = {-2, -1, 1, 2, 2, 1, -1, -2};
-int dy[] = {1, 2, 2, 1, -1, -2, -2, -1};
-int n;
+int a[501][501];
+int d[501][501];
+int dx[] = {0, 0, 1, -1};
+int dy[] = {-1, 1, 0, 0};
+int n, m;
 
-void bfs(int x, int y){
-    check[x][y] = true;
-    queue<pair<int, int>> q;
-    q.push(make_pair(x, y));
+int go(int x, int y){
     
-    while(!q.empty()){
-        x = q.front().first;
-        y = q.front().second;
-        q.pop();
-        
-        for(int i=0; i<8; i++){
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            
-            if(nx>=0 && nx<n && ny>=0 && ny<n){
-            
-                if(check[nx][ny] == false){
-                    check[nx][ny] = true;
-                    d[nx][ny] = d[x][y] + 1;
-                    q.push(make_pair(nx, ny));
-                }
-                
+    if(x == 0 && y ==0){
+        return 1;
+    }
+    
+    if(d[x][y] > -1){
+        return d[x][y];
+    }
+    
+    d[x][y] = 0;
+    for(int i=0; i<4; i++){
+        int nx = x + dx[i];
+        int ny = y + dy[i];
+        if(nx>=0 && nx<n && ny>=0 && ny<m){
+            if(a[nx][ny] > a[x][y]){
+                d[x][y] = d[x][y] + go(nx, ny);
             }
         }
     }
-}
-
-void clear_map(){
     
-    for(int i=0; i<301; i++){
-        for(int j=0; j<301; j++){
-            d[i][j] = 0;
-            check[i][j] = false;
-        }
-    }
+    return d[x][y];
 }
 
 int main(){
     
-    int t;
-    cin >> t;
-
-    while(t--){
+    cin >> n >> m;
     
-        cin >> n;
-    
-        int start_x, start_y;
-        int end_x, end_y;
-        
-        cin >> start_x >> start_y;
-        cin >> end_x >> end_y;
-    
-        bfs(start_x, start_y);
-        cout << d[end_x][end_y] << endl;
-        
-        clear_map();
-        
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            cin >> a[i][j];
+            d[i][j] = -1;
+        }
     }
+
+    cout << go(n-1, m-1) << endl;
     
 }
