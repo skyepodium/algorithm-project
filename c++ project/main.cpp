907ec953cc0093ce88b72169bb85fa6bb0336191
김정윤 <skyepodium@gmail.com>
2018-03-16 02:24:02 +0900

    baekjoon 2670

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,12 +1,25 @@
 #include <iostream>
-#include <cmath>
+#include <algorithm>
 
 using namespace std;
 
+double d[10001];
+double a[10001];
+
 int main(){
-    long long int n, m;
-    cin >> n >> m;
-    cout << abs(n-m) << endl;
+
+    int n;
+    cin >> n;
     
+    for(int i=0; i<n; i++){
+        cin >> a[i];
+    }
+
+    d[0] = a[0];
+    for(int i=1; i<n; i++){
+        d[i] = max(d[i-1]*a[i], a[i]);
+    }
+    
+    printf("%.3f\n", *max_element(d , d+n));
 }
 
