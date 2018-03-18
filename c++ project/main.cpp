d51e5926a89f97e81444cec7d9e1eb54ac01dcfc
김정윤 <skyepodium@gmail.com>
2018-03-18 21:27:36 +0900

    baekjoon 2668 DFS

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,38 +1,62 @@
 #include <iostream>
-#include <cmath>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
+//시간 복잡도: O(n^2)
+//공간 복잡도: O(n)
+//사용한 알고리즘: DFS
+//사용한 자료구조: 1차원 배열,2차원 벡터
+
+vector<int> a[101];
 bool check[101];
 
-int main(){
-
-    for(int i=2; i<101; i++){
-        for(int j=i; j<101; j=j+i){
-            if(check[j] == false){
-                check[j] = true;
-            }else{
-                check[j] = false;
-            }
-            
+void dfs(int node){
+    
+    for(int i=0; i<a[node].size(); i++){
+        int next = a[node][i];
+        if(check[next] == false){
+            check[next] = true;
+            dfs(next);
         }
     }
-
-    int t;
-    cin >> t;
     
-    while(t--){
-        int n;
-        cin >> n;
-        int cnt = 0;
-        for(int i=1; i<=n; i++){
-            if(check[i] == false){
-                cnt++;
-            }
-        }
-        cout << cnt << endl;
+}
 
+void clear_check(){
+    for(int i=0; i<101; i++){
+        check[i] = false;
     }
 }
 
+int main(){
+    
+    int n;
+    cin >> n;
+    
+    for(int i=1; i<=n; i++){
+        int u;
+        cin >> u;
+        a[i].push_back(u);
+    }
+    
+    int cnt = 0;
+    vector<int> result;
+    for(int i=1; i<=n; i++){
+        if(check[i] == false){
+            dfs(i);
+        }
+        if(check[i] == true){
+            result.push_back(i);
+            cnt++;
+        }
+        clear_check();
+    }
+ 
+    cout << cnt << endl;
+    sort(result.begin(), result.end());
+    for(int i=0; i<result.size(); i++){
+        cout << result[i] << endl;
+    }
+}
