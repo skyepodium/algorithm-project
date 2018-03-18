dbb7c8a9cb189fe2dd5b7008050efa871fd5e9b6
김정윤 <skyepodium@gmail.com>
2018-03-18 22:06:57 +0900

    03.18 baekjoon 2669

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,62 +1,60 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
 
 using namespace std;
 
 //시간 복잡도: O(n^2)
-//공간 복잡도: O(n)
+//공간 복잡도: O(n^2)
 //사용한 알고리즘: DFS
-//사용한 자료구조: 1차원 배열,2차원 벡터
+//사용한 자료구조: 2차원 배열
 
-vector<int> a[101];
-bool check[101];
+int d[101][101];
+bool check[101][101];
+int dx[] = {0, 0, 1, -1};
+int dy[] = {-1, 1, 0, 0};
+int result = 0;
 
-void dfs(int node){
+void dfs(int x, int y){
+    check[x][y] = true;
+    result++;
     
-    for(int i=0; i<a[node].size(); i++){
-        int next = a[node][i];
-        if(check[next] == false){
-            check[next] = true;
-            dfs(next);
+    for(int i=0; i<4; i++){
+        int nx = x + dx[i];
+        int ny = y + dy[i];
+    
+        if(nx>=0 && nx<101 && ny>=0 && ny<101){
+            if(check[nx][ny] == false && d[nx][ny] == 1){
+                dfs(nx, ny);
+            }
         }
     }
     
-}
-
-void clear_check(){
-    for(int i=0; i<101; i++){
-        check[i] = false;
-    }
 }
 
 int main(){
     
-    int n;
-    cin >> n;
-    
-    for(int i=1; i<=n; i++){
-        int u;
-        cin >> u;
-        a[i].push_back(u);
-    }
-    
-    int cnt = 0;
-    vector<int> result;
-    for(int i=1; i<=n; i++){
-        if(check[i] == false){
-            dfs(i);
+    for(int i=0; i<4; i++){
+        int y1, x1, y2, x2;
+        cin >> y1 >> x1 >> y2 >> x2;
+        
+        y2 = y2 - 1;
+        x2 = x2 - 1;
+        for(int i=x1; i<=x2; i++){
+            for(int j=y1; j<=y2; j++){
+                d[i][j] = 1;
+            }
         }
-        if(check[i] == true){
-            result.push_back(i);
-            cnt++;
+        
+    }
+    
+    for(int i=0; i<101; i++){
+        for(int j=0; j<101; j++){
+            if(check[i][j] == false && d[i][j] == 1){
+                dfs(i, j);
+            }
         }
-        clear_check();
     }
- 
-    cout << cnt << endl;
-    sort(result.begin(), result.end());
-    for(int i=0; i<result.size(); i++){
-        cout << result[i] << endl;
-    }
+    
+    cout << result << endl;
+    
 }
