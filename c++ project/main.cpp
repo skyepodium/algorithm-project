81ecefbec0b1a9cf9566b779df4ad404739d4e55
김정윤 <skyepodium@gmail.com>
2018-03-24 20:45:14 +0900

    03.24

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,53 +1,42 @@
 #include <iostream>
-#include <algorithm>
 
 using namespace std;
 
-int d[101][101];
-bool check[101][101];
-int dx[] = {0, 0, 1, -1};
-int dy[] = {-1, 1, 0, 0};
-int n, m;
-int cnt = 0;
-void dfs(int x, int y, int color){
-    check[x][y] = true;
-    cnt++;
+int d[101];
+int a[101];
+
+int main(int argc, char** argv)
+{
+    int test_case;
+    int T;
     
-    for(int i=0; i<4; i++){
-        int nx = x + dx[i];
-        int ny = y + dy[i];
-        if(nx >= 0  && nx<n && ny>=0 && ny<m){
-            if(check[nx][ny] == false && d[nx][ny] == color && d[nx][ny] != 0){
-                dfs(nx, ny, d[nx][ny]);
+    cin>>T;
+    
+    for(test_case = 1; test_case <= T; ++test_case)
+    {
+        
+        int n;
+        cin >> n;
+        for(int i=0; i<n; i++){
+            cin >> a[i];
+        }
+        
+        for(int i=0; i<n; i++){
+            d[i] = 0;
+            for(int j=0; j<i; j++){
+                if(a[i] > a[j]){
+                    d[i] = d[i] + a[i]-a[j];
+                }
             }
         }
-    }
-    
-}
-
-int main(){
-    cin >> n >> m;
-    
-    for(int i=0; i<n; i++){
-        for(int j=0; j<m; j++){
-            cin >> d[i][j];
+        
+        for(int i=0; i<n; i++){
+            cout << d[i] << endl;
         }
-    }
-    
-    int total_cnt = 0;
-    int max_cnt = 0;
-    for(int i=0; i<n; i++){
-        for(int j=0; j<m; j++){
-            if(check[i][j] == false && d[i][j] != 0){
-                dfs(i, j, d[i][j]);
-                total_cnt++;
-                max_cnt = max(max_cnt, cnt);
-                cnt = 0;
-            }
-        }
-    }
 
-    cout << total_cnt <<" "<< max_cnt << endl;
-    
-    
+        
+        
+        
+    }
+    return 0;
 }
