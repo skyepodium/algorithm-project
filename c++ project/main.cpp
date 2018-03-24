a268c4ac7a74398aa4030902106fc4dd0bd457fd
김정윤 <skyepodium@gmail.com>
2018-03-24 21:23:00 +0900

    samsung expert academy 159

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
+#include <vector>
+
+int v[1000001];
 
 using namespace std;
-
-int d[101];
-int a[101];
 
 int main(int argc, char** argv)
 {
@@ -14,27 +14,23 @@ int main(int argc, char** argv)
     
     for(test_case = 1; test_case <= T; ++test_case)
     {
-        
         int n;
         cin >> n;
         for(int i=0; i<n; i++){
-            cin >> a[i];
+            cin >> v[i];
         }
         
-        for(int i=0; i<n; i++){
-            d[i] = 0;
-            for(int j=0; j<i; j++){
-                if(a[i] > a[j]){
-                    d[i] = d[i] + a[i]-a[j];
-                }
+        int current = v[n-1];
+        long long int result = 0;
+        for(int j=n-1; j>=0; j--){
+            if(current > v[j]){
+                result = result + (current - v[j]);
+            }else{
+                current = v[j];
             }
         }
         
-        for(int i=0; i<n; i++){
-            cout << d[i] << endl;
-        }
-
-        
+        cout <<"#"<<test_case<<" "<<result<<endl;
         
         
     }
