ea863d4cca0e584f2fd5d1786141546625b94d04
김정윤 <skyepodium@gmail.com>
2018-03-27 17:25:27 +0900

    baekjoon 2011 top down

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,44 +1,48 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-//시간 복잡도: O(n^3)
-//공간 복잡도: O(n^2)
+//시간 복잡도: O(n)
+//공간 복잡도: O(n)
 //사용한 알고리즘: 동적계획법 Top Down
-//사용한 자료구조: 2차원 배열
+//사용한 자료구조: 1차원 배열
 
-int d[201][201];
-int MOD = 1000000000;
+int d[5001];
+int a[5001];
+int MOD = 1000000;
 
-int n, k;
-
-int go(int i, int h){
+int go(int i){
     
-    if(h==1){
-        d[i][1] = 1;
+    if(d[i] > 0){
+        return d[i];
     }
     
-    if(d[i][h] > 0){
-        return d[i][h];
+    if(a[i] !=0) d[i] = go(i-1)%MOD;
+    
+    if(a[i-1]*10 + a[i]>=10 && a[i-1]*10 + a[i] <=26){
+        d[i] = (d[i] + go(i-2))%MOD;
+        d[i] %= MOD;
     }
     
-    for(int j=0; j<=n; j++){
-        if(i-j >= 0){
-            d[i][h] = (d[i][h]%MOD + go(j, h-1)%MOD)%MOD;
-            d[i][h] %= MOD;
-        }
-    }
-    
-    return d[i][h]%MOD;
+    return d[i]%MOD;
 }
-
 
 int main(){
     
-    
-    cin >> n >> k;
-    
-    cout << go(n, k)%MOD << endl;
-}
+    string number;
+    cin >> number;
 
+    int number_size = (int)number.size();
+
+    for(int i=1; i<=number_size; i++){
+        a[i] = number[i-1] - 48;
+    }
+    
+    d[0] = 1;
+    if(a[1] != 0) d[1] = 1;
+    
+    cout << go(number_size) << endl;
+    
+}
 
