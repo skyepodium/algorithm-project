8cd7032d91495384c83585e9d01051db6a0d7b2b
김정윤 <skyepodium@gmail.com>
2018-03-28 20:21:37 +0900

    03.28 top down

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -3,6 +3,11 @@
 
 using namespace std;
 
+//시간 복잡도: O(n^2)
+//공간 복잡도: O(n)
+//사용한 알고리즘: 동적 계획법 Top Down
+//사용한 자료구조: 1차원 배열
+
 long long int d[101];
 
 long long int go(int i){
@@ -16,7 +21,7 @@ long long int go(int i){
         return d[i];
     }
     
-    for(int j=3; j<=i-1; j++){
+    for(int j=3; j<=i-3; j++){
         d[i] = max(d[i], go(i-j) * (j-1));
     }
     
