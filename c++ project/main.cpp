eeb908f9af85e7788be4f5a97f346c4c40e03dd8
김정윤 <skyepodium@gmail.com>
2018-03-28 21:32:51 +0900

    03.28

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,39 +1,26 @@
 #include <iostream>
-#include <algorithm>
 
 using namespace std;
 
-//시간 복잡도: O(n^2)
-//공간 복잡도: O(n)
-//사용한 알고리즘: 동적 계획법 Top Down
-//사용한 자료구조: 1차원 배열
-
-long long int d[101];
-
-long long int go(int i){
+long long int gcd(long long int a, long long int b){
     
-    if( i <= 6){
-        d[i] = i;
-        return d[i];
+    if(b==0){
+        return a;
+    }else{
+        return gcd(b, a%b);
     }
-    
-    if( d[i] > 0){
-        return d[i];
-    }
-    
-    for(int j=3; j<=i-3; j++){
-        d[i] = max(d[i], go(i-j) * (j-1));
-    }
-    
-    return d[i];
 }
 
 
 int main() {
     
-    int n;
-    cin >> n;
+    int t;
+    cin >> t;
     
-    cout << go(n) << endl;
+    while(t--){
+        long long int a, b;
+        cin >> a >> b;
     
+        cout << (a*b)/gcd(a, b) <<endl;
+    }
 }
