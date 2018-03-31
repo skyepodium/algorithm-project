456546be8374120147f73389284a683bd8aaab25
김정윤 <skyepodium@gmail.com>
2018-03-31 16:55:14 +0900

    baekjoon 11382

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -5,36 +5,12 @@
 
 using namespace std;
 
-struct position{
-    int x;
-    int y;
-};
-
-bool cmp(const position &a, const position &b){
- 
-    if(a.x == b.x){
-        
-        return a.y < b.y;
-    }else{
-        return a.x < b.x;
-    }
-}
-
 int main(){
     
-    int n;
-    scanf("%d", &n);
+    long long int a, b, c;
+    cin >> a >> b >> c;
     
-    vector<position> position_list(n);
-    for(int i=0; i<n; i++){
-        scanf("%d %d", &position_list[i].x, &position_list[i].y);
-    }
-    
-    sort(position_list.begin(), position_list.end(), cmp);
-    
-    for(int i=0; i<n; i++){
-        printf("%d %d\n", position_list[i].x, position_list[i].y);
-    }
+    cout << a + b + c << endl;
     
     
 }
