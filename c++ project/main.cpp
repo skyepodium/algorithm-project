917a72d930dfaf4d76cbed0c67b37efa94ba32c0
김정윤 <skyepodium@gmail.com>
2018-03-31 17:21:28 +0900

    baekjoon 11383

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,16 +1,42 @@
 #include <iostream>
-#include <string>
 #include <vector>
-#include <algorithm>
+#include <string>
 
 using namespace std;
 
 int main(){
+
+    int n, m;
+    cin >> n >> m;
     
-    long long int a, b, c;
-    cin >> a >> b >> c;
+    vector<string> word_first(n);
+    vector<string> word_second(n);
     
-    cout << a + b + c << endl;
+    for(int i=0; i<n; i++){
+        cin >> word_first[i];
+    }
+
+    for(int i=0; i<n; i++){
+        cin >> word_second[i];
+    }
+
     
+    bool is_result = true;
+    for(int i=0; i<n; i++){
+        int origin_index = 0;
+        for(int j=0; j<2*m; j++){
+            if(word_first[i][origin_index] != word_second[i][j]){
+                is_result = false;
+                break;
+            }
+            
+            if(j%2 == 1){
+                origin_index++;
+            }
+        }
+    }
+    
+    if(is_result) cout << "Eyfa" << endl;
+    else cout << "Not Eyfa" << endl;
     
 }
