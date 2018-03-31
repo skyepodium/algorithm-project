73485232efec264f2fb0c23b2e7ff462cc73f4a9
김정윤 <skyepodium@gmail.com>
2018-03-31 18:47:47 +0900

    samsung 03.31

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,61 +1,31 @@
 #include <iostream>
-#include <vector>
-#include <queue>
+
 using namespace std;
 
-//시간 복잡도: O(n^2)
-//공간 복잡도: O(n)
-//사용한 알고리즘: dfs
-//사용한 자료구조: 1차원 배열, 2차원 벡터(링크드 리스트)
-
-
-vector<int> a[100001];
-bool check[100001];
-bool node_list[100001];
-
-void dfs(int node){
-    check[node] = true;
+int main(int argc, char** argv)
+{
+    int test_case;
+    int T;
     
-    for(int i=0; i<a[node].size(); i++){
+    cin>>T;
+    
+    for(test_case = 1; test_case <= T; ++test_case)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
         
-        int next = a[node][i];
-        if(check[next] == false){
-            dfs(next);
+        cout << "#" << test_case <<" ";
+        if(c<a){
+            cout << a-c << endl;
+        }else if(c>b){
+            cout << -1 << endl;
+        }else{
+            cout << 0 << endl;
         }
         
-    }
-}
-
-int main(int argc, const char *argv[]) {
-    int N;
-    cin >> N;
-    
-    
-    
-    for (int i = 0; i < N; ++i) {
-        int x, y;
-        cin >> x >> y;
-        a[x].push_back(y);
-        a[y].push_back(x);
         
-        node_list[x] = true;
-        node_list[y] = true;
+        
         
     }
-    
-    int group_cnt = 0;
-    
-    
-    for(int i=0; i<=100001; i++){
-        if(node_list[i] == true && check[i] == false){
-            dfs(i);
-            group_cnt++;
-        }
-    }
-    
-    cout << group_cnt << endl;
-    
     return 0;
 }
-
-
