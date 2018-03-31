3f6558ab5b6dfcd0ecb12c51dd2d3b1c7024bbcf
김정윤 <skyepodium@gmail.com>
2018-03-31 16:52:08 +0900

    baekjoon 11650 sort

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,19 +1,40 @@
 #include <iostream>
-#include <algorithm>
 #include <string>
-#include <set>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
 
-long long int a[1000001];
+struct position{
+    int x;
+    int y;
+};
+
+bool cmp(const position &a, const position &b){
+ 
+    if(a.x == b.x){
+        
+        return a.y < b.y;
+    }else{
+        return a.x < b.x;
+    }
+}
 
 int main(){
-
-    set<int> s;
     
-    s.insert(20);
-    s.insert(30);
-    s.insert(10);
+    int n;
+    scanf("%d", &n);
     
-    int a = s.find(22);
-    cout << a <<endl;
+    vector<position> position_list(n);
+    for(int i=0; i<n; i++){
+        scanf("%d %d", &position_list[i].x, &position_list[i].y);
+    }
+    
+    sort(position_list.begin(), position_list.end(), cmp);
+    
+    for(int i=0; i<n; i++){
+        printf("%d %d\n", position_list[i].x, position_list[i].y);
+    }
+    
+    
 }
