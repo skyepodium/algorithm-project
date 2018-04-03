16ad62384fb20adc3cef58ac806f5c03a1ed69bb
김정윤 <skyepodium@gmail.com>
2018-04-03 12:39:49 +0900

    04.03

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,33 +1,46 @@
 #include <iostream>
-#include <vector>
-#include <queue>
+#include <string>
 
 using namespace std;
 
-int main(int argc, char** argv)
-{
-    int test_case;
+//시간 복잡도: O(n)
+//공간 복잡도: O(n)
+//사용한 알고리즘: 리니어 서치
+//사용한 자료구조: 스트링
+
+int main(int argc, const char *argv[]) {
     int T;
+    cin >> T;
     
-    cin>>T;
-    
-    for(test_case = 1; test_case <= T; ++test_case)
-    {
-        int a, b, c;
-        cin >> a >> b >> c;
+    while(T--){
+        string word;
+        cin >> word;
         
-        cout << "#" << test_case <<" ";
-        if(c<a){
-            cout << a-c << endl;
-        }else if(c>b){
-            cout << -1 << endl;
-        }else{
-            cout << 0 << endl;
+        char current_c = word[0];
+        int cnt = 1;
+        
+        for(int i=1; i<word.size(); i++){
+            if(word[i] != word[i-1]){
+                cout << cnt;
+                printf("%c", current_c);
+                current_c = word[i];
+                cnt = 1;
+            }else{
+                cnt++;
+            }
+            
+            if(i == (word.size()-1)){
+                cout << cnt;
+                printf("%c", current_c);
+            }
+            
         }
         
         
-        
-        
+        cout << endl;
     }
+    
+    
     return 0;
 }
+
