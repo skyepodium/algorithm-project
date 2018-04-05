5e20b3d66a30f9449c9fdf4a8fd190386419fea6
김정윤 <skyepodium@gmail.com>
2018-04-05 19:22:35 +0900

    04.05

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -3,37 +3,17 @@
 
 using namespace std;
 
-//시간 복잡도: O(n^2)
-//공간 복잡도: O(n)
-//사용한 알고리즘: 동적 계획법 Top Down
-//사용한 자료구조: 1차원 배열
-
-long long int d[101];
-
-long long int go(int i){
+//call by reference
+int calc(int &a){
     
-    if( i <= 6){
-        d[i] = i;
-        return d[i];
-    }
     
-    if( d[i] > 0){
-        return d[i];
-    }
-    
-    for(int j=3; j<=i-3; j++){
-        d[i] = max(d[i], go(i-j) * (j-1));
-    }
-    
-    return d[i];
+    return a+1;
 }
 
+int main(){
+    
+    int a = 3;
+    
+    cout << calc(a) << endl;
 
-int main() {
-    
-    int n;
-    cin >> n;
-    
-    cout << go(n) << endl;
-    
 }
