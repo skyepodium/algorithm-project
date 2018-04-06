0311710a45dd6b98cfdc51255559093720e09df3
김정윤 <skyepodium@gmail.com>
2018-04-06 19:28:14 +0900

    algospot jumpgame bottomup

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,43 +1,65 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 
 using namespace std;
 
-//시간 복잡도: O(n)
-//공간 복잡도: O(n)
-//사용한 알고리즘: 동적 계획법 Bottom Up
-//사용한 자료구조: 2차원 벡터
+int a[101][101];
+long long d[101][101];
 
+//시간 복잡도: O(n^3)
+//공간 복잡도: O(n^2)
+//사용한 알고리즘: 동적 계획법, Bottom up
+//사용한 자료구조: 2차원 배열
 
-int hopscotch(vector<vector<int> > board)
-{
+int n;
+
+void clear_array(){
     
-    int row = (int)board.size();
-    
-    int answer = 0;
-    
-    for(int i=1; i<row; i++){
-        for(int j=0; j<4; j++){
-            
-            int max_val = 0;
-            for(int k=0; k<4; k++){
-                if(j == k) continue;
-                max_val = max(max_val, board[i-1][k]);
-            }
-            board[i][j] += max_val;
-            
-            if(i==row-1) answer = max(answer, board[i][j]);
-            
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n; j++){
+            d[i][j] = 0;
         }
     }
     
-    return answer;
 }
 
-int main()
-{
-    vector<vector<int> > test{{1,2,3,5},{5,6,7,8},{4,3,2,1}};
+int main(){
     
-    cout << hopscotch(test) << endl;
+    int t;
+    cin >> t;
+    
+    while(t--){
+        
+
+        cin >> n;
+        
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                cin >> a[i][j];
+            }
+        }
+        
+        d[1][1] = 1;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                
+                if(i==1 && j ==1) continue;
+                
+                for(int k=1; k<j; k++){
+                    if(k+a[i][k] == j) d[i][j] += d[i][k];
+                }
+                
+                for(int k=1; k<i; k++){
+                    if(k+a[k][j] == i) d[i][j] += d[k][j];
+                }
+                
+            }
+        }
+        
+        if(d[n][n] > 0) cout << "YES" << endl;
+        else cout << "NO" << endl;
+
+        clear_array();
+    }
+
+    
 }
