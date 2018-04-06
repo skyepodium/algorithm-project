b7709105bd07bc2dda745f9c4321eaa0b3cf813a
김정윤 <skyepodium@gmail.com>
2018-04-06 19:41:09 +0900

    alogospot 외발뛰기 Top Down

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -7,7 +7,7 @@ long long d[101][101];
 
 //시간 복잡도: O(n^3)
 //공간 복잡도: O(n^2)
-//사용한 알고리즘: 동적 계획법, Bottom up
+//사용한 알고리즘: 동적 계획법, Top Down
 //사용한 자료구조: 2차원 배열
 
 int n;
@@ -20,6 +20,28 @@ void clear_array(){
         }
     }
     
+}
+
+long long int go(int i, int j){
+
+    if(i==1 && j==1){
+        d[1][1] = 1;
+        return d[i][j];
+    }
+    
+    if(d[i][j] > 0){
+        return d[i][j];
+    }
+    
+    for(int k=1; k<j; k++){
+        if(k+a[i][k] == j) d[i][j] += go(i, k);
+    }
+    
+    for(int k=1; k<i; k++){
+        if(k+a[k][j] == i) d[i][j] += go(k, j);
+    }
+    
+    return d[i][j];
 }
 
 int main(){
@@ -38,24 +60,7 @@ int main(){
             }
         }
         
-        d[1][1] = 1;
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=n; j++){
-                
-                if(i==1 && j ==1) continue;
-                
-                for(int k=1; k<j; k++){
-                    if(k+a[i][k] == j) d[i][j] += d[i][k];
-                }
-                
-                for(int k=1; k<i; k++){
-                    if(k+a[k][j] == i) d[i][j] += d[k][j];
-                }
-                
-            }
-        }
-        
-        if(d[n][n] > 0) cout << "YES" << endl;
+        if(go(n, n) > 0) cout << "YES" << endl;
         else cout << "NO" << endl;
 
         clear_array();
