f49af193cca689c0ca1ae9f97e562ad6f97787e4
김정윤 <skyepodium@gmail.com>
2018-04-06 19:59:36 +0900

    algospot JOSEPHUS

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,48 +1,13 @@
 #include <iostream>
+#include <queue>
+#include <vector>
 
 using namespace std;
 
-int a[101][101];
-long long d[101][101];
-
-//시간 복잡도: O(n^3)
-//공간 복잡도: O(n^2)
-//사용한 알고리즘: 동적 계획법, Top Down
-//사용한 자료구조: 2차원 배열
-
-int n;
-
-void clear_array(){
-    
-    for(int i=1; i<=n; i++){
-        for(int j=1; j<=n; j++){
-            d[i][j] = 0;
-        }
-    }
-    
-}
-
-long long int go(int i, int j){
-
-    if(i==1 && j==1){
-        d[1][1] = 1;
-        return d[i][j];
-    }
-    
-    if(d[i][j] > 0){
-        return d[i][j];
-    }
-    
-    for(int k=1; k<j; k++){
-        if(k+a[i][k] == j) d[i][j] += go(i, k);
-    }
-    
-    for(int k=1; k<i; k++){
-        if(k+a[k][j] == i) d[i][j] += go(k, j);
-    }
-    
-    return d[i][j];
-}
+//시간 복잡도: O(n)
+//공간 복잡도: O(n)
+//사용한 알고리즘: 반복문
+//사용한 자료구조: 큐
 
 int main(){
     
@@ -51,20 +16,35 @@ int main(){
     
     while(t--){
         
-
-        cin >> n;
+        int n, k;
+        cin >> n >> k;
         
+        queue<int> q;
         for(int i=1; i<=n; i++){
-            for(int j=1; j<=n; j++){
-                cin >> a[i][j];
-            }
+            q.push(i);
         }
         
-        if(go(n, n) > 0) cout << "YES" << endl;
-        else cout << "NO" << endl;
+        while(q.size()>2){
+            
+            q.pop();
+            
+            for(int i=0; i<k-1; i++){
+                q.push(q.front());
+                q.pop();
+            }
+            
+        }
+        vector<int> v;
+        while(!q.empty()){
+            v.push_back(q.front());
+            q.pop();
+        }
+        
+        if(v[0] < v[1]){
+            cout << v[0] <<" "<<v[1] << endl;
+        }else{
+            cout << v[1] <<" "<<v[0] << endl;
+        }
 
-        clear_array();
     }
-
-    
 }
