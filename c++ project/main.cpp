cfeaa046dcb8acf7b482f1bc6791a76f477eebc5
김정윤 <skyepodium@gmail.com>
2018-04-06 15:39:59 +0900

    programmers 땅따먹기 게임 수

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,33 +1,20 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <cstring>
 
 using namespace std;
 
 //시간 복잡도: O(n)
 //공간 복잡도: O(n)
 //사용한 알고리즘: 동적 계획법 Bottom Up
-//사용한 자료구조: 2차원 배열 (동적할당)
+//사용한 자료구조: 2차원 벡터
 
 
 int hopscotch(vector<vector<int> > board)
 {
     
     int row = (int)board.size();
-    int col = (int)board[0].size();
     
-    int **d = new int*[row];
-    for(int i=0; i<row; i++){
-        d[i] = new int[col];
-        memset(d[i], 0, sizeof(int)*col);
-    }
-    
-    d[0][0] = board[0][0];
-    d[0][1] = board[0][1];
-    d[0][2] = board[0][2];
-    d[0][3] = board[0][3];
-
     int answer = 0;
     
     for(int i=1; i<row; i++){
@@ -36,11 +23,11 @@ int hopscotch(vector<vector<int> > board)
             int max_val = 0;
             for(int k=0; k<4; k++){
                 if(j == k) continue;
-                max_val = max(max_val, d[i-1][k]);
+                max_val = max(max_val, board[i-1][k]);
             }
-            d[i][j] = max_val + board[i][j];
+            board[i][j] += max_val;
             
-            if(i==row-1) answer = max(answer, d[i][j]);
+            if(i==row-1) answer = max(answer, board[i][j]);
             
         }
     }
@@ -51,7 +38,6 @@ int hopscotch(vector<vector<int> > board)
 int main()
 {
     vector<vector<int> > test{{1,2,3,5},{5,6,7,8},{4,3,2,1}};
-
+    
     cout << hopscotch(test) << endl;
 }
-
