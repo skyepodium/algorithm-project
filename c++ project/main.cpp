c24e4741daf6b4ebd500e2b247505d07ba997fbc
김정윤 <skyepodium@gmail.com>
2018-04-08 20:24:22 +0900

    04.08

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,14 +1,26 @@
 #include <iostream>
-#include <queue>
-#include <vector>
-#include <stack>
 
 using namespace std;
 
-//시간 복잡도: O(nk)
-//공간 복잡도: O(n)
-//사용한 알고리즘: 반복문
-//사용한 자료구조: 큐
+int a[101][101];
+long long d[101][101];
+
+//시간 복잡도: O(n^3)
+//공간 복잡도: O(n^2)
+//사용한 알고리즘: 동적 계획법, Bottom up
+//사용한 자료구조: 2차원 배열
+
+int n;
+
+void clear_array(){
+    
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n; j++){
+            d[i][j] = 0;
+        }
+    }
+    
+}
 
 int main(){
     
@@ -17,35 +29,38 @@ int main(){
     
     while(t--){
         
-        int n, k;
-        cin >> n >> k;
         
-        queue<int> q;
+        cin >> n;
+        
         for(int i=1; i<=n; i++){
-            q.push(i);
-        }
-        
-        while(q.size()>2){
-            
-            q.pop();
-            
-            for(int i=0; i<k-1; i++){
-                q.push(q.front());
-                q.pop();
+            for(int j=1; j<=n; j++){
+                cin >> a[i][j];
             }
-            
-        }
-        vector<int> v;
-        while(!q.empty()){
-            v.push_back(q.front());
-            q.pop();
         }
         
-        if(v[0] < v[1]){
-            cout << v[0] <<" "<<v[1] << endl;
-        }else{
-            cout << v[1] <<" "<<v[0] << endl;
+        d[1][1] = 1;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                
+                if(i==1 && j ==1) continue;
+                
+                for(int k=1; k<j; k++){
+                    if(k+a[i][k] == j) d[i][j] += d[i][k];
+                }
+                
+                for(int k=1; k<i; k++){
+                    if(k+a[k][j] == i) d[i][j] += d[k][j];
+                }
+                
+            }
         }
-
+        
+        if(d[n][n] > 0) cout << "YES" << endl;
+        else cout << "NO" << endl;
+        
+        clear_array();
     }
+    
+    
 }
+
