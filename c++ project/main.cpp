53632b394fadca00604f2fa44e54cf5c5b47c9ca
김정윤 <skyepodium@gmail.com>
2018-04-09 20:03:20 +0900

    samsung 요리

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -4,6 +4,8 @@
 
 using namespace std;
 
+int d[16][16];
+
 int main(int argc, char** argv)
 {
     int test_case;
@@ -16,46 +18,54 @@ int main(int argc, char** argv)
         int n;
         cin >> n;
         
-        vector<int> cal;
-        for(int i=0; i<4; i++){
-            int a;
-            cin >> a;
-            for(int j=0; j<a; j++){
-                cal.push_back(i);
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                cin >> d[i][j];
             }
         }
         
-        vector<int> num(n);
-        for(int i=0; i<n; i++){
-            cin >> num[i];
+        vector<int> v;
+        for(int i=0; i<n/2; i++){
+            v.push_back(0);
+        }
+
+        for(int i=0; i<n/2; i++){
+            v.push_back(1);
         }
         
-        
-        int min_result = 100000000;
-        int max_result = -100000000;
+        int result = 20000;
         
         do{
-
-            int cal_result = num[0];
-            for(int i=1; i<n; i++){
-                if(cal[i-1] == 0){
-                    cal_result = cal_result + num[i];
-                }else if(cal[i-1] == 1){
-                    cal_result = cal_result - num[i];
-                }else if(cal[i-1] == 2){
-                    cal_result = cal_result * num[i];
+            vector<int> left;
+            vector<int> right;
+            
+            for(int i=0; i<n; i++){
+                if(v[i] == 0){
+                    left.push_back(i+1);
                 }else{
-                    cal_result = cal_result / num[i];
+                    right.push_back(i+1);
                 }
             }
             
-            min_result = min(min_result, cal_result);
-            max_result = max(max_result, cal_result);
+            int left_power = 0;
+            for(int i=0; i<left.size(); i++){
+                for(int j=i+1; j<left.size(); j++){
+                    left_power = left_power + d[left[i]][left[j]] + d[left[j]][left[i]];
+                }
+            }
 
-        }while(next_permutation(cal.begin(), cal.end()));
+            int right_power = 0;
+            for(int i=0; i<right.size(); i++){
+                for(int j=i+1; j<right.size(); j++){
+                    right_power = right_power + d[right[i]][right[j]] + d[right[j]][right[i]];
+                }
+            }
+            
+            result = min(result, abs(left_power - right_power));
+            
+        }while(next_permutation(v.begin(), v.end()));
         
-        cout << "#" << test_case << " " << max_result - min_result << endl;
-        
+        cout << "#" << test_case <<" "<<result << endl;
         
     }
     return 0;
