10b4bf925c667f9d1400a78715d8bf27d5752813
김정윤 <skyepodium@gmail.com>
2018-04-10 15:46:19 +0900

    baekjoon 1182

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,70 +1,38 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
-#include <stack>
-#include <queue>
-#include <cstring>
 
 using namespace std;
 
+int n, s;
+int a[21];
+int cnt = 0, current_sum = 0;
 
-vector<int> a[1001];
-bool check[1001];
-
-void dfs(int node){
-    check[node] = true;
-    cout << node <<" ";
+void dfs(int current){
     
-    for(int i=0; i<a[node].size(); i++){
-        int next = a[node][i];
-        if(check[next] == false){
-            dfs(next);
-        }
-    }
-}
-
-void bfs(int start){
-    check[start] = true;
-    queue<int> q;
-    q.push(start);
+    if(current == n) return;
     
-    while(!q.empty()){
-        
-        int node = q.front();
-        q.pop();
-        cout << node <<" ";
-
-        for(int i=0; i<a[node].size(); i++){
-            int next = a[node][i];
-            if(check[next] == false){
-                check[next] = true;
-                q.push(next);
-            }
-        }
-        
-    }
+    if(current_sum + a[current] == s) cnt++;
+    
+    //이번 것 포함시키지 않고 진행
+    dfs(current+1);
+    
+    //이번 것 포함시키고 진행
+    current_sum += a[current];
+    dfs(current+1);
+    
+    current_sum -= a[current];
+    
 }
-
 
 int main(){
     
-    int n, m, v;
-    cin >> n >> m >> v;
-    
-    for(int i=0; i<m; i++){
-        int u, v;
-        cin >> u >> v;
-        a[u].push_back(v);
-        a[v].push_back(u);
-    }
+    cin >> n >> s;
     
     for(int i=0; i<n; i++){
-        sort(a[i].begin(), a[i].end());
+        cin >> a[i];
     }
     
-    dfs(v);
-    cout << endl;
-    memset(check, false, sizeof(check));
-    bfs(v);
-    cout << endl;
+    dfs(0);
+    cout << cnt << endl;
+    
 }
+
