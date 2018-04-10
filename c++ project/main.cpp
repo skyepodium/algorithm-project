44dc9617fdab6969aef6586a0a3d2ee109c2ab44
김정윤 <skyepodium@gmail.com>
2018-04-10 22:03:35 +0900

    baekjoon 1941 prev_permutation

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,98 +1,127 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
+#include <queue>
+#include <string>
 
 using namespace std;
 
-int n, k;
-int result = 0;
-int max_height = 0;
+//시간 복잡도: O(2^25)
+//공간 복잡도: O(25)
+//사용한 알고리즘: prev_permutation, bfs
+//사용한 자료구조: 2차원 배열
 
-int d[10][10];
-bool check[10][10];
+
+int d[5][5];
+int a[5][5];
+bool check[5][5];
+
 int dx[] = {0, 0, 1, -1};
 int dy[] = {-1, 1, 0, 0};
 
-struct position{
-    int x;
-    int y;
-    int height;
-    bool used;
-    int dist;
-};
+int result = 0;
 
-void dfs(position current){
-    result = max(result, current.dist);
+void bfs(int x, int y){
+    check[x][y] = true;
+    queue<pair<int, int>> q;
+    q.push(make_pair(x, y));
     
-    for(int i=0; i<4; i++){
-        position next = current;
-        next.x += dx[i];
-        next.y += dy[i];
-        next.height = d[next.x][next.y];
+    while(!q.empty()){
+        x = q.front().first;
+        y = q.front().second;
+        q.pop();
         
-        if(next.x>=0 && next.x<n && next.y>=0 && next.y < n){
-            if(check[next.x][next.y] == false){
-                
-                // 다음 장소가 현재 높이보다 작을 경우
-                if(next.height < current.height){
-                    check[next.x][next.y] = true;
-                    next.dist++;
-                    dfs(next);
-                    check[next.x][next.y] = false;
+        for(int i=0; i<4; i++){
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            
+            if(nx>=0 && nx<5 && ny>=0 && ny<5){
+                if(check[nx][ny] == false && a[nx][ny] == 1){
+                    check[nx][ny] = true;
+                    q.push(make_pair(nx, ny));
                 }
-                
-                else{
-                    if(current.used == false && next.height - k < current.height){
-                        check[next.x][next.y] = true;
-                        next.dist++;
-                        next.used = true;
-                        next.height = current.height - 1;
-                        dfs(next);
-                        check[next.x][next.y] = false;
-                    }
-                }
-                
             }
         }
-        
-        
+    }
+}
+
+void clear_check(){
+    
+    for(int i=0; i<5; i++){
+        for(int j=0; j<5; j++){
+            a[i][j] = 0;
+            check[i][j] = false;
+        }
     }
     
 }
 
-
-int main(int argc, char** argv)
-{
-    int test_case;
-    int T;
+bool check_cnt(){
     
-    cin>>T;
-    
-    for(test_case = 1; test_case <= T; ++test_case)
-    {
-        cin >> n >> k;
-        result = max_height = 0;
-        
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                cin >> d[i][j];
-                max_height = max(max_height, d[i][j]);
+    bool search_result = false;
+    int cnt = 0;
+    for(int i=0; i<5; i++){
+        for(int j=0; j<5; j++){
+            if(check[i][j] == true){
+                cnt++;
             }
         }
-
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                if(d[i][j] == max_height){
-                    check[i][j] = true;
-                    position cur = {i, j, max_height, false, 1};
-                    dfs(cur);
-                    check[i][j] = false;
-                }
-            }
-        }
-        
-        cout << "#" << test_case << " " << result << endl;
-
-        
     }
-    return 0;
+    
+    if(cnt == 7){
+        search_result = true;
+    }
+        
+    return search_result;
+}
+
+int main(){
+    
+    vector<pair<int, int>> position;
+    for(int i=0; i<5; i++){
+        string word;
+        cin >> word;
+        for(int j=0; j<5; j++){
+            d[i][j] = word[j];
+            position.push_back(make_pair(i, j));
+        }
+    }
+    
+    vector<int> v;
+    
+    for(int i=0; i<7; i++){
+        v.push_back(1);
+    }
+    
+    for(int i=0; i<18; i++){
+        v.push_back(0);
+    }
+
+    do{
+        int dasom_cnt = 0;
+        int start_x = 0;
+        int start_y = 0;
+        for(int i=0; i<25; i++){
+            if(v[i] == 1){
+                if(d[position[i].first][position[i].second] == 83){
+                    dasom_cnt++;
+                }
+                a[position[i].first][position[i].second] = 1;
+                start_x = position[i].first;
+                start_y = position[i].second;
+            }
+        }
+        
+        
+        if(dasom_cnt >= 4){
+            bfs(start_x, start_y);
+        }
+        
+        if(check_cnt() == true) result++;
+        
+        clear_check();
+        
+    }while(prev_permutation(v.begin(), v.end()));
+    
+    cout << result << endl;
 }
