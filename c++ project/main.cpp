7463a7159386a9d72225ce6092f2a8d6f004d755
김정윤 <skyepodium@gmail.com>
2018-04-12 20:09:11 +0900

    삼성 expert academy 요리사 by 재귀함

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -13,6 +13,8 @@ int n;
 int d[20][20];
 int result = 1000000;
 
+
+//힘 차이 계산
 void cal_power_diff(vector<int> &left, vector<int> &right){
     
     int left_power = 0;
@@ -27,11 +29,12 @@ void cal_power_diff(vector<int> &left, vector<int> &right){
     result = min(result, abs(left_power - right_power));
 }
 
-
+//call by reference를 써서 시간 아끼자
+//조합을 구한다.
 void go(int index, vector<int> &left, vector<int> &right){
     
     if(index == n){
-       
+        
         if(left.size() == n/2 && right.size() == n/2){
             
             cal_power_diff(left, right);
@@ -40,32 +43,48 @@ void go(int index, vector<int> &left, vector<int> &right){
         
         return;
     }
-
+    
     
     //이번 원소 왼쪽에 포함
     left.push_back(index);
     go(index+1, left, right);
     left.pop_back();
-
+    
     //이번 원소 오른쪽에 포함
     right.push_back(index);
     go(index+1, left, right);
     right.pop_back();
-
+    
 }
 
-int main(){
+
+int main(int argc, char** argv)
+{
+    int test_case;
+    int T;
     
-    cin >> n;
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            cin >> d[i][j];
+    cin>>T;
+    
+    for(test_case = 1; test_case <= T; ++test_case)
+    {
+        result = 1000000;
+        cin >> n;
+        
+        //문제는 1~n인데 나는 0~n을 넣었다.
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                cin >> d[i][j];
+            }
         }
+        
+        vector<int> left, right;
+        
+        go(0, left, right);
+        
+        cout << "#" << test_case << " " << result << endl;
+        
+        
+        
     }
-    
-    vector<int> left, right;
-    
-    go(0, left, right);
-    
-    cout << result << endl;
+    return 0;
 }
