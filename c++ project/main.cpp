565c9076f91d9fe87c93f3c679ac9724f20827de
김정윤 <skyepodium@gmail.com>
2018-04-12 19:57:17 +0900

    백준 스타트와 링크 재귀함수 풀

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,55 +1,71 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
 
 using namespace std;
 
 //시간 복잡도: O(2^n)
-//공간 복잡도: O(n)
+//공간 복잡도: O(n^2)
 //사용한 알고리즘: 재귀함수
-//사용한 자료구조: 1차원 배열
+//사용한 자료구조: 2차원 배열
 
 int n;
-int minus_health[20];
-int plus_happy[20];
-int result = 0;
+int d[20][20];
+int result = 1000000;
 
-void go(int num, int happy, int health){
+void cal_power_diff(vector<int> &left, vector<int> &right){
     
-    if(num == n){
-        
-        if(health > 0){
-            result = max(result, happy);
+    int left_power = 0;
+    int right_power = 0;
+    for(int i=0; i<n/2; i++){
+        for(int j=i+1; j<n/2; j++){
+            left_power = left_power + d[left[i]][left[j]] + d[left[j]][left[i]];
+            right_power = right_power + d[right[i]][right[j]] + d[right[j]][right[i]];
+        }
+    }
+    
+    result = min(result, abs(left_power - right_power));
+}
+
+
+void go(int index, vector<int> &left, vector<int> &right){
+    
+    if(index == n){
+       
+        if(left.size() == n/2 && right.size() == n/2){
+            
+            cal_power_diff(left, right);
+            
         }
         
         return;
     }
 
-    //인사를 할때
-    //num은 index 인사하든 안하든 증가한다.
-    //happy는 증가한다. plus_happy[num]만큼
-    //health는 감소한다. minus_health[num]만큼
-    go(num+1, happy + plus_happy[num], health - minus_health[num]);
     
+    //이번 원소 왼쪽에 포함
+    left.push_back(index);
+    go(index+1, left, right);
+    left.pop_back();
 
-    //인사를 안할때
-    go(num+1, happy, health);
+    //이번 원소 오른쪽에 포함
+    right.push_back(index);
+    go(index+1, left, right);
+    right.pop_back();
+
 }
-
 
 int main(){
     
     cin >> n;
-    
     for(int i=0; i<n; i++){
-        cin >> minus_health[i];
+        for(int j=0; j<n; j++){
+            cin >> d[i][j];
+        }
     }
     
-    for(int i=0; i<n; i++){
-        cin >> plus_happy[i];
-    }
-
-    go(0, 0, 100);
+    vector<int> left, right;
+    
+    go(0, left, right);
     
     cout << result << endl;
-    
 }
