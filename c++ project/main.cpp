a45de65e72b1c44bcaf73caec047b859fef7a66b
김정윤 <skyepodium@gmail.com>
2018-04-14 20:03:45 +0900

    samsung expert academy cook

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,114 +1,77 @@
 #include <iostream>
-#include <algorithm>
-#include <queue>
 #include <vector>
+#include <algorithm>
+#include <cmath>
 
 using namespace std;
 
-//시간 복잡도: O(n^5)
+//시간 복잡도: O(2^n)
 //공간 복잡도: O(n^2)
-//사용한 알고리즘: 3중 for문, BFS
+//사용한 알고리즘: 재귀함수
 //사용한 자료구조: 2차원 배열
 
+int d[16][16];
+int n;
+int result = 999999999;
 
-int n,m;
-int dx[] = {0, 0, 1, -1};
-int dy[] = {-1, 1, 0, 0};
-int d[8][8];
-bool check[8][8];
-int result = 0;
-
-int main(){
+void cal_power(vector<int> &left, vector<int> &right){
     
-    cin >> n >> m;
-
-    vector<pair<int, int>> virus;
-    vector<pair<int, int>> wall;
+    int left_power = 0;
+    int right_power = 0;
     
-    for(int i=0; i<n; i++){
-        for(int j=0; j<m; j++){
-            cin >> d[i][j];
-            
-            // 바이러스 벡터
-            if(d[i][j] == 2){
-                virus.push_back(make_pair(i, j));
-            }
-            
-            // 빈공간 벡터
-            if(d[i][j] == 0){
-                wall.push_back(make_pair(i, j));
-            }
-
+    for(int i=0; i<n/2; i++){
+        for(int j=i+1; j<n/2; j++){
+            left_power = left_power + d[left[i]][left[j]] + d[left[j]][left[i]];
+            right_power = right_power + d[right[i]][right[j]] + d[right[j]][right[i]];
         }
     }
     
-    for(int i=0; i<wall.size(); i++){
-        for(int j=i+1; j<wall.size(); j++){
-            for(int k=j+1; k<wall.size(); k++){
-                
-                //3중 for문으로 벽세우기
-                d[wall[i].first][wall[i].second] = 1;
-                d[wall[j].first][wall[j].second] = 1;
-                d[wall[k].first][wall[k].second] = 1;
+    result = min(result, abs(left_power - right_power));
+}
 
-                // BFS를 상용해서 바이러스 전파하기 위해 q벡터 생성
-                queue<pair<int, int>> q;
-                for(int i=0; i<virus.size(); i++){
-                    q.push(make_pair(virus[i].first, virus[i].second));
-                }
-                
-                //바이러스 전파 with BFS
-                while(!q.empty()){
-                    int x = q.front().first;
-                    int y = q.front().second;
-                    check[x][y] = true;
-                    q.pop();
-                    
-                    for(int i=0; i<4; i++){
-                        int nx = x + dx[i];
-                        int ny = y + dy[i];
-                        
-                        if(nx>=0 && nx<n && ny>=0 && ny< m){
-                            if(check[nx][ny] == false && d[nx][ny] == 0){
-                                check[nx][ny] = true;
-                                q.push(make_pair(nx, ny));
-                            }
-                        }
-                    }
-                }
-                
-                // 안정영역의 갯수 검사
-                int cnt = 0;
-                for(int i=0; i<n; i++){
-                    for(int j=0; j<m; j++){
-                        if(d[i][j] == 0 && check[i][j] == false){
-                            cnt++;
-                        }
-                    }
-                }
-                
-                // 최대값 갱신
-                result = max(result, cnt);
-                
-                // 체크 배열 초기화
-                for(int i=0; i<n; i++){
-                    for(int j=0; j<m; j++){
-                        check[i][j] = false;
-                    }
-                }
-                
-                // 벽정보 초기화
-                d[wall[i].first][wall[i].second] = 0;
-                d[wall[j].first][wall[j].second] = 0;
-                d[wall[k].first][wall[k].second] = 0;
+void go(int index, vector<int> &left, vector<int> & right){
 
-            }
+    if(index == n){
+        if(left.size() == n/2 && right.size() == n/2){
+            cal_power(left, right);
         }
+        return;
     }
+
+    left.push_back(index);
+    go(index+1, left, right);
+    left.pop_back();
     
-    cout << result << endl;
-    
-    
+    right.push_back(index);
+    go(index+1, left, right);
+    right.pop_back();
     
 }
 
+int main(int argc, char** argv)
+{
+    int test_case;
+    int T;
+    
+    cin>>T;
+    
+    for(test_case = 1; test_case <= T; ++test_case)
+    {
+        result = 999999999;
+        cin >> n;
+        
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                cin >> d[i][j];
+            }
+        }
+        
+        vector<int> left;
+        vector<int> right;
+        go(0, left, right);
+        
+        cout << "#" << test_case << " " << result << endl;
+        
+    }
+    return 0;
+}
