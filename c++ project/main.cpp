4ee09c42a3d7be437a5cb952b50faab98b3e69a9
김정윤 <skyepodium@gmail.com>
2018-04-14 16:47:36 +0900

    baekjoon 14890 경사로 clear

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,57 +1,172 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
+#include <cstring>
+#include <cmath>
 
 using namespace std;
 
-//시간 복잡도: O(2^c)
-//공간 복잡도: O(c)
-//사용한 알고리즘: 재귀 함수, 정렬
-//사용한 자료구조: 1차원 배열, 1차원 벡터
-
-int l, c;
-char word[15];
-bool is_vowel[26];
-
-void go(int index, vector<char> &pick, int vowel_cnt, int consonant_cnt){
-    
-    if(index == c){
-        
-        if(pick.size() == l){
-            if(vowel_cnt >= 1 && consonant_cnt >=2){
-                for(int i=0; i<pick.size(); i++){
-                    cout << pick[i];
-                }
-                cout << endl;
-            }
-        }
-        
-        return;
-    }
-    
-    //이번 인덱스 글자 포함
-    pick.push_back(word[index]);
-    go(index+1, pick, vowel_cnt + is_vowel[word[index]-97], consonant_cnt + !is_vowel[word[index]-97]);
-    pick.pop_back();
-    
-    
-    //이번 인덱스 글자 포함 안함
-    go(index+1, pick, vowel_cnt, consonant_cnt);
-    
-}
+int d[101][101];
+bool check[101][101];
+int n, l;
+int cnt = 0;
 
 int main(){
+
+    cin >> n >> l;
     
-    cin >> l >> c;
-    
-    for(int i=0; i<c; i++){
-        cin >> word[i];
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cin >> d[i][j];
+        }
     }
     
-    sort(word, word+c);
     
-    is_vowel[0] = is_vowel[4] = is_vowel[8] = is_vowel[14] = is_vowel[20] = true;
+    // 가로 검사
+    for(int i=0; i<n; i++){
+        int current_height = d[i][0];
+        int start_index = 0;
+        int change_index = 0;
+        int is_possible = true;
+        for(int j=1; j<n; j++){
+            
+            // 높이 차이가 1인지 검사
+            if(d[i][j] != current_height){
+                int diff = abs(d[i][j] - current_height);
+                if(diff != 1){
+                    is_possible = false;
+                    break;
+                }
+            }
+            
+            // 올라가자
+            if(current_height < d[i][j]){
+                change_index = j;
+                
+                //길이 계산
+                if(change_index - start_index < l){
+                    is_possible = false;
+                    break;
+                }
+                
+                //활주로 건설되어 있는지 검사
+                for(int k=0; k<l; k++){
+                    if(check[i][change_index-1-k] != false){
+                        is_possible = false;
+                        break;
+                    }else{
+                        check[i][change_index-1-k] = true;
+                    }
+
+                }
+                
+                start_index = j;
+                current_height = d[i][j];
+            }
+            
+            //내려가자
+            else if( current_height > d[i][j]){
+                change_index = j;
+                for(int k=0; k<l; k++){
+
+                    if(check[i][change_index+k] != false){
+                        is_possible = false;
+                        break;
+                    }else{
+                        check[i][change_index+k] = true;
+                    }
+                    
+                    if(d[i][change_index] != d[i][change_index+k]){
+                        is_possible = false;
+                        break;
+                    }
+                }
+                start_index = j;
+                current_height = d[i][j];
+            }
+            
+            
+            if(j==n-1){
+                if(is_possible == true){
+//                    cout << "가로 검사 " << i << endl;
+                    cnt++;
+                }
+            }
+        }
+    }
+
+    memset(check, false, sizeof(check));
+    //세로 검사
+    for(int j=0; j<n; j++){
+        int current_height = d[0][j];
+        int start_index = 0;
+        int change_index = 0;
+        int is_possible = true;
+        for(int i=1; i<n; i++){
+            
+            // 높이 차이가 1인지 검사
+            if(d[i][j] != current_height){
+                int diff = abs(d[i][j] - current_height);
+                if(diff != 1){
+                    is_possible = false;
+                    break;
+                }
+            }
+            
+            // 올라가자
+            if(current_height < d[i][j]){
+                change_index = i;
+                
+                //길이 계산
+                if(change_index - start_index < l){
+                    is_possible = false;
+                    break;
+                }
+                
+                //활주로 건설되어 있는지 검사
+                for(int k=0; k<l; k++){
+                    if(check[change_index-1-k][j] != false){
+                        is_possible = false;
+                        break;
+                    }else{
+                        check[change_index-1-k][j] = true;
+                    }
+                    
+                }
+                
+                start_index = i;
+                current_height = d[i][j];
+            }
+            
+            //내려가자
+            else if( current_height > d[i][j]){
+                change_index = i;
+                for(int k=0; k<l; k++){
+                    
+                    if(check[change_index+k][j] != false){
+                        is_possible = false;
+                        break;
+                    }else{
+                        check[change_index+k][j] = true;
+                    }
+                    
+                    if(d[change_index][j] != d[change_index+k][j]){
+                        is_possible = false;
+                        break;
+                    }
+                }
+                start_index = i;
+                current_height = d[i][j];
+            }
+            
+            
+            if(i==n-1){
+                if(is_possible == true){
+//                    cout << "세로 검사 " << j << endl;
+                    cnt++;
+                }
+            }
+        }
+    }
+
+    cout << cnt << endl;
     
-    vector<char> pick;
-    go(0, pick, 0, 0);
 }
