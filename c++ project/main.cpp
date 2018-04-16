b69a8d2fcc6fad9a56a216c4f93966940b570468
김정윤 <skyepodium@gmail.com>
2018-04-16 20:08:21 +0900

    baekjoon 15686 치킨 배달

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,202 +1,85 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <cmath>
 
 using namespace std;
 
-//시간 복잡도: O(2^21)
-//공간 복잡도: O(nm)
-//사용한 알고리즘: dfs backtracking
+//시간 복잡도: O(m^3*n^2)
+//공간 복잡도: O(n^2)
+//사용한 알고리즘: 재귀함수(백트래킹)
 //사용한 자료구조: 구조체, 2차원 배열, 1차원 벡터
 
+int n, m;
+int d[50][50];
 
-int h, w;
-int result;
-int d[8][8];
-int test[8][8];
-int cctv_size = 0;
+vector<pair<int, int>> chicken;
+vector<pair<int, int>> house;
+int max_chicken;
+int result = 2147483647;
 
-struct info{
-    int x;
-    int y;
-    int dir;
-    int type;
-};
-
-
-void go0(int x, int y){
+void chicken_dist(vector<pair<int, int>> &pick){
     
-    for(int j=y; j<w; j++){
-        if(test[x][j] == 6) break;
-        if(test[x][j] == 0) test[x][j] = -1;
+    int total_dist = 0;
+    
+    for(int i=0; i<house.size(); i++){
+        int x = house[i].first;
+        int y = house[i].second;
+        int dist = 2147483647;
+        
+        for(int j=0; j<pick.size(); j++){
+            
+            int nx = pick[j].first;
+            int ny = pick[j].second;
+            int ndist = abs(nx-x) + abs(ny-y);
+            dist = min(dist, ndist);
+        }
+        
+        total_dist += dist;
     }
+    
+    result = min(result, total_dist);
     
 }
 
-void go1(int x, int y){
+void go(int index, vector<pair<int, int>> &pick){
     
-    for(int i=x; i<h; i++){
-        if(test[i][y] == 6) break;
-        if(test[i][y] == 0) test[i][y] = -1;
-    }
-    
-}
+    if(index == max_chicken){
+        
+        if((int)pick.size() == m){
 
-void go2(int x, int y){
-    
-    for(int j=y; j>=0; j--){
-        if(test[x][j] == 6) break;
-        if(test[x][j] == 0) test[x][j] = -1;
-    }
-}
-
-void go3(int x, int y){
-    
-    for(int i=x; i>=0; i--){
-        if(test[i][y] == 6) break;
-        if(test[i][y] == 0) test[i][y] = -1;
-    }
-}
-
-void do_test(vector<info> &cctv){
-    
-    for(int i=0; i<h; i++){
-        for(int j=0; j<w; j++){
-            test[i][j] = d[i][j];
+            chicken_dist(pick);
         }
-    }
-    
-    for(int k=0; k<cctv.size(); k++){
-        
-        if(cctv[k].type == 1){
-            if(cctv[k].dir == 0) go0(cctv[k].x, cctv[k].y);
-            if(cctv[k].dir == 1) go1(cctv[k].x, cctv[k].y);
-            if(cctv[k].dir == 2) go2(cctv[k].x, cctv[k].y);
-            if(cctv[k].dir == 3) go3(cctv[k].x, cctv[k].y);
-        }
-        
-        if(cctv[k].type == 2){
-            
-            if(cctv[k].dir == 0 || cctv[k].dir == 2){
-                go0(cctv[k].x, cctv[k].y);
-                go2(cctv[k].x, cctv[k].y);
-            }
-            
-            if(cctv[k].dir == 1 || cctv[k].dir == 3){
-                go1(cctv[k].x, cctv[k].y);
-                go3(cctv[k].x, cctv[k].y);
-            }
-        }
-        
-        if(cctv[k].type == 3){
-            
-            if(cctv[k].dir == 0){
-                go2(cctv[k].x, cctv[k].y);
-                go3(cctv[k].x, cctv[k].y);
-            }
-            
-            if(cctv[k].dir == 1){
-                go0(cctv[k].x, cctv[k].y);
-                go3(cctv[k].x, cctv[k].y);
-            }
-            
-            if(cctv[k].dir == 2){
-                go0(cctv[k].x, cctv[k].y);
-                go1(cctv[k].x, cctv[k].y);
-            }
-            
-            if(cctv[k].dir == 3){
-                go1(cctv[k].x, cctv[k].y);
-                go2(cctv[k].x, cctv[k].y);
-            }
-        }
-        
-        if(cctv[k].type == 4){
-            
-            if(cctv[k].dir == 0){
-                go0(cctv[k].x, cctv[k].y);
-                go1(cctv[k].x, cctv[k].y);
-                go2(cctv[k].x, cctv[k].y);
-            }
-            
-            if(cctv[k].dir == 1){
-                go1(cctv[k].x, cctv[k].y);
-                go2(cctv[k].x, cctv[k].y);
-                go3(cctv[k].x, cctv[k].y);
-            }
-            
-            if(cctv[k].dir == 2){
-                go0(cctv[k].x, cctv[k].y);
-                go2(cctv[k].x, cctv[k].y);
-                go3(cctv[k].x, cctv[k].y);
-            }
-            
-            if(cctv[k].dir == 3){
-                go0(cctv[k].x, cctv[k].y);
-                go1(cctv[k].x, cctv[k].y);
-                go3(cctv[k].x, cctv[k].y);
-            }
-        }
-        
-        if(cctv[k].type == 5){
-            go0(cctv[k].x, cctv[k].y);
-            go1(cctv[k].x, cctv[k].y);
-            go2(cctv[k].x, cctv[k].y);
-            go3(cctv[k].x, cctv[k].y);
-        }
-        
-    }
-    
-    int cnt = 0;
-    for(int i=0; i<h; i++){
-        for(int j=0; j<w; j++){
-            if(test[i][j] == 0) cnt++;
-        }
-    }
-    
-    result = min(result, cnt);
-}
-
-void go(int index, vector<info> &cctv){
-    
-    if(index == cctv_size){
-        
-        do_test(cctv);
-        
         return;
     }
     
-    cctv[index].dir = 0;
-    go(index+1, cctv);
-    
-    cctv[index].dir = 1;
-    go(index+1, cctv);
-    
-    cctv[index].dir = 2;
-    go(index+1, cctv);
-    
-    cctv[index].dir = 3;
-    go(index+1, cctv);
+    //치킨집 고름
+    pick.push_back(make_pair(chicken[index].first, chicken[index].second));
+    go(index+1, pick);
+    pick.pop_back();
+
+    // 치킨집 안고름
+    go(index+1, pick);
 }
 
-int main(int argc, char** argv)
-{
-    result = 99999999;
-    cin >> h >> w;
-        
-    vector<info> cctv;
-        
-    for(int i=0; i<h; i++){
-        for(int j=0; j<w; j++){
+int main(){
+    
+    cin >> n >> m;
+    
+    
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
             cin >> d[i][j];
-            if(d[i][j] != 0 && d[i][j] != 6){
-                    cctv.push_back({i, j, 0, d[i][j]});
-            }
+            if(d[i][j] == 1) house.push_back(make_pair(i, j));
+            if(d[i][j] == 2) chicken.push_back(make_pair(i, j));
         }
     }
-        
-    cctv_size = (int)cctv.size();
-    go(0, cctv);
-        
+    
+    max_chicken = (int)chicken.size();
+    
+    vector<pair<int, int>> pick;
+    go(0, pick);
+    
     cout << result << endl;
+    
 }
