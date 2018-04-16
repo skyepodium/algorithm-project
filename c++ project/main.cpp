0ab2b0032093bfb31a7c5212ac834a57e52b07a0
김정윤 <skyepodium@gmail.com>
2018-04-17 02:41:35 +0900

    baekjoon 15651

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,13 +1,9 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
 int n, m;
-
-vector<int> num;
-bool check[10];
 
 void go(int cnt, vector<int> &pick){
     
@@ -20,15 +16,12 @@ void go(int cnt, vector<int> &pick){
         return;
     }
     
-    for(int i=0; i<n; i++){
+    for(int i=1; i<=n; i++){
         
-        if(check[i] == false){
-            check[i] = true;
-            pick.push_back(num[i]);
-            go(cnt+1, pick);
-            check[i] = false;
-            pick.pop_back();
-        }
+        pick.push_back(i);
+        go(cnt+1, pick);
+        pick.pop_back();
+        
     }
     
 }
@@ -36,14 +29,6 @@ void go(int cnt, vector<int> &pick){
 int main(){
     
     scanf("%d %d", &n, &m);
-    
-    for(int i=0; i<n; i++){
-        int a;
-        cin >> a;
-        num.push_back(a);
-    }
-    
-    sort(num.begin(), num.end());
     
     vector<int> pick;
     go(0, pick);
