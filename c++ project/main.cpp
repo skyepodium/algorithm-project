f34e5eb223a7bb6ca48bb69453964b4fc453b07b
김정윤 <skyepodium@gmail.com>
2018-04-20 16:52:09 +0900

    baekjoon 1965 bottom-up

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -4,31 +4,12 @@
 using namespace std;
 
 //시간 복잡도: O(n^2)
-//공간 복잡도: O(n^2)
-//사용한 알고리즘: 동적 계획법, Top-Down
-//사용한 자료구조: 2차원 배열
+//공간 복잡도: O(n)
+//사용한 알고리즘: 동적 계획법, Bottom-Up
+//사용한 자료구조: 1차원 배열
 
-int a[501][501];
-int d[501][501];
-
-int go(int i, int j){
-    
-    if(i<1){
-        d[0][0] = a[0][0];
-        return d[i][j];
-    }
-    
-    if(d[i][j] > 0){
-        return d[i][j];
-    }
-    
-    if(j==0) d[i][j] = a[i][j] + go(i-1, j);
-    else if(j==i) d[i][j] = a[i][j] + go(i-1, j-1);
-    else d[i][j] = a[i][j] + max(go(i-1, j-1), go(i-1, j));
-    
-    return d[i][j];
-}
-
+int a[1001];
+int d[1001];
 
 int main(){
     
@@ -36,15 +17,23 @@ int main(){
     cin >> n;
     
     for(int i=0; i<n; i++){
-        for(int j=0; j<=i; j++){
-            cin >> a[i][j];
+        cin >> a[i];
+    }
+    
+    for(int i=0; i<n; i++){
+        d[i] = 1;
+        int max_val = 0;
+        for(int j=0; j<i; j++){
+            
+            if(a[j] < a[i]){
+                max_val = max(d[j], max_val);
+            }
         }
+        d[i] = max_val + 1;
+        
     }
     
-    int result = 0;
-    for(int j=0; j<n; j++){
-        result = max(result, go(n-1, j));
-    }
+    cout << *max_element(d, d+n) <<endl;
     
-    cout << result << endl;
 }
+
