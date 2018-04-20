eb1727e94a0a3bc85baf37d17438862d90b78f32
김정윤 <skyepodium@gmail.com>
2018-04-20 17:27:06 +0900

    baekjoon 1965 Top-Down

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -5,11 +5,29 @@ using namespace std;
 
 //시간 복잡도: O(n^2)
 //공간 복잡도: O(n)
-//사용한 알고리즘: 동적 계획법, Bottom-Up
+//사용한 알고리즘: 동적 계획법, Top-Down
 //사용한 자료구조: 1차원 배열
 
 int a[1001];
 int d[1001];
+
+int go(int i){
+    
+    if(d[i] > 1){
+        return d[i];
+    }
+    
+    int max_val = 0;
+    for(int j=0; j<i; j++){
+        if(a[j] < a[i]){
+            max_val = max(max_val, go(j));
+        }
+    }
+    
+    d[i] = max_val + 1;
+
+    return d[i];
+}
 
 int main(){
     
@@ -18,22 +36,14 @@ int main(){
     
     for(int i=0; i<n; i++){
         cin >> a[i];
-    }
-    
-    for(int i=0; i<n; i++){
         d[i] = 1;
-        int max_val = 0;
-        for(int j=0; j<i; j++){
-            
-            if(a[j] < a[i]){
-                max_val = max(d[j], max_val);
-            }
-        }
-        d[i] = max_val + 1;
-        
     }
     
-    cout << *max_element(d, d+n) <<endl;
+    int result = 0;
+    for(int i=0; i<n; i++){
+        result = max(result, go(i));
+    }
     
+    cout << result << endl;
 }
 
