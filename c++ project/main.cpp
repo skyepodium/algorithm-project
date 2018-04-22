a659470b5032aa7fe01036bd09e1d67531cc7f20
김정윤 <skyepodium@gmail.com>
2018-04-23 02:41:50 +0900

    baekjoon 15701

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,12 +1,25 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
-#include <queue>
 
 using namespace std;
 
-
 int main(){
-
+    
+    int n;
+    cin >> n;
+    
+    int cnt = 0;
+    bool is_check = false;
+    for(int i=1; i*i<=n; i++){
+        
+        if(n%i == 0) cnt++;
+        if(i*i == n) is_check = true;
+        
+    }
+    
+    int result = 0;
+    if(is_check == true) result = (cnt-1) * 2 + 1;
+    else result = cnt*2;
+    
+    cout << result << endl;
     
 }
