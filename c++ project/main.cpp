3bcea72a55499c94873b25b82990b244d11ea1ba
김정윤 <skyepodium@gmail.com>
2018-04-23 02:47:51 +0900

    baekjoon 15702

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,25 +1,45 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
+int d[100];
+
+int person = 1;
+int score = -1;
+
 int main(){
     
-    int n;
-    cin >> n;
+    int n, m;
+    cin >> n >> m;
     
-    int cnt = 0;
-    bool is_check = false;
-    for(int i=1; i*i<=n; i++){
+    for(int i=0; i<n; i++){
+        cin >> d[i];
+    }
+    
+    for(int k=0; k<m; k++){
+        int sum = 0;
+        int num = 0;
+        cin >> num;
         
-        if(n%i == 0) cnt++;
-        if(i*i == n) is_check = true;
+        for(int i=0; i<n; i++){
+            char c;
+            cin >> c;
+            
+            if(c== 'O'){
+                sum = sum + d[i];
+            }
+        }
+        
+        if(sum > score){
+            person = num;
+            score = sum;
+        }else if(sum == score){
+            person = min(person, num);
+        }
         
     }
     
-    int result = 0;
-    if(is_check == true) result = (cnt-1) * 2 + 1;
-    else result = cnt*2;
-    
-    cout << result << endl;
+    cout << person << " " << score << endl;
     
 }
