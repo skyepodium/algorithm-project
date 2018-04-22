c10006fd1c9a026461873017a65393ba68703663
김정윤 <skyepodium@gmail.com>
2018-04-23 02:51:07 +0900

    baekjoon 15700

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,45 +1,13 @@
 #include <iostream>
-#include <algorithm>
 
 using namespace std;
 
-int d[100];
-
-int person = 1;
-int score = -1;
-
 int main(){
     
-    int n, m;
-    cin >> n >> m;
+    long long int a, b;
+    cin >> a >> b;
     
-    for(int i=0; i<n; i++){
-        cin >> d[i];
-    }
-    
-    for(int k=0; k<m; k++){
-        int sum = 0;
-        int num = 0;
-        cin >> num;
-        
-        for(int i=0; i<n; i++){
-            char c;
-            cin >> c;
-            
-            if(c== 'O'){
-                sum = sum + d[i];
-            }
-        }
-        
-        if(sum > score){
-            person = num;
-            score = sum;
-        }else if(sum == score){
-            person = min(person, num);
-        }
-        
-    }
-    
-    cout << person << " " << score << endl;
+    cout << (a*b)/2 << endl;
     
 }
+
