b6ab2fa4eb69521a5dc7c0f4e03dd3c57e4772a8
김정윤 <skyepodium@gmail.com>
2018-04-23 17:41:48 +0900

    baekjoon 11651

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,13 +1,39 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int main(){
+struct info{
+    int x;
+    int y;
+};
+
+bool cmp(const info &a, const info &b){
     
-    long long int a, b;
-    cin >> a >> b;
-    
-    cout << (a*b)/2 << endl;
-    
+    if(a.y == b.y){
+        return a.x < b.x;
+    }else{
+        return a.y < b.y;
+    }
 }
 
+int main(){
+    
+    int n;
+    cin >> n;
+    
+    vector<info> v;
+    for(int i=0; i<n; i++){
+        int x, y;
+        scanf("%d %d", &x, &y);
+        v.push_back({x, y});
+    }
+    
+    sort(v.begin(), v.end(), cmp);
+    
+    for(int i=0; i<v.size(); i++){
+        printf("%d %d\n", v[i].x, v[i].y);
+    }
+    
+}
