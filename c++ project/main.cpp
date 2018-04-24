8280b200e21f9c2ce34e7018efa9c912c1bc0ca4
김정윤 <skyepodium@gmail.com>
2018-04-24 18:38:59 +0900

    baekjoon 9465 스티커 top-down helped by baekjoon

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,39 +1,56 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
 
 using namespace std;
 
-struct info{
-    int x;
-    int y;
-};
+//시간 복잡도: O(n)
+//공간 복잡도: O(n)
+//사용한 알고리즘: 동적 계획법 Top-down
+//사용한 자료구조: 2차원 배열
 
-bool cmp(const info &a, const info &b){
+int d[2][100001];
+int a[2][100001];
+
+int go(int i, int j){
     
-    if(a.y == b.y){
-        return a.x < b.x;
-    }else{
-        return a.y < b.y;
+    if(j<2){
+        return d[i][j];
     }
+    
+    if(d[i][j] >= 0){
+        return d[i][j];
+    }
+    
+    if(i==0) d[0][j] = max(go(1, j-1), go(1, j-2)) + a[0][j];
+    else d[1][j] = max(go(0, j-1), go(0, j-2)) + a[1][j];
+    
+    return d[i][j];
 }
 
 int main(){
     
-    int n;
-    cin >> n;
+    int t;
+    cin >> t;
     
-    vector<info> v;
-    for(int i=0; i<n; i++){
-        int x, y;
-        scanf("%d %d", &x, &y);
-        v.push_back({x, y});
+    while(t--){
+        int n;
+        cin >> n;
+        for(int i=0; i<2; i++){
+            for(int j=0; j<n; j++){
+                scanf("%d", &a[i][j]);
+                d[i][j] = -1;
+            }
+        }
+        
+        d[0][0] = a[0][0];
+        d[1][0] = a[1][0];
+        d[0][1] = d[1][0] + a[0][1];
+        d[1][1] = d[0][0] + a[1][1];
+        
+        printf("%d\n", max(go(0, n-1), go(1, n-1)));
+        
     }
     
-    sort(v.begin(), v.end(), cmp);
-    
-    for(int i=0; i<v.size(); i++){
-        printf("%d %d\n", v[i].x, v[i].y);
-    }
     
 }
+
