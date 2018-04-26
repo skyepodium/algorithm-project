5ae0dc05a6ffd9637a0215df25358346dee5e63c
김정윤 <skyepodium@gmail.com>
2018-04-26 16:49:04 +0900

    04.26 baekjoon 15701 순서쌍 주석 포함

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,95 +1,39 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 
 using namespace std;
 
-//시간 복잡도: O(n)
-//공간 복잡도: O(n)
-//사용한 알고리즘: 동적 계획법 Top-down
-//사용한 자료구조: 2차원 배열
-
-int d[9][9];
-bool check[9][9];
-
-void check_sudoku(){
-    bool is_possible = true;
-    //가로 검사
-    for(int i=0; i<9; i++){
-        int origin = d[i][0];
-        for(int j=1; j<9; j++){
-            
-            if(d[i][j] == origin){
-                is_possible = false;
-                break;
-            }
-        }
-    }
-    
-    //세로검사
-    for(int j=0; j<9; j++){
-        int origin = d[0][j];
-        for(int i=1; i<9; i++){
-            
-            if(d[i][j] == origin){
-                is_possible = false;
-                break;
-            }
-        }
-    }
-
-    if(is_possible == true){
-        for(int i=0; i<9; i++){
-            for(int j=0; j<9; j++){
-                cout << d[i][j] <<" ";
-            }
-            cout << endl;
-        }
-    }
-    
-    
-}
-
-void go(int index, vector<pair<int, int>> &v){
-    
-    if(index == (int)v.size()){
-        
-        check_sudoku();
-        
-        return;
-    }
-    
-    for(int i=1; i<=9; i++){
-        
-        int x = v[index].first;
-        int y = v[index].second;
-        if(check[x][y] == false){
-            
-            d[x][y] = i;
-            check[x][y] = true;
-            go(index+1, v);
-            d[x][y] = 0;
-            check[x][y] = false;
-            
-        }
-        
-    }
-    
-}
+//시간 복잡도: O(루트 n)
+//공간 복잡도: 0
+//사용한 알고리즘: 선형 탐색
+//사용한 자료구조: 없음
 
 int main(){
     
-    //2차원 배열에 입력 받고
+    int n;
+    cin >> n;
     
-    vector<pair<int, int>> v;
-    for(int i=0; i<9; i++){
-        for(int j=0; j<9; j++){
-            cin >> d[i][j];
-            if(d[i][j] > 0) check[i][j] = true;
-            else if(d[i][j] == 0) v.push_back(make_pair(i, j));
-        }
+    int cnt = 0;
+    bool is_check = false;
+    
+    //루트n까지만 for문을 돌린다.
+    for(int i=1; i*i<=n; i++){
+        
+        //나누어서 0이되는 수가 있으면 갯수를 1증가시킨다.
+        if(n%i == 0) cnt++;
+        
+        //제곱해서 n이 되는 수가 있으면 체크해준다.
+        if(i*i == n) is_check = true;
+        
     }
     
-    go(0, v);
+    int result = 0;
+    //제곱해서 n이 되는 수가 있으면
+    if(is_check == true) result = (cnt-1) * 2 + 1;
+    
+    //없으면
+    else result = cnt*2;
+    
+    cout << result << endl;
     
 }
+
