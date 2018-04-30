1531737e22743695006ad3c3ffec1c0a77c3de8b
김정윤 <skyepodium@gmail.com>
2018-04-30 23:21:22 +0900

    04.30

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,17 +1,61 @@
 #include <iostream>
+#include <cstring>
+#include <queue>
 
 using namespace std;
+
+bool check[9];
+
+
+bool check_num(int a, int b){
+
+    bool is_possible = true;
+    
+    while(a>0){
+        
+        if(check[a%10] == false){
+            check[a%10] = true;
+        }else{
+            is_possible = false;
+            break;
+        }
+        a = a/10;
+    }
+
+    
+    if(is_possible == true){
+        while(b>0){
+            if(check[b%10] == false){
+                check[b%10] = true;
+            }else{
+                is_possible = false;
+                break;
+            }
+            b = b/10;
+        }
+    }
+    
+    if(is_possible == true) return true;
+    else return false;
+}
 
 int main(){
     int n;
     cin >> n;
     
-    n = n - 1;
-    
-    for(int i=1; i<=10101; i++){
-        if(i*(i+1) == n){
-            cout << i << endl;
-            break;
+    bool is_possible = false;
+    for(int i=n; i>=1; i--){
+        
+        if(n-i > 0){
+            memset(check, false, sizeof(check));
+            if(check_num(i, n-i) == true){
+                is_possible = true;
+                cout << i << " + " << n-i << endl;
+                break;
+            }
         }
     }
+    
+    if(is_possible == false) cout << -1 << endl;
+    
 }
