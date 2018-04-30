587bd24717a4b672d423ee13dfcb087bae7e84d7
김정윤 <skyepodium@gmail.com>
2018-04-30 16:47:20 +0900

    baekjoon 15667

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -6,8 +6,12 @@ int main(){
     int n;
     cin >> n;
     
-    if(n == 0) cout << "YONSEI" << endl;
-    else cout << "Leading the Way to the Future" << endl;
+    n = n - 1;
     
-    
+    for(int i=1; i<=10101; i++){
+        if(i*(i+1) == n){
+            cout << i << endl;
+            break;
+        }
+    }
 }
