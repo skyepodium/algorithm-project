ebd5d4185167c3226c6874c980af92f746178eca
김정윤 <skyepodium@gmail.com>
2018-05-02 23:00:16 +0900

    05.02

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,62 +1,31 @@
 #include <iostream>
-#include <cstring>
-#include <queue>
-#include <vector>
 
 using namespace std;
 
-bool check[9];
+//시간 복잡도: O(n^3)
+//공간 복잡도: O(n^2)
+//사용한 알고리즘: 동적계획법 Bottom Up
+//사용한 자료구조: 2차원 배열
 
-
-bool check_num(int a, int b){
-
-    bool is_possible = true;
-    
-    while(a>0){
-        
-        if(check[a%10] == false){
-            check[a%10] = true;
-        }else{
-            is_possible = false;
-            break;
-        }
-        a = a/10;
-    }
-
-    
-    if(is_possible == true){
-        while(b>0){
-            if(check[b%10] == false){
-                check[b%10] = true;
-            }else{
-                is_possible = false;
-                break;
-            }
-            b = b/10;
-        }
-    }
-    
-    if(is_possible == true) return true;
-    else return false;
-}
+int d[201][201];
+int MOD = 1000000000;
 
 int main(){
-    int n;
-    cin >> n;
     
-    bool is_possible = false;
-    for(int i=n; i>=1; i--){
-        
-        if(n-i > 0){
-            memset(check, false, sizeof(check));
-            if(check_num(i, n-i) == true){
-                is_possible = true;
-                cout << i << " + " << n-i << endl;
-                break;
+    int n, k;
+    cin >> n >> k;
+    
+    for(int i=0; i<=n; i++){
+        d[i][1] = 1;
+        for(int j=0; j<=n; j++){
+            for(int h=1; h<=k; h++){
+                if(i-j >= 0){
+                    d[i][h] = (d[i][h]%MOD + d[j][h-1]%MOD)%MOD;
+                    d[i][h] %= MOD;
+                }
             }
         }
     }
     
-    if(is_possible == false) cout << -1 << endl;
-    
+    cout << d[n][k]%MOD << endl;
 }
