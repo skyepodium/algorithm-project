fef095780d58ef07f759dfd6f603cf10b140ee08
김정윤 <skyepodium@gmail.com>
2018-05-08 15:35:58 +0900

    05.08 sorting practice

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -2,33 +2,23 @@
 
 using namespace std;
 
-//시간 복잡도: O(n^2)
-//공간 복잡도: O(1)
-//사용한 알고리즘: 거품 정렬(bubble sort)
-//사용한 자료구조: 없음
-
 int main(){
+ 
+    int d[5] = {5, 2, 3, 4, 1};
     
-    int d[10] = {3, 4, 10, 1, 2, 9, 6, 5, 7, 8};
-    
-    for(int i=0; i<10; i++){
-        
-        for(int j=1; j<10 - i; j++){
-            if(d[j] < d[j-1]){
-
+    for(int i=0; i<5; i++){
+        for(int j=1; j<5-i; j++){
+            if(d[j-1] > d[j]){
                 int temp;
                 temp = d[j-1];
                 d[j-1] = d[j];
                 d[j] = temp;
-                
             }
         }
     }
     
-    for(int i=0; i<10; i++){
-        cout << d[i] << " ";
-    }
-    cout << endl;
     
+    for(int i=0; i<5; i++){
+        cout << d[i] << endl;
+    }
 }
-
