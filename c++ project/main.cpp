00ffd3ca956891d132d40baec3caae6b5d115c9f
김정윤 <skyepodium@gmail.com>
2018-05-09 18:21:22 +0900

    05.09

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,24 +1,46 @@
 #include <iostream>
+#include <algorithm>
+#include <stdlib.h>
 
 using namespace std;
 
+int ar1[100001];
+int ar2[100001];
+
+bool bsearch(int start, int end, int key){
+    
+    if(start > end){
+        return false;
+    }
+    
+    int mid = (start + end) / 2;
+    
+    if(ar1[mid] == key) return true;
+    else if(ar1[mid] < key) return bsearch(mid+1, end, key);
+    else return bsearch(start, mid-1, key);
+    
+}
+
+
+
 int main(){
- 
-    int d[5] = {5, 2, 3, 4, 1};
     
-    for(int i=0; i<5; i++){
-        for(int j=1; j<5-i; j++){
-            if(d[j-1] > d[j]){
-                int temp;
-                temp = d[j-1];
-                d[j-1] = d[j];
-                d[j] = temp;
-            }
-        }
+    int n;
+    cin >> n;
+    
+    for(int i=0; i<n; i++){
+        cin >> ar1[i];
     }
     
+    sort(ar1, ar1+n);
     
-    for(int i=0; i<5; i++){
-        cout << d[i] << endl;
+    int m;
+    cin >> m;
+    
+    for(int i=0; i<m; i++){
+        int num;
+        cin >> num;
+        cout << bsearch(0, n-1,  num) << endl;
     }
+    
 }
