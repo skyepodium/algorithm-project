5a7e58e437bca92c02e8d804a946c8a2a1a52fcd
김정윤 <skyepodium@gmail.com>
2018-05-12 18:42:56 +0900

    05.12 baekjoon 9084

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,46 +1,41 @@
 #include <iostream>
-#include <algorithm>
-#include <queue>
 
 using namespace std;
 
-int ar1[100001];
-int ar2[100001];
+//시간 복잡도: O(n)
+//공간 복잡도: O(n+m)
+//사용한 알고리즘: 동적 계획법 Bottom up
+//사용한 자료구조: 1차원 배열
 
-bool bsearch(int start, int end, int key){
-    
-    if(start > end){
-        return false;
-    }
-    
-    int mid = (start + end) / 2;
-    
-    if(ar1[mid] == key) return true;
-    else if(ar1[mid] < key) return bsearch(mid+1, end, key);
-    else return bsearch(start, mid-1, key);
-    
-}
-
-
+int d[10001];
+int a[21];
 
 int main(){
     
-    int n;
-    cin >> n;
+    int t;
+    cin >> t;
     
-    for(int i=0; i<n; i++){
-        cin >> ar1[i];
-    }
-    
-    sort(ar1, ar1+n);
-    
-    int m;
-    cin >> m;
-    
-    for(int i=0; i<m; i++){
-        int num;
-        cin >> num;
-        cout << bsearch(0, n-1,  num) << endl;
+    while(t--){
+        int n, m;
+        cin >> n;
+        
+        for(int i=1; i<=n; i++){
+            cin >> a[i];
+        }
+        
+        cin >> m;
+        
+        d[0] = 1;
+        for(int i=1; i<=n; i++){
+            for(int j=a[i]; j<=m; j++){
+                d[j] += d[j-a[i]];
+            }
+        }
+        cout << d[m] << endl;
+        
+        for(int i=0; i<=m; i++){
+            d[i] = 0;
+        }
     }
     
 }
