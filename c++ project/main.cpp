5ac2e68b00fe9fb8a80d20f12e3bcabd5d33c5ea
김정윤 <skyepodium@gmail.com>
2018-05-15 20:47:46 +0900

    swea 1989

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,25 +1,6 @@
 #include <iostream>
-#include <algorithm>
-
+#include <string>
 using namespace std;
-
-int d[16][16];
-
-int result = 0;
-int n, m;
-
-void cal_sum(int x, int y){
-    
-    int sum_result = 0;
-    for(int i=x; i<x+m; i++){
-        for(int j=y; j<y+m; j++){
-            sum_result += d[i][j];
-        }
-    }
-    
-    result = max(result, sum_result);
-}
-
 
 int main(int argc, char** argv)
 {
@@ -30,25 +11,25 @@ int main(int argc, char** argv)
     
     for(test_case = 1; test_case <= T; ++test_case)
     {
-        result = 0;
+        string word;
+        cin >> word;
+        
+        bool is_true = true;
+        
+        int size = (int)word.size();
 
-        cin >> n >> m;
-        
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                cin >> d[i][j];
+        for(int i=0; i<size/2; i++){
+            
+            if(word[i] != word[size-1-i]){
+                is_true = false;
+                break;
             }
+            
         }
         
-        for(int i=0; i<= n-m; i++){
-            for(int j=0; j<= n-m; j++){
-                
-                cal_sum(i, j);
-                
-            }
-        }
         
-        cout << "#" << test_case <<" " << result << endl;
+        cout << "#" << test_case << " " << is_true << endl;
+        
         
     }
     return 0;
