7f9bc03c1b3dbfd015a3c0725a4c314239d9e4d5
김정윤 <skyepodium@gmail.com>
2018-05-15 17:53:45 +0900

    baekjoon 15735

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,45 +1,34 @@
 #include <iostream>
-#include <algorithm>
-
-int a[5] = {100, 50, 30, 10, 1};
-int d[5000];
 
 using namespace std;
 
 //시간 복잡도: O(n)
 //공간 복잡도: O(n)
-//사용한 알고리즘: 동적 계획법, Bottom-Up
+//사용한 알고리즘: 선형 탐색
 //사용한 자료구조: 1차원 배열
 
-int go(int i){
-    
-    if(i<1){
-        d[i] = 0;
-        return d[i];
-    }
-    
-    if(d[i] > 0){
-        return d[i];
-    }
-    
-    int min_val = 5000;
-    for(int j=0; j<5; j++){
-        if(i-a[j] >= 0){
-            min_val = min(min_val, go(i-a[j]) + 1);
-        }
-    }
-    d[i] = min_val;
-    
-    
-    return d[i];
-}
+long long int a[10001];
 
 int main(){
-    
+
     int n;
     cin >> n;
+
+    a[1] = 1;
+    for(int i=2; i<=n; i++){
+        a[i] = a[i-1] + i;
+    }
+
+    long long int up_sum = 0;
+    for(int i=1; i<=n; i++){
+        up_sum = up_sum + a[i];
+    }
     
-    cout << go(n) << endl;
+    long long int down_sum = 0;
+    for(int i=n-1; i>=1; i=i-2){
+        down_sum = down_sum + a[i];
+    }
+    
+    cout << up_sum + down_sum << endl;
     
 }
-
