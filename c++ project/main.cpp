ec72278d388ec8776162d7097bb6ca38444397b1
김정윤 <skyepodium@gmail.com>
2018-05-16 18:01:50 +0900

    swea 2046

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -2,23 +2,14 @@
 
 using namespace std;
 
-int cal_result(int a){
-    
-    int result = 0;
-    
-    while(a > 0){
-        
-        result = result + a%10;
-        a = a / 10;
-    }
-    
-    return result;
-}
-
 int main(){
-    
+
     int n;
     cin >> n;
     
-    cout << cal_result(n) << endl;
+    for(int i=0; i<n; i++){
+        cout << '#';
+    }
+    cout << endl;
+    
 }
