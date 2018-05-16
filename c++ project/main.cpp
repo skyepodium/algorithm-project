d903af4de20bdbc7e78e8d470c7a6c67b972e6dc
김정윤 <skyepodium@gmail.com>
2018-05-16 15:52:41 +0900

    baekjoon 14226

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,27 +1,65 @@
 #include <iostream>
-#include <string>
+#include <queue>
+#include <algorithm>
 
 using namespace std;
 
-int main(){
+//시간 복잡도: O(n^2)
+//공간 복잡도: O(n^2)
+//사용한 알고리즘: BFS
+//사용한 자료구조: 2차원배열, queue
+
+int dist[1001][1001];
+bool check[1001][1001];
+int s;
+
+void bfs(int s, int c){
+    check[s][c] = true;
+    queue<pair<int, int>> q;
+    q.push(make_pair(s, c));
     
-    string word;
-    cin >> word;
-    
-    bool is_true = true;
-    
-    int size = (int)word.size();
-    for(int i=0; i<size/2; i++){
-        if(word[i] != word[size-i-1]){
-            is_true = false;
-            break;
+    while(!q.empty()){
+        s = q.front().first;
+        c = q.front().second;
+        q.pop();
+        
+        if(check[s][s] == false && s>0){
+            check[s][s] = true;
+            dist[s][s] = dist[s][c] + 1;
+            q.push(make_pair(s, s));
         }
+        
+        if(s+c<=1000 && check[s+c][c] == false && c>0){
+            check[s+c][c] = true;
+            dist[s+c][c] = dist[s][c] + 1;
+            q.push(make_pair(s+c, c));
+        }
+        
+        if(s-1 >= 0 && check[s-1][c] == false){
+            check[s-1][c] = true;
+            dist[s-1][c] = dist[s][c] + 1;
+            q.push(make_pair(s-1, c));
+        }
+        
+        
     }
     
-    if(is_true) cout << "true" << endl;
-    else cout << "false" << endl;
     
 }
 
 
-
+int main(){
+    
+    cin >> s;
+    bfs(1, 0);
+    
+    int min_day = 1000;
+    
+    for(int i=0; i<1000; i++){
+        if(dist[s][i] > 0){
+            min_day = min(min_day, dist[s][i]);
+        }
+    }
+    
+    cout << min_day << endl;
+}
