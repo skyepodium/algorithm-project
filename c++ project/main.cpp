3a9fba112dd94842dd2167a173b6888a7c76c260
김정윤 <skyepodium@gmail.com>
2018-05-16 18:07:02 +0900

    swea 2027

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -2,23 +2,12 @@
 
 using namespace std;
 
-int main(int argc, char** argv)
-{
-    int test_case;
-    int T;
+int main(){
     
-    cin>>T;
+    cout << "#++++" << endl;
+    cout << "+#+++" << endl;
+    cout << "++#++" << endl;
+    cout << "+++#+" << endl;
+    cout << "++++#" << endl;
     
-    for(test_case = 1; test_case <= T; ++test_case)
-    {
-        int a, b;
-        cin >> a >> b;
-        
-        cout << "#" << test_case << " " << a/b << " " << a%b << endl;
-        
-        
-        
-        
-    }
-    return 0;
 }
