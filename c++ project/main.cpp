f1573acc1f38672f337bde503e7b0a6582c951bd
김정윤 <skyepodium@gmail.com>
2018-05-21 20:17:04 +0900

    swea 2056

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,17 +1,96 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-int main(){
+int main(int argc, char** argv)
+{
+    int test_case;
+    int T;
     
-    int a, b;
-    cin >> a >> b;
+    cin>>T;
     
-    if(a==1 && b==2) cout << "B" << endl;
-    else if(a==1 && b==3) cout << "A" << endl;
-    else if(a==2 && b==1) cout << "A" << endl;
-    else if(a==2 && b==3) cout << "B" << endl;
-    else if(a==3 && b==1) cout << "B" << endl;
-    else cout << "A" << endl;
-    
+    for(test_case = 1; test_case <= T; ++test_case)
+    {
+        string num;
+        cin >> num;
+        
+        bool is_possible = true;
+
+        
+        int month = (num[4] - 48) * 10 + (num[5] - 48);
+        
+        if(month <= 0 || month >= 13) is_possible = false;
+        
+        int day = (num[6] - 48) * 10 + (num[7] - 48);
+        
+        if(month == 1){
+            if(day<1 || day > 31) is_possible = false;
+        }
+
+        if(month == 2){
+            if(day<1 || day > 28) is_possible = false;
+        }
+
+        if(month == 3){
+            if(day<1 || day > 31) is_possible = false;
+        }
+
+        if(month == 4){
+            if(day<1 || day > 30) is_possible = false;
+        }
+
+        if(month == 5){
+            if(day<1 || day > 31) is_possible = false;
+        }
+
+        if(month == 6){
+            if(day<1 || day > 30) is_possible = false;
+        }
+
+        if(month == 7){
+            if(day<1 || day > 31) is_possible = false;
+        }
+
+        if(month == 8){
+            if(day<1 || day > 31) is_possible = false;
+        }
+
+        if(month == 9){
+            if(day<1 || day > 30) is_possible = false;
+        }
+
+        if(month == 10){
+            if(day<1 || day > 31) is_possible = false;
+        }
+
+        if(month == 11){
+            if(day<1 || day > 30) is_possible = false;
+        }
+
+        if(month == 12){
+            if(day<1 || day > 31) is_possible = false;
+        }
+
+        cout << "#" << test_case << " ";
+        if(is_possible){
+            for(int i=0; i<4; i++){
+                cout << num[i];
+            }
+            cout << "/";
+            for(int i=4; i<6; i++){
+                cout << num[i];
+            }
+            cout << "/";
+            for(int i=6; i<8; i++){
+                cout << num[i];
+            }
+            cout << endl;
+        }else{
+            cout << -1 << endl;
+        }
+        
+        
+    }
+    return 0;
 }
