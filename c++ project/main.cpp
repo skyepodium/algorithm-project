ac8f8384dece7ea162f22e8e49a2cd80b7f55fb8
김정윤 <skyepodium@gmail.com>
2018-05-30 15:56:13 +0900

    05.30

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,62 +1,24 @@
 #include <iostream>
-#include <queue>
+#include <vector>
 
 using namespace std;
 
-//시간 복잡도: O(n)
-//공간 복잡도: O(n)
-//사용한 알고리즘: BFS
-//사용한 자료구조: 1차원 배열
-
-int height = 0;
-int start_point = 0, end_point = 0;
-int top_dist = 0;
-int down_dist = 0;
-
-int d[1000001];
-bool check[1000001];
-
-void bfs(int start){
-    check[start] = true;
-    queue<int> q;
-    q.push(start);
-    
-    while(!q.empty()){
-        int node = q.front();
-        q.pop();
-        
-        int next;
-        next = node + top_dist;
-        if(check[next] == false && next >0 && next <= height){
-            check[next] = true;
-            d[next] = d[node] + 1;
-            q.push(next);
-        }
-        
-        next = node - down_dist;
-        if(check[next] == false && next >0 && next <= height){
-            check[next] = true;
-            d[next] = d[node] + 1;
-            q.push(next);
-        }
-        
-    }
-}
+int d[201][3];
 
 int main(){
     
-    cin >> height >> start_point >> end_point >> top_dist >> down_dist;
+    int n;
+    cin >> n;
     
-    bfs(start_point);
-    
-    if(start_point == end_point){
-        cout << 0 << endl;
-    }else{
-        if(d[end_point] > 0){
-            cout << d[end_point] << endl;
-        }else{
-            cout << "use the stairs" << endl;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<3; j++){
+            cin >> d[i][j];
         }
     }
     
+    for(int j=0; j<3; j++){
+        for(int i=0; i<n; i++){
+            
+        }
+    }
 }
