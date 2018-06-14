3a51ac3c5775956acc22fbfd6a03b19c07618b50
김정윤 <skyepodium@gmail.com>
2018-06-14 21:31:14 +0900

    swea 4466

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,21 +1,35 @@
 #include <iostream>
-#include <queue>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int main(){
+int main(int argc, char** argv)
+{
+    int test_case;
+    int T;
     
-    int num;
-    cin >> num;
+    cin>>T;
     
-    if(num == 1) cout << 'a' << endl;
-    else if(num == 2) cout << 'b' << endl;
-    else if(num == 3) cout << 'b' << endl;
-    else if(num == 4) cout << 'c' << endl;
-    else if(num == 5) cout << 'd' << endl;
-    else if(num == 6) cout << 'd' << endl;
-    else if(num == 7) cout << 'd' << endl;
-    else cout << 'c' << endl;
-    
-    
+    for(test_case = 1; test_case <= T; ++test_case)
+    {
+        int n, k;
+        cin >> n >> k;
+        
+        vector<int> v(n);
+        for(int i=0; i<n; i++){
+            cin >> v[i];
+        }
+        
+        sort(v.begin(), v.end());
+        
+        int result = 0;
+
+        for(int i=n-1; i>n-1-k; i--){
+            result = result + v[i];
+        }
+        
+        cout << "#" << test_case << " " << result << endl;
+    }
+    return 0;
 }
