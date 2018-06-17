33926981b77f6ced6f9a0cc0e262602e9962c576
김정윤 <skyepodium@gmail.com>
2018-06-17 21:53:07 +0900

    coding party line problem2

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,72 +1,89 @@
 #include <iostream>
-#include <sstream>
 #include <string>
-#include <stack>
+#include <cmath>
 
 using namespace std;
 
-//Implement this class
-//Please use stack only
+int line_size = 0;
 
-stack<int> first;
-stack<int> second;
+void hex_to_decimal(string num);
 
-class Queue {
-public:
-    /*
-     * Description: ENQUEUE가 입력되면, first 스택에 값을 넣는다.
-     * Time complexity:O(1)
-     * Space complexity:O(n)
-     */
-    void enqueue(int v) {
-        first.push(v);
-    }
+void line1();
+void line2();
+void line3();
+void line4();
+void line5();
+
+int main() {
     
-    /*
-     * Description:
-     * Time complexity:O(n^2)
-     * Space complexity:O(n)
-     */
-    int dequeue() {
-        
-        //first 스택의 모든 값을 second 스택으로 거꾸로 옮김
-        while(!first.empty()){
-            int num = first.top();
-            first.pop();
-            second.push(num);
-        }
-        
-        int result = 0;
-        result = second.top();
-        second.pop();
-        
-        //second 스택의 모든 값을 first 스택으로 거꾸로 옮김
-        while(!second.empty()){
-            int num = second.top();
-            second.pop();
-            first.push(num);
-        }
-        
-        return result;
-    }
-};
-
-int main(int argc, const char *argv[]) {
-    Queue queue;
-    string line;
-    getline(cin, line);
-    int count = stoi(line);
-    for (int i = 0; i < count; ++i) {
-        getline(cin, line);
-        stringstream ss(line);
-        string token;
-        getline(ss, token, ' ');
-        if (token == "ENQUEUE") {
-            getline(ss, token, ' ');
-            queue.enqueue(stoi(token));
-        } else if (token == "DEQUEUE") {
-            cout<<queue.dequeue()<<endl;
-        }
-    }
+    line1();
+    line2();
+    line3();
+    line4();
+    line5();
 }
 
+void hex_to_decimal(string num){
+    int decimal = 0;
+    
+    int position = 0;
+    for (int i = (int)num.size() - 1; i >= 0; i--){
+        char ch = num[i];
+        
+        if (ch >= 48 && ch <= 57){
+            decimal += (ch - 48) * pow(16, position);
+        }
+        else if (ch >= 65 && ch <= 70){
+            decimal += (ch - (65 - 10)) * pow(16, position);
+        }
+        else if (ch >= 97 && ch <= 102){
+            decimal += (ch - (97 - 10)) * pow(16, position);
+        }
+        
+        position++;
+    }
+    
+    cout << decimal << endl;
+}
+
+
+void line1(){
+    string s1, s2, s3, s4, s5;
+    cin >> s1 >> s2 >> s3 >> s4 >> s5;
+    cout << s3 << endl;
+    cout << s5 << endl;
+}
+
+void line2(){
+    string s1, s2, s3;
+    cin >> s1 >> s2 >> s3;
+    hex_to_decimal(s3);
+}
+
+
+void line3(){
+    string s1, s2, s3, s4;
+    cin >> s1 >> s2 >> s3 >> s4;
+    cout << s4 << endl;
+    line_size = stoi(s4);
+}
+
+void line4(){
+    string s1, s2, s3, s4, s5;
+    cin >> s1 >> s2 >> s3 >> s4 >> s5;
+}
+
+void line5(){
+    string s1, alpha;
+    int width, height, duration;
+
+    for(int i=0; i<line_size; i++){
+        cin >> s1 >> width >> height >> alpha >> duration;
+        
+        int result = 0;
+        if(alpha == "yes") result = 1;
+        else result = 0;
+        cout << width << "x" << height << ", "<< duration << ", " << result <<endl;
+    }
+    
+}
