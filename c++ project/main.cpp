af109331ce59c8ef352f196f636694cc9cfaf70a
김정윤 <skyepodium@gmail.com>
2018-06-17 22:53:31 +0900

    online coding party NHN entertainment problem3

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,89 +1,44 @@
 #include <iostream>
-#include <string>
-#include <cmath>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int line_size = 0;
+//시간 복잡도: O(n)
+//공간 복잡도: O(n)
+//사용한 알고리즘: 정렬
+//사용한 자료구조: 1차원 벡터
 
-void hex_to_decimal(string num);
-
-void line1();
-void line2();
-void line3();
-void line4();
-void line5();
-
-int main() {
+int main(){
     
-    line1();
-    line2();
-    line3();
-    line4();
-    line5();
-}
-
-void hex_to_decimal(string num){
-    int decimal = 0;
+    long long int diff =  2147483647;
+    long long int a = 0;
+    long long int b = 0;
     
-    int position = 0;
-    for (int i = (int)num.size() - 1; i >= 0; i--){
-        char ch = num[i];
-        
-        if (ch >= 48 && ch <= 57){
-            decimal += (ch - 48) * pow(16, position);
-        }
-        else if (ch >= 65 && ch <= 70){
-            decimal += (ch - (65 - 10)) * pow(16, position);
-        }
-        else if (ch >= 97 && ch <= 102){
-            decimal += (ch - (97 - 10)) * pow(16, position);
-        }
-        
-        position++;
+    int n;
+    cin >> n;
+
+    vector<long long int> v(n);
+    for(int i=0; i<n; i++){
+        cin >> v[i];
     }
     
-    cout << decimal << endl;
-}
-
-
-void line1(){
-    string s1, s2, s3, s4, s5;
-    cin >> s1 >> s2 >> s3 >> s4 >> s5;
-    cout << s3 << endl;
-    cout << s5 << endl;
-}
-
-void line2(){
-    string s1, s2, s3;
-    cin >> s1 >> s2 >> s3;
-    hex_to_decimal(s3);
-}
-
-
-void line3(){
-    string s1, s2, s3, s4;
-    cin >> s1 >> s2 >> s3 >> s4;
-    cout << s4 << endl;
-    line_size = stoi(s4);
-}
-
-void line4(){
-    string s1, s2, s3, s4, s5;
-    cin >> s1 >> s2 >> s3 >> s4 >> s5;
-}
-
-void line5(){
-    string s1, alpha;
-    int width, height, duration;
-
-    for(int i=0; i<line_size; i++){
-        cin >> s1 >> width >> height >> alpha >> duration;
+    sort(v.begin(), v.end());
+    
+    diff = v[1] - v[0];
+    a = v[0];
+    b = v[1];
+    
+    for(int i=1; i<n-1; i++){
+        long long int cal_diff = v[i+1] - v[i];
         
-        int result = 0;
-        if(alpha == "yes") result = 1;
-        else result = 0;
-        cout << width << "x" << height << ", "<< duration << ", " << result <<endl;
+        if(cal_diff < diff){
+            a = v[i];
+            b = v[i+1];
+            diff = cal_diff;
+        }
+        
     }
     
+    cout << a << " " << b << endl;
 }
