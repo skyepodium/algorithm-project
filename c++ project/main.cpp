b320ca8603c0523f0af7f98ecc4eb4d425f88f9a
김정윤 <skyepodium@gmail.com>
2018-06-17 19:50:04 +0900

    codingparty smilegate problem1

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,35 +1,46 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 
 using namespace std;
 
-int main(int argc, char** argv)
+int main()
 {
-    int test_case;
-    int T;
-    
-    cin>>T;
-    
-    for(test_case = 1; test_case <= T; ++test_case)
-    {
-        int n, k;
-        cin >> n >> k;
-        
-        vector<int> v(n);
-        for(int i=0; i<n; i++){
-            cin >> v[i];
-        }
-        
-        sort(v.begin(), v.end());
-        
-        int result = 0;
-
-        for(int i=n-1; i>n-1-k; i--){
-            result = result + v[i];
-        }
-        
-        cout << "#" << test_case << " " << result << endl;
+    int player[5];
+    for(int i=0; i<5; i++){
+        player[i] = 0;
     }
+    
+    bool check[3];
+    for(int i=0; i<3; i++){
+        check[i] = false;
+    }
+    
+    for(int i=0; i < 5; i++){
+        cin >> player[i];
+        check[player[i]-1] = true;
+    }
+    
+    int num_cnt = 0;
+    int num_blank = 0;
+    for(int i=0; i<3; i++){
+        if(check[i] == true) num_cnt++;
+        else num_blank = i+1;
+    }
+    
+    int winner = 0;
+    int winner_num = 0;
+    if(num_cnt == 3 || num_cnt == 1) winner = 0;
+    else{
+        if(num_blank == 1) winner_num = 3;
+        else if(num_blank == 2) winner_num = 1;
+        else winner_num = 2;
+        
+        for(int i=0; i<5; i++){
+            if(winner_num == player[i]) winner++;
+        }
+    }
+    
+    cout << winner << endl;
     return 0;
 }
+
+
