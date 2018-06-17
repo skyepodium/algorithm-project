a71c3198bf2cb5ffaf5e4e1e0ef7223c1d2c3b49
김정윤 <skyepodium@gmail.com>
2018-06-17 19:51:02 +0900

    codingparty smilegate problem3

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,18 +1,46 @@
 #include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
+struct info{
+    double l;
+    int num;
+};
+
+bool cmp(const info &a, const info &b){
+    
+    return a.l < b.l;
+}
+
 int main() {
     
-    int t;
-    cin >> t;
+    double x, y, r;
+    cin >> x >> y >> r;
     
-    int n = 1;
-    for(int i=0; i<t; i++){
-        int a;
-        cin >> a;
-        n = n + a;
+    
+    vector<info> v;
+    for(int i=0; i<5; i++){
+        int a, b;
+        cin >> a >> b;
+        
+        double diff_x = abs(a-x);
+        double diff_y = abs(b-y);
+        
+        double length = sqrt(diff_x*diff_x + diff_y*diff_y);
+        v.push_back({length, i+1});
     }
     
-    cout << n - t <<endl;
+    
+    sort(v.begin(), v.end(), cmp);
+    
+    double result_num = v[0].num;
+    double result_l = v[0].l;
+    
+    if(r<result_l) cout << -1 << endl;
+    else cout << result_num << endl;
+    
 }
+
