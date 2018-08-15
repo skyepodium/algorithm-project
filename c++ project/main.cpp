b45b4b5cb063cfadaad66953174fe73017bafdd0
김정윤 <skyepodium@gmail.com>
2018-08-15 19:37:30 +0900

    SWEA 1954

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -2,13 +2,15 @@
 
 using namespace std;
 
-//시간 복잡도: O(1)
-//공간 복잡도: O(1)
-//사용한 알고리즘: 없음
-//사용한 자료구조: 없음
+//시간 복잡도: O(n^2)
+//공간 복잡도: O(n^2)
+//사용한 알고리즘: for문
+//사용한 자료구조: 2차원 배열
 
-
-int p, q, r, s, w;
+int n;
+int d[10][10];
+int dx[] = {0, 1, 0, -1};
+int dy[] = {1, 0, -1, 0};
 
 int main(int argc, char** argv)
 {
@@ -19,18 +21,48 @@ int main(int argc, char** argv)
     
     for(test_case = 1; test_case <= T; ++test_case)
     {
-        cin >> p >> q >> r >> s >> w;
+        for(int i=0; i<10; i++){
+            for(int j=0; j<10; j++){
+                d[i][j] = 0;
+            }
+        }
         
-        int a = w*p;
+        cin >> n;
+        int num = 1;
+        int c_x = 0;
+        int c_y = 0;
+        int c_dir = 0;
         
-        int b = 0;
-        if(r>=w) b = q;
-        else b = q + s*(w-r);
+        while(num<=n*n){
+            if(d[c_x][c_y] == 0){
+                d[c_x][c_y] = num;
+                num++;
+            }
+            
+            int n_x = c_x + dx[c_dir];
+            int n_y = c_y + dy[c_dir];
+            
+            if(n_x>=0 && n_x<n && n_y>=0 && n_y<n){
+                if(d[n_x][n_y] == 0){
+                    c_x = n_x;
+                    c_y = n_y;
+                }else{
+                    c_dir = (c_dir+3)%4;
+                }
+            }else{
+                c_dir = (c_dir+3)%4;
+            }
+            
+        }
         
-        cout << "#" << test_case << " ";
-        if(a<b) cout << a << endl;
-        else if(a>b) cout << b << endl;
-        else cout << a << endl;
+        cout << "#" << test_case << endl;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                cout << d[i][j] <<" ";
+            }
+            cout << endl;
+        }
+        
     }
     return 0;
 }
