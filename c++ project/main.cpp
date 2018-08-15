39a35bf3c920807a9900ff9ab704e563f627aecf
김정윤 <skyepodium@gmail.com>
2018-08-15 18:38:27 +0900

    SWEA 1945

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
-#include <string>
 
 using namespace std;
 
-//시간 복잡도: O(n^2)
+//시간 복잡도: O(logn)
 //공간 복잡도: O(n)
-//사용한 알고리즘: 2중 반복문
-//사용한 자료구조: string
+//사용한 알고리즘: 반복문
+//사용한 자료구조: 1차원 배열
 
-
-int result;
+int d[5] = {2, 3, 5, 7, 11};
+int result[5];
+int num;
 
 int main(int argc, char** argv)
 {
@@ -20,56 +20,27 @@ int main(int argc, char** argv)
     
     for(test_case = 1; test_case <= T; ++test_case)
     {
-        //초기화
-        result = 0;
+        //배열 초기화
+        for(int i=0; i<5; i++) result[i] = 0;
         
-        //문자열 입력받는다.
-        string word1;
-        cin >> word1;
+        cin >> num;
         
-        int count = 1;
-        
-        //문자열을 찾았는지 알려주는 변수
-        bool find_complete = false;
-
-        while(find_complete == false){
-
-            //count의 크기만큼 문자열을 생성한다.
-            string temp;
-            for(int i=0; i<count; i++){
-                temp = temp + word1[i];
+        //반복문으로 배열을 돌면서 주어진 수를 소인수분해한다.
+        for(int i=0; i<5; i++){
+            int count = 0;
+            while(num%d[i] == 0){
+                num = num/d[i];
+                count++;
             }
-            
-            //새로 생성한 문자열을 반복하여 word2를 생성한다.
-            int num = 30/count;
-            int num_remain = 30%count;
-        
-            string word2;
-            for(int i=0; i<num; i++){
-                word2 = word2 + temp;
-            }
-            for(int i=0; i<num_remain; i++){
-                word2 = word2 + temp[i];
-            }
-        
-            //word1, word2 두 문자열이 같은지를 검사한다.
-            bool is_find = true;
-            for(int i=0; i<30; i++){
-                if(word1[i] != word2[i]){
-                    is_find = false;
-                    break;
-                }
-            }
-        
-            //두 문자열이 같으면 반복문을 종료한다.
-            if(is_find == true){
-                result = count;
-                find_complete = true;
-            }
-        
-            count++;
+            result[i] = count;
         }
-        cout <<"#" << test_case << " " << result << endl;
+        
+        //출력
+        cout << "#" << test_case << " ";
+        for(int i=0; i<5; i++){
+            cout << result [i] << " ";
+        }
+        cout << endl;
     }
     return 0;
 }
