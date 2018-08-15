9fcbe206b9bcfbe33ab46cd60633a2a15d1ef876
김정윤 <skyepodium@gmail.com>
2018-08-15 15:33:51 +0900

    SWEA 1979

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 
 using namespace std;
 
-//시간 복잡도: O(n^2)
-//공간 복잡도: O(n)
-//사용한 알고리즘: 2중 for문
-//사용한 자료구조: 1차원 배열, 1차원 벡터
+//시간 복잡도: O(n^3)
+//공간 복잡도: O(n^2)
+//사용한 알고리즘: 3중 for문
+//사용한 자료구조: 2차원 배열
 
-//두개의 숫자열을 저장할 배열 2개 생성
-int a1[20];
-int a2[20];
+//퍼즐의 정보를 저장할 2차원 배열
+int d[16][16];
 
-//계산의 최대값을 저장할 변수 result
+//퍼즐의 크기와 단어의 크기를 저장할 변수
+int n, word_size;
+
+//들어갈 수 있는 자리를 저장할 변수
 int result;
 
 int main(int argc, char** argv)
@@ -25,71 +25,85 @@ int main(int argc, char** argv)
     
     for(test_case = 1; test_case <= T; ++test_case)
     {
-        //정수 최소값으로 매번 초기화 시켜준다.
-        result = -2147483647;
-        int a, b;
-        cin >> a >> b;
+        //변수 초기화
+        result = 0;
         
-        //첫번째 숫자열을 입력받는다.
-        for(int i=0; i<a; i++){
-            cin >> a1[i];
+        cin >> n >> word_size;
+        
+        //2중for문으로 퍼즐의 정보를 저장한다.
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                cin >> d[i][j];
+            }
         }
-        
-        //두번째 숫자열을 입력받는다.
-        for(int i=0; i<b; i++){
-            cin >> a2[i];
-        }
-        
-        //반복문의 횟수 계산
-        int loop_order = b-a;
-        
-        //짧은 숫자열을 저장할 벡터v1, 긴 숫자열을 저장할 벡터v2
-        vector<int> v1;
-        vector<int> v2;
-        
-        //만약 첫번째로 주어진 숫자열의 길이가 두번째 보다 크다면
-        if(a>b){
-            loop_order = a-b;
-            
-            for(int i=0; i<b; i++){
-                v1.push_back(a2[i]);
-            }
-            
-            for(int i=0; i<a; i++){
-                v2.push_back(a1[i]);
-            }
-            
-            //a,b 스왑
-            int c;
-            c = a;
-            a = b;
-            b = c;
-            
-        }else{
-            for(int i=0; i<a; i++){
-                v1.push_back(a1[i]);
-            }
-            
-            for(int i=0; i<b; i++){
-                v2.push_back(a2[i]);
+       
+        //가로 검색
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(d[i][j] == 1){
+                    
+                    bool check = true;
+
+                    //1. 갯수만큼 1이 있는 지 검사
+                    for(int k=j; k<word_size+j; k++){
+                        if(k<n){
+                            if(d[i][k] == 0){
+                                check = false;
+                            }
+                        }else{
+                            check = false;
+                        }
+                    }
+                    
+                    //2. 바로 앞에 1이 또 있는지 검사
+                    if(j-1 >=0 && d[i][j-1] == 1) check = false;
+
+                    //3. 바로 뒤에 1이 또 있는 지 검사
+                    if(j+word_size <= n && d[i][j+word_size] == 1) check = false;
+                    
+                    //검사를 다 통과했다면 갯수를 1 증가시킨다.
+                    if(check == true){
+                        result++;
+                    }
+                }
             }
         }
         
-        //두개의 벡터를 비교하면서 값을 계산한다.
-        for(int i=0; i<loop_order+1; i++){
-            
-            //각 횟수의 계산값을 저장할 변수 total
-            int total = 0;
-            for(int j=0; j<a; j++){
-                total = total + v1[j] * v2[j+i];
+        //세로 검색
+        for(int j=0; j<n; j++){
+            for(int i=0; i<n; i++){
+                if(d[i][j] == 1){
+                    
+                    bool check = true;
+                    
+                    //1. 갯수만큼 1이 있는 지 검사
+                    for(int k=i; k<word_size+i; k++){
+                        if(k<n){
+                            if(d[k][j] == 0){
+                                check = false;
+                            }
+                        }else{
+                            check = false;
+                        }
+                    }
+                    
+                    //2. 바로 앞에 1이 또 있는지 검사
+                    if(i-1 >= 0 && d[i-1][j] == 1) check = false;
+                    
+                    //3. 바로 뒤에 1이 또 있는 지 검사
+                    if(i+word_size <= n && d[i+word_size][j] == 1) check = false;
+                    
+                    //검사를 다 통과했다면 갯수를 1 증가시킨다.
+                    if(check == true){
+                        result++;
+                    }
+                }
             }
-            
-            //계산의 최대값 갱신
-            result = max(result, total);
         }
         
         cout << "#" << test_case << " " << result << endl;
+        
+        
     }
     return 0;
 }
-
