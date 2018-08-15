6c6cd6088bba98b63082bdbbf4cdd584ff7a4111
김정윤 <skyepodium@gmail.com>
2018-08-15 16:49:24 +0900

    SWEA 1926

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -2,14 +2,23 @@
 
 using namespace std;
 
+//시간 복잡도: O(n)
+//공간 복잡도: O(1)
+//사용한 알고리즘: 선형 탐색
+//사용한 자료구조: 1차원 배열
+
 void cal_clap(int a){
 
     int origin_num = a;
     int clap_num = 0;
     
     while(a>0){
+        //각 자릿수 검사
         int b = a%10;
+        
+        //만약 숫자가 3또는 6또는 9이면 박수횟수 증가
         if(b==3 || b==6 || b==9) clap_num++;
+        
         a = a/10;
     }
     
@@ -29,7 +38,10 @@ int main(){
     int n;
     cin >> n;
     
+    //for문으로 n번까지 검사
     for(int i=1; i<=n; i++){
+        
+        //박수횟수 검사
         cal_clap(i);
     }
     cout << endl;
