f9b5df2eebbf862cb03e3696ecbed4a5f1ad9e5d
김정윤 <skyepodium@gmail.com>
2018-08-15 16:11:58 +0900

    SWEA 1948

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -2,11 +2,36 @@
 
 using namespace std;
 
-int main(){
+int day[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+int main(int argc, char** argv)
+{
+    int test_case;
+    int T;
     
-    long long int a, b;
-    cin >> a >> b;
+    cin>>T;
     
-    if(a == b) cout << 1 << endl;
-    else cout << 0 << endl;
+    for(test_case = 1; test_case <= T; ++test_case)
+    {
+        int a, b, c, d;
+        cin >> a >> b >> c >> d;
+        
+        int month1 = 0;
+        for(int i=0; i<a-1; i++){
+            month1 = month1 + day[i];
+        }
+        
+        int first_day = month1 + b;
+
+        int month2 = 0;
+        for(int i=0; i<c-1; i++){
+            month2 = month2 + day[i];
+        }
+        
+        int second_day = month2 + d;
+        
+        cout << "#" << test_case << " " << second_day - first_day + 1<< endl;
+        
+    }
+    return 0;
 }
