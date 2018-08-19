27f64f9026c008df2b8100b12fe9dab175be89ec
김정윤 <skyepodium@gmail.com>
2018-08-19 18:49:45 +0900

    baekjoon 15953

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,68 +1,47 @@
 #include <iostream>
-#include <algorithm>
-#include <queue>
 
 using namespace std;
 
-int d[500][500];
-bool check[500][500];
+int a1[6] = {1, 3, 6, 10, 15, 21};
+int a2[6] = {500, 300, 200, 50, 30, 10};
 
-int dx[] = {0, 0, 1, -1};
-int dy[] = {-1, 1, 0, 0};
-int n, m;
-
-int cnt = 0;
-int area = 0;
-int max_area = 0;
-
-void bfs(int x, int y){
-    
-    check[x][y] = true;
-    queue<pair<int, int>> q;
-    q.push(make_pair(x, y));
-    
-    while(!q.empty()){
-        x = q.front().first;
-        y = q.front().second;
-        area++;
-        q.pop();
-        
-        for(int i=0; i<4; i++){
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            if(check[nx][ny] == false && d[nx][ny] == 1){
-                if(nx>=0 && nx<n && ny>=0 && ny<m) {
-                    check[nx][ny] = true;
-                    q.push(make_pair(nx, ny));
-                }
-            }
-        }
-    }
-}
+int b1[5] = {1, 3, 7, 15, 31};
+int b2[5] = {512, 256, 128, 64, 32};
 
 
 int main(){
+
+    int t;
+    cin >> t;
     
-    cin >> n >> m;
-    
-    for(int i=0; i<n; i++){
-        for(int j=0; j<m; j++){
-            cin >> d[i][j];
-        }
-    }
-    
-    for(int i=0; i<n; i++){
-        for(int j=0; j<m; j++){
-            if(check[i][j] == false && d[i][j] == 1){
-                bfs(i, j);
-                cnt++;
-                max_area = max(area, max_area);
-                area = 0;
+    while(t--){
+        
+        int a, b;
+        cin >> a >> b;
+        
+        int result = 0;
+        
+        if(a>0 && a<=21){
+            for(int i=0; i<6; i++){
+                if(a <= a1[i]){
+                    result = result + a2[i];
+                    break;
+                }
             }
         }
+        
+        if(b>0 && b<=31){
+            for(int i=0; i<6; i++){
+                if(b <= b1[i]){
+                    result = result + b2[i];
+                    break;
+                }
+            }
+        }
+        
+        cout << result*10000 << endl;
+        
     }
-
-    cout << cnt << endl;
-    cout << max_area << endl;
+    
     
 }
