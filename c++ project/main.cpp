e3d106f6e73547b623c30d5f810d06b7b0bf3a5e
김정윤 <skyepodium@gmail.com>
2018-08-19 18:09:06 +0900

    baekjoon 1926 bfs

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <algorithm>
+#include <queue>
 
 using namespace std;
 
@@ -9,28 +10,36 @@ bool check[500][500];
 int dx[] = {0, 0, 1, -1};
 int dy[] = {-1, 1, 0, 0};
 int n, m;
-int cnt = 0;
-int max_area = 0;
-int area = 0;
 
-void dfs(int x, int y){
+int cnt = 0;
+int area = 0;
+int max_area = 0;
+
+void bfs(int x, int y){
     
     check[x][y] = true;
-    area++;
+    queue<pair<int, int>> q;
+    q.push(make_pair(x, y));
     
-    for(int i=0; i<4; i++){
-        int nx = x + dx[i];
-        int ny = y + dy[i];
+    while(!q.empty()){
+        x = q.front().first;
+        y = q.front().second;
+        area++;
+        q.pop();
         
-        if(nx>=0 && nx<n && ny>=0 && ny<m){
+        for(int i=0; i<4; i++){
+            int nx = x + dx[i];
+            int ny = y + dy[i];
             if(check[nx][ny] == false && d[nx][ny] == 1){
-                dfs(nx, ny);
+                if(nx>=0 && nx<n && ny>=0 && ny<m) {
+                    check[nx][ny] = true;
+                    q.push(make_pair(nx, ny));
+                }
             }
         }
     }
-    
-    
 }
+
 
 int main(){
     
@@ -44,15 +53,16 @@ int main(){
     
     for(int i=0; i<n; i++){
         for(int j=0; j<m; j++){
-            if(d[i][j] == 1 && check[i][j] == false){
-                dfs(i, j);
+            if(check[i][j] == false && d[i][j] == 1){
+                bfs(i, j);
                 cnt++;
                 max_area = max(area, max_area);
                 area = 0;
             }
         }
     }
-    
+
     cout << cnt << endl;
     cout << max_area << endl;
+    
 }
