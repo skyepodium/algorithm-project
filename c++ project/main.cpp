430beb12a050d61c68a867ddf40d9bb4dbf73a3c
김정윤 <skyepodium@gmail.com>
2018-08-25 17:51:49 +0900

    baekjoon 2225

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,47 +1,28 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
-//시간 복잡도: O(logn)
-//공간 복잡도: O(n)
-//사용한 알고리즘: while문
-//사용한 자료구조: 1차원 벡터
+int mod = 1000000000;
 
+int d[201][201];
 
 int main(){
     
-    //n, b를 입력받는다.
-    int n, b;
-    cin >> n >> b;
+    int n, k;
+    cin >> n >> k;
     
-    //결과를 저장할 벡터 result 선언
-    //문자도 섞여있기 때문에 char로 선언해준다.
-    vector<char> result;
-    
-    //일반적인 진법변환과 같은 방식으로 진행한다.
-    while(n > 0){
+    for(int i=0; i<=n; i++){
         
-        //나머지가 10보다 작을때는 정수형태로 출력한다.
-        //아스키코드에서 0은 48, 따라서 48을 더해준다.
-        if(n%b < 10){
-            
-            result.push_back(n%b + 48);
+        d[i][1] = 1;
         
-        //나머지가 10보다 크거나 같을때는 알파벳 대문자로 출력한다.
-        //아스키코드에서 알파벳A는 65, 따라서 55를 더해준다.
-        }else{
-            result.push_back(n%b + 55);
+        for(int j=0; j<=n; j++){
+            for(int h=1; h<=k; h++){
+                d[i][h] = d[i][h]%mod + d[j][h-1]%mod;
+                d[i][h] %= mod;
+            }
         }
-        
-        //다음을 위해 n으로 나눠주고 갱신한다.
-        n = n/b;
     }
     
-    //뒤에서 부터 출력
-    for(int i = (int)result.size() - 1; i>=0; i--){
-        cout << result[i];
-    }
-    cout << endl;
-    
+    cout << d[n][k] << endl;
 }
+
