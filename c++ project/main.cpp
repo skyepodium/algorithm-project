1c4e943257e4375dce9b7b11ef32c08a675c951f
김정윤 <skyepodium@gmail.com>
2018-08-25 14:20:49 +0900

    baekjoon 11722

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,41 +1,44 @@
 #include <iostream>
-#include <string>
+#include <algorithm>
 
-//시간 복잡도: O(n)
-//공간 복잡도: O(1)
-//사용한 알고리즘: 없음
-//사용한 자료구조: string class(스택)
+//시간 복잡도: O(n^2)
+//공간 복잡도: O(n)
+//사용한 알고리즘: 동적 계획법 Bottom-Up
+//사용한 자료구조: 1차원 배열
 
 using namespace std;
 
+int a[1001];
+int d[1001];
+
+int go(int i){
+    
+    if(d[i] > 1){
+        return d[i];
+    }
+    
+    for(int j=0; j<i; j++){
+        if(a[j] > a[i]){
+            d[i] = max(d[i], d[j]+1);
+        }
+    }
+    
+    return d[i];
+}
+
 int main(){
     
-    string word;
-    getline(cin, word);
+    int n;
+    cin >> n;
     
-    for(int i=0; i<word.size(); i++){
-        
-        char ch = word[i];
-        if(ch >= 97){
-            
-            if(ch + 13 > 122){
-                printf("%c", ch + 13 - 26);
-            }else{
-                printf("%c", ch + 13);
-            }
-            
-        }else if(ch >= 65){
-
-            if(ch + 13 > 90){
-                printf("%c", ch + 13 - 26);
-            }else{
-                printf("%c", ch + 13);
-            }
-
-        }else{
-            printf("%c", ch);
-        }
-        
+    for(int i=0; i<n; i++){
+        cin >> a[i];
+        d[i] = 1;
     }
-    cout << endl;
+    
+    for(int i=0; i<n; i++){
+        go(i);
+    }
+    
+    cout << *max_element(d, d+n) << endl;
 }
