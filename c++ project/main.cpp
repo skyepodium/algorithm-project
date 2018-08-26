58bf68335f5415c15bfef5c880815e67781158a8
김정윤 <skyepodium@gmail.com>
2018-08-26 11:50:43 +0900

    baekjoon 2631

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -3,31 +3,40 @@
 
 using namespace std;
 
-long long int d[1000001];
-long long int mod = 1000000009;
+int a[201];
+int d[201];
+
+int n;
+
+int go(int i){
+
+    d[i] = 1;
+    if(d[i] > 1) return d[i];
+    
+    for(int j=0; j<n; j++){
+        if(a[j] < a[i]){
+            d[i] = max(d[i], d[j]+1);
+        }
+    }
+    
+    return d[i];
+}
 
 int main(){
     
-    d[1] = 1;
-    d[2] = 2;
-    d[3] = 4;
-    
-    for(long long int i=4; i<=1000000; i++){
-        d[i] = d[i-1]%mod + d[i-2]%mod + d[i-3]%mod;
-        d[i] %= mod;
-    }
-    
-    long long int n;
+
     cin >> n;
     
-    for(long long int i=0; i<n; i++){
-        long long int num;
-        cin >> num;
-        
-        cout << d[num]%mod << endl;
+    for(int i=0; i<n; i++){
+        cin >> a[i];
     }
     
+    for(int i=0; i<n; i++){
+        go(i);
+    }
     
+    cout << n - *max_element(d, d+n) << endl;
     
 }
+
 
