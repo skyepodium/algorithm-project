a43fa2921fa40d8c97adbe8653c0618bf92a6055
김정윤 <skyepodium@gmail.com>
2018-08-30 20:00:14 +0900

    baekjoon 10815

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,25 +1,30 @@
 #include <iostream>
 #include <algorithm>
 
-#define max_val 1000001
+#define max_val 500001
 
 using namespace std;
 
-int n, m, input;
+//시간 복잡도: O(mlogn)
+//공간 복잡도: O(n)
+//사용한 알고리즘: 이분 탐색
+//사용한 자료구조: 1차원 배열
 
-int a[100001];
+int n, m, input;
+int d[max_val];
 
 bool binary_search(int num){
     
     int start = 0;
     int end = n;
     int mid = 0;
+    
     while(start <= end){
         
         mid = (start + end)/2;
         
-        if(a[mid] == num) return true;
-        else if(num < a[mid]) end = mid - 1;
+        if(d[mid] == num) return true;
+        else if(num < d[mid]) end = mid - 1;
         else start = mid + 1;
     }
     
@@ -29,13 +34,15 @@ bool binary_search(int num){
 int main(){
     
     cin >> n;
-    for(int i=0; i<n; i++) scanf("%d", &a[i]);
-    sort(a, a+n);
+    for(int i=0; i<n; i++) scanf("%d", &d[i]);
+    
+    sort(d, d+n);
     
     cin >> m;
     for(int i=0; i<m; i++){
         scanf("%d", &input);
-        printf("%d\n", binary_search(input));
+        printf("%d ", binary_search(input));
     }
+    cout << endl;
     
 }
