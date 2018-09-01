84fcbce6cf668e9723d0a867ff264a7eb9eda30b
김정윤 <skyepodium@gmail.com>
2018-09-01 15:34:34 +0900

    baekjoon 10815 binary_search

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <algorithm>
 
-#define max_val 100001
+#define max_val 500001
 
 using namespace std;
 
 //시간 복잡도: O(mlogn)
 //공간 복잡도: O(n)
-//사용한 알고리즘: 이분 탐색(반복문), STL sort
+//사용한 알고리즘: 이분 탐색(재귀), STL sort
 //사용한 자료구조: 1차원 배열
 
 
@@ -15,24 +15,16 @@ int n, m, num;
 int d[max_val];
 
 
-//이분 탐색 재귀적 구현
-bool binary_search(int val){
+//이분 탐색 재귀
+bool binary_search(int val, int start, int end){
     
-    int start = 0;
-    int end = n;
-    int mid = 0;
+    if(start > end) return false;
     
-    while(start <= end){
-        
-        mid = (start + end)/2;
-        
-        if(d[mid] == val) return true;
-        else if(d[mid] < val) start = mid + 1;
-        else end = mid - 1;
-        
-    }
+    int mid = (start + end)/2;
     
-    return false;
+    if(d[mid] == val) return true;
+    else if(d[mid] < val) return binary_search(val, mid+1, end);
+    else return binary_search(val, start, mid-1);
 }
 
 
@@ -48,6 +40,7 @@ int main(){
     
     for(int i=0; i<m; i++){
         scanf("%d", &num);
-        printf("%d\n", binary_search(num));
+        printf("%d ", binary_search(num, 0, n));
     }
+    printf("\n");
 }
