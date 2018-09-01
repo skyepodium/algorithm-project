a3585d2f9098528fb64e0d2e2327f1a014217de0
김정윤 <skyepodium@gmail.com>
2018-09-01 19:48:01 +0900

    baekjoon 11403

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,45 +1,54 @@
 #include <iostream>
-#include <algorithm>
+#include <vector>
+#include <cstring>
 
-#define max_int 1000001
-#define lld long long int
+#define max_int 101
 
 using namespace std;
 
-//시간 복잡도: O(nlogn)
-//공간 복잡도: O(n)
-//사용한 알고리즘: STL sort
-//사용한 자료구조: 1차원 배열
+//시간 복잡도: O(n^2)
+//공간 복잡도: O(n^2)
+//사용한 알고리즘: DFS
+//사용한 자료구조: 인접 리스트(2차원 벡터)
 
 
-lld d[max_int];
+vector<int> a[max_int];
+bool check[max_int];
 int n = 0;
-lld result = 0;
-int max_cnt = 0;
-int cur_cnt = 0;
+int num = 0;
+
+void dfs(int node){
+    
+    for(int i=0; i<a[node].size(); i++){
+        int next = a[node][i];
+        
+        if(check[next] == false){
+            check[next] = true;
+            dfs(next);
+        }
+    }
+}
+
 
 int main(){
     
-    scanf("%d", &n);
+    cin >> n;
     
-    for(int i=0; i<n; i++) scanf("%lld", &d[i]);
-    
-    sort(d, d+n);
-    
-    result = d[0];
-    max_cnt = 1;
-    cur_cnt = 1;
-    for(int i=1; i<n; i++){
-        
-        if(d[i] == d[i-1]) cur_cnt++;
-        else cur_cnt = 1;
-        
-        //반복문 종료되었을때도 갱신하기 위해서, 매번 검사해준다.
-        if(max_cnt < cur_cnt){
-            max_cnt = cur_cnt;
-            result = d[i];
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cin >> num;
+            if(num == 1) a[i].push_back(j);
         }
     }
     
-    printf("%lld\n", result);
+    for(int i=0; i<n; i++){
+        dfs(i);
+        for(int j=0; j<n; j++){
+            if(check[j] == true) cout << "1 ";
+            else cout << "0 ";
+        }
+        cout << endl;
+        memset(check, false, sizeof(check));
+    }
+    
 }
