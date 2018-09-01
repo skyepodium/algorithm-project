fe30b5c5dc2834615959b9c375c9af036c34bf8b
김정윤 <skyepodium@gmail.com>
2018-09-01 19:23:57 +0900

    baekjoon 11652

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,68 +1,45 @@
 #include <iostream>
+#include <algorithm>
 
-#define max_val 10001
+#define max_int 1000001
+#define lld long long int
 
 using namespace std;
 
 //시간 복잡도: O(nlogn)
 //공간 복잡도: O(n)
-//사용한 알고리즘: 이분 탐색(재귀)
+//사용한 알고리즘: STL sort
 //사용한 자료구조: 1차원 배열
 
 
-int d[max_val];
-int max_cost = 0;
-int n, m;
-int result = 0;
-int sum_cost = 0;
-
-int max(int a, int b){
-    if(a<b) return b;
-    else return a;
-}
-
-int cal_cost(int mid){
-    
-    int ret = 0;
-    
-    for(int i=0; i<n; i++){
-        if(d[i] < mid) ret = ret + d[i];
-        else ret = ret + mid;
-    }
-    
-    return ret;
-}
-
-void binary_search(int start, int end){
-    
-    if(start >= end) return;
-    int mid = (start + end)/2;
-    int tmp = cal_cost(mid);
-    
-    if(tmp <= m){
-        result = mid;
-        binary_search(mid+1, end);
-    }else{
-        binary_search(start, mid);
-    }
-}
+lld d[max_int];
+int n = 0;
+lld result = 0;
+int max_cnt = 0;
+int cur_cnt = 0;
 
 int main(){
     
     scanf("%d", &n);
-    for(int i=0; i<n; i++){
-        scanf("%d", &d[i]);
-        sum_cost = sum_cost + d[i];
-        max_cost = max(max_cost, d[i]);
+    
+    for(int i=0; i<n; i++) scanf("%lld", &d[i]);
+    
+    sort(d, d+n);
+    
+    result = d[0];
+    max_cnt = 1;
+    cur_cnt = 1;
+    for(int i=1; i<n; i++){
+        
+        if(d[i] == d[i-1]) cur_cnt++;
+        else cur_cnt = 1;
+        
+        //반복문 종료되었을때도 갱신하기 위해서, 매번 검사해준다.
+        if(max_cnt < cur_cnt){
+            max_cnt = cur_cnt;
+            result = d[i];
+        }
     }
     
-    scanf("%d", &m);
-    
-    if(sum_cost <= m){
-        printf("%d\n", max_cost);
-    }else{
-        binary_search(0, max_cost);
-        printf("%d\n", result);
-    }
-    
+    printf("%lld\n", result);
 }
