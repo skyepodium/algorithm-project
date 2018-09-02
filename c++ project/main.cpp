45ee03d5edb72ad6e164e43bbb76bef724e061b2
김정윤 <skyepodium@gmail.com>
2018-09-02 19:24:18 +0900

    baekjoon 2375

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,34 +1,25 @@
 #include <iostream>
-#define max_int 1000000
-#define lld long long int
+#include <algorithm>
 
+#define max_int 100000
+#define max_val 1000000000
 using namespace std;
 
-lld tree[max_int*4];
-
-int n, m, k;
-int a, b, c;
+int n, m;
+int tree_min[max_int*4];
+int tree_max[max_int*4];
 int idx;
+int a, b;
 
-//5. 인덱스 트리를 업데이트 한다.
-void update_tree(int node, lld delta){
-    
-    //노드가 1일때도 넣어야하기 때문에 0보다 크다로 설정한다.
-    while(node > 0){
-        tree[node] = tree[node] + delta;
-        node = node/2;
-    }
-}
 
-//6. 인덱스 트리에서 구간합을 계산한다.
-lld sum_data(int start, int end){
+//5. 구간의 최소값을 계산한다.
+int min_data(int start, int end){
     
-    lld result = 0;
-    
-    while(start <= end){
+    int result = max_val;
+    while(start<=end){
         
-        if(start%2 == 1) result = result + tree[start];
-        if(end%2 == 0) result = result + tree[end];
+        if(start%2 == 1) result = min(result, tree_min[start]);
+        if(end%2 == 0) result = min(result, tree_min[end]);
         
         start = (start + 1)/2;
         end = (end - 1)/2;
@@ -36,44 +27,50 @@ lld sum_data(int start, int end){
     return result;
 }
 
+//6. 구간의 최대값을 계산한다.
+int max_data(int start, int end){
+    
+    int result = 0;
+    while(start<=end){
+        
+        if(start%2 == 1) result = max(result, tree_max[start]);
+        if(end%2 == 0) result = max(result, tree_max[end]);
+        
+        start = (start + 1)/2;
+        end = (end - 1)/2;
+    }
+    return result;
+}
 
 int main(){
     
-    scanf("%d %d %d", &n, &m, &k);
+    scanf("%d %d", &n, &m);
     
-    //1. n을 넘는 최소의 리프노드 수 2^n를 찾는다.
+    //1. n을 넘는 2의 제곱수를 계산한다.
     idx = 1;
     while(idx<n){
         idx = idx*2;
     }
-    //1부터 시작, 1을 빼준다.
+    //트리는 1부터 시작하기 때문에 1을 빼준다.
     idx--;
-
+    
     //2. 리프노드에 수를 입력받는다.
     for(int i=1; i<=n; i++){
-        scanf("%lld", &tree[idx+i]);
+        scanf("%d", &tree_min[i+idx]);
+        tree_max[i+idx] = tree_min[i+idx];
     }
     
-    //3. sum_indexed tree를 구성한다.
+    //3. Min, Max Indexed Tree를 구성한다.
     for(int i=idx; i>=1; i--){
-        tree[i] = tree[i*2] + tree[i*2+1];
+        tree_min[i] = min(tree_min[i*2], tree_min[i*2+1]);
+        tree_max[i] = max(tree_max[i*2], tree_max[i*2+1]);
     }
     
-    //4. m+k개의 명령을 입력받는다.
-    for(int i=1; i<=m+k; i++){
-        scanf("%d %d %d", &a, &b, &c);
-
-        //5. 인덱스 트리를 업데이트한다.
-        if(a == 1){
+    //4. m개의 명령을 입력받는다.
+    for(int i=1; i<=m; i++){
+        scanf("%d %d", &a, &b);
         
-            lld delta = c - tree[idx+b];
-            update_tree(idx+b, delta);
-        }
-        //6. 구간합을 출력한다.
-        else{
-            printf("%lld\n", sum_data(idx+b, idx+c));
-        }
+        printf("%d %d\n", min_data(a+idx, b+idx), max_data(a+idx, b+idx));
     }
+    
 }
-
-
