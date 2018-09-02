c61ff33d716c42fb282a2752b0eaf04f27d97592
김정윤 <skyepodium@gmail.com>
2018-09-02 22:07:19 +0900

    baekjoon mul indexed tree.....

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,76 +1,101 @@
 #include <iostream>
-#include <algorithm>
+#define max_int 2000000
+#define lld long long int
 
-#define max_int 100000
-#define max_val 1000000000
 using namespace std;
 
-int n, m;
-int tree_min[max_int*4];
-int tree_max[max_int*4];
+lld tree[max_int*4];
+int n, m, k;
 int idx;
-int a, b;
+int a, b, c;
+int mod = 1000000007;
 
-
-//5. 구간의 최소값을 계산한다.
-int min_data(int start, int end){
+void init(){
     
-    int result = max_val;
-    while(start<=end){
-        
-        if(start%2 == 1) result = min(result, tree_min[start]);
-        if(end%2 == 0) result = min(result, tree_min[end]);
-        
-        start = (start + 1)/2;
-        end = (end - 1)/2;
-    }
-    return result;
+    for(int i=0;i<=max_int; i++) tree[i] = 1;
 }
 
-//6. 구간의 최대값을 계산한다.
-int max_data(int start, int end){
+void update_tree(int node, lld son, lld mom){
     
-    int result = 0;
-    while(start<=end){
+    while(node>0){
         
-        if(start%2 == 1) result = max(result, tree_max[start]);
-        if(end%2 == 0) result = max(result, tree_max[end]);
-        
-        start = (start + 1)/2;
-        end = (end - 1)/2;
+        if(tree[node] == 0){
+            tree[node] = (tree[node*2]%mod * tree[node*2+1]%mod)%mod;
+            tree[node] = tree[node]%mod;
+        }
+        else{
+            tree[node] = ((tree[node]/3)*(mom))%mod;
+            tree[node] = tree[node]%mod;
+        }
+
+        node = node/2;
     }
-    return result;
 }
+
+lld mul_data(int start, int end){
+    
+    lld result = 1;
+    while(start <= end){
+        
+        if(start%2 == 1){
+            result = (result%mod * tree[start]%mod)%mod;
+            result = result%mod;
+        }
+        if(end%2 == 0){
+            result = (result%mod * tree[end]%mod)%mod;
+            result = result%mod;
+        }
+        
+        start = (start+1)/2;
+        end = (end-1)/2;
+    }
+    
+    return result%mod;
+}
+
 
 int main(){
     
-    scanf("%d %d", &n, &m);
+    scanf("%d %d %d", &n, &m, &k);
     
-    //1. n을 넘는 2의 제곱수를 계산한다.
+    //0. 트리를 모두 1로 초기화한다.
+    init();
+    
+    //1. n을 넘는 리프노드의 최대 개수를 계산한다.
     idx = 1;
     while(idx<n){
         idx = idx*2;
     }
-    //트리는 1부터 시작하기 때문에 1을 빼준다.
+    //트리가 1부터 시작하기 때문에 1을 빼준다.
     idx--;
     
     //2. 리프노드에 수를 입력받는다.
     for(int i=1; i<=n; i++){
-        scanf("%d", &tree_min[i+idx]);
-        tree_max[i+idx] = tree_min[i+idx];
+        scanf("%lld", &tree[i+idx]);
     }
     
-    //3. Min, Max Indexed Tree를 구성한다.
+    //3. Mul Indexed Tree를 구성한다.
     for(int i=idx; i>=1; i--){
-        tree_min[i] = min(tree_min[i*2], tree_min[i*2+1]);
-        tree_max[i] = max(tree_max[i*2], tree_max[i*2+1]);
+        tree[i] = (tree[i*2]%mod * tree[i*2+1]%mod)%mod;
+        tree[i] = tree[i]%mod;
     }
     
-    //4. m개의 명령을 입력받는다.
-    for(int i=1; i<=m; i++){
-        scanf("%d %d", &a, &b);
+    //4. m+k개의 명령을 입력받는다.
+    for(int i=1; i<=m+k; i++){
+        scanf("%d %d %d", &a, &b, &c);
         
-        printf("%d %d\n", min_data(a+idx, b+idx), max_data(a+idx, b+idx));
+        //5. 트리를 업데이트 한다.
+        if(a==1){
+            if(tree[idx+b] == 0){
+                tree[(idx+b)*2] = (lld)c%mod;
+            }
+            update_tree(idx+b, tree[idx+b], (lld)c);
+        }
+        
+        //6. 구간합을 계산한다.
+        else{
+            printf("%lld\n", mul_data(idx+b, idx+c)%mod);
+        }
     }
     
 }
