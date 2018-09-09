956d5e5f7043cef92e8409a82c5c65268b5eb60b
김정윤 <skyepodium@gmail.com>
2018-09-10 02:54:43 +0900

    baekjoon 1535 Bottom Up (0/1 knapsack)

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,68 +1,67 @@
 #include <iostream>
+#include <algorithm>
 #define max_int 21
-#define lld long long int
 
 using namespace std;
 
-//n!의 갯수를 저장할 배열
-lld facto[max_int];
+//시간 복잡도: O(n)
+//공간 복잡도: O(n)
+//사용한 알고리즘: 동적 계획법, Bottom_Up
+//사용한 자료구조: 1차원, 2차원 배열
 
-//순열은 1~n까지의 수가 1번씩 사용된다.
-//어떤 숫자가 사용되었는지 기록하는 배열
-bool check[max_int];
-int n, a, num;
-lld k;
+/*
+ 0/1 knapsack 문제
+ 점화식 설명
+ d[i][j] = max(d[i-1][j], d[i-1][j+health[i] + joy[i]);
+ d[i][j]는 i번재 사람에게 왔을 때 얻을 수 있는 최대 기쁨(인사하든, 안하든), j는 남은 체력
+ 
+ d[i-1][j]는 i번째 사람에게 왔을때 인사 안하는 경우
+ d[i-1][j + health[i]] + joy[i]는 i번째 사람에게 인사하고 joy[i]를 얻는 경우
+ j가 남은 체력이기 때문에 i-1번째 사람일떼 j+health[i]만큼 가지고 있어야
+ i번째에 왔을때 남은 체력이 j가 된다.
+ ex) d[2][78] = d[1][99] + 30
+ */
+
+int health[max_int];
+int joy[max_int];
+int d[max_int][101];
+int n;
 
 int main(){
-    
-    //1. 1! ~ 20!까지의 n! 갯수를 미리 계산한다.
-    facto[1] = 1;
-    for(int i=2; i<=20; i++){
-        facto[i] = facto[i-1]*i;
-    }
-    
     scanf("%d", &n);
     
-    scanf("%d", &a);
+    for(int i=1; i<=n; i++) scanf("%d", &health[i]);
+    for(int i=1; i<=n; i++) scanf("%d", &joy[i]);
 
-    //2. k번째 수열을 나타내는 n개의 수를 출력한다.
-    if(a == 1){
-        scanf("%lld", &k);
-        
-        for(int i=n-1; i>=0; i--){
-            for(int j=1; j<=n; j++){
-                if(check[j] == true) continue;
-                
-                if(facto[i] < k){
-                    k = k - facto[i];
-                }
-                else{
-                    printf("%d ", j);
-                    check[j] = true;
-                    break;
-                }
-                if(i==0 && check[j] == false){
-                    printf("%d ", j);
-                }
+    //1. 첫번째 사람에게 인사하고 얻는 기쁨을 초기화 해준다.
+    d[1][100-health[1]] = joy[1];
+    
+    //2. 점화식을 실행한다.
+    for(int i=2; i<=n; i++){
+        for(int j=100; j>0; j--){
+            
+            //1) 남은 체력이 health[i]보다 커서 인사할 수 있는 경우
+            if(j + health[i] <= 100){
+                d[i][j] = max(d[i-1][j], d[i-1][j+health[i]] + joy[i]);
+            }
+            
+            //2) 남은 체력이 health[i]보다 작아서 인사를 할 수 없는 경우
+            else{
+                d[i][j] = max(d[i-1][j], d[i][j]);
             }
         }
-        printf("\n");
-        
     }
     
-    //3. 몇번째 수열인지 출력한다.
-    else{
-        lld result = 0;
-        for(int i=n; i>=1; i--){
-            scanf("%d", &num);
-            
-            for(int j=1; j<num; j++){
-                if(check[j] == false){
-                    result = result += facto[i-1];
-                }
-            }
-            check[num] = true;
-        }
-        printf("%lld\n", result+1);
+    //3. 결과 출력
+    //가장 큰 값은 n번째 사람까지 왔을때 1~100까지의 체력을 가진 경우 중 하나이다.
+    int result = 0;
+    for(int j=1; j<=100; j++){
+        result = max(result, d[n][j]);
     }
+    
+    printf("%d\n", result);
+    
+    return 0;
 }
+
+
