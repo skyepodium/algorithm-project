b041dc803727f67bce7fb4688b829cd354b5bfd4
김정윤 <skyepodium@gmail.com>
2018-09-10 04:08:41 +0900

    swea 5431

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,50 +1,37 @@
 #include <iostream>
-#include <algorithm>
+#include <cstring>
 #define max_int 101
 
 using namespace std;
 
-//시간 복잡도: O(nk)
-//공간 복잡도: O(nk)
-//사용한 알고리즘: 동적 계획법, Bottom_Up
-//사용한 자료구조: 1차원, 2차원 배열
+bool check[max_int];
+int n, k, num;
 
-
-int w[max_int];
-int v[max_int];
-int d[max_int][100001];
-
-int n, k;
-
-int main(){
+int main(int argc, char** argv)
+{
+    int test_case;
+    int T;
     
-    scanf("%d %d", &n, &k);
+    cin>>T;
     
-    for(int i=1; i<=n; i++){
-        scanf("%d %d", &w[i], &v[i]);
-    }
-    
-    
-    d[1][k-w[1]] = v[1];
-    for(int i=2; i<=n; i++){
-        for(int j=0; j<=k; j++){
-            
-            if(j+w[i] <= k){
-                d[i][j] = max(d[i-1][j], d[i-1][j+w[i]] + v[i]);
-            }
-            else{
-                d[i][j] = max(d[i-1][j], d[i][j]);
+    for(test_case = 1; test_case <= T; ++test_case)
+    {
+        memset(check, false, sizeof(check));
+        scanf("%d %d", &n, &k);
+        
+        for(int i=0; i<k; i++){
+            scanf("%d", &num);
+            check[num] = true;
+        }
+        
+        printf("#%d ", test_case);
+        for(int i=1; i<=n; i++){
+            if(check[i] == false){
+                printf("%d ", i);
             }
         }
+        printf("\n");
+        
     }
-    
-    
-    int result = 0;
-    for(int j=0; j<=k; j++){
-        result = max(result, d[n][j]);
-    }
-    
-    printf("%d\n", result);
-    
     return 0;
 }
