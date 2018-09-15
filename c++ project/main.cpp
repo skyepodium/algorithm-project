57474ec2d6a1a97e7386b0e3618e1beb32cfed1f
김정윤 <skyepodium@gmail.com>
2018-09-15 22:54:42 +0900

    no message

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,35 +1,13 @@
 #include <iostream>
-#include <algorithm>
-#define max_int 500001
 
-using namespace std;
-
-int t, n, m;
-char num[max_int];
-
-bool cmp(char &a, char &b){
-    return a > b;
-}
-
-int main(){
-    scanf("%d", &t);
-    
-    for(int test_case = 1; test_case <=t; test_case++){
-        
-        scanf("%d %d", &n, &m);
-        
-        scanf("%s", num);
-
-        sort(num, num+n, cmp);
-        
-        
-        printf("#%d ", test_case);
-        for(int i=0; i<n-m; i++){
-            printf("%c", num[i]);
         }
         printf("\n");
         
     }
+}
+
+int main(){
+    scanf("%d", &T);
     
-    
+
 }
