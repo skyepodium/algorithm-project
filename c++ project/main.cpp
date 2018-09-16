6455e6e3142df5c8f87432817db3b96dd5860445
김정윤 <skyepodium@gmail.com>
2018-09-16 16:50:12 +0900

    baekjoon eratos

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,13 +1,31 @@
 #include <iostream>
 
-        }
-        printf("\n");
-        
-    }
-}
+using namespace std;
+
+bool check[1001];
+int a[101];
+int n;
 
 int main(){
-    scanf("%d", &T);
+    scanf("%d", &n);
     
-
+    for(int i=0; i<n; i++){
+        scanf("%d", &a[i]);
+    }
+    
+    check[1] = true;
+    for(int i=2; i*i<1001; i++){
+        for(int j=i*i; j<1001; j=j+i){
+            check[j] = true;
+        }
+    }
+    
+    int cnt = 0;
+    for(int i=0; i<n; i++){
+        if(check[a[i]] == false){
+            cnt++;
+        }
+    }
+    
+    cout << cnt << endl;
 }
