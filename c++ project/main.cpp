b9fe506b06b7400d618ea52478bb8b6c4d62eb70
김정윤 <skyepodium@gmail.com>
2018-09-21 23:07:59 +0900

    programmers 09.21

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,12 +1,16 @@
-#include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
-int t;
-
-int main(){
-    scanf("%d", &t);
+int solution(int n) {
+    int answer = 0;
     
+    for(int i=1; i*i<=n; i++){
+        if(n%i == 0) answer = answer + i + n/i;
+        if(i*i == n) answer = answer - i;
+    }
     
-    
+    return answer;
 }
+
