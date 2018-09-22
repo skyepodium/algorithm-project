ea109fcc25c4282b5e432ba4698d659c93ab7d25
김정윤 <skyepodium@gmail.com>
2018-09-22 10:32:51 +0900

    swea 5549

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,16 +1,27 @@
-#include <string>
-#include <vector>
-
+#include <iostream>
+#include <cstring>
 using namespace std;
 
-int solution(int n) {
-    int answer = 0;
+int t;
+char word[100];
+
+int main(int argc, char** argv)
+{
+    scanf("%d", &t);
     
-    for(int i=1; i*i<=n; i++){
-        if(n%i == 0) answer = answer + i + n/i;
-        if(i*i == n) answer = answer - i;
+    for(int test_case = 1; test_case <= t; ++test_case)
+    {
+        scanf("%s", word);
+        int length = (int)strlen(word);
+ 
+        int last_number = word[length-1] - '0';
+
+        bool is_odd = true;
+        if(last_number%2 == 0) is_odd = false;
+        
+        if(is_odd) printf("#%d Odd\n", test_case);
+        else printf("#%d Even\n", test_case);
     }
     
-    return answer;
+    return 0;
 }
-
