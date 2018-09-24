d4f3c924040325b950b20ca5296fecfe2357e8a5
김정윤 <skyepodium@gmail.com>
2018-09-24 19:04:22 +0900

    baekjoon 5379

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,22 +1,67 @@
-#include <string>
-#include <vector>
+#include <iostream>
+#include <cstring>
+#include <stack>
+#define max_int 1000001
 
 using namespace std;
 
-bool solution(string s) {
-    bool answer = true;
+//시간 복잡도: O(n)
+//공간 복잡도: O(n)
+//사용한 알고리즘: 없음
+//사용한 자료구조: 스택
+
+
+char cmd[max_int];
+int t;
+
+int main(){
+    scanf("%d", &t);
     
-    int size = (int)s.size();
-    if(size != 4 && size != 6){
-        answer = false;
-    }
-    
-    for(int i=0; i<size; i++){
-        if(s[i] - '0' < 0 || s[i] - '0' >9){
-            answer = false;
-            break;
+    while(t--){
+        
+        scanf("%s", cmd);
+        
+        stack<char> left;
+        stack<char> right;
+        
+        int length = (int)strlen(cmd);
+
+        for(int i=0; i<length; i++){
+            if(cmd[i] == '<'){
+                if(!left.empty()){
+                    right.push(left.top());
+                    left.pop();
+                }
+            }
+            
+            else if(cmd[i] == '>'){
+                if(!right.empty()){
+                    left.push(right.top());
+                    right.pop();
+                }
+            }
+            
+            else if(cmd[i] == '-'){
+                if(!left.empty()){
+                    left.pop();
+                }
+            }
+            
+            else{
+                left.push(cmd[i]);
+            }
         }
+        
+        while(!left.empty()){
+            right.push(left.top());
+            left.pop();
+        }
+        
+        while(!right.empty()){
+            printf("%c", right.top());
+            right.pop();
+        }
+        printf("\n");
+        
     }
-    
-    return answer;
 }
