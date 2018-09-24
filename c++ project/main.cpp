4eb10d76274be2b6b349ee8f9ab994c8c69338c5
김정윤 <skyepodium@gmail.com>
2018-09-24 21:25:34 +0900

    백준 10942

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,67 +1,47 @@
 #include <iostream>
-#include <cstring>
-#include <stack>
-#define max_int 1000001
+#define max_int 2001
 
 using namespace std;
 
-//시간 복잡도: O(n)
-//공간 복잡도: O(n)
-//사용한 알고리즘: 없음
-//사용한 자료구조: 스택
+//시간 복잡도: O(n^2 + m)
+//공간 복잡도: O(n^2)
+//사용한 알고리즘: 반복문
+//사용한 자료구조: 2차원 배열
 
-
-char cmd[max_int];
-int t;
+int n, m, s, e;
+int a[max_int];
+int d[max_int][max_int];
 
 int main(){
-    scanf("%d", &t);
     
-    while(t--){
-        
-        scanf("%s", cmd);
-        
-        stack<char> left;
-        stack<char> right;
-        
-        int length = (int)strlen(cmd);
-
-        for(int i=0; i<length; i++){
-            if(cmd[i] == '<'){
-                if(!left.empty()){
-                    right.push(left.top());
-                    left.pop();
-                }
-            }
-            
-            else if(cmd[i] == '>'){
-                if(!right.empty()){
-                    left.push(right.top());
-                    right.pop();
-                }
-            }
-            
-            else if(cmd[i] == '-'){
-                if(!left.empty()){
-                    left.pop();
-                }
-            }
-            
-            else{
-                left.push(cmd[i]);
-            }
-        }
-        
-        while(!left.empty()){
-            right.push(left.top());
-            left.pop();
-        }
-        
-        while(!right.empty()){
-            printf("%c", right.top());
-            right.pop();
-        }
-        printf("\n");
-        
+    scanf("%d", &n);
+    for(int i=1; i<=n; i++){
+        scanf("%d", &a[i]);
     }
+    
+    for(int i=1; i<=n; i++){
+        d[i][i] = 1;
+    }
+    
+    for(int i=1; i<=n-1; i++){
+        if(a[i] == a[i+1]){
+            d[i][i+1] = 1;
+        }
+    }
+    
+    for(int j=3; j<=n; j++){
+        for(int i=1; i<=j; i++){
+            if(a[i] == a[j] && d[i+1][j-1] == 1){
+                d[i][j] = 1;
+            }
+        }
+    }
+    
+    scanf("%d", &m);
+    for(int i=0; i<m; i++){
+        scanf("%d %d", &s, &e);
+        printf("%d\n", d[s][e]);
+    }
+    
+    return 0;
 }
