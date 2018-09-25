fcb1701fe4c5e2c123f658478a3b913b02eb4d80
김정윤 <skyepodium@gmail.com>
2018-09-25 21:26:12 +0900

    baekjoon 12813

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,21 +1,53 @@
 #include <iostream>
+#include <cstring>
+#define max_int 100001
 
 using namespace std;
 
-int t, n, m;
+char word1[max_int];
+char word2[max_int];
 
 int main(){
     
-    for(int i=0; i<10; i++){
-
-        scanf("%d %d %d", &t, &n, &m);
-        
-        int mul = n;
-        for(int i=0; i<m-1; i++){
-            n = n*mul;
-        }
-        
-        printf("#%d %d\n", t, n);
-
+    scanf("%s", word1);
+    scanf("%s", word2);
+    
+    int size = (int)strlen(word1);
+    
+    //&
+    for(int i=0; i<size; i++){
+        if(word1[i]-'0' & word2[i]-'0') printf("1");
+        else printf("0");
     }
+    printf("\n");
+
+    //|
+    for(int i=0; i<size; i++){
+        if(word1[i]-'0' | word2[i]-'0') printf("1");
+        else printf("0");
+    }
+    printf("\n");
+
+    //^
+    for(int i=0; i<size; i++){
+        if(word1[i]-'0' ^ word2[i]-'0') printf("1");
+        else printf("0");
+    }
+    printf("\n");
+    
+    //~A
+    for(int i=0; i<size; i++){
+        if(!(word1[i]-'0')) printf("1");
+        else printf("0");
+    }
+    printf("\n");
+
+    //~B
+    for(int i=0; i<size; i++){
+        if(!(word2[i]-'0')) printf("1");
+        else printf("0");
+    }
+    printf("\n");
+
+    
 }
