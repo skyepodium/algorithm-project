8685275c39e13c8802b40a8727cfb8e3785fa322
김정윤 <skyepodium@gmail.com>
2018-09-25 19:34:35 +0900

    SWEA 1217

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,43 +1,21 @@
 #include <iostream>
-#define max_int 50
+
 using namespace std;
 
-int t, n;
-int d[max_int][max_int];
+int t, n, m;
 
-int main(int argc, char** argv)
-{
-    scanf("%d", &t);
+int main(){
     
-    for(int test_case = 1; test_case <= t; ++test_case)
-    {
-        scanf("%d", &n);
-        
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                scanf("%1d", &d[i][j]);
-            }
-        }
+    for(int i=0; i<10; i++){
 
-        int sum = 0;
-        for(int i=0; i<n; i++){
-            
-            if(i<=n/2){
-                for(int j=n/2-i; j<=n/2+i; j++){
-                    sum = sum + d[i][j];
-                }
-            }
-            else{
-                for(int j=n/2-(n-1-i); j<=n/2+(n-1-i); j++){
-                    sum = sum + d[i][j];
-                }
-            }
-            
+        scanf("%d %d %d", &t, &n, &m);
+        
+        int mul = n;
+        for(int i=0; i<m-1; i++){
+            n = n*mul;
         }
         
-        printf("#%d %d\n", test_case, sum);
-        
+        printf("#%d %d\n", t, n);
+
     }
-    
-    return 0;
 }
