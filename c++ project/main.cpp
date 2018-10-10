23e7ec3ab028551d0548d52f5b17300fa586d2f7
김정윤 <skyepodium@gmail.com>
2018-10-10 21:14:58 +0900

    baekjoon 11404 Floyd-Warshall Algorithm

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,64 +1,55 @@
 #include <iostream>
-#include <vector>
-#include <queue>
 
-#define max_int 20001
-#define max_val 2147483647
+#define max_int 101
+#define max_val 100000
 
 using namespace std;
 
-int n, m, start_node, a, b, c;
-int dist[max_int];
+//시간 복잡도: O(n^3)
+//공간 복잡도: O(n^2)
+//사용한 알고리즘: 플로이드 와샬 알고리즘
+//사용한 자료구조: 인접행렬
 
-struct info{
-    int cur;
-    int cost;
-};
-vector<info> v[max_int];
 
-//c++의 priority_queue는 max_heap이어서 min_heap으로 바꿔준다.
-struct cmp{
-    
-    bool operator()(struct info &a, struct info &b){
-        return a.cost > b.cost;
-    }
-};
+int n, m, a, b, c;
+int d[max_int][max_int];
 
 int main(){
-    scanf("%d %d %d", &n, &m, &start_node);
+    scanf("%d %d", &n, &m);
     
-    //초기화
-    for(int i=1; i<=n; i++) dist[i] = max_val;
-    
-    //경로 입력
-    for(int i=0; i<m; i++){
-        scanf("%d %d %d", &a, &b, &c);
-        v[a].push_back({b, c});
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n; j++){
+            if(i == j) d[i][j] = 0;
+            else d[i][j] = max_val;
+        }
     }
     
-    priority_queue<info, vector<info>, cmp> pq;
-    dist[start_node] = 0;
-    pq.push({start_node, 0});
+    for(int i=0; i<m; i++){
+        scanf("%d %d %d", &a, &b, &c);
+        if(c < d[a][b]) d[a][b] = c;
+    }
     
-    while(!pq.empty()){
-        
-        info node = pq.top();
-        pq.pop();
-        
-        for(int i=0; i<v[node.cur].size(); i++){
-            info next = v[node.cur][i];
-            
-            if(dist[next.cur] > dist[node.cur] + next.cost){
-                dist[next.cur] = dist[node.cur] + next.cost;
-                pq.push(next);
+    //1. 경유지
+    for(int k=1; k<=n; k++){
+        //2. 시작점
+        for(int i=1; i<=n; i++){
+            //3. 끝점
+            for(int j=1; j<=n; j++){
+                
+                if(d[i][j] > d[i][k] + d[k][j]){
+                    d[i][j] = d[i][k] + d[k][j];
+                }
+                
             }
         }
     }
     
     for(int i=1; i<=n; i++){
-        if(dist[i] == max_val) printf("INF\n");
-        else printf("%d\n", dist[i]);
+        for(int j=1; j<=n; j++){
+            if(d[i][j] == max_val) printf("0 ");
+            else printf("%d ", d[i][j]);
+        }
+        printf("\n");
     }
     
 }
-
