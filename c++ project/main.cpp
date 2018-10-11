2df487cd1903821ac57ea80ef3018743dfe92dd6
김정윤 <skyepodium@gmail.com>
2018-10-11 19:17:01 +0900

    add .gitignore

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,25 +1,20 @@
 #include <iostream>
 
-#define max_int 501
-#define max_val 501
+#define max_int 21
+#define max_val 21000;
 
 using namespace std;
 
-int n, m, first, second, q, a, b;
+int n;
 int d[max_int][max_int];
 
 int main(){
-    scanf("%d %d", &n, &m);
+    scanf("%d", &n);
     
     for(int i=1; i<=n; i++){
         for(int j=1; j<=n; j++){
-            if(i != j) d[i][j] = max_val;
+            scanf("%d", &d[i][j]);
         }
-    }
-    
-    for(int i=0; i<m; i++){
-        scanf("%d %d", &first, &second);
-        d[first][second] = 1;
     }
     
     for(int k=1; k<=n; k++){
@@ -32,18 +27,10 @@ int main(){
         }
     }
     
-    
-    int cnt = 0;
     for(int i=1; i<=n; i++){
-        bool is_right = true;
         for(int j=1; j<=n; j++){
-            if(d[i][j] == max_val && d[j][i] == max_val){
-                is_right = false;
-                break;
-            }
+            cout << d[i][j] << " ";
         }
-        if(is_right) cnt++;
+        cout << endl;
     }
-    
-    printf("%d\n", cnt);
 }
