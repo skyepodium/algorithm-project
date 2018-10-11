f0002faea8049a7922f8e3843594375528559e2d
김정윤 <skyepodium@gmail.com>
2018-10-11 20:03:55 +0900

    baekjoon 1922 dijoint-set

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,47 +1,53 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
-#define max_int 401
-#define max_val 4400000
+#define max_int 1001
 
 using namespace std;
 
-int n, m, a, b, c;
-int d[max_int][max_int];
+struct info{
+    int start;
+    int end;
+    int cost;
+};
 
-int min(int a, int b){
-    return a < b ? a : b;
+int n, m, a, b, c;
+int d[max_int];
+vector<info> v;
+
+bool cmp(const info &a, const info &b){
+    return a.cost < b.cost;
+}
+
+int find(int node){
+    if(d[node] == node) return node;
+    else return d[node] = find(d[node]);
 }
 
 int main(){
     scanf("%d %d", &n, &m);
     
-    for(int i=1; i<=n; i++){
-        for(int j=1; j<=n; j++){
-            d[i][j] = max_val;
-        }
-    }
+    for(int i=1; i<=n; i++) d[i] = i;
     
     for(int i=0; i<m; i++){
         scanf("%d %d %d", &a, &b, &c);
-        d[a][b] = c;
+        v.push_back({a, b, c});
     }
     
-    for(int k=1; k<=n; k++){
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=n; j++){
-                if(d[i][j] > d[i][k] + d[k][j]){
-                    d[i][j] = d[i][k] + d[k][j];
-                }
-            }
+    sort(v.begin(), v.end(), cmp);
+    
+    int result = 0;
+    for(int i=0; i<v.size(); i++){
+        int s = v[i].start;
+        int e = v[i].end;
+        
+        if(find(s) != find(e)){
+            d[find(s)] = find(e);
+            result = result + v[i].cost;
         }
     }
     
-    int result = max_val;
-    for(int i=1; i<=n; i++){
-        result = min(result, d[i][i]);
-    }
-    
-    if(result == max_val) printf("-1\n");
-    else printf("%d\n", result);
+    printf("%d\n", result);
     
 }
