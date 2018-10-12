3ab88fb26e4bc39a6e9b1efc13bb595b165b2c47
김정윤 <skyepodium@gmail.com>
2018-10-12 20:00:04 +0900

    baekjoon 1516 topological sorting

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,75 +1,60 @@
 #include <iostream>
-#include <vector>
 #include <queue>
+#include <vector>
 #include <algorithm>
 
-#define max_int 1001
+#define max_int 501
 
 using namespace std;
 
-int t, n, k, start_node, end_node, final_node;
-int cost[max_int];
-int d[max_int];
+int n, num;
 int ind[max_int];
+int d[max_int];
+int cost[max_int];
 vector<int> v[max_int];
 
-void init(){
-    for(int i=1; i<=n; i++){
-        cost[i] = 0;
-        d[i] = 0;
-        ind[i] = 0;
-        v[i].clear();
-    }
-}
-
 int main(){
-    scanf("%d", &t);
+    scanf("%d", &n);
     
-    while(t--){
-        scanf("%d %d", &n, &k);
+    for(int i=1; i<=n; i++){
+        scanf("%d", &cost[i]);
         
-        init();
-
-        
-        for(int i=1; i<=n; i++){
-            scanf("%d", &cost[i]);
-        }
-        
-        for(int i=1; i<=k; i++){
-            scanf("%d %d", &start_node, &end_node);
-            v[start_node].push_back(end_node);
-            ind[end_node] += 1;
-        }
-        
-        scanf("%d", &final_node);
-        
-        queue<int> q;
-        for(int i=1; i<=n; i++){
-            if(ind[i] == 0){
-                q.push(i);
-                d[i] = cost[i];
-            }
-        }
-        
-        while(!q.empty()){
-            int node = q.front();
-            q.pop();
+        while(true){
+            scanf("%d", &num);
+            if(num == -1) break;
             
-            for(int i=0; i<v[node].size(); i++){
-                int next = v[node][i];
-                
-                d[next] = max(d[next], d[node] + cost[next]);
-                
-                ind[next] -= 1;
-                
-                if(ind[next] == 0){
-                    q.push(next);
-                }
+            v[num].push_back(i);
+            ind[i] += 1;
+        }
+    }
+    
+    queue<int> q;
+    for(int i=1; i<=n; i++){
+        if(ind[i] == 0){
+            q.push(i);
+            d[i] = cost[i];
+        }
+    }
+    
+    while(!q.empty()){
+        int node = q.front();
+        q.pop();
+        
+        for(int i=0; i<v[node].size(); i++){
+            int next = v[node][i];
+            
+            d[next] = max(d[next], d[node] + cost[next]);
+            
+            ind[next] -= 1;
+            
+            if(ind[next] == 0){
+                q.push(next);
             }
         }
-        
-        printf("%d\n", d[final_node]);
-        
+    }
+    
+    for(int i=1; i<=n; i++){
+        printf("%d\n", d[i]);
     }
     
 }
