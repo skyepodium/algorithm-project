ddf31e35491084a8335f368db7e3f6afdd8195e9
김정윤 <skyepodium@gmail.com>
2018-10-12 18:16:54 +0900

    baekjoon 14889

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -2,52 +2,71 @@
 #include <vector>
 #include <algorithm>
 
-#define max_int 1001
-
 using namespace std;
 
-struct info{
-    int start;
-    int end;
-    int cost;
-};
+//시간 복잡도: O(n^6)
+//공간 복잡도: O(n^2)
+//사용한 알고리즘: 순열, 2중 for문
+//사용한 자료구조: 2차원 배열
 
-int n, m, a, b, c;
-int d[max_int];
-vector<info> v;
+int d[21][21];
 
-bool cmp(const info &a, const info &b){
-    return a.cost < b.cost;
-}
-
-int find(int node){
-    if(d[node] == node) return node;
-    else return d[node] = find(d[node]);
-}
 
 int main(){
-    scanf("%d %d", &n, &m);
     
-    for(int i=1; i<=n; i++) d[i] = i;
+    int n;
+    cin >> n;
     
-    for(int i=0; i<m; i++){
-        scanf("%d %d %d", &a, &b, &c);
-        v.push_back({a, b, c});
-    }
-    
-    sort(v.begin(), v.end(), cmp);
-    
-    int result = 0;
-    for(int i=0; i<v.size(); i++){
-        int s = v[i].start;
-        int e = v[i].end;
-        
-        if(find(s) != find(e)){
-            d[find(s)] = find(e);
-            result = result + v[i].cost;
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n; j++){
+            cin >> d[i][j];
         }
     }
     
-    printf("%d\n", result);
+    vector<int> v;
+    for(int i=0; i<n/2; i++){
+        v.push_back(0);
+    }
+    for(int i=0; i<n/2; i++){
+        v.push_back(1);
+    }
+    
+    
+    int result = 4500;
+    
+    vector<int> right;
+    vector<int> left;
+    do{
+        right.clear();
+        left.clear();
+        
+        for(int i=0; i<n; i++){
+            if(v[i] == 1){
+                right.push_back(i+1);
+            }else{
+                left.push_back(i+1);
+            }
+        }
+        
+        int right_sum = 0;
+        for(int i=0; i<right.size(); i++){
+            for(int j=i+1; j<right.size(); j++){
+                right_sum = right_sum + d[right[i]][right[j]] + d[right[j]][right[i]];
+            }
+        }
+        
+        int left_sum = 0;
+        for(int i=0; i<left.size(); i++){
+            for(int j=i+1; j<left.size(); j++){
+                left_sum = left_sum + d[left[i]][left[j]] + d[left[j]][left[i]];
+            }
+        }
+        
+        result = min(abs(right_sum - left_sum), result);
+        
+    }while(next_permutation(v.begin(), v.end()));
+    
+    cout << result << endl;
     
 }
+
