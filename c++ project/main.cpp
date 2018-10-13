d6ece8f173f40ee028bd095a87ee9124db803bfb
김정윤 <skyepodium@gmail.com>
2018-10-13 14:43:58 +0900

    baekjoon 1197 disjoint-set

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,49 +1,52 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
-#define max_int 1000001
+#define max_int 10001
 
 using namespace std;
 
-int t, n, m, a, b, c;
+int n, m, a, b, c;
+struct info{
+    int start;
+    int end;
+    int cost;
+};
 int d[max_int];
 
+bool cmp(const info &a, const info &b){
+    return a.cost < b.cost;
+}
+
 int find(int node){
-    
     if(d[node] == node) return node;
     else return d[node] = find(d[node]);
 }
 
-int main(int argc, char** argv)
-{
-    scanf("%d", &t);
+int main(){
+    scanf("%d %d", &n, &m);
     
-    for(int test_case = 1; test_case <= t; ++test_case)
-    {
-        scanf("%d %d", &n, &m);
-        
-        for(int i=1; i<=n; i++) d[i] = i;
-        
-        printf("#%d ", test_case);
-        for(int i=0; i<m; i++){
-            scanf("%d %d %d", &a, &b, &c);
-            
-            //합집합
-            if(a == 0){
-                d[find(b)] = find(c);
-            }
-            //찾는다.
-            else{
-                if(find(b) == find(c)){
-                    printf("1");
-                }
-                else{
-                    printf("0");
-                }
-            }
-        }
-        printf("\n");
+    for(int i=1; i<=n; i++) d[i] = i;
+    
+    vector<info> v;
+    for(int i=0; i<m; i++){
+        scanf("%d %d %d", &a, &b, &c);
+        v.push_back({a, b, c});
     }
     
-    return 0;
+    sort(v.begin(), v.end(), cmp);
+ 
+    int result = 0;
+    for(int i=0; i<(int)v.size(); i++){
+        int start = v[i].start;
+        int end = v[i].end;
+        int cost = v[i].cost;
+        
+        if(find(start) != find(end)){
+            d[find(start)] = find(end);
+            result = result + cost;
+        }
+    }
+    
+    printf("%d\n", result);
 }
-
