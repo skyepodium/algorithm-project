6108b08414ed6dc2331e8dd0440742d3479714f2
김정윤 <skyepodium@gmail.com>
2018-10-14 15:19:53 +0900

    baekjoon 1389 floyd-warshall

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,52 +1,63 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
+#include <vector>
 
-#define max_int 10001
+#define max_int 101
+#define max_val 1010000
 
 using namespace std;
 
-int n, m, a, b, c;
+int n, m;
+int d[max_int][max_int];
+
 struct info{
-    int start;
-    int end;
-    int cost;
+    int index;
+    int num;
 };
-int d[max_int];
 
 bool cmp(const info &a, const info &b){
-    return a.cost < b.cost;
-}
-
-int find(int node){
-    if(d[node] == node) return node;
-    else return d[node] = find(d[node]);
+    return a.num < b.num;
 }
 
 int main(){
     scanf("%d %d", &n, &m);
     
-    for(int i=1; i<=n; i++) d[i] = i;
-    
-    vector<info> v;
-    for(int i=0; i<m; i++){
-        scanf("%d %d %d", &a, &b, &c);
-        v.push_back({a, b, c});
-    }
-    
-    sort(v.begin(), v.end(), cmp);
- 
-    int result = 0;
-    for(int i=0; i<(int)v.size(); i++){
-        int start = v[i].start;
-        int end = v[i].end;
-        int cost = v[i].cost;
-        
-        if(find(start) != find(end)){
-            d[find(start)] = find(end);
-            result = result + cost;
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n; j++){
+            if(i!=j) d[i][j] = max_val;
         }
     }
     
-    printf("%d\n", result);
+    for(int i=0; i<m; i++){
+        int a, b;
+        scanf("%d %d", &a, &b);
+        d[a][b] = 1;
+        d[b][a] = 1;
+    }
+    
+    for(int k=1; k<=n; k++){
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                
+                if(d[i][j] > d[i][k] + d[k][j]){
+                    d[i][j] = d[i][k] + d[k][j];
+                }
+            }
+        }
+    }
+    
+    vector<info> result;
+    for(int i=1; i<=n; i++){
+        int ret = 0;
+        for(int j=1; j<=n; j++){
+            if(i!=j) ret += d[i][j];
+        }
+        result.push_back({i, ret});
+    }
+
+    sort(result.begin(), result.end(), cmp);
+    
+    
+    
+    printf("%d\n", result[0].index);
 }
