2c4ae87a41baea66317c21614d12c9dd45670d93
김정윤 <skyepodium@gmail.com>
2018-10-16 23:06:06 +0900

    baekjoon 11722

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,55 +1,33 @@
 #include <iostream>
-#include <queue>
-#include <vector>
 #include <algorithm>
 
-#define max_int 10001
+#define max_int 1001
 
 using namespace std;
 
-int ind[max_int];
 int d[max_int];
-int n, m, start_node, end_node, a, b, c;
-
-struct info{
-    int cur;
-    int cost;
-};
-
-vector<info> v[max_int];
+int a[max_int];
+int n;
 
 int main(){
-    scanf("%d %d", &n, &m);
+    scanf("%d", &n);
     
-    for(int i=0; i<m; i++){
-        scanf("%d %d %d", &a, &b, &c);
-        v[a].push_back({b, c});
-        ind[b] += 1;
+    for(int i=1; i<=n; i++){
+        scanf("%d", &a[i]);
+        d[i] = 1;
     }
     
-    scanf("%d %d", &start_node, &end_node);
-    
-    queue<info> q;
-    d[start_node] = 0;
-    q.push({start_node, d[start_node]});
-    
-    while(!q.empty()){
-        
-        info node = q.front();
-        q.pop();
-        
-        for(int i=0; i<v[node.cur].size(); i++){
-            info next = v[node.cur][i];
-            
-            d[next.cur] = max(d[next.cur], d[node.cur] + next.cost);
-            
-            ind[next.cur] -= 1;
-            
-            if(ind[next.cur] == 0){
-                q.push(next);
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<i; j++){
+            if(a[j] > a[i]){
+                d[i] = max(d[i], d[j]+1);
             }
         }
     }
     
-    cout <<d[end_node] << endl;
+    int result = 0;
+    for(int i=1; i<=n; i++){
+        result = max(result, d[i]);
+    }
+    printf("%d\n", result);
 }
