a0a322ef08227ba09a9a0f4f010fe17d13da21f1
김정윤 <skyepodium@gmail.com>
2018-10-20 23:15:49 +0900

    baekjoon 10833

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,20 +1,16 @@
 #include <iostream>
-#include <algorithm>
 
-#define max_int 1001
 using namespace std;
 
-int n;
-int d[max_int];
-int max_score = 0;
-int min_score = 1001;
+int n, a, b, result = 0;
 
 int main(){
     scanf("%d", &n);
-    for(int i=1; i<=n; i++){
-        scanf("%d", &d[i]);
-        max_score = max(max_score, d[i]);
-        min_score = min(min_score, d[i]);
+    
+
+    for(int i=0; i<n; i++){
+        scanf("%d %d", &a, &b);
+        result += b%a;
     }
-    printf("%d\n", max_score - min_score);
+    printf("%d\n", result);
 }
