2fbfe4c60d1ba29957e7fe9120e613666b4db0f8
김정윤 <skyepodium@gmail.com>
2018-10-20 16:34:16 +0900

    beakjoon 2056 topological sorting

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,27 +1,53 @@
 #include <iostream>
-#include <algorithm>
 #include <queue>
+#include <vector>
+#include <algorithm>
 
-#define max_int 1001
+#define max_int 10001
 
 using namespace std;
 
+int n, cnt, num;
+int cost[max_int];
 int d[max_int];
-int a[max_int];
-int n;
+int ind[max_int];
+vector<int> v[max_int];
 
 int main(){
     scanf("%d", &n);
     
     for(int i=1; i<=n; i++){
-        scanf("%d", &a[i]);
-        d[i] = 1;
+        scanf("%d", &cost[i]);
+        
+        scanf("%d", &cnt);
+        
+        for(int j=0; j<cnt; j++){
+            scanf("%d", &num);
+            v[num].push_back(i);
+            ind[i] += 1;
+        }
     }
     
+    queue<int> q;
     for(int i=1; i<=n; i++){
-        for(int j=1; j<i; j++){
-            if(a[j] < a[i]){
-                d[i] = max(d[i], d[j]+1);
+        if(ind[i] == 0){
+            q.push(i);
+            d[i] = cost[i];
+        }
+    }
+    
+    while(!q.empty()){
+        int node = q.front();
+        q.pop();
+        
+        for(int i=0; i<v[node].size(); i++){
+            int next = v[node][i];
+            
+            d[next] = max(d[next], d[node] + cost[next]);
+            ind[next] -= 1;
+            
+            if(ind[next] == 0){
+                q.push(next);
             }
         }
     }
@@ -30,5 +56,7 @@ int main(){
     for(int i=1; i<=n; i++){
         result = max(result, d[i]);
     }
+    
     printf("%d\n", result);
+    
 }
