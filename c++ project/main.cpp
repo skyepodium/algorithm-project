29d5c2ccabd6ca1c06cc73c583f976528298d922
김정윤 <skyepodium@gmail.com>
2018-10-20 23:38:16 +0900

    SWEA 5948 by SET

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,16 +1,41 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
 
 using namespace std;
 
-int n, a, b, result = 0;
+int t;
+int d[7];
 
-int main(){
-    scanf("%d", &n);
+int main(int argc, char** argv)
+{
+    scanf("%d", &t);
     
+    for(int test_case = 1; test_case <= t; ++test_case)
+    {
+        for(int i=0; i<7; i++) scanf("%d", &d[i]);
 
-    for(int i=0; i<n; i++){
-        scanf("%d %d", &a, &b);
-        result += b%a;
+        set<int> result;
+        for(int i=0; i<7; i++){
+            for(int j=i+1; j<7; j++){
+                for(int k=j+1; k<7; k++){
+                    result.insert(d[i]+d[j]+d[k]);
+                }
+            }
+        }
+        
+        set<int>::iterator iter;
+        int cnt = 1;
+        for(iter = --result.end(); iter != result.begin(); iter--){
+            if(cnt == 5){
+                printf("#%d %d\n", test_case, *iter);
+            }
+            cnt++;
+        }
+        
     }
-    printf("%d\n", result);
+    
+    return 0;
 }
+
