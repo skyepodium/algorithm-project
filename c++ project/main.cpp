9409439c4881213278583a49650e947fde33568d
김정윤 <skyepodium@gmail.com>
2018-10-25 21:10:59 +0900

    baekjoon 11003 sliding window add information

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,13 +1,20 @@
 #include <iostream>
 #include <queue>
-#include <vector>
 
 #define max_int 5000001
+
 using namespace std;
+
+//시간 복잡도: O(n)
+//공간 복잡도: O(n)
+//사용한 알고리즘: 슬라이딩 윈도우
+//사용한 자료구조: 덱
+
 
 int n, l;
 int a[max_int];
 int d[max_int];
+int ans[max_int];
 
 int main(){
     scanf("%d %d", &n, &l);
@@ -17,9 +24,9 @@ int main(){
     }
     
     deque<pair<int, int>> q;
-    vector<int> ans(n);
     for(int i=0; i<n; i++){
         int cur = a[i];
+        
         if(!q.empty() && q.front().second <= i-l){
             q.pop_front();
         }
@@ -30,13 +37,11 @@ int main(){
         
         q.push_back(make_pair(cur, i));
         ans[i] = q.front().first;
+        
     }
     
     for(int i=0; i<n; i++){
         printf("%d ", ans[i]);
     }
     printf("\n");
-    
 }
-
-
