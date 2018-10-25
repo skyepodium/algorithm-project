5c1f3ca3ddea3a92e9fd8623044eb145d7df7c5e
김정윤 <skyepodium@gmail.com>
2018-10-25 20:50:07 +0900

    baekjoon 11003 by sliding window

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,24 +1,42 @@
 #include <iostream>
+#include <queue>
+#include <vector>
 
-#define max_int 100001
-#define lld long long int
-
+#define max_int 5000001
 using namespace std;
 
+int n, l;
 int a[max_int];
-lld d[max_int];
-int n, m, s, e;
+int d[max_int];
 
 int main(){
-    scanf("%d %d", &n, &m);
+    scanf("%d %d", &n, &l);
     
-    for(int i=1; i<=n; i++){
+    for(int i=0; i<n; i++){
         scanf("%d", &a[i]);
-        d[i] = a[i] + d[i-1];
     }
     
-    for(int i=0; i<m; i++){
-        scanf("%d %d", &s, &e);
-        printf("%lld\n", d[e] - d[s-1]);
+    deque<pair<int, int>> q;
+    vector<int> ans(n);
+    for(int i=0; i<n; i++){
+        int cur = a[i];
+        if(!q.empty() && q.front().second <= i-l){
+            q.pop_front();
+        }
+        
+        while(!q.empty() && q.back().first > cur){
+            q.pop_back();
+        }
+        
+        q.push_back(make_pair(cur, i));
+        ans[i] = q.front().first;
     }
+    
+    for(int i=0; i<n; i++){
+        printf("%d ", ans[i]);
+    }
+    printf("\n");
+    
 }
+
+
