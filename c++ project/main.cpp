5bbb5c38abe9e711df141ba9fca6c4df11b7453a
김정윤 <skyepodium@gmail.com>
2018-10-26 21:37:58 +0900

    baekjoon 10986

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,50 +1,33 @@
 #include <iostream>
 
-#define max_int 100001
+#define max_int 1000001
+#define lld long long int
+
 using namespace std;
 
-int n, m, s, e, idx;
-int tree[max_int*4];
-
-int find_data(int start, int end){
-    
-    int result = 0;
-    
-    while(start <= end){
-        
-        if(start%2 == 1) result += tree[start];
-        if(end %2 == 0) result += tree[end];
-        
-        start = (start + 1) / 2;
-        end = (end - 1) / 2;
-    }
-    return result;
-}
+int a[max_int];
+lld cnt[max_int];
+int n, m;
+lld sum = 0;
 
 int main(){
     scanf("%d %d", &n, &m);
-    
-    idx = 1;
-    while(idx < n){
-        idx *= 2;
-    }
-    idx--;
-    
-    //리프노드에 수를 입력받는다.
-    for(int i=1; i<=n; i++){
-        scanf("%d", &tree[idx+i]);
+    for(int i=0; i<n; i++){
+        scanf("%d", &a[i]);
+        a[i] %= m;
     }
     
-    //sum_index_tree를 구성한다.
-    for(int i=idx; i>=1; i--){
-        tree[i] = tree[i*2] + tree[i*2+1];
+    cnt[0] = 1;
+    for(int i=0; i<n; i++){
+        sum += a[i];
+        sum %= m;
+        cnt[sum]++;
     }
-    
-    //질문을 입력받는다.
+
+    lld ans = 0;
     for(int i=0; i<m; i++){
-        scanf("%d %d", &s, &e);
-        printf("%d\n", find_data(idx+s, idx+e));
+        ans += (cnt[i] * (cnt[i] - 1))/2;
     }
-    
+    printf("%lld\n", ans);
     
 }
