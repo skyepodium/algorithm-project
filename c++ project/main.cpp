5c4511123fb4c830a839b30feda44b503ddfcc21
김정윤 <skyepodium@gmail.com>
2018-10-26 19:34:01 +0900

    baekjoon 10868 by segement tree & i retry again again

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
 #include <algorithm>
 
-#define max_int 100001
 #define max_val 2147483647
+#define max_int 100001
 
 using namespace std;
 
-int n, m, s, e;
-int idx;
+int n, m, idx, a, b;
 int tree[max_int*4];
 
 int find_data(int start, int end){
@@ -19,11 +18,10 @@ int find_data(int start, int end){
         if(start%2 == 1) result = min(result, tree[start]);
         if(end%2 == 0) result = min(result, tree[end]);
         
-        start = (start + 1)/2;
-        end = (end - 1)/2;
+        start = (start + 1) / 2;
+        end = (end - 1) / 2;
         
     }
-    
     
     return result;
 }
@@ -31,27 +29,26 @@ int find_data(int start, int end){
 int main(){
     scanf("%d %d", &n, &m);
     
-    //1. n을 넘는 2의 제곱수를 구한다.
-    idx=1;
+    idx = 1;
     while(idx<n){
         idx = idx*2;
     }
     idx--;
     
-    //2. 리프노드에 수를 입력받는다.
+    //리프노드에 수를 입력받는다.
     for(int i=1; i<=n; i++){
-        scanf("%d", &tree[i+idx]);
+        scanf("%d", &tree[idx+i]);
     }
     
-    //3. min_index 트리를 구성한다.
+    //min_indxed_tree를 구성한다.
     for(int i=idx; i>=1; i--){
         tree[i] = min(tree[i*2], tree[i*2+1]);
     }
     
-    //4. 질문을 입력받는다.
+    //쿼리를 입력받는다.
     for(int i=0; i<m; i++){
-        scanf("%d %d", &s, &e);
-        printf("%d\n", find_data(s+idx, e+idx));
+        scanf("%d %d", &a, &b);
+        printf("%d\n", find_data(a+idx, b+idx));
     }
     
 }
