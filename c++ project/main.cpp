9d2ca5228b4ffd855ee9eab36b3483a93aa72109
김정윤 <skyepodium@gmail.com>
2018-10-26 19:45:52 +0900

    baekjoon 2042 by segement tree, sum_indexed_tree

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,25 +1,33 @@
 #include <iostream>
 #include <algorithm>
 
-#define max_val 2147483647
-#define max_int 100001
+#define lld long long int
+#define max_int 1000001
 
 using namespace std;
 
-int n, m, idx, a, b;
-int tree[max_int*4];
+int n, m, k, idx, a, b, c;
+lld tree[max_int*4];
 
-int find_data(int start, int end){
+void update_tree(int node, lld delta){
     
-    int result = max_val;
+    while(node>0){
+        tree[node] += delta;
+        node = node/2;
+    }
+}
+
+lld find_data(int start, int end){
+    
+    lld result = 0;
     
     while(start <= end){
         
-        if(start%2 == 1) result = min(result, tree[start]);
-        if(end%2 == 0) result = min(result, tree[end]);
+        if(start%2 == 1) result += tree[start];
+        if(end%2 == 0) result += tree[end];
         
-        start = (start + 1) / 2;
-        end = (end - 1) / 2;
+        start = (start + 1)/2;
+        end = (end - 1)/2;
         
     }
     
@@ -27,28 +35,36 @@ int find_data(int start, int end){
 }
 
 int main(){
-    scanf("%d %d", &n, &m);
+    scanf("%d %d %d", &n, &m, &k);
     
     idx = 1;
-    while(idx<n){
-        idx = idx*2;
+    while(idx < n){
+        idx *= 2;
     }
     idx--;
     
-    //리프노드에 수를 입력받는다.
+    //단말 노드에 수를 입력받는다.
     for(int i=1; i<=n; i++){
-        scanf("%d", &tree[idx+i]);
+        scanf("%lld", &tree[idx+i]);
     }
     
-    //min_indxed_tree를 구성한다.
+    //sum_indexed_tree 를 구성한다.
     for(int i=idx; i>=1; i--){
-        tree[i] = min(tree[i*2], tree[i*2+1]);
+        tree[i] = tree[i*2] + tree[i*2+1];
     }
     
-    //쿼리를 입력받는다.
-    for(int i=0; i<m; i++){
-        scanf("%d %d", &a, &b);
-        printf("%d\n", find_data(a+idx, b+idx));
+    for(int i=0; i<m+k; i++){
+        scanf("%d %d %d", &a, &b, &c);
+        
+        //update_tree
+        if(a == 1){
+            update_tree(b+idx, c-tree[idx+b]);
+        }
+        //find_data
+        else{
+            printf("%lld\n", find_data(b+idx, c+idx));
+        }
+        
     }
     
 }
