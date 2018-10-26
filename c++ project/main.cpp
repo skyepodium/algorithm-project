24242413522a451adbedeea5f9f2846ede5c2d4e
김정윤 <skyepodium@gmail.com>
2018-10-26 20:36:08 +0900

    baekjoon 11659 by dynamic programming

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,70 +1,31 @@
 #include <iostream>
-#include <algorithm>
 
-#define lld long long int
-#define max_int 1000001
+#define max_int 100001
 
 using namespace std;
 
-int n, m, k, idx, a, b, c;
-lld tree[max_int*4];
+//시간 복잡도: O(n)
+//공간 복잡도: O(n)
+//사용한 알고리즘: dynamic programming
+//사용한 자료구조: 1차원 배열
 
-void update_tree(int node, lld delta){
-    
-    while(node>0){
-        tree[node] += delta;
-        node = node/2;
-    }
-}
 
-lld find_data(int start, int end){
-    
-    lld result = 0;
-    
-    while(start <= end){
-        
-        if(start%2 == 1) result += tree[start];
-        if(end%2 == 0) result += tree[end];
-        
-        start = (start + 1)/2;
-        end = (end - 1)/2;
-        
-    }
-    
-    return result;
-}
+int n, m, s, e;
+int a[max_int];
+int d[max_int];
 
 int main(){
-    scanf("%d %d %d", &n, &m, &k);
+    scanf("%d %d", &n, &m);
     
-    idx = 1;
-    while(idx < n){
-        idx *= 2;
-    }
-    idx--;
-    
-    //단말 노드에 수를 입력받는다.
     for(int i=1; i<=n; i++){
-        scanf("%lld", &tree[idx+i]);
+        scanf("%d", &a[i]);
+        d[i] = d[i-1] + a[i];
     }
     
-    //sum_indexed_tree 를 구성한다.
-    for(int i=idx; i>=1; i--){
-        tree[i] = tree[i*2] + tree[i*2+1];
+    for(int i=0; i<m; i++){
+        scanf("%d %d", &s, &e);
+        printf("%d\n", d[e] - d[s-1]);
     }
     
-    for(int i=0; i<m+k; i++){
-        scanf("%d %d %d", &a, &b, &c);
-        
-        //update_tree
-        if(a == 1){
-            update_tree(b+idx, c-tree[idx+b]);
-        }
-        //find_data
-        else{
-            printf("%lld\n", find_data(b+idx, c+idx));
-        }
-        
-    }
     
 }
