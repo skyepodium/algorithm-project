fcf5b9272841a92efb6034f9552c25aca2332862
김정윤 <skyepodium@gmail.com>
2018-10-26 20:43:35 +0900

    baekjoon 11659 by segement tree

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,30 +1,49 @@
 #include <iostream>
 
 #define max_int 100001
-
 using namespace std;
 
-//시간 복잡도: O(n)
-//공간 복잡도: O(n)
-//사용한 알고리즘: dynamic programming
-//사용한 자료구조: 1차원 배열
+int n, m, s, e, idx;
+int tree[max_int*4];
 
-
-int n, m, s, e;
-int a[max_int];
-int d[max_int];
+int find_data(int start, int end){
+    
+    int result = 0;
+    
+    while(start <= end){
+        
+        if(start%2 == 1) result += tree[start];
+        if(end %2 == 0) result += tree[end];
+        
+        start = (start + 1) / 2;
+        end = (end - 1) / 2;
+    }
+    return result;
+}
 
 int main(){
     scanf("%d %d", &n, &m);
     
+    idx = 1;
+    while(idx < n){
+        idx *= 2;
+    }
+    idx--;
+    
+    //리프노드에 수를 입력받는다.
     for(int i=1; i<=n; i++){
-        scanf("%d", &a[i]);
-        d[i] = d[i-1] + a[i];
+        scanf("%d", &tree[idx+i]);
     }
     
+    //sum_index_tree를 구성한다.
+    for(int i=idx; i>=1; i--){
+        tree[i] = tree[i*2] + tree[i*2+1];
+    }
+    
+    //질문을 입력받는다.
     for(int i=0; i<m; i++){
         scanf("%d %d", &s, &e);
-        printf("%d\n", d[e] - d[s-1]);
+        printf("%d\n", find_data(idx+s, idx+e));
     }
     
     
