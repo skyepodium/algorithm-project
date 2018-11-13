195d78379aa8fe8672993a97478470de22b19318
김정윤 <skyepodium@gmail.com>
2018-11-13 21:45:09 +0900

    baekjoon 1766

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -3,42 +3,47 @@
 #include <vector>
 
 #define max_int 32001
-
 using namespace std;
 
 int n, m, a, b;
 int ind[max_int];
 vector<int> v[max_int];
 
+struct cmp{
+    bool operator()(const int &a, const int &b){
+        return a > b;
+    }
+};
+
 int main(){
     scanf("%d %d", &n, &m);
-
+    
     for(int i=0; i<m; i++){
         scanf("%d %d", &a, &b);
         v[a].push_back(b);
         ind[b]++;
     }
     
-    queue<int> q;
+    priority_queue<int, vector<int>, cmp> pq;
     for(int i=1; i<=n; i++){
         if(ind[i] == 0){
-            q.push(i);
+            pq.push(i);
         }
     }
     
-    while(!q.empty()){
-        int node = q.front();
+    while(!pq.empty()){
+        int node = pq.top();
+        pq.pop();
         printf("%d ", node);
-        q.pop();
         
         for(int i=0; i<v[node].size(); i++){
             int next = v[node][i];
             
             ind[next]--;
-            if(ind[next] == 0) q.push(next);
+            if(ind[next] == 0) pq.push(next);
         }
+        
     }
+    
     printf("\n");
-    
-    
 }
