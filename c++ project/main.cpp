7d478d6f5e9aa55981bbc64978daaa63055aee47
김정윤 <skyepodium@gmail.com>
2018-11-13 20:10:43 +0900

    baekjoon 2056 topological sorting

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,47 +1,57 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <algorithm>
 
-#define max_int 32001
+#define max_int 10001
+
 using namespace std;
 
-int n, m;
+int n, a, b;
 vector<int> v[max_int];
 int ind[max_int];
-
-struct cmp{
-    bool operator()(const int &a, const int &b){
-        return a > b;
-    }
-};
+int d[max_int];
+int cost[max_int];
+int result = 0;
 
 int main(){
-    scanf("%d %d", &n, &m);
-    
-    for(int i=0; i<m; i++){
-        int a, b;
-        scanf("%d %d", &a, &b);
-        v[a].push_back(b);
-        ind[b]++;
-    }
-    
-    priority_queue<int, vector<int>, cmp> pq;
+    scanf("%d", &n);
     for(int i=1; i<=n; i++){
-        if(ind[i] == 0) pq.push(i);
+        scanf("%d %d", &cost[i], &a);
+        
+        for(int j=0; j<a; j++){
+            scanf("%d", &b);
+            v[b].push_back(i);
+            ind[i]++;
+        }
     }
     
-    while(!pq.empty()){
-        int node = pq.top();
-        printf("%d ", node);
-        pq.pop();
+    queue<int> q;
+    for(int i=1; i<=n; i++){
+        if(ind[i] == 0){
+            q.push(i);
+            d[i] = cost[i];
+        }
+    }
+    
+    while(!q.empty()){
+        int node = q.front();
+        q.pop();
         
         for(int i=0; i<v[node].size(); i++){
             int next = v[node][i];
             
+            d[next] = max(d[next], d[node] + cost[next]);
             ind[next]--;
-            if(ind[next] == 0) pq.push(next);
+            
+            if(ind[next] == 0) q.push(next);
         }
     }
-    printf("\n");
+    
+    for(int i=1; i<=n; i++){
+        result = max(result, d[i]);
+    }
+    
+    printf("%d\n", result);
     
 }
