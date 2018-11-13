2f86809cb5f8d28dd2f56d4d91f90a301f3811cc
김정윤 <skyepodium@gmail.com>
2018-11-13 21:38:22 +0900

    baekjoon 2252

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,57 +1,44 @@
 #include <iostream>
-#include <vector>
 #include <queue>
-#include <algorithm>
+#include <vector>
 
-#define max_int 10001
+#define max_int 32001
 
 using namespace std;
 
-int n, a, b;
-vector<int> v[max_int];
+int n, m, a, b;
 int ind[max_int];
-int d[max_int];
-int cost[max_int];
-int result = 0;
+vector<int> v[max_int];
 
 int main(){
-    scanf("%d", &n);
-    for(int i=1; i<=n; i++){
-        scanf("%d %d", &cost[i], &a);
-        
-        for(int j=0; j<a; j++){
-            scanf("%d", &b);
-            v[b].push_back(i);
-            ind[i]++;
-        }
+    scanf("%d %d", &n, &m);
+
+    for(int i=0; i<m; i++){
+        scanf("%d %d", &a, &b);
+        v[a].push_back(b);
+        ind[b]++;
     }
     
     queue<int> q;
     for(int i=1; i<=n; i++){
         if(ind[i] == 0){
             q.push(i);
-            d[i] = cost[i];
         }
     }
     
     while(!q.empty()){
         int node = q.front();
+        printf("%d ", node);
         q.pop();
         
         for(int i=0; i<v[node].size(); i++){
             int next = v[node][i];
             
-            d[next] = max(d[next], d[node] + cost[next]);
             ind[next]--;
-            
             if(ind[next] == 0) q.push(next);
         }
     }
+    printf("\n");
     
-    for(int i=1; i<=n; i++){
-        result = max(result, d[i]);
-    }
-    
-    printf("%d\n", result);
     
 }
