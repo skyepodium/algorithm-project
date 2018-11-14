7e34ca25cae4de2791411bfe5bb907dd3999fcd9
김정윤 <skyepodium@gmail.com>
2018-11-14 20:36:32 +0900

    baekjoon 1516

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,46 +1,56 @@
 #include <iostream>
-#include <queue>
 #include <vector>
+#include <queue>
+#include <algorithm>
 
-#define max_int 32001
-
+#define max_int 501
 using namespace std;
 
-int n, m, a, b;
-vector<int> v[max_int];
+int n, num;
+int cost[max_int];
+int d[max_int];
 int ind[max_int];
-
-struct cmp{
-    bool operator()(const int &a, const int &b){
-        return a > b;
-    }
-};
+vector<int> v[max_int];
 
 int main(){
-    scanf("%d %d", &n, &m);
-
-    for(int i=0; i<m; i++){
-        scanf("%d %d", &a, &b);
-        v[a].push_back(b);
-        ind[b]++;
-    }
-    
-    priority_queue<int, vector<int>, cmp> pq;
+    scanf("%d", &n);
     for(int i=1; i<=n; i++){
-        if(ind[i] == 0) pq.push(i);
+        scanf("%d", &cost[i]);
+        
+        while(true){
+            scanf("%d", &num);
+            if(num == -1) break;
+            
+            v[num].push_back(i);
+            ind[i]++;
+        }
     }
     
-    while(!pq.empty()){
-        int node = pq.top();
-        printf("%d ", node);
-        pq.pop();
+    queue<int> q;
+    for(int i=1; i<=n; i++){
+        if(ind[i] == 0){
+            q.push(i);
+            d[i] = cost[i];
+        }
+    }
+    
+    while(!q.empty()){
+        int node = q.front();
+        q.pop();
         
         for(int i=0; i<v[node].size(); i++){
             int next = v[node][i];
             
+            d[next] = max(d[next], cost[next] + d[node]);
+            
             ind[next]--;
-            if(ind[next] == 0) pq.push(next);
+            if(ind[next] == 0) q.push(next);
         }
     }
-    printf("\n");
+    
+    for(int i=1; i<=n; i++){
+        printf("%d\n", d[i]);
+    }
+    
+    
 }
