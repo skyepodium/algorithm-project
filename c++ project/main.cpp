5094b0bd5689a06ceff916dab931eec3fa129f28
김정윤 <skyepodium@gmail.com>
2018-11-14 20:23:47 +0900

    baekjoon 1766 topological sorting

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,46 +1,46 @@
 #include <iostream>
+#include <queue>
 #include <vector>
-#include <algorithm>
 
-#define max_int 10001
+#define max_int 32001
+
 using namespace std;
 
-int n, m;
-int d[max_int];
-struct info{
-    int cur;
-    int next;
-    int cost;
+int n, m, a, b;
+vector<int> v[max_int];
+int ind[max_int];
+
+struct cmp{
+    bool operator()(const int &a, const int &b){
+        return a > b;
+    }
 };
-
-int find(int node){
-    if(d[node] == node) return node;
-    else return d[node] = find(d[node]);
-}
-
-bool cmp(const info &a, const info &b){
-    return a.cost < b.cost;
-}
 
 int main(){
     scanf("%d %d", &n, &m);
-    for(int i=1; i<=n; i++) d[i] = i;
-    
-    
-    vector<info> v(m);
+
     for(int i=0; i<m; i++){
-        scanf("%d %d %d", &v[i].cur, &v[i].next, &v[i].cost);
+        scanf("%d %d", &a, &b);
+        v[a].push_back(b);
+        ind[b]++;
     }
     
-    sort(v.begin(), v.end(), cmp);
+    priority_queue<int, vector<int>, cmp> pq;
+    for(int i=1; i<=n; i++){
+        if(ind[i] == 0) pq.push(i);
+    }
     
-    int result = 0;
-    for(int i=0; i<v.size(); i++){
-        if(find(v[i].cur) != find(v[i].next)){
-            d[find(v[i].cur)] = find(v[i].next);
-            result += v[i].cost;
+    while(!pq.empty()){
+        int node = pq.top();
+        printf("%d ", node);
+        pq.pop();
+        
+        for(int i=0; i<v[node].size(); i++){
+            int next = v[node][i];
+            
+            ind[next]--;
+            if(ind[next] == 0) pq.push(next);
         }
     }
-    
-    printf("%d\n", result);
+    printf("\n");
 }
