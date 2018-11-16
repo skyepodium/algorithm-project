d3dcf75c644961d7e426c984fff549800ed30c4f
김정윤 <skyepodium@gmail.com>
2018-11-16 19:48:55 +0900

    baekjoon 4195 by disjoint-set, map

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,30 +1,60 @@
 #include <iostream>
+#include <map>
 
-#define max_int 1000001
-
+#define max_int 100001
 using namespace std;
 
-int n, m, a, b, c;
-int d[max_int];
+int t, n;
+int d[max_int*2];
+int num[max_int*2];
+char a[21];
+char b[21];
 
 int find(int node){
     if(d[node] == node) return node;
     else return d[node] = find(d[node]);
 }
 
+int merge(int x, int y){
+    
+    x = find(x);
+    y = find(y);
+    
+    if(x != y){
+        d[x] = y;
+        num[y] += num[x];
+        num[x] = 1;
+    }
+    
+    return num[y];
+}
+
+
 int main(){
-    scanf("%d %d", &n, &m);
+    scanf("%d", &t);
     
-    for(int i=1; i<=n; i++) d[i] = i;
-    
-    for(int i=0; i<m; i++){
-        scanf("%d %d %d", &a, &b, &c);
-        if(a == 0){
-            d[find(b)] = find(c);
+    while(t--){
+        scanf("%d", &n);
+        
+        for(int i=1; i<=2*n; i++){
+            d[i] = i;
+            num[i] = 1;
         }
-        else{
-            if(find(b) == find(c)) printf("YES\n");
-            else printf("NO\n");
+        
+        int idx = 1;
+        map<string, int> mp;
+        for(int i=0; i<n; i++){
+            scanf("%s %s", a, b);
+            
+            if(mp.count(a) == 0){
+                mp[a] = idx++;
+            }
+
+            if(mp.count(b) == 0){
+                mp[b] = idx++;
+            }
+
+            printf("%d\n", merge(mp[a], mp[b]));
         }
     }
 }
