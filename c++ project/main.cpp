9d494077f3033d5fcdece5ecd81715a7f7eb65aa
김정윤 <skyepodium@gmail.com>
2018-11-18 13:21:02 +0900

    baekjoon 9372 by prim's algorithm

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,51 +1,62 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 
 #define max_int 1001
 using namespace std;
 
 int t, n, m, a, b;
 int d[max_int];
+bool check[max_int];
 
 struct info{
     int cur;
     int next;
 };
 
-int find(int node){
-    if(d[node] == node) return node;
-    else return d[node] = find(d[node]);
-}
-
 int main(){
     scanf("%d", &t);
     
     while(t--){
+        
         scanf("%d %d", &n, &m);
-        for(int i=1; i<=n; i++) d[i] = i;
-
-        vector<info> v;
+        for(int i=1; i<=n; i++){
+            d[i] = i;
+            check[i] = false;
+        }
+        
+        vector<int> v[max_int];
         for(int i=0; i<m; i++){
             scanf("%d %d", &a, &b);
-            v.push_back({a, b});
+            v[a].push_back(b);
+            v[b].push_back(a);
+        }
+        
+        check[1] = true;
+        queue<int> q;
+        for(int i=0; i<v[1].size(); i++){
+            q.push(v[1][i]);
         }
         
         int result = 0;
-        for(int i=0; i<v.size(); i++){
-            int cur = find(v[i].cur);
-            int next = find(v[i].next);
+        for(int i=0; i<n-1; i++){
+            int next;
+            while(!q.empty()){
+                next = q.front();
+                q.pop();
+                
+                if(check[next] == false){
+                    check[next] = true;
+                    result++;
+                    break;
+                }
+            }
             
-            if(cur != next){
-                d[cur] = next;
-                result++;
+            for(int i=0; i<v[next].size(); i++){
+                q.push(v[next][i]);
             }
         }
         printf("%d\n", result);
         
     }
-    
-    
-    
-    
-    
 }
