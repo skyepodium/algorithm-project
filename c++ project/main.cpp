cb30b1053cb4b2a40175387b2d90bccaeb2a5264
김정윤 <skyepodium@gmail.com>
2018-11-18 16:11:59 +0900

    baekjoon 1865 by Bellman-Ford algorithm

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,54 +1,59 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
-#define max_int 100001
+#define max_int 501
+#define max_val 2147483647
 
 using namespace std;
 
-int d[max_int];
-int n, m, a, b, c;
+int t, n, m, w, a, b, c;
+int dist[max_int];
+
 struct info{
     int cur;
     int next;
     int cost;
 };
 
-vector<info> v;
-
-bool cmp(const info &a, const info &b){
-    return a.cost < b.cost;
-}
-
-int find(int node){
-    if(d[node] == node) return node;
-    else return d[node] = find(d[node]);
-}
-
 int main(){
-    scanf("%d %d", &n, &m);
-    for(int i=1; i<=n; i++) d[i] = i;
-    
-    for(int i=0; i<m; i++){
-        scanf("%d %d %d", &a, &b, &c);
-        v.push_back({a, b, c});
-    }
-    
-    sort(v.begin(), v.end(), cmp);
-    
-    int result = 0;
-    int max_weight = 0;
-    for(int i=0; i<v.size(); i++){
-        int a = find(v[i].cur);
-        int b = find(v[i].next);
-        
-        if(a!=b){
-            d[a] = b;
-            result += v[i].cost;
-            max_weight = max(max_weight, v[i].cost);
+    scanf("%d", &t);
+
+    while(t--){
+        scanf("%d %d %d", &n, &m, &w);
+        for(int i=1; i<=n; i++) dist[i] = max_val;
+        vector<info> v;
+        for(int i=0; i<m; i++){
+            scanf("%d %d %d", &a, &b, &c);
+            v.push_back({a, b, c});
+            v.push_back({b, a, c});
+            
         }
+        for(int i=0; i<w; i++){
+            scanf("%d %d %d", &a, &b, &c);
+            v.push_back({a, b, -c});
+        }
+        
+        dist[1] = 0;
+        bool negative_cycle = false;
+        for(int i=1; i<=n; i++){
+            for(int j=0; j<v.size(); j++){
+                int cur = v[j].cur;
+                int next = v[j].next;
+                int cost = v[j].cost;
+                
+                if(dist[cur] != max_val && dist[next] > dist[cur] + cost){
+                    dist[next] = dist[cur] + cost;
+                    if(i == n){
+                        negative_cycle = true;
+                    }
+                }
+            }
+        }
+        
+        if(negative_cycle) printf("YES\n");
+        else printf("NO\n");
+
     }
     
-    printf("%d\n", result - max_weight);
     
 }
