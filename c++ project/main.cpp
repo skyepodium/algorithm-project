2f5920280a4ac4b0946fbc00fcd54a2635dc3a72
김정윤 <skyepodium@gmail.com>
2018-11-19 19:59:07 +0900

    baekjoon 2206 by dijkstra algorithm

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,64 +1,64 @@
 #include <iostream>
 #include <queue>
+#include <algorithm>
 
-#define max_int 126
+#define max_int 1001
 #define max_val 2147483647
 using namespace std;
 
-int n, idx = 1;
-int d[max_int][max_int];
+int n, m;
 int a[max_int][max_int];
+int check[max_int][max_int][2];
 int dx[] = {0, 0, 1, -1};
 int dy[] = {-1, 1, 0, 0};
 
 struct info{
     int x;
     int y;
-    int cost;
-};
-
-struct cmp{
-    bool operator()(const info &a, const info &b){
-        return a.cost > b.cost;
-    }
+    bool use;
 };
 
 int main(){
-    while(true){
-        scanf("%d", &n);
-        if(n == 0) break;
-        
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                scanf("%d", &a[i][j]);
-                d[i][j] = max_val;
-            }
+    scanf("%d %d", &n, &m);
+    
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            scanf("%1d", &a[i][j]);
+            check[i][j][0] = max_val;
+            check[i][j][1] = max_val;
         }
+    }
+    
+    queue<info> q;
+    check[0][0][0] = 1;
+    q.push({0, 0, false});
+    while(!q.empty()){
+        info node = q.front();
+        q.pop();
+        int x = node.x;
+        int y = node.y;
+        bool use = node.use;
         
-        d[0][0] = a[0][0];
-        priority_queue<info, vector<info>, cmp> pq;
-        pq.push({0, 0, d[0][0]});
-        while(!pq.empty()){
-            info node = pq.top();
-            pq.pop();
+        for(int i=0; i<4; i++){
+            int nx = x + dx[i];
+            int ny = y + dy[i];
             
-            int x = node.x;
-            int y = node.y;
-            
-            for(int i=0; i<4; i++){
-                int nx = x + dx[i];
-                int ny = y + dy[i];
+            if(nx>=0 && nx<n && ny>=0 && ny<m){
                 
-                if(nx>=0 && nx<n && ny>=0 && ny<n){
-                    if(d[nx][ny] > d[x][y] + a[nx][ny]){
-                        d[nx][ny] = d[x][y] + a[nx][ny];
-                        pq.push({nx, ny, d[nx][ny]});
-                    }
+                if(a[nx][ny] == 0 && check[nx][ny][use] > check[x][y][use] + 1){
+                    check[nx][ny][use] = check[x][y][use] + 1;
+                    q.push({nx, ny, use});
+                }
+                
+                if(a[nx][ny] == 1 && use == false && check[nx][ny][1] > check[x][y][0] + 1){
+                    check[nx][ny][1] = check[x][y][0] + 1;
+                    q.push({nx, ny, 1});
                 }
             }
         }
-        
-        printf("Problem %d: %d\n", idx, d[n-1][n-1]);
-        idx++;
     }
+    
+    if(check[n-1][m-1][false] == max_val && check[n-1][m-1][true] == max_val) printf("-1\n");
+    else printf("%d\n", min(check[n-1][m-1][false], check[n-1][m-1][true]));
 }
+
