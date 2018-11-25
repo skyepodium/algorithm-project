26d6102fafe3887b6667a0c280657a975b53b7f9
김정윤 <skyepodium@gmail.com>
2018-11-25 21:48:24 +0900

    baekjoon 1806 by two pointer

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,16 +1,42 @@
 #include <iostream>
+#include <algorithm>
 
+#define max_int 100001
 using namespace std;
 
-int a, b, temp;
+int n, s;
+int a[max_int];
+int result;
 
 int main(){
-    scanf("%d %d", &a, &b);
-    if(a > b){
-        temp = a;
-        a = b;
-        b = a;
+    scanf("%d %d", &n, &s);
+    
+    for(int i=0; i<n; i++) scanf("%d", &a[i]);
+    
+    int left = 0;
+    int right = 0;
+    int cur = a[0];
+    result = n+1;
+    
+    while(left < n && right < n){
+        
+        if(cur < s){
+            right++;
+            if(right < n) cur += a[right];
+        }
+        else if(cur > s){
+            result = min(result, right-left+1);
+            cur -= a[left];
+            left++;
+        }
+        else{
+            result = min(result, right-left+1);
+            right++;
+            if(right < n) cur += a[right];
+        }
+        
     }
     
-    printf("%d %d\n", b-a, b);
+    if(result > n) printf("0\n");
+    else printf("%d\n", result);
 }
