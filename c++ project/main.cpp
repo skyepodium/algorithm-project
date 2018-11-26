cb7bf4645811d1b80bc931a94c8723a565796a01
김정윤 <skyepodium@gmail.com>
2018-11-26 21:44:30 +0900

    11.26

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,42 +1,121 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
 
-#define max_int 100001
+#define lld long long int
+#define max_int 41
 using namespace std;
 
-int n, s;
+int n, idx1, idx2;
+lld s;
 int a[max_int];
-int result;
+int b[max_int];
+bool check1[1000001];
+bool check2[1000001];
+vector<lld> v;
+vector<lld> w;
 
-int main(){
-    scanf("%d %d", &n, &s);
-    
-    for(int i=0; i<n; i++) scanf("%d", &a[i]);
-    
-    int left = 0;
-    int right = 0;
-    int cur = a[0];
-    result = n+1;
-    
-    while(left < n && right < n){
+void go1(int index, lld sum){
+    if(index == idx1+1){
         
-        if(cur < s){
-            right++;
-            if(right < n) cur += a[right];
-        }
-        else if(cur > s){
-            result = min(result, right-left+1);
-            cur -= a[left];
-            left++;
+        if(sum > 0){
+            if(check1[sum] == false){
+                check1[sum] = true;
+                v.push_back(sum);
+            }
         }
         else{
-            result = min(result, right-left+1);
-            right++;
-            if(right < n) cur += a[right];
+            if(sum != 0 && check2[sum*-1] == false){
+                check2[sum*-1] = true;
+                v.push_back(sum);
+            }
         }
         
+        return;
     }
     
-    if(result > n) printf("0\n");
-    else printf("%d\n", result);
+    sum = sum + a[index];
+    go1(index+1, sum);
+    sum = sum - a[index];
+    go1(index+1, sum);
 }
+
+void go2(int index, lld sum){
+    if(index == idx2+1){
+        
+        if(sum > 0){
+            if(check1[sum] == false){
+                check1[sum] = true;
+                w.push_back(sum);
+            }
+        }
+        else{
+            if(sum != 0 && check2[sum*-1] == false){
+                check2[sum*-1] = true;
+                w.push_back(sum);
+            }
+        }
+        
+        return;
+    }
+    
+    sum = sum + b[index];
+    go2(index+1, sum);
+    sum = sum - b[index];
+    go2(index+1, sum);
+}
+
+int binary_search(int num){
+    int cnt = 0;
+    int start = 0;
+    int end = (int)w.size()-1;
+    int mid = 0;
+    
+    while(start <= end){
+        mid = (start+end)/2;
+        
+        if(w[mid] < num){
+            start = mid + 1;
+        }
+        else if(w[mid] > num){
+            end = mid - 1;
+        }
+        else{
+            cnt++;
+            start = mid + 1;
+        }
+    }
+    
+    return cnt;
+}
+
+int main(){
+    scanf("%d %lld", &n, &s);
+    
+    idx1 = n/2;
+    idx2 = n/2;
+    if(n%2 == 1){
+        idx1 = n/2;
+        idx2 = n/2+1;
+    }
+    for(int i=0; i<idx1; i++) scanf("%d", &a[i]);
+    for(int i=0; i<idx2; i++) scanf("%d", &b[i]);
+    
+    go1(0, 0);
+    for(int i=0; i<=n; i++){
+        check1[i] = false;
+        check2[i] = false;
+    }
+    go2(0, 0);
+    
+    sort(v.begin(), v.end());
+    sort(w.begin(), w.end());
+    
+    int result = 0;
+    for(int i=0; i<(int)v.size(); i++){
+        result += binary_search(s-v[i]);
+        result += binary_search(v[i]-s);
+    }
+    printf("%d\n", result);
+}
+
