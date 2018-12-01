15cdc5147580b066241c88efb6522acb28093dde
김정윤 <skyepodium@gmail.com>
2018-12-01 12:13:18 +0900

    baekjoon 1495 by 0/1 knapsack

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,39 +1,38 @@
 #include <iostream>
-#include <algorithm>
 
-#define max_int 21
-#define health 101
+#define max_int 101
 using namespace std;
 
-int n;
-int d[max_int][health];
-int h[max_int];
-int p[max_int];
+int n, s, m;
+int v[max_int];
+bool d[max_int][1001];
 
 int main(){
-    scanf("%d", &n);
+    scanf("%d %d %d", &n, &s, &m);
     
-    for(int i=1; i<=n; i++) scanf("%d", &h[i]);
-    for(int i=1; i<=n; i++) scanf("%d", &p[i]);
-
-    d[1][100 - h[1]] = p[1];
+    for(int i=1; i<=n; i++) scanf("%d", &v[i]);
+    
+    if(s+v[1] <= m) d[1][s+v[1]] = true;
+    if(s-v[1] >= 0) d[1][s-v[1]] = true;
     
     for(int i=2; i<=n; i++){
-        for(int j=1; j<=100; j++){
+        for(int j=0; j<=m; j++){
             
-            if(100 - h[i] >= j){
-                d[i][j] = max(d[i-1][j], d[i-1][j+h[i]] + p[i]);
+            if(j+v[i] <= m && d[i-1][j+v[i]] != false){
+                d[i][j] = d[i-1][j+v[i]];
             }
-            else{
-                d[i][j] = max(d[i-1][j], d[i][j]);
+            if(j-v[i] >= 0 && d[i-1][j-v[i]] != false){
+                d[i][j] = d[i-1][j-v[i]];
             }
             
         }
     }
-    
-    int result = 0;
-    for(int j=1; j<=100; j++){
-        result = max(result, d[n][j]);
+    int result = -1;
+    for(int j=m; j>=0; j--){
+        if(d[n][j]){
+            result = j;
+            break;
+        }
     }
     printf("%d\n", result);
     
