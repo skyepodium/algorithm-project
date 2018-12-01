a73b713d03d0488e14812d67ed70266ae7ff4e16
김정윤 <skyepodium@gmail.com>
2018-12-01 20:05:27 +0900

    baekjoon 1761 by lca

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -6,28 +6,38 @@
 #define max_int 50001
 using namespace std;
 
-int n, m, a, b;
+int n, m, a, b, c;
 int parent[max_int];
 int depth[max_int];
 bool check[max_int];
-vector<int> v[max_int];
+int dist[max_int];
+
+struct info{
+    int cur;
+    int cost;
+};
+
+vector<info> v[max_int];
 
 int lca(int first, int second){
-    
+    int result = 0;
     if(depth[first] < depth[second]){
         swap(first, second);
     }
     
     while(depth[first] != depth[second]){
+        result += dist[first];
         first = parent[first];
     }
     
     while(first != second){
+        result += dist[first];
+        result += dist[second];
         first = parent[first];
         second = parent[second];
     }
     
-    return first;
+    return result;
 }
 
 
@@ -35,9 +45,9 @@ int main(){
     scanf("%d", &n);
     
     for(int i=0; i<n-1; i++){
-        scanf("%d %d", &a, &b);
-        v[a].push_back(b);
-        v[b].push_back(a);
+        scanf("%d %d %d", &a, &b, &c);
+        v[a].push_back({b, c});
+        v[b].push_back({a, c});
     }
     
     check[1] = true;
@@ -46,16 +56,19 @@ int main(){
     queue<int> q;
     q.push(1);
     while(!q.empty()){
-        int node = q.front();
+        int c_node = q.front();
         q.pop();
         
-        for(int i=0; i<v[node].size(); i++){
-            int next = v[node][i];
-            if(check[next] == false){
-                check[next] = true;
-                parent[next] = node;
-                depth[next] = depth[node] + 1;
-                q.push(next);
+        for(int i=0; i<v[c_node].size(); i++){
+            info next = v[c_node][i];
+            int n_node = next.cur;
+            int n_cost = next.cost;
+            if(check[n_node] == false){
+                check[n_node] = true;
+                parent[n_node] = c_node;
+                depth[n_node] = depth[c_node] + 1;
+                dist[n_node] = n_cost;
+                q.push(n_node);
             }
         }
     }
