4892e341f88fe165c34cd6adf763709a3fd4ebba
김정윤 <skyepodium@gmail.com>
2018-12-02 14:36:33 +0900

    SWEA 1238 by lca, timer

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,102 +1,85 @@
 #include <iostream>
-#include <queue>
 #include <vector>
-#include <algorithm>
 
-#define max_int 30001
+#define max_int 10001
 using namespace std;
 
-int n, m, a, b, c;
-bool check[max_int];
-int parent[max_int];
-int depth[max_int];
-int dist[max_int];
+int n, m, a, b, first_node, second_node;
 int p[max_int][18];
+int log;
+int timer;
+int tin[max_int];
+int tout[max_int];
 vector<int> v[max_int];
+int t;
+
+void dfs(int node, int parent){
+    tin[node] = ++timer;
+    p[node][0] = parent;
+    for(int i=1; i<=log; i++){
+        p[node][i] = p[p[node][i-1]][i-1];
+    }
+    for(int i=0; i<v[node].size(); i++){
+        int next = v[node][i];
+        if(next != parent){
+            dfs(next, node);
+        }
+    }
+    tout[node] = ++timer;
+}
+
+int upper(int first, int second){
+    return (tin[first] <= tin[second] && tout[first] >= tout[second]);
+}
 
 int lca(int first, int second){
-    if(depth[first] < depth[second]){
-        swap(first, second);
-    }
-    
-    int log = 1;
-    for(log=1; (1<<log) <= depth[first]; log++);
-    log--;
+    if(upper(first, second)) return first;
+    if(upper(second, first)) return second;
     
     for(int i=log; i>=0; i--){
-        if(depth[first] - (1<<i) >= depth[second]){
+        if(!upper(p[first][i], second)){
             first = p[first][i];
         }
     }
-    
-    if(first == second){
-        return first;
-    }
-    else{
-        for(int i=log; i>=0; i--){
-            if(p[first][i] !=0 && p[first][i] != p[second][i]){
-                first = p[first][i];
-                second = p[second][i];
-            }
+    return p[first][0];
+}
+
+void init(){
+    for(int i=0; i<=n; i++){
+        tin[i] = 0;
+        tout[i] = 0;
+        log = 0;
+        timer = 0;
+        v[i].clear();
+        for(int j=0; j<18; j++){
+            p[i][j] = 0;
         }
-        return parent[first];
     }
 }
 
-
-int main(){
-    scanf("%d", &n);
-    for(int i=0; i<n-1; i++){
-        scanf("%d %d", &a, &b);
-        v[a].push_back(b);
-        v[b].push_back(a);
-    }
+int main(int argc, char** argv)
+{
+    scanf("%d", &t);
     
-    check[1] = true;
-    parent[1] = 0;
-    depth[1] = 0;
-    dist[1] = 0;
-    queue<int> q;
-    q.push(1);
-    while(!q.empty()){
-        int node = q.front();
-        q.pop();
+    for(int test_case = 1; test_case <= t; ++test_case)
+    {
+        scanf("%d %d %d %d", &n, &m, &first_node, &second_node);
+        init();
         
-        for(int i=0; i<v[node].size(); i++){
-            int next = v[node][i];
-            
-            if(check[next] == false){
-                check[next] = true;
-                parent[next] = node;
-                depth[next] = depth[node] + 1;
-                dist[next] = dist[node] + 1;
-                q.push(next);
-            }
+        for(int i=0; i<m; i++){
+            scanf("%d %d", &a, &b);
+            v[a].push_back(b);
+            v[b].push_back(a);
         }
+        
+        for(log = 1; (1<<log) < n; log++);
+        log--;
+        dfs(1, 1);
+        
+        int lca_node = lca(first_node, second_node);
+        printf("#%d %d %d\n", test_case, lca_node, (tout[lca_node]-1-tin[lca_node])/2 + 1);
     }
     
-    for(int i=1; i<=n; i++){
-        p[i][0] = parent[i];
-    }
-    
-    for(int j=1; (1<<j) < n; j++){
-        for(int i=1; i<=n; i++){
-            if(p[i][j-1] != 0){
-                p[i][j] = p[p[i][j-1]][j-1];
-            }
-        }
-    }
-    
-    int result = 0;
-    int c_node = 1;
-    int n_node = 0;
-    scanf("%d", &m);
-    for(int i=0; i<m; i++){
-        scanf("%d", &n_node);
-        result += dist[c_node] + dist[n_node] - 2*dist[lca(c_node, n_node)];
-        c_node = n_node;
-    }
-    printf("%d\n", result);
-    
-    
+    return 0;
 }
+
