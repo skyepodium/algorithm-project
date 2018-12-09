2def20006d90a4b79031eac3b1892aab310be59a
김정윤 <skyepodium@gmail.com>
2018-12-09 14:21:58 +0900

    baekjoon 11060 by BFS

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,32 +1,44 @@
 #include <iostream>
+#include <queue>
 
-#define lld long long int
-#define max_int 70
+#define max_int 1001
 using namespace std;
 
-int t;
-lld d[max_int];
+int n;
+int a[max_int];
+int check[max_int];
 
-lld go(int i){
-    if(i<=3) return d[i];
-    
-    if(d[i] > 0) return d[i];
+void bfs(int start){
+    check[start] = 0;
+    queue<int> q;
+    q.push(start);
+    while(!q.empty()){
+        int node = q.front();
+        q.pop();
         
-    d[i] = go(i-1) + go(i-2) + go(i-3) + go(i-4);
-    return d[i];
+        int cur_val = a[node];
+        for(int i=1; i<=cur_val; i++){
+            int next = node + i;
+            if(next <= n && check[next] == 0){
+                check[next] = check[node] + 1;
+                q.push(next);
+            }
+        }
+
+    }
+    
 }
 
 int main(){
-    d[0] = 1;
-    d[1] = 1;
-    d[2] = 2;
-    d[3] = 4;
+    scanf("%d", &n);
     
-    scanf("%d", &t);
-    while(t--){
-        int num;
-        scanf("%d", &num);
-        printf("%lld\n", go(num));
+    for(int i=1; i<=n; i++){
+        scanf("%d", &a[i]);
     }
+    
+    bfs(1);
+    if(check[n] == 0 && n!=1) check[n] = -1;
+    printf("%d\n", check[n]);
+
     
 }
