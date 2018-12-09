ed9ed479edb83e33fb26bcc3c5eafbfa80e56dd0
김정윤 <skyepodium@gmail.com>
2018-12-09 13:46:44 +0900

    baekjoon 9507 by dp

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,38 +1,32 @@
 #include <iostream>
 
-#define max_int 5001
+#define lld long long int
+#define max_int 70
 using namespace std;
 
-int t, n, a, b, p, num;
-int check[max_int];
+int t;
+lld d[max_int];
 
-int main(int argc, char** argv)
-{
-    scanf("%d", &t);
+lld go(int i){
+    if(i<=3) return d[i];
     
-    for(int test_case = 1; test_case <= t; ++test_case)
-    {
-        scanf("%d", &n);
-        for(int i=0; i<=5000; i++) check[i] = 0;
-        for(int i=0; i<n; i++){
-            scanf("%d %d", &a, &b);
-            for(int j=a; j<=b; j++){
-                check[j]++;
-            }
-        }
-        printf("#%d ", test_case);
-        scanf("%d", &p);
-        for(int i=0; i<p; i++){
-            scanf("%d", &num);
-            printf("%d ", check[num]);
-        }
-        printf("\n");
+    if(d[i] > 0) return d[i];
         
-        
-        
-        
-    }
-    
-    return 0;
+    d[i] = go(i-1) + go(i-2) + go(i-3) + go(i-4);
+    return d[i];
 }
 
+int main(){
+    d[0] = 1;
+    d[1] = 1;
+    d[2] = 2;
+    d[3] = 4;
+    
+    scanf("%d", &t);
+    while(t--){
+        int num;
+        scanf("%d", &num);
+        printf("%lld\n", go(num));
+    }
+    
+}
