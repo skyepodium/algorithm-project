c534334eddde70dd5b5b83d8b578b216a8bab656
김정윤 <skyepodium@gmail.com>
2018-12-09 12:36:23 +0900

    SWEA 6485

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,61 +1,38 @@
 #include <iostream>
-#include <queue>
 
-#define max_int 126
-#define max_val 2147483647
+#define max_int 5001
 using namespace std;
 
-int n, t;
-int d[max_int][max_int];
-int a[max_int][max_int];
-int dx[] = {0, 0, 1, -1};
-int dy[] = {-1, 1, 0, 0};
-struct info{
-    int x;
-    int y;
-    int cost;
-};
+int t, n, a, b, p, num;
+int check[max_int];
 
-struct cmp{
-    bool operator()(const info &a, const info &b){
-        return a.cost > b.cost;
-    }
-};
-
-int main(){
+int main(int argc, char** argv)
+{
     scanf("%d", &t);
-    for(int test_case = 1;  test_case<=t; test_case++){
-        scanf("%d", &n);
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                scanf("%1d", &a[i][j]);
-                d[i][j] = max_val;
-            }
-        }
-        d[0][0] = a[0][0];
-        priority_queue<info, vector<info>, cmp> pq;
-        pq.push({0, 0, d[0][0]});
-        while(!pq.empty()){
-            info node = pq.top();
-            pq.pop();
-            
-            int x = node.x;
-            int y = node.y;
-            
-            for(int i=0; i<4; i++){
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-                
-                if(nx>=0 && nx<n && ny>=0 && ny<n){
-                    if(d[nx][ny] > d[x][y] + a[nx][ny]){
-                        d[nx][ny] = d[x][y] + a[nx][ny];
-                        pq.push({nx, ny, d[nx][ny]});
-                    }
-                }
-            }
-        }
     
-        printf("#%d %d\n", test_case, d[n-1][n-1]);
+    for(int test_case = 1; test_case <= t; ++test_case)
+    {
+        scanf("%d", &n);
+        for(int i=0; i<=5000; i++) check[i] = 0;
+        for(int i=0; i<n; i++){
+            scanf("%d %d", &a, &b);
+            for(int j=a; j<=b; j++){
+                check[j]++;
+            }
+        }
+        printf("#%d ", test_case);
+        scanf("%d", &p);
+        for(int i=0; i<p; i++){
+            scanf("%d", &num);
+            printf("%d ", check[num]);
+        }
+        printf("\n");
+        
+        
+        
+        
     }
+    
+    return 0;
 }
 
