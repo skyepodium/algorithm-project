3329e75f0d0620cc596d9e49412973c79c45a0e4
김정윤 <skyepodium@gmail.com>
2018-12-14 20:09:46 +0900

    baekjoon 10924 by DP

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,42 +1,34 @@
 #include <iostream>
-#include <vector>
-#include <queue>
 
-#define max_int 32001
+#define max_int 2001
 using namespace std;
 
-int n, m, a, b;
-int ind[max_int];
-vector<int> v[max_int];
+int n, m, s, e;
+int a[max_int];
+int d[max_int][max_int];
 
 int main(){
-    scanf("%d %d", &n, &m);
-    for(int i=0; i<m; i++){
-        scanf("%d %d", &a, &b);
-        v[a].push_back(b);
-        ind[b]++;
-    }
-    
-    queue<int> q;
+    scanf("%d", &n);
     for(int i=1; i<=n; i++){
-        if(ind[i] == 0){
-            q.push(i);
+        scanf("%d", &a[i]);
+    }
+    
+    for(int i=1; i<=n; i++) d[i][i] = 1;
+    
+    for(int i=1; i<=n-1; i++){
+        if(a[i] == a[i+1]) d[i][i+1] = 1;
+    }
+    
+    for(int k=2; k<n; k++){
+        for(int i=0; i<=n-k; i++){
+            int j = i+k;
+            if(a[i] == a[j] && d[i+1][j-1] == 1) d[i][j] = 1;
         }
     }
     
-    while(!q.empty()){
-        int node = q.front();
-        printf("%d ", node);
-        q.pop();
-        
-        for(int i=0; i<v[node].size(); i++){
-            int next = v[node][i];
-            ind[next]--;
-            
-            if(ind[next] == 0){
-                q.push(next);
-            }
-        }
+    scanf("%d", &m);
+    for(int i=0; i<m; i++){
+        scanf("%d %d", &s, &e);
+        printf("%d\n", d[s][e]);
     }
-    printf("\n");
 }
