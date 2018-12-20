1422ca01344be98cec90ebb253c39553d3a4ddc4
김정윤 <skyepodium@gmail.com>
2018-12-20 22:39:08 +0900

    12.20

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -2,24 +2,6 @@
 
 using namespace std;
 
-int t, num;
-int result = 0;
-
 int main(){
-    scanf("%d", &t);
-    
-    for(int test_case = 1; test_case <=t; test_case++){
-        scanf("%d", &num);
-        
-        if(num>=1000000) result = 5;
-        else if(num>=100000) result = 4;
-        else if(num>=10000) result = 3;
-        else if(num>=1000) result = 2;
-        else if(num>=100) result = 1;
-        else result = 0;
-        
-        printf("#%d %d\n", test_case, result);
-        
-    }
     
 }
