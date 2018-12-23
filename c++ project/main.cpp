b21745fe173399068e070e52140bcd2e6d083111
김정윤 <skyepodium@gmail.com>
2018-12-23 14:27:52 +0900

    baekjoon 11656

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
-
+vector<string> result;
+string word;
 int main(){
-    
+    cin >> word;
+    for(int i=0; i<(int)word.size(); i++){
+        string temp;
+        for(int j=i; j<(int)word.size(); j++){
+            temp += word[j];
+        }
+        result.push_back(temp);
+    }
+    sort(result.begin(), result.end());
+    for(int i=0; i<(int)result.size(); i++){
+        printf("%s\n", result[i].c_str());
+    }
 }
