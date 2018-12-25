eb180a2df255b1ea130339eb8dc18174249a4b3e
김정윤 <skyepodium@gmail.com>
2018-12-25 13:30:17 +0900

    baekjoon 2156 by DP

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,13 +1,25 @@
 #include <iostream>
+#include <algorithm>
 
-#define max_int 101
+#define max_int 10001
 using namespace std;
 
 int n;
-int d[max_int];
-int mod = 10007;
+int a[max_int];
+int d[max_int][3];
 
 int main(){
     scanf("%d", &n);
-    printf("%d\n", n);
+    for(int i=1; i<=n; i++){
+        scanf("%d", &a[i]);
+    }
+
+    d[1][1] = a[1];
+    for(int i=2; i<=n; i++){
+        d[i][0] = max(d[i-1][0], max(d[i-1][1], d[i-1][2]));
+        d[i][1] = d[i-1][0] + a[i];
+        d[i][2] = d[i-1][1] + a[i];
+    }
+
+    printf("%d\n", max(d[n][0], max(d[n][1], d[n][2])));
 }
