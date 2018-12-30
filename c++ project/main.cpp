130437fc123269725cd0f2b89f8434c10fcbbcba
김정윤 <skyepodium@gmail.com>
2018-12-30 15:58:19 +0900

    baekjoon 11650 by struct sorting

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,19 +1,32 @@
 #include <iostream>
 #include <algorithm>
 
-#define max_int 1000001
+#define max_int 100001
 using namespace std;
 
 int n;
-int a[max_int];
+struct info{
+    int x;
+    int y;
+};
+info a[max_int];
+
+bool cmp(const info &a, const info &b){
+    if(a.x == b.x){
+        return a.y < b.y;
+    } else{
+        return a.x < b.x;
+    }
+}
 
 int main(){
     scanf("%d", &n);
     for(int i=0; i<n; i++){
-        scanf("%d", &a[i]);
+        scanf("%d %d", &a[i].x, &a[i].y);
     }
-    sort(a, a+n);
+
+    sort(a, a+n, cmp);
     for(int i=0; i<n; i++){
-        printf("%d\n", a[i]);
+        printf("%d %d\n", a[i].x, a[i].y);
     }
 }
