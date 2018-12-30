63b6130595d1f9f6c4273aed076fe8030464bade
김정윤 <skyepodium@gmail.com>
2018-12-31 00:13:52 +0900

    baekjoon 11931

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,54 +1,29 @@
 #include <iostream>
-#include <algorithm>
 
-#define max_int 300
+#define max_int 1000001
 using namespace std;
 
-int t, n;
-int a[max_int][max_int];
-int d[max_int][max_int];
-
+int n, num;
+bool a[max_int];
+bool b[max_int];
 int main(){
-    scanf("%d", &t);
-    for(int test_case=1; test_case<=t; test_case++){
-        scanf("%d", &n);
-        for(int i=0; i<=n; i++){
-            for(int j=0; j<=n; j++){
-                a[i][j] = 0;
-                d[i][j] = 0;
-            }
-        }
-        for(int i=1; i<=2*n-1; i++){
-            if(i <= n){
-                for(int j=1; j<=i; j++){
-                    scanf("%d", &a[i][j]);
-                }
-            }
-            else{
-                for(int j=1; j<=2*n-i; j++){
-                    scanf("%d", &a[i][j]);
-                }
-            }
-        }
+    scanf("%d", &n);
 
-        d[1][1] = a[1][1];
-        for(int i=2; i<=2*n-1; i++){
-            if(i <= n){
-                for(int j=1; j<=i; j++){
-                    if(j==1){
-                        d[i][j] = d[i-1][j] + a[i][j];
-                    }
-                    else{
-                        d[i][j] = max(d[i-1][j-1], d[i-1][j]) + a[i][j];
-                    }
-                }
-            }
-            else{
-                for(int j=1; j<=2*n-i; j++){
-                    d[i][j] = max(d[i-1][j], d[i-1][j+1]) + a[i][j];
-                }
-            }
+    for(int i=0; i<n; i++){
+        scanf("%d", &num);
+        if(num >=0){
+            a[num] = true;
         }
-        printf("%d\n", d[2*n-1][1]);
+        else{
+            num *= -1;
+            b[num] = true;
+        }
     }
+    for(int i=max_int-1; i>=0; i--){
+        if(a[i]) printf("%d\n", i);
+    }
+    for(int i=1; i<=max_int-1; i++){
+        if(b[i]) printf("%d\n", i*-1);
+    }
+
 }
