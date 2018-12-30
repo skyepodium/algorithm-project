c79ea34dee5ac32605f816986c2df668415cf2cd
김정윤 <skyepodium@gmail.com>
2018-12-30 16:02:51 +0900

    baekjoon 10814 by stable_sort

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,32 +1,36 @@
 #include <iostream>
+#include <string>
+#include <vector>
 #include <algorithm>
 
-#define max_int 100001
 using namespace std;
 
 int n;
+int age;
+string name;
+
 struct info{
-    int x;
-    int y;
+    int age;
+    string name;
 };
-info a[max_int];
+
+vector<info> v;
 
 bool cmp(const info &a, const info &b){
-    if(a.y == b.y){
-        return a.x < b.x;
-    } else{
-        return a.y < b.y;
-    }
+    return a.age < b.age;
 }
 
 int main(){
     scanf("%d", &n);
     for(int i=0; i<n; i++){
-        scanf("%d %d", &a[i].x, &a[i].y);
+        cin >> age >> name;
+        v.push_back({age, name});
     }
 
-    sort(a, a+n, cmp);
+    stable_sort(v.begin(), v.end(), cmp);
+
     for(int i=0; i<n; i++){
-        printf("%d %d\n", a[i].x, a[i].y);
+        cout << v[i].age << " " << v[i].name << '\n';
     }
+
 }
