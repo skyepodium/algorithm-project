7b500c288e558d2cd7a8118abc4b6a6f09c2a145
김정윤 <skyepodium@gmail.com>
2018-12-30 15:55:31 +0900

    baekjoon 2751 by sort

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,20 +1,19 @@
 #include <iostream>
 #include <algorithm>
-#define max_int 5000001
+
+#define max_int 1000001
 using namespace std;
 
-int n, m;
+int n;
 int a[max_int];
 
 int main(){
-
-    scanf("%d %d", &n, &m);
-
+    scanf("%d", &n);
     for(int i=0; i<n; i++){
         scanf("%d", &a[i]);
     }
-
     sort(a, a+n);
-    printf("%d\n", a[m-1]);
-
+    for(int i=0; i<n; i++){
+        printf("%d\n", a[i]);
+    }
 }
