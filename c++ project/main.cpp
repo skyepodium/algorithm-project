a39556e1a4a6e9e47993cc6898f65a1ee47d2d03
김정윤 <skyepodium@gmail.com>
2018-12-30 20:07:25 +0900

    baekjoon 1212

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,32 +1,43 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <cmath>
 
 using namespace std;
 
 string n;
 vector<int> result;
 
+void cal_num(int num){
+    int cnt = 0;
+    while(num > 0){
+        result.push_back(num%2);
+        cnt++;
+        num /= 2;
+    }
+    if(cnt != 3){
+        for(int i=0; i<3-cnt; i++){
+            result.push_back(0);
+        }
+    }
+}
+
 int main(){
     cin >> n;
-
     int size = (int)n.size();
+    for(int i=size - 1; i>=0; i--){
+        cal_num(n[i] - '0');
+    }
 
-    int idx = 0;
-    int temp = 0;
-    for(int i=size-1; i>=0; i--){
-        if(idx==3){
-            result.push_back(temp);
-            temp = 0;
-            idx = 0;
+    bool startZero = true;
+    for(int i=(int)result.size()-1; i>=0; i--){
+        if(result[i] == 1){
+            startZero = false;
         }
-        temp = temp + (n[i] - '0') * pow(2, idx);
-        idx++;
+        if(startZero == false){
+            printf("%d", result[i]);
+        }
     }
-    result.push_back(temp);
-    for(int i=(int)result.size() - 1; i>=0; i--){
-        printf("%d", result[i]);
+    if(result.size() == 3 && result[0] == 0 && result[1] == 0 && result[2] == 0){
+        printf("0\n");
     }
-    printf("\n");
 }
