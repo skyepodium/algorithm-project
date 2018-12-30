3101daa00a4069fe919e958a74b87e5a0b4c280f
김정윤 <skyepodium@gmail.com>
2018-12-30 19:07:16 +0900

    baekjoon 11005 by base conversion

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,19 +1,26 @@
 #include <iostream>
+#include <vector>
 
+#define lld long long int
 using namespace std;
 
-int n, a, b;
-
-int gcd(int a, int b){
-    if(b==0) return a;
-    else return gcd(b, a%b);
-}
+lld n, b;
+vector<int> result;
 
 int main(){
-    scanf("%d", &n);
-    for(int i=0; i<n; i++){
-        scanf("%d %d", &a, &b);
-        printf("%d\n", (a*b)/gcd(a, b));
+    scanf("%d %d", &n, &b);
+
+    while(n>0){
+        result.push_back(n%b);
+        n /= b;
+    }
+    for(int i=(int)result.size()-1; i>=0; i--){
+        if(result[i] >= 10){
+            printf("%c", result[i] + 55);
+        }
+        else{
+            printf("%d", result[i]);
+        }
     }
 
 }
