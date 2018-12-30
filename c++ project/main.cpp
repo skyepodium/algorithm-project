066d8e37491d80e841449b682077702fd84bef34
김정윤 <skyepodium@gmail.com>
2018-12-31 00:54:56 +0900

    SWEA 1803 by dijkstra

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,29 +1,62 @@
 #include <iostream>
+#include <vector>
+#include <queue>
 
-#define max_int 1000001
+#define lld long long int
+#define max_val 100000000001
+#define max_int 100001
 using namespace std;
 
-int n, num;
-bool a[max_int];
-bool b[max_int];
-int main(){
-    scanf("%d", &n);
+int t, n, m, start_node, end_node, a, b, c;
+lld d[max_int];
+struct info{
+    int cur;
+    lld cost;
+};
+vector<info> v[max_int];
 
-    for(int i=0; i<n; i++){
-        scanf("%d", &num);
-        if(num >=0){
-            a[num] = true;
+struct cmp{
+    bool operator()(const info &a, const info &b){
+        return a.cost > b.cost;
+    }
+};
+
+int main(){
+    scanf("%d", &t);
+    for(int test_case=1; test_case<=t; test_case++){
+        scanf("%d %d %d %d", &n, &m, &start_node, &end_node);
+
+        for(int i=0; i<=n; i++){
+            d[i] = max_val;
+            v[i].clear();
         }
-        else{
-            num *= -1;
-            b[num] = true;
+
+        for(int i=0; i<m; i++){
+            scanf("%d %d %lld", &a, &b, &c);
+            v[a].push_back({b, c});
+            v[b].push_back({a, c});
         }
+
+        d[start_node] = 0;
+        priority_queue<info, vector<info>, cmp> pq;
+        pq.push({start_node, d[start_node]});
+        while(!pq.empty()){
+            info cur = pq.top();
+            int c_node = cur.cur;
+            pq.pop();
+
+            for(int i=0; i<v[cur.cur].size(); i++){
+                info next = v[c_node][i];
+                int n_node = next.cur;
+                lld n_cost = next.cost;
+                if(d[n_node] > d[c_node] + n_cost){
+                    d[n_node] = d[c_node] + n_cost;
+                    pq.push({n_node, d[n_node]});
+                }
+            }
+        }
+        printf("#%d %lld\n", test_case, d[end_node]);
     }
-    for(int i=max_int-1; i>=0; i--){
-        if(a[i]) printf("%d\n", i);
-    }
-    for(int i=1; i<=max_int-1; i++){
-        if(b[i]) printf("%d\n", i*-1);
-    }
+
 
 }
