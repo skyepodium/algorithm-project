2bcd2eb26138272c424720a89c444c85dec3a8b2
김정윤 <skyepodium@gmail.com>
2018-12-30 17:20:28 +0900

    baekjoon 1943 by GCD

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,37 +1,19 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 
-#define max_int 500001
 using namespace std;
 
-int n, num;
-struct info{
-    int num;
-    int idx;
-};
-info a[max_int];
-bool cmp(const info &a, const info &b){
-    return a.num < b.num;
-}
+int n, a, b;
 
+int gcd(int a, int b){
+    if(b==0) return a;
+    else return gcd(b, a%b);
+}
 
 int main(){
     scanf("%d", &n);
-
-    for(int i=1; i<=n; i++){
-        scanf("%d", &num);
-        a[i].num = num;
-        a[i].idx = i;
+    for(int i=0; i<n; i++){
+        scanf("%d %d", &a, &b);
+        printf("%d\n", (a*b)/gcd(a, b));
     }
-
-    stable_sort(a+1, a+n+1, cmp);
-
-    int result = 0;
-    for(int i=1; i<=n; i++){
-        result = max(result, a[i].idx - i);
-    }
-    printf("%d\n", result + 1);
 
 }
-
