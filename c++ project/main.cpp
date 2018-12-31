44bd4d89a6b3acc9f13c9037ab7710cf1ad3b38e
김정윤 <skyepodium@gmail.com>
2018-12-31 14:22:26 +0900

    baekjoon 3347 by for loop

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,37 +1,49 @@
 #include <iostream>
 #include <algorithm>
-#define max_int 201
+
+#define max_int 1001
 using namespace std;
 
-int t, n, k;
-float a[max_int];
-float d[max_int][max_int];
+int t, n, m;
+int a[max_int];
+int b[max_int];
+int vote[max_int];
 
 int main(){
     scanf("%d", &t);
 
     for(int test_case = 1; test_case<=t; test_case++){
-        scanf("%d %d", &n, &k);
+        scanf("%d %d", &n, &m);
+
         for(int i=1; i<=n; i++){
-            scanf("%f", &a[i]);
-            for(int j=1; j<=k; j++){
-                d[i][j] = 0;
+            scanf("%d", &a[i]);
+            vote[i] = 0;
+        }
+
+        for(int i=1; i<=m; i++){
+            scanf("%d", &b[i]);
+        }
+
+
+        for(int j=1; j<=m; j++){
+            int min_idx = n;
+            for(int i=1; i<=n; i++){
+                if(a[i] <= b[j]){
+                    min_idx = min(min_idx, i);
+                }
+            }
+            vote[min_idx]++;
+        }
+
+        int max_vote = 0;
+        int result = 0;
+        for(int i=1; i<=n; i++){
+            if(vote[i] > max_vote){
+                max_vote = vote[i];
+                result = i;
             }
         }
 
-        sort(a+1, a+n+1);
-
-        d[1][1] = a[1]/2;
-        for(int i=2; i<=n; i++){
-            for(int j=1; j<=k; j++){
-                d[i][j] = max(d[i-1][j], (d[i-1][j-1] + a[i])/2);
-            }
-        }
-
-        float result = 0;
-        for(int i=1; i<=n; i++){
-            result = max(result, d[i][k]);
-        }
-        printf("#%d %f\n", test_case, result);
+        printf("#%d %d\n", test_case, result);
     }
 }
