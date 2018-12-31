28fd34c26b18ccf6ed183d27d0df16b30ecab267
김정윤 <skyepodium@gmail.com>
2018-12-31 14:09:27 +0900

    SWEA 6719 by DP, Sorting

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,62 +1,37 @@
 #include <iostream>
-#include <vector>
-#include <queue>
-
-#define lld long long int
-#define max_val 100000000001
-#define max_int 100001
+#include <algorithm>
+#define max_int 201
 using namespace std;
 
-int t, n, m, start_node, end_node, a, b, c;
-lld d[max_int];
-struct info{
-    int cur;
-    lld cost;
-};
-vector<info> v[max_int];
-
-struct cmp{
-    bool operator()(const info &a, const info &b){
-        return a.cost > b.cost;
-    }
-};
+int t, n, k;
+float a[max_int];
+float d[max_int][max_int];
 
 int main(){
     scanf("%d", &t);
-    for(int test_case=1; test_case<=t; test_case++){
-        scanf("%d %d %d %d", &n, &m, &start_node, &end_node);
 
-        for(int i=0; i<=n; i++){
-            d[i] = max_val;
-            v[i].clear();
-        }
-
-        for(int i=0; i<m; i++){
-            scanf("%d %d %lld", &a, &b, &c);
-            v[a].push_back({b, c});
-            v[b].push_back({a, c});
-        }
-
-        d[start_node] = 0;
-        priority_queue<info, vector<info>, cmp> pq;
-        pq.push({start_node, d[start_node]});
-        while(!pq.empty()){
-            info cur = pq.top();
-            int c_node = cur.cur;
-            pq.pop();
-
-            for(int i=0; i<v[cur.cur].size(); i++){
-                info next = v[c_node][i];
-                int n_node = next.cur;
-                lld n_cost = next.cost;
-                if(d[n_node] > d[c_node] + n_cost){
-                    d[n_node] = d[c_node] + n_cost;
-                    pq.push({n_node, d[n_node]});
-                }
+    for(int test_case = 1; test_case<=t; test_case++){
+        scanf("%d %d", &n, &k);
+        for(int i=1; i<=n; i++){
+            scanf("%f", &a[i]);
+            for(int j=1; j<=k; j++){
+                d[i][j] = 0;
             }
         }
-        printf("#%d %lld\n", test_case, d[end_node]);
+
+        sort(a+1, a+n+1);
+
+        d[1][1] = a[1]/2;
+        for(int i=2; i<=n; i++){
+            for(int j=1; j<=k; j++){
+                d[i][j] = max(d[i-1][j], (d[i-1][j-1] + a[i])/2);
+            }
+        }
+
+        float result = 0;
+        for(int i=1; i<=n; i++){
+            result = max(result, d[i][k]);
+        }
+        printf("#%d %f\n", test_case, result);
     }
-
-
 }
