12907528629f2782385f95e76ee04444aea55495
김정윤 <skyepodium@gmail.com>
2019-01-01 17:44:50 +0900

    baekjoon 1676

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,14 +1,32 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
 int num;
+int a[2] = {5, 2};
+int cnt[2];
+int result = 0;
 
 int main(){
     scanf("%d", &num);
-    int result = 1;
+
     for(int i=1; i<=num; i++){
-        result = result * i;
+        int base = i;
+        for(int j=0; j<2; j++){
+            while(base%a[j] == 0){
+                base /= a[j];
+                cnt[j]++;
+            }
+        }
     }
+    if(cnt[0] == 0 || cnt[1] == 0){
+        result = 0;
+    }
+    else{
+        result = min(cnt[0], cnt[1]);
+    }
+
+
     printf("%d\n", result);
 }
