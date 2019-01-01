39c96ba81b2f3b6789dafa21f0e3026eba6b8a3f
김정윤 <skyepodium@gmail.com>
2019-01-01 17:32:41 +0900

    baekjoon 10872

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,31 +1,14 @@
 #include <iostream>
-#include <vector>
-
-#define max_int 10000001
 
 using namespace std;
-int n;
-vector<int> v;
-bool check[max_int];
-void eratos(){
-    for(int i=2; i*i<=n; i++){
-        for(int j=i*i; j<=n; j=j+i){
-            check[j] = true;
-        }
-    }
-}
+
+int num;
+
 int main(){
-    scanf("%d", &n);
-    eratos();
-    for(int i=2; i<=n; i++){
-        if(!check[i]) v.push_back(i);
+    scanf("%d", &num);
+    int result = 1;
+    for(int i=1; i<=num; i++){
+        result = result * i;
     }
-
-    for(int i=0; i<v.size(); i++){
-        while(n%v[i] == 0){
-            n /= v[i];
-            printf("%d\n", v[i]);
-        }
-    }
-
+    printf("%d\n", result);
 }
