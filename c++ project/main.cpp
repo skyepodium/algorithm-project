1eb2a03a2a3d3b0c6ef257d42a5abace19288afe
김정윤 <skyepodium@gmail.com>
2019-01-01 17:12:25 +0900

    baekjoon 1929 by eratos

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,22 +1,26 @@
 #include <iostream>
 
+#define max_int 1000001
 using namespace std;
 
-int n, num, cnt=0;
+int n, m;
+bool d[max_int];
+
+void eratos(){
+    for(int i=2; i*i<=n; i++){
+        for(int j=i*2; j<=n; j=j+i){
+            d[j] = true;
+        }
+    }
+}
 
 int main(){
-    scanf("%d", &n);
-    for(int i=0; i<n; i++){
-        scanf("%d", &num);
-        if(num==1) continue;
-        bool isPrime = true;
-        for(int j=2; j*j<=num; j++){
-            if(num%j==0){
-                isPrime = false;
-                break;
-            }
+    scanf("%d %d", &m, &n);
+    eratos();
+    d[1] = true;
+    for(int i=m; i<=n; i++){
+        if(d[i] == false){
+            printf("%d\n", i);
         }
-        if(isPrime) cnt++;
     }
-    printf("%d\n", cnt);
 }
