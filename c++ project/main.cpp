22623d1c4e2040d550954ade313021b18c197015
김정윤 <skyepodium@gmail.com>
2019-01-01 15:31:42 +0900

    SWEA 3124 by Kruskal's algorithm

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,54 +1,59 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
-#include <cstring>
 
-#define max_int 11
+#define lld long long int
+#define max_int 100001
 using namespace std;
 
-int t, n, result;
-char a[max_int];
-bool d[max_int][max_int];
+int t, n, m;
+lld result;
+struct info{
+    int start;
+    int end;
+    int cost;
+};
+
+bool cmp(const info &a, const info &b){
+    return a.cost < b.cost;
+}
+
+int d[max_int];
 
 void init(){
-    n = strlen(a);
     result = 0;
-    for(int i=0; i<=n; i++){
-        for(int j=0; j<=n; j++){
-            d[i][j] = false;
-        }
+    for(int i=1; i<=n; i++){
+        d[i] = i;
     }
+}
+
+int find(int node){
+    if(node == d[node]) return node;
+    else return d[node] = find(d[node]);
 }
 
 int main(){
     scanf("%d", &t);
     for(int test_case=1; test_case<=t; test_case++){
-        scanf("%s", a);
+        scanf("%d %d", &n, &m);
         init();
-
-        sort(a, a + n);
-
-        for(int i=0; i<n; i++){
-            d[i][i] = true;
-            result++;
+        vector<info> v(m);
+        for(int i=0; i<m; i++){
+            scanf("%d %d %d", &v[i].start, &v[i].end, &v[i].cost);
         }
 
-        for(int i=0; i<n-1; i++){
-            if(a[i] == a[i+1]){
-                d[i][i+1] = true;
-                result++;
+        sort(v.begin(), v.end(), cmp);
+
+        for(int i=0; i<m; i++){
+            int x = v[i].start;
+            int y = v[i].end;
+            x = find(x);
+            y = find(y);
+            if(x!=y){
+                d[x] = y;
+                result += v[i].cost;
             }
         }
-
-        for(int k=2; k<n; k++){
-            for(int i=0; i<n-k; i++){
-                int j=i+k;
-                if(a[i] == a[j] && d[i+1][j-1]){
-                    d[i][j] = true;
-                    result++;
-                }
-            }
-        }
-
-        printf("#%d %d\n", test_case, result);
+        printf("#%d %lld\n", test_case, result);
     }
 }
