16eb6359568816d4306e05fe2806e0b1251db545
김정윤 <skyepodium@gmail.com>
2019-01-01 15:53:36 +0900

    SWEA 3064 by segment tree(Binary Indexed Tree)

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,59 +1,72 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 
-#define lld long long int
 #define max_int 100001
+#define lld long long int
 using namespace std;
 
-int t, n, m;
-lld result;
-struct info{
-    int start;
-    int end;
-    int cost;
-};
-
-bool cmp(const info &a, const info &b){
-    return a.cost < b.cost;
-}
-
-int d[max_int];
+int t, n, m, idx, a, b, c;
+lld tree[max_int*4];
 
 void init(){
-    result = 0;
-    for(int i=1; i<=n; i++){
-        d[i] = i;
+    for(int i=0; i<=n*4; i++){
+        tree[i] = 0;
     }
 }
 
-int find(int node){
-    if(node == d[node]) return node;
-    else return d[node] = find(d[node]);
+void update_tree(int node, int data){
+    while(node>=1){
+        tree[node] += data;
+        node /= 2;
+    }
+}
+
+lld sum_data(int start, int end){
+    lld ret = 0;
+    while(start <= end){
+        if(start%2==1) ret += tree[start];
+        if(end%2==0) ret += tree[end];
+
+        start = (start + 1)/2;
+        end = (end - 1)/2;
+    }
+    return ret;
 }
 
 int main(){
     scanf("%d", &t);
     for(int test_case=1; test_case<=t; test_case++){
         scanf("%d %d", &n, &m);
+
         init();
-        vector<info> v(m);
-        for(int i=0; i<m; i++){
-            scanf("%d %d %d", &v[i].start, &v[i].end, &v[i].cost);
+
+        //1. find idx;
+        for(idx = 1; idx<n; idx=idx*2);
+        idx--;
+
+        //2. insert data to leafnode
+        for(int i=1; i<=n; i++){
+            scanf("%lld", &tree[idx+i]);
         }
 
-        sort(v.begin(), v.end(), cmp);
+        //3. create_index_tree
+        for(int i=idx; i>=1; i--){
+            tree[i] = tree[i*2] + tree[i*2+1];
+        }
 
+        //4. commands
+        printf("#%d ", test_case);
         for(int i=0; i<m; i++){
-            int x = v[i].start;
-            int y = v[i].end;
-            x = find(x);
-            y = find(y);
-            if(x!=y){
-                d[x] = y;
-                result += v[i].cost;
+            scanf("%d %d %d", &a, &b, &c);
+
+            //4-1 update_data
+            if(a == 1){
+                update_tree(b+idx, c);
+            }
+            //4-2 sum_data
+            else{
+                printf("%lld ", sum_data(b+idx, c+idx));
             }
         }
-        printf("#%d %lld\n", test_case, result);
+        printf("\n");
     }
 }
