db075adb62a88052193395c2804494cd64bf30ac
김정윤 <skyepodium@gmail.com>
2019-01-01 17:19:57 +0900

    baekjoon 6588 by eratos

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,26 +1,33 @@
 #include <iostream>
 
 #define max_int 1000001
+
 using namespace std;
-
-int n, m;
-bool d[max_int];
-
+int num;
+bool check[max_int];
 void eratos(){
-    for(int i=2; i*i<=n; i++){
-        for(int j=i*2; j<=n; j=j+i){
-            d[j] = true;
+    for(int i=2; i*i<=1000000; i++){
+        for(int j=i*i; j<=1000000; j=j+i){
+            check[j] = true;
         }
     }
 }
-
 int main(){
-    scanf("%d %d", &m, &n);
     eratos();
-    d[1] = true;
-    for(int i=m; i<=n; i++){
-        if(d[i] == false){
-            printf("%d\n", i);
+    while(true){
+        scanf("%d", &num);
+        if(num == 0) break;
+
+        bool find = false;
+        for(int i=2; i<=num/2; i++){
+            int a = i;
+            int b = num-i;
+            if(check[a] == false && check[b] == false){
+                printf("%d = %d + %d\n", num, a, b);
+                find=true;
+                break;
+            }
         }
+        if(!find) printf("Goldbach's conjecture is wrong.\n");
     }
 }
