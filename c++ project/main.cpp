2320e84e49435b18a39d4242eb0748773b0df928
김정윤 <skyepodium@gmail.com>
2019-01-02 22:02:35 +0900

    SWEA 5685

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,43 +1,40 @@
 #include <iostream>
-#include <vector>
-#define max_int 1000
+
+#define lld long long int
+#define max_int 101
 using namespace std;
 
-int t, num;
-bool check[max_int];
-int cnt[max_int];
-vector<int> v;
-void eratos(){
-    check[1] = true;
-    for(int i=2; i<=max_int-1; i++){
-        for(int j=i*2; j<=max_int-1; j=j+i){
-            check[j] = true;
-        }
-    }
-    for(int i=2; i<max_int-1; i++){
-        if(check[i] == false) v.push_back(i);
-    }
-
-}
+int t, n;
+int a[max_int];
+lld d[max_int][21];
+lld mod = 1234567891;
 
 int main(){
     scanf("%d", &t);
-    eratos();
 
-    int size = (int)v.size();
-    for(int i=0; i<size; i++){
-        for(int j=i; j<size; j++){
-            for(int k=j; k<size; k++){
-                int temp = v[i] + v[j] + v[k];
-                if(temp <max_int){
-                    cnt[temp]++;
+    for(int test_case = 1; test_case <=t; test_case++){
+            scanf("%d", &n);
+
+            for(int i=0; i<=n; i++){
+                for(int j=0; j<21; j++){
+                    d[i][j] = 0;
                 }
             }
-        }
+            for(int i=0; i<n; i++){
+                scanf("%d", &a[i]);
+            }
+
+            d[0][a[0]] = 1;
+            for(int i=1; i<n-1; i++){
+                for(int j=0; j<21; j++){
+                    if(j-a[i] >=0) d[i][j] = (d[i][j] + d[i-1][j-a[i]])%mod;
+                    if(j+a[i] <21) d[i][j] = (d[i][j] + d[i-1][j+a[i]])%mod;
+                }
+            }
+
+            printf("#%d %lld\n", test_case, d[n-2][a[n-1]]);
+
     }
 
-    for(int test_case = 1; test_case<=t; test_case++){
-        scanf("%d", &num);
-        printf("#%d %d\n", test_case, cnt[num]);
-    }
+
 }
