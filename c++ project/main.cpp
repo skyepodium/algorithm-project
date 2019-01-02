3a42245d58fbd476b9e2cd1c2f0b20a4c10b1288
김정윤 <skyepodium@gmail.com>
2019-01-02 21:19:23 +0900

    SWEA 5986

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,31 +1,43 @@
 #include <iostream>
 #include <vector>
+#define max_int 1000
 using namespace std;
 
-int n;
-vector<int> result;
-int main(){
-    scanf("%d", &n);
-    if(n != 0) {
-        while (n != 0) {
-
-            if (n % 2 == 0) {
-                n = (-n / 2);
-                result.push_back(0);
-            } else {
-                if (n > 0) {
-                    n = (-n / 2);
-                } else {
-                    n = ((-n + 1) / 2);
-                }
-                result.push_back(1);
-            }
-        }
-        for (int i = (int) result.size() - 1; i >= 0; i--) {
-            printf("%d", result[i]);
+int t, num;
+bool check[max_int];
+int cnt[max_int];
+vector<int> v;
+void eratos(){
+    check[1] = true;
+    for(int i=2; i<=max_int-1; i++){
+        for(int j=i*2; j<=max_int-1; j=j+i){
+            check[j] = true;
         }
     }
-    else{
-        printf("0\n");
+    for(int i=2; i<max_int-1; i++){
+        if(check[i] == false) v.push_back(i);
+    }
+
+}
+
+int main(){
+    scanf("%d", &t);
+    eratos();
+
+    int size = (int)v.size();
+    for(int i=0; i<size; i++){
+        for(int j=i; j<size; j++){
+            for(int k=j; k<size; k++){
+                int temp = v[i] + v[j] + v[k];
+                if(temp <max_int){
+                    cnt[temp]++;
+                }
+            }
+        }
+    }
+
+    for(int test_case = 1; test_case<=t; test_case++){
+        scanf("%d", &num);
+        printf("#%d %d\n", test_case, cnt[num]);
     }
 }
