bc77a6a57654d2d6a3a71dfca10979d0d832b347
김정윤 <skyepodium@gmail.com>
2019-01-04 01:34:57 +0900

    SWEA 1225 by queue

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,59 +1,38 @@
 #include <iostream>
-
-#define max_int 101
+#include <queue>
 using namespace std;
 
-int dx[] = {0, 0, 1, -1};
-int dy[] = {-1, 1, 0, 0};
-int n, start_x, start_y, end_x, end_y;
-int d[max_int][max_int];
-char temp[101];
-bool check[max_int][max_int];
-
-void dfs(int x, int y){
-    check[x][y] = true;
-    for(int i=0; i<4; i++){
-        int nx = x + dx[i];
-        int ny = y + dy[i];
-        if(check[nx][ny] == false && d[nx][ny] == 0){
-            dfs(nx, ny);
-        }
-    }
-}
-
-void init(){
-    for(int i=0; i<=99; i++){
-        for(int j=0; j<=99; j++){
-            d[i][j] = 0;
-            check[i][j] =false;
-        }
-    }
-}
+int n, num;
 
 int main(){
     for(int test_case=1; test_case<=10; test_case++){
         scanf("%d", &n);
-        init();
-        for(int i=0; i<100; i++){
-            scanf("%s", temp);
-            for(int j=0; j<100; j++){
-                d[i][j] = temp[j] - '0';
-                if(temp[j] == '2'){
-                    start_x = i;
-                    start_y = j;
-                }
-                else if(temp[j] == '3'){
-                    d[i][j] = 0;
-                    end_x = i;
-                    end_y = j;
-                }
+        queue<int> q;
+        for(int i=0; i<8; i++){
+            scanf("%d", &num);
+            q.push(num);
+        }
+
+        int idx = 1;
+        while(true){
+            int cur = q.front();
+            q.pop();
+            int temp = cur - idx;
+            if(temp < 0) temp = 0;
+            q.push(temp);
+            if(temp == 0)break;
+            idx++;
+            if(idx == 6){
+                idx = 1;
             }
         }
 
-        dfs(start_x, start_y);
-        printf("#%d %d\n", test_case, check[end_x][end_y]);
-
+        printf("#%d ", test_case);
+        while(!q.empty()){
+            printf("%d ", q.front());
+            q.pop();
+        }
+        printf("\n");
     }
-
 
 }
