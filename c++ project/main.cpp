19c516cc999196cbae89b6899fc82b4eaded2879
김정윤 <skyepodium@gmail.com>
2019-01-03 20:39:03 +0900

    baekjoon 11725 by DFS

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,50 +1,36 @@
 #include <iostream>
 #include <vector>
 
-#define max_int 27
+#define max_int 100001
 using namespace std;
 
-int n;
-char a, b, c;
-vector<char> v[max_int];
+int n, a, b;
+vector<int> v[max_int];
+bool check[max_int];
+int p[max_int];
 
-void preorder(int node){
-    if(node + 'A' == '.') return;
-
-    printf("%c", node + 'A');
-    preorder(v[node][0]);
-    preorder(v[node][1]);
+void dfs(int node){
+    check[node] = true;
+    for(int i=0; i<v[node].size(); i++){
+        int next = v[node][i];
+        if(check[next] == false){
+            p[next] = node;
+            dfs(next);
+        }
+    }
 }
-
-void inorder(int node){
-    if(node + 'A' == '.') return;
-
-    inorder(v[node][0]);
-    printf("%c", node + 'A');
-    inorder(v[node][1]);
-}
-
-void postorder(int node){
-    if(node + 'A' == '.') return;
-
-    postorder(v[node][0]);
-    postorder(v[node][1]);
-    printf("%c", node + 'A');
-}
-
 
 int main(){
     scanf("%d", &n);
-    for(int i=0; i<n; i++){
-        cin >> a >> b >> c;
-        v[a-'A'].push_back(b-'A');
-        v[a-'A'].push_back(c-'A');
+    for(int i=0; i<n-1; i++){
+        scanf("%d %d", &a, &b);
+        v[a].push_back(b);
+        v[b].push_back(a);
     }
 
-    preorder(0);
-    printf("\n");
-    inorder(0);
-    printf("\n");
-    postorder(0);
+    dfs(1);
+    for(int i=2; i<=n; i++){
+        printf("%d\n", p[i]);
+    }
 
 }
