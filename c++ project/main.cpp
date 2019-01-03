2e9079e47961858f6adc6c0c8ec489a3b0d03990
김정윤 <skyepodium@gmail.com>
2019-01-04 01:25:37 +0900

    SWEA 1227

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 
-#define max_int 17
+#define max_int 101
 using namespace std;
 
 int dx[] = {0, 0, 1, -1};
 int dy[] = {-1, 1, 0, 0};
 int n, start_x, start_y, end_x, end_y;
 int d[max_int][max_int];
-char temp[17];
+char temp[101];
 bool check[max_int][max_int];
 
 void dfs(int x, int y){
@@ -22,8 +22,8 @@ void dfs(int x, int y){
 }
 
 void init(){
-    for(int i=0; i<=16; i++){
-        for(int j=0; j<=16; j++){
+    for(int i=0; i<=99; i++){
+        for(int j=0; j<=99; j++){
             d[i][j] = 0;
             check[i][j] =false;
         }
@@ -34,9 +34,9 @@ int main(){
     for(int test_case=1; test_case<=10; test_case++){
         scanf("%d", &n);
         init();
-        for(int i=0; i<16; i++){
+        for(int i=0; i<100; i++){
             scanf("%s", temp);
-            for(int j=0; j<16; j++){
+            for(int j=0; j<100; j++){
                 d[i][j] = temp[j] - '0';
                 if(temp[j] == '2'){
                     start_x = i;
