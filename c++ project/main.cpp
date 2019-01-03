8c2aa264a2e3244a08a996be7ccec6f186f8f69a
김정윤 <skyepodium@gmail.com>
2019-01-04 01:39:41 +0900

    SWEA 1217

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -2,37 +2,20 @@
 #include <queue>
 using namespace std;
 
-int n, num;
+int n, a, b;
+
+int go(int a, int b){
+    int ret = 1;
+    for(int i=0; i<b; i++){
+        ret = ret*a;
+    }
+    return ret;
+}
 
 int main(){
     for(int test_case=1; test_case<=10; test_case++){
-        scanf("%d", &n);
-        queue<int> q;
-        for(int i=0; i<8; i++){
-            scanf("%d", &num);
-            q.push(num);
-        }
-
-        int idx = 1;
-        while(true){
-            int cur = q.front();
-            q.pop();
-            int temp = cur - idx;
-            if(temp < 0) temp = 0;
-            q.push(temp);
-            if(temp == 0)break;
-            idx++;
-            if(idx == 6){
-                idx = 1;
-            }
-        }
-
-        printf("#%d ", test_case);
-        while(!q.empty()){
-            printf("%d ", q.front());
-            q.pop();
-        }
-        printf("\n");
+        scanf("%d %d %d", &n, &a, &b);
+        printf("#%d %d", test_case, go(a, b));
     }
 
 }
