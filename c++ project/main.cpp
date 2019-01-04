1dad61df49fa296f814554aa9b33ed319063b038
김정윤 <skyepodium@gmail.com>
2019-01-04 20:18:59 +0900

    SWEA 1238 by BFS

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,47 +1,60 @@
 #include <iostream>
-#include <algorithm>
+#include <vector>
+#include <queue>
 
-#define max_val 100001
-#define max_int 1001
+#define max_int 101
 using namespace std;
 
-int t, n, num, result;
-int a[max_int][max_int];
+int n, start_node, a, b, result, max_node;
+bool d[max_int][max_int];
+int check[max_int];
+
+void bfs(int start){
+    check[start] = true;
+    queue<int> q;
+    q.push(start);
+    while(!q.empty()){
+        int node = q.front();
+        q.pop();
+        for(int next=1; next<=100; next++){
+            if(d[node][next] && check[next] == false){
+                check[next] = check[node] + 1;
+                q.push(next);
+            }
+        }
+    }
+}
+
+
+void init(){
+    result = 0;
+    max_node = 0;
+    for(int i=1; i<max_int; i++){
+        check[i] = 0;
+        for(int j=1; j<max_int; j++){
+            d[i][j] = false;
+        }
+    }
+}
 
 int main(){
+    for(int test_case=1; test_case<=1; test_case++){
+        scanf("%d %d", &n, &start_node);
+        init();
 
-    scanf("%d", &t);
-    for(int test_case = 1; test_case<=t; test_case++){
-        result = max_val;
-        scanf("%d", &n);
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=n; j++){
-                a[i][j] = max_val;
-                scanf("%d", &num);
-                if(num == 1) a[i][j] = 1;
-                if(i==j) a[i][j] = 0;
+        for(int i=0; i<n/2; i++){
+            scanf("%d %d", &a, &b);
+            if(d[a][b] == false){
+                d[a][b] = true;
             }
         }
-
-        for(int k=1; k<=n; k++){
-            for(int i=1; i<=n; i++){
-                for(int j=1; j<=n; j++){
-                    if(a[i][j] > a[i][k] + a[k][j]){
-                        a[i][j] = a[i][k] + a[k][j];
-                    }
-                }
+        bfs(start_node);
+        for(int i=100; i>=1; i--){
+            if(result < check[i]){
+                result = check[i];
+                max_node = i;
             }
         }
-
-        for(int i=1; i<=n; i++){
-            int temp = 0;
-            for(int j=1; j<=n; j++){
-                if(i!=j) temp += a[i][j];
-            }
-            result = min(result, temp);
-        }
-
-
-        printf("#%d %d\n", test_case, result);
+        printf("#%d %d\n", test_case, max_node);
     }
 }
