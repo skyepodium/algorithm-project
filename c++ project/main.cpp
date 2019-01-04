19ea2821250ef29fe7ec3844c55cbbff89828ce8
김정윤 <skyepodium@gmail.com>
2019-01-04 19:32:13 +0900

    SWEA 1215 by forloop

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -4,50 +4,53 @@
 #define max_int 101
 using namespace std;
 
-int n, result, temp;
-int a[max_int][max_int];
+int n, result;
+char a[8][8];
+
 
 int main(){
     for(int test_case = 1; test_case<=10; test_case++){
         scanf("%d", &n);
         result = 0;
 
-        for(int i=0; i<100; i++){
-            for(int j=0; j<100; j++){
-                scanf("%d", &a[i][j]);
-            }
+        for(int i=0; i<8; i++){
+            scanf("%s", a[i]);
         }
 
         //1. 가로
-        for(int i=0; i<100; i++){
-            temp = 0;
-            for(int j=0; j<100; j++){
-                temp += a[i][j];
+        for(int i=0; i<8; i++){
+            for(int j=0; j<8; j++){
+                if(j+n <= 8){
+                    bool isPal = true;
+                    for(int k=0; k<n/2; k++){
+                        if(a[i][j+k] != a[i][j+(n-1-k)]){
+                            isPal = false;
+                            break;
+                            //1 - 4
+                            //2 - 3
+                        }
+                    }
+                    if(isPal) result++;
+                }
             }
-            result = max(result, temp);
         }
 
         //2. 세로
-        for(int j=0; j<100; j++){
-            temp = 0;
-            for(int i=0; i<100; i++){
-                temp += a[i][j];
+        for(int j=0; j<8; j++){
+            for(int i=0; i<8; i++){
+                if(i+n <= 8){
+                    bool isPal = true;
+                    for(int k=0; k<n/2; k++){
+                        if(a[i+k][j] != a[i+(n-1-k)][j]){
+                            isPal = false;
+                            break;
+                        }
+                    }
+                    if(isPal) result++;
+                }
             }
-            result = max(result, temp);
         }
 
-        //3. 대각선1
-        temp = 0;
-        for(int i=0; i<100; i++){
-            temp += a[i][i];
-        }
-        result= max(result, temp);
-
-        temp = 0;
-        for(int i=0; i<100; i++){
-            temp += a[i][100 - 1- i];
-        }
-        result= max(result, temp);
 
         printf("#%d %d\n", test_case, result);
     }
