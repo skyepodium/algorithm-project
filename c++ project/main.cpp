f0d68234e0742379708c0b5d52b292fb987bc073
김정윤 <skyepodium@gmail.com>
2019-01-04 19:13:56 +0900

    SWEA 1209 by for loop

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,21 +1,54 @@
 #include <iostream>
-#include <queue>
+#include <algorithm>
+
+#define max_int 101
 using namespace std;
 
-int n, a, b;
-
-int go(int a, int b){
-    int ret = 1;
-    for(int i=0; i<b; i++){
-        ret = ret*a;
-    }
-    return ret;
-}
+int n, result, temp;
+int a[max_int][max_int];
 
 int main(){
-    for(int test_case=1; test_case<=10; test_case++){
-        scanf("%d %d %d", &n, &a, &b);
-        printf("#%d %d", test_case, go(a, b));
-    }
+    for(int test_case = 1; test_case<=10; test_case++){
+        scanf("%d", &n);
+        result = 0;
 
+        for(int i=0; i<100; i++){
+            for(int j=0; j<100; j++){
+                scanf("%d", &a[i][j]);
+            }
+        }
+
+        //1. 가로
+        for(int i=0; i<100; i++){
+            temp = 0;
+            for(int j=0; j<100; j++){
+                temp += a[i][j];
+            }
+            result = max(result, temp);
+        }
+
+        //2. 세로
+        for(int j=0; j<100; j++){
+            temp = 0;
+            for(int i=0; i<100; i++){
+                temp += a[i][j];
+            }
+            result = max(result, temp);
+        }
+
+        //3. 대각선1
+        temp = 0;
+        for(int i=0; i<100; i++){
+            temp += a[i][i];
+        }
+        result= max(result, temp);
+
+        temp = 0;
+        for(int i=0; i<100; i++){
+            temp += a[i][100 - 1- i];
+        }
+        result= max(result, temp);
+
+        printf("#%d %d\n", test_case, result);
+    }
 }
