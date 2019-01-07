40bdd169e203423d59d799b61691c37ff8edd6cd
김정윤 <skyepodium@gmail.com>
2019-01-07 20:36:15 +0900

    baekjoon 1761 by LCA and path tracing

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,59 +1,81 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 
-#define max_int 50001
+#define lld long long int
+#define max_int 40001
 using namespace std;
 
-int n, m, a, b;
-vector<int> v[max_int];
-int p[max_int];
-int d[max_int];
+int n, m, a, b, c;
+struct info{
+    int cur;
+    int cost;
+};
+vector<info> v[max_int];
 bool check[max_int];
+int l[max_int];
+int d[max_int];
+int p[max_int];
 
-void dfs(int node){
-    check[node] = true;
-    for(int i=0; i<v[node].size(); i++){
-        int next = v[node][i];
-        if(!check[next]){
-            p[next] = node;
-            d[next] = d[node] + 1;
-            dfs(next);
+void bfs(int start){
+    check[start] = true;
+    l[start] = 1;
+    d[start] = 0;
+    p[start] = 1;
+    queue<int> q;
+    q.push(start);
+    while(!q.empty()){
+        int c_node = q.front();
+        q.pop();
+
+        for(int i=0; i<v[c_node].size(); i++){
+            info next = v[c_node][i];
+            int n_node = next.cur;
+            int n_cost = next.cost;
+            if(check[n_node] == false){
+                check[n_node] = true;
+                l[n_node] = l[c_node] + 1;
+                d[n_node] = n_cost;
+                p[n_node] = c_node;
+                q.push(n_node);
+            }
         }
     }
 }
 
-int lca(int first, int second){
-    if(d[first] < d[second]){
+lld lca(int first, int second){
+    if(l[first] < l[second]){
         swap(first, second);
     }
 
-    while(d[first] != d[second]){
+    lld result = 0;
+    while(l[first] != l[second]){
+        result += d[first];
         first = p[first];
     }
+
     while(first != second){
+        result += d[first];
+        result += d[second];
         first = p[first];
         second = p[second];
     }
-    return first;
+    return result;
 }
 
 int main(){
     scanf("%d", &n);
     for(int i=0; i<n-1; i++){
-        scanf("%d %d", &a, &b);
-        v[a].push_back(b);
-        v[b].push_back(a);
+        scanf("%d %d %d", &a, &b, &c);
+        v[a].push_back({b, c});
+        v[b].push_back({a, c});
     }
 
-    p[1] = 0;
-    d[1] = 1;
-    dfs(1);
+    bfs(1);
 
     scanf("%d", &m);
     for(int i=0; i<m; i++){
         scanf("%d %d", &a, &b);
-        printf("%d\n", lca(a, b));
+        printf("%lld\n", lca(a, b));
     }
-
-
 }
