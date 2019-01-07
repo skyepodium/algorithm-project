d8e6d3518cf18b864482397466fbda6704f5b640
김정윤 <skyepodium@gmail.com>
2019-01-07 19:02:12 +0900

    baekjoon 1890 by DP(tabulation)

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,28 +1,34 @@
 #include <iostream>
 
-#define max_int 1001
-
+#define lld long long int
+#define max_int 101
 using namespace std;
 
-int n, m;
+int n;
 int a[max_int][max_int];
-int d[max_int][max_int];
+lld d[max_int][max_int];
 
 int main(){
-    scanf("%d %d", &n, &m);
+    scanf("%d", &n);
 
     for(int i=1; i<=n; i++){
-        for(int j=1; j<=m; j++){
+        for(int j=1; j<=n; j++){
             scanf("%d", &a[i][j]);
         }
     }
 
+    d[1][1] = 1;
     for(int i=1; i<=n; i++){
-        for(int j=1; j<=m; j++){
-            d[i][j] = max(d[i-1][j], d[i][j-1]) + a[i][j];
+        for(int j=1; j<=n; j++){
+            if(a[i][j] == 0) continue;
+
+            //가로
+            if(j+ a[i][j] <=n ) d[i][j + a[i][j]] += d[i][j];
+
+            //세로
+            if(i+ a[i][j] <=n ) d[i + a[i][j]][j] += d[i][j];
         }
     }
 
-    printf("%d\n", d[n][m]);
-
+    printf("%lld\n", d[n][n]);
 }
