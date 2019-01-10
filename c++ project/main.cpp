c2f7abb7c291782dbe71cd81aa3a6dd13909b6ff
김정윤 <skyepodium@gmail.com>
2019-01-10 23:54:37 +0900

    01.10

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,70 +1,27 @@
 #include <iostream>
-#include <vector>
 
-#define max_int 40001
+#define max_int 101
 using namespace std;
 
-int n, m, a, b, c;
-bool check[max_int];
-struct info{
-    int cur;
-    int cost;
-};
-vector<info> v[max_int];
-int l[max_int];
-int d[max_int];
-int p[max_int];
+int t, n, start_node;
+int pre[max_int];
+int in[max_int];
 
-void dfs(int node){
-    check[node] = true;
-    for(int i=0; i<v[node].size(); i++){
-        info next = v[node][i];
-        int n_node = next.cur;
-        int n_cost = next.cost;
-        if(!check[n_node]){
-            l[n_node] = l[node] + 1;
-            d[n_node] = d[node] + n_cost;
-            p[n_node] = node;
-            dfs(n_node);
-        }
-    }
-}
-
-int lca(int first, int second){
-    if(l[first] < l[second]){
-        swap(first, second);
-    }
-
-    while(l[first] != l[second]){
-        first = p[first];
-    }
-
-    while(first != second){
-        first = p[first];
-        second = p[second];
-    }
-
-    return first;
-}
 
 
 int main(){
-    scanf("%d", &n);
-    for(int i=0; i<n-1; i++){
-        scanf("%d %d %d", &a, &b, &c);
-        v[a].push_back({b, c});
-        v[b].push_back({a, c});
-    };
+    scanf("%d", &t);
+    for(int test_case = 1; test_case<=t; test_case++){
+        scanf("%d", &n);
+        for(int i=0; i<n; i++){
+            scanf("%d", &pre[i]);
+        }
 
-    l[1] =1;
-    p[1] =1;
-    d[1] = 0;
-    dfs(1);
-
-    scanf("%d", &m);
-    for(int i=0; i<m; i++){
-        scanf("%d %d", &a, &b);
-        printf("%d\n", d[a] + d[b] - 2*d[lca(a, b)]);
+        for(int i=0; i<n; i++){
+            scanf("%d", &in[i]);
+        }
     }
+
+    start_node = pre[0];
 
 }
