1445e773e1d3e500435e79b96b01feb3aa7662cf
김정윤 <skyepodium@gmail.com>
2019-01-15 23:40:25 +0900

    01.15

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,24 +1,42 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
-#define max_int 201
 using namespace std;
 
-int n;
-int a[max_int][max_int];
-int d[max_int][max_int];
+int N, S, A[41];
 
-int main(){
-    scanf("%d", &n);
-    for(int i=1; i<=n; i++){
-        for(int j=1; j<=n; j++){
-            scanf("%d", &a[i][j]);
-        }
+void func(int pos, int end, int sum, vector<int>& vi) {
+    if (pos == end) {
+        vi.push_back(sum);
+        return;
+    }
+    func(pos + 1, end, sum, vi);
+    func(pos + 1, end, sum + A[pos], vi);
+}
+
+int main() {
+    scanf("%d%d", &N, &S);
+    for (int i = 0 ; i < N ; ++i) {
+        scanf("%d", &A[i]);
+    }
+    vector<int> L, R;
+    func(0, N / 2, 0, L);
+    func(N / 2, N, 0, R);
+
+    sort(R.begin(), R.end());
+    long long ans = 0;
+    for (int val : L) {
+        int target = S - val;
+        auto hi = upper_bound(R.begin(), R.end(), target);
+        auto lo = lower_bound(R.begin(), R.end(), target);
+        printf("%lld %lld\n", hi, lo);
+        cout << hi - lo << endl;
+        ans += hi - lo;
     }
 
-    for(int i=1; i<=n; i++){
-        for(int j=1; j<=n; j++){
-            d[i][j] = max(d[i-1][j], d[i][j-1]) + a[i][j];
-        }
+    if (S == 0) {
+        --ans;
     }
-    printf("%d\n", d[n][n]);
+    printf("%lld\n", ans);
 }
