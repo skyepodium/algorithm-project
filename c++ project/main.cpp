7c3d92a8cab008017b415611c8772d8e98710591
김정윤 <skyepodium@gmail.com>
2019-01-16 00:54:26 +0900

    baekjoon 1208 by lower_bound, upper_bound(binary_search)

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -2,41 +2,42 @@
 #include <vector>
 #include <algorithm>
 
+#define max_int 41
+#define lld long long int
 using namespace std;
 
-int N, S, A[41];
+int n, s;
+int a[max_int];
+vector<int> first, second;
+lld cnt = 0;
+void go(int idx, int end, int sum, vector<int> &v){
 
-void func(int pos, int end, int sum, vector<int>& vi) {
-    if (pos == end) {
-        vi.push_back(sum);
+    if(idx == end){
+        v.push_back(sum);
         return;
     }
-    func(pos + 1, end, sum, vi);
-    func(pos + 1, end, sum + A[pos], vi);
+
+    go(idx+1, end, sum + a[idx], v);
+    go(idx+1, end, sum, v);
 }
 
-int main() {
-    scanf("%d%d", &N, &S);
-    for (int i = 0 ; i < N ; ++i) {
-        scanf("%d", &A[i]);
-    }
-    vector<int> L, R;
-    func(0, N / 2, 0, L);
-    func(N / 2, N, 0, R);
 
-    sort(R.begin(), R.end());
-    long long ans = 0;
-    for (int val : L) {
-        int target = S - val;
-        auto hi = upper_bound(R.begin(), R.end(), target);
-        auto lo = lower_bound(R.begin(), R.end(), target);
-        printf("%lld %lld\n", hi, lo);
-        cout << hi - lo << endl;
-        ans += hi - lo;
-    }
+int main(){
+    scanf("%d %d", &n, &s);
+    for(int i=0; i<n; i++) scanf("%d", &a[i]);
 
-    if (S == 0) {
-        --ans;
+    go(0, n/2, 0, first);
+    go(n/2, n, 0, second);
+    sort(second.begin(), second.end());
+
+    for(int i=0; i<(int)first.size(); i++){
+        int cur = s - first[i];
+
+        auto lo = lower_bound(second.begin(), second.end(), cur);
+        auto up = upper_bound(second.begin(), second.end(), cur);
+        cnt += up - lo;
     }
-    printf("%lld\n", ans);
+    if(s==0) cnt--;
+
+    printf("%lld\n", cnt);
 }
