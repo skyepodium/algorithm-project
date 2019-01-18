a9b11768c6198b5d07227056633f37645a844093
김정윤 <skyepodium@gmail.com>
2019-01-18 21:19:33 +0900

    programmers 49189 가장 먼 노드

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,30 +1,51 @@
 #include <string>
 #include <vector>
 #include <queue>
+#include <iostream>
+#include <algorithm>
+#define max_int 20001
 using namespace std;
 
-bool check[201];
+int check[max_int];
+int max_distance = 0;
+vector<int> v[max_int];
 
-void dfs(int node, vector<vector<int>> &computers, int n){
-    check[node] = true;
-    for(int i=0; i<n; i++){
-        int next = i;
-        if(check[next] == false && computers[node][i] == 1){
-            dfs(next, computers, n);
+void bfs(int node){
+    check[node] = 0;
+    queue<int> q;
+    q.push(node);
+    while(!q.empty()){
+        node = q.front();
+        q.pop();
+        for(int i=0; i<v[node].size(); i++){
+            cout << "asdf" << endl;
+            int next = v[node][i];
+            if(check[next] == -1){
+                check[next] = check[node] + 1;
+                max_distance = max(max_distance, check[next]);
+                q.push(next);
+            }
         }
     }
 }
 
-int solution(int n, vector<vector<int>> computers) {
+int solution(int n, vector<vector<int>> edge) {
     int answer = 0;
-    for(int i=0; i<=n; i++) check[i] = false;
-
     for(int i=0; i<n; i++){
-        if(check[i] == false){
-            dfs(i, computers, n);
-            answer++;
-        }
+        check[i] = false;
+        cout << edge[i][0] << " " << edge[i][1] << endl;
+        v[edge[i][0]].push_back(edge[i][1]);
+        v[edge[i][1]].push_back(edge[i][0]);
     }
 
+
+    check[1] = 0;
+    bfs(1);
+    for(int i=1; i<=n; i++){
+        cout << check[i] << endl;
+    }
+    for(int i=1; i<=n; i++){
+        if(check[i] == max_distance) answer++;
+    }
     return answer;
 }
