6049e1490877afd0604adc07e4a5c5875e34fbd1
김정윤 <skyepodium@gmail.com>
2019-01-19 20:01:12 +0900

    programmers 12940 by gcd

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,14 +1,16 @@
-#include <string>
-#include <vector>
+#include <iostream>
 
 using namespace std;
 
-double solution(vector<int> arr) {
-    double answer = 0;
-    double total = 0;
-    for(int i=0; i<(int)arr.size(); i++){
-        total += arr[i];
-    }
-    answer = total/(int)arr.size();
-    return answer;
+int gcd(int a, int b){
+    if(b==0) return a;
+    else return gcd(b, a%b);
+}
+
+int main(){
+    int a, b;
+    scanf("%d %d", &a, &b);
+    cout << gcd(a, b) << endl;
+    cout << a*b/gcd(a, b) << endl;
+
 }
