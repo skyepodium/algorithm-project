68db5c7e7e6d91a1297b6011a9b29afc02a1739a
김정윤 <skyepodium@gmail.com>
2019-01-19 19:45:01 +0900

    programmers 12903 by string

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,46 +1,15 @@
 #include <iostream>
-#include <queue>
-#include <vector>
+#include <string>
 using namespace std;
 
-int a[4] = {2, 1, 3, 2};
-int location = 2;
-struct info{
-    int idx;
-    int cost;
-};
+string s = "qwer";
 
-struct cmp{
-    bool operator()(const info  &a, const info &b){
-        return a.cost < b.cost;
+int main() {
+    int size =(int)s.size();
+    if(size%2 == 1){
+        cout << s[size/2] << endl;
     }
-};
-
-int main(){
-    queue<info> q;
-    priority_queue<info, vector<info>, cmp> pq;
-
-    for(int i=0; i<4; i++){
-        q.push({i, a[i]});
-        pq.push({i, a[i]});
-    }
-
-    int cnt = 1;
-    while(!q.empty()){
-        info cur = q.front();
-        q.pop();
-        int max_val = pq.top().cost;
-        if(cur.cost == max_val){
-            if(cur.idx == location){
-                printf("%d\n", cnt);
-                break;
-            }else{
-                pq.pop();
-                cnt++;
-            }
-        }
-        else{
-            q.push(cur);
-        }
+    else{
+        cout << s[size/2 - 1] << s[size/2] << endl;
     }
 }
