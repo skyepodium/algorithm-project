aaec2479263d8bac99b43a133f56fa880c909276
김정윤 <skyepodium@gmail.com>
2019-01-20 21:00:41 +0900

    baekjoon 2003 by two pointer

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
-#include <cmath>
 
+#define max_int 10001
 using namespace std;
 
+int n, m;
+int a[max_int];
+
 int main(){
-    int num = sqrt(121);
-    cout << num << endl;
+    scanf("%d %d", &n, &m);
+    for(int i=0; i<n; i++){
+        scanf("%d", &a[i]);
+    }
+
+    int left = 0;
+    int right = 0;
+    int cur = a[0];
+    int cnt = 0;
+    while(left < n && right < n){
+        if(cur < m){
+            right++;
+            cur += a[right];
+        }
+        else if(cur > m){
+            cur -= a[left];
+            left++;
+        }
+        else{
+            cnt++;
+            cur -= a[left];
+            left++;
+        }
+    }
+    printf("%d\n", cnt);
+
 }
