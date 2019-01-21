2e3216d2f142a3a9d992ae16626318f91896fc21
김정윤 <skyepodium@gmail.com>
2019-01-21 22:08:21 +0900

    koitp allicane by disjoint-set

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,36 +1,36 @@
 #include <iostream>
 
-#define max_int 10001
+#define max_int 100001
 using namespace std;
 
-int n, m;
-int a[max_int];
+int n, q, a, b, c;
+int d[max_int];
+
+int find(int node){
+    if(node == d[node]) return node;
+    else return d[node] = find(d[node]);
+}
 
 int main(){
-    scanf("%d %d", &n, &m);
-    for(int i=0; i<n; i++){
-        scanf("%d", &a[i]);
+    scanf("%d %d", &n, &q);
+    for(int i=1; i<=n; i++){
+        d[i] = i;
     }
 
-    int left = 0;
-    int right = 0;
-    int cur = a[0];
-    int cnt = 0;
-    while(left < n && right < n){
-        if(cur < m){
-            right++;
-            cur += a[right];
+    for(int i=0; i<q; i++){
+        scanf("%d %d %d", &a, &b, &c);
+        if(a == 0){// 동맹을 맺는다.
+            d[find(b)] = find(c);
         }
-        else if(cur > m){
-            cur -= a[left];
-            left++;
-        }
-        else{
-            cnt++;
-            cur -= a[left];
-            left++;
+        else{ // 동맹인지를 알아본다.
+            b = find(b);
+            c = find(c);
+            if(b == c){
+                printf("1\n");
+            }
+            else{
+                printf("0\n");
+            }
         }
     }
-    printf("%d\n", cnt);
-
 }
