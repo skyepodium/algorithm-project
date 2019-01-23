b16ac432bf8e97db235108210323d04f917f81ec
김정윤 <skyepodium@gmail.com>
2019-01-23 22:12:45 +0900

    koitp BFS by BFS

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,47 +1,41 @@
 #include <iostream>
+#include <queue>
 #include <vector>
-#include <algorithm>
 
-#define max_int 50001
+#define max_int 100001
+
 using namespace std;
 
-int n, m, result = 0;
-int d[max_int];
-struct info{
-    int start;
-    int end;
-    int cost;
-};
+int n, m, start_node, a, b;
+bool check[max_int];
+vector<int> v[max_int];
 
-bool cmp(const info &a, const info &b){
-    return a.cost < b.cost;
-}
-
-int find(int node){
-    if(node == d[node]) return node;
-    else return d[node] = find(d[node]);
+void bfs(int start){
+    queue<int> q;
+    check[start] = true;
+    q.push(start);
+    while(!q.empty()){
+        int node = q.front();
+        printf("%d ", node);
+        q.pop();
+        for(int i=0; i<(int)v[node].size(); i++){
+            int next = v[node][i];
+            if(!check[next]){
+                check[next] = true;
+                q.push(next);
+            }
+        }
+    }
 }
 
 int main(){
-    scanf("%d %d", &n, &m);
-    for(int i=1; i<=n; i++) d[i] = i;
-
-    vector<info> v(m);
+    scanf("%d %d %d", &n, &m, &start_node);
     for(int i=0; i<m; i++){
-        scanf("%d %d %d", &v[i].start, &v[i].end, &v[i].cost);
+        scanf("%d %d", &a, &b);
+        v[a].push_back(b);
+        v[b].push_back(a);
     }
 
-    sort(v.begin(), v.end(), cmp);
-
-    for(int i=0; i<m; i++){
-        int a = find(v[i].start);
-        int b = find(v[i].end);
-
-        if(a!=b){
-            d[a] = b;
-            result += v[i].cost;
-        }
-    }
-    printf("%d\n", result);
-
+    bfs(start_node);
+    printf("\n");
 }
