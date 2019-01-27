b07a8ca62bda6f08c2cb6f5a668e86e1e307354f
김정윤 <skyepodium@gmail.com>
2019-01-27 17:20:45 +0900

    programmers 점프와 순간이동 by while loop

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,36 +1,23 @@
-#include <vector>
 #include <iostream>
 
 using namespace std;
 
-//시간 복잡도: O(n^3logK) (n은 숫자의 개수: 1<=n<=50, k는 숫자의 크기 1<=k<=1000)
-//공간 복잡도: O(n)
+//시간 복잡도: O(logn)
+//공간 복잡도: O(1)
 //사용한 알고리즘: 반복문
-//사용한 자료구조: 1차원 배열
+//사용한 자료구조: 없음
 
-bool isPrime(int num){
-    bool flag = true;
-    for(int i=2; i*i<=num; i++){
-        if(num%i == 0){
-            flag = false;
-            break;
-        }
-    }
-    return flag;
-}
-
-int solution(vector<int> nums) {
+int solution(int n)
+{
     int answer = 0;
-    int num, size = (int)nums.size();
-
-    for(int i=0; i<size; i++){
-        for(int j=i+1; j<size; j++){
-            for(int k=j+1; k<size; k++){
-                num = nums[i] + nums[j] + nums[k];
-                if(isPrime(num)) answer++;
-            }
+    while(n != 0){
+        if(n%2==0){
+            n = n/2;
+        }
+        else{
+            n = n-1;
+            answer++;
         }
     }
-
     return answer;
 }
