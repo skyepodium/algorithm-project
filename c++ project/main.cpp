cb1799a260db4224e5e508b34de1e3f64503d56b
김정윤 <skyepodium@gmail.com>
2019-01-27 16:00:37 +0900

    programmers 배달 by dijkstra algorithm

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,65 +1,76 @@
-#include <string>
 #include <vector>
-#include <algorithm>
+#include <queue>
+#define max_int 51
+#define max_val 2500001
 
-#define max_int 101
 using namespace std;
 
-//시간 복잡도: O(elogn) (n: 정점의 개수, e: 간선의 개수)
-//공간 복잡도: O(en)
-//사용한 알고리즘: 크루스칼 알고리즘
+//시간 복잡도: O(ElogN) (N: 정점의 개수 50 이하, E: 간선의 개수 2000이하)
+//공간 복잡도: O(EN)
+//사용한 알고리즘: 다익스트라 알고리즘
 //사용한 자료구조: 인접리스트
 
-
-// d[i]는 정점 i의 부모 정점을 넣는다.
+// d[i] 는 1번 정점(시작 정점)에서 i번 정점까지 최단 시간
 int d[max_int];
 
+struct info{
+    int cur;
+    int cost;
+};
 
-// find는 최상위 부모를 찾는 함수
-int find(int node){
-    // 1) 만약 부모가 자기자신이면 현재 노드가 최상위 부모이다.
-    if(node == d[node]) return node;
+// c++의 우선순위 큐는 max_heap이기 때문에 min_heap으로 바꿔준다.
+struct cmp{
+    bool operator()(const info &a, const info &b){
+        return a.cost > b.cost;
+    }
+};
 
-        // 2) 아니라면 최상위 부모의 부모를 찾는다.
-    else return d[node] = find(d[node]);
-}
+vector<info> v[max_int];
 
-
-// 간선의 가중치를 기준으로 오름차순 정렬
-bool cmp(const vector<int> &a, const vector<int> &b){
-    return a[2] < b[2];
-}
-
-
-int solution(int n, vector<vector<int>> costs) {
+int solution(int N, vector<vector<int> > road, int K) {
     int answer = 0;
 
-    // 1. d[i] = i의 부모를 담는다. 처음에는 자기 자신이 부모이다.
-    // disjoint-set을 사용하기 위해 초기화
-    for(int i=0; i<n; i++){
-        d[i] = i;
+    // 1. 간선의 정보를 인접리스트에 담는다, 양방향 그래프의 정보를 넣어준다.
+    for(int i=0; i<road.size(); i++){
+        int node = road[i][0];
+        int cur = road[i][1];
+        int cost = road[i][2];
+        v[node].push_back({cur, cost});
+        v[cur].push_back({node, cost});
     }
 
+    // 2. 모든 정점까지의 시간을 최대값으로 초기화한다.
+    for(int i=1; i<=N; i++) d[i] = max_val;
 
-    // 2. 간선의 가중치 기준 오름차순 정렬
-    sort(costs.begin(), costs.end(), cmp);
+    // 3. 우선순위 큐를 생성하고, 1번 정점까지의 시간은 0으로 초기화 한다.
+    priority_queue<info, vector<info>, cmp> pq;
+    d[1] = 0;
+    pq.push({1, d[1]});
 
+    // 4. 다익스트라 알고리즘 수행
+    while(!pq.empty()){
+        // 1) 인접한 정점중 가장 가까운 정점을 선택한다.
+        info cur = pq.top();
+        pq.pop();
+        int c_node = cur.cur;
 
-    // 3. 모든 간선을 검사한다.
-    for(int i=0; i<costs.size(); i++){
-        int start = find(costs[i][0]);
-        int end = find(costs[i][1]);
-        int cost = costs[i][2];
+        // 2) 현재 정점에서 인접한 정점 검사
+        for(int i=0; i<v[c_node].size(); i++){
+            info next = v[c_node][i];
+            int n_node = next.cur;
+            int n_cost = next.cost;
 
-
-        // 4. start와 end가 아직 연결되지 않았다면
-        if(start != end){
-            // 1) start의 부모를 end로 설정하고
-            d[start] = end;
-
-            // 2) 간선의 가중치를 결과에 더해준다.
-            answer += cost;
+            // 3) 만약 현재 정점에서 다음 정점까지 가는 시간이 더 짧으면 시간을 갱신해준다.
+            if(d[n_node] > d[c_node] + n_cost){
+                d[n_node] = d[c_node] + n_cost;
+                pq.push({n_node, d[n_node]});
+            }
         }
+    }
+
+    // 5. 해당 정점까지의 시간이 K 이하라면 결과 1증가
+    for(int i=1; i<=N; i++){
+        if(d[i] <= K) answer++;
     }
 
     return answer;
