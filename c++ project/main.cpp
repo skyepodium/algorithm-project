14ad9f535ccee7e68ed0e2b77e590b0b468d4832
김정윤 <skyepodium@gmail.com>
2019-01-27 16:51:08 +0900

    programmers 소수 만들기

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,50 +1,36 @@
 #include <vector>
-#include <algorithm>
+#include <iostream>
 
-#define max_int 100001
 using namespace std;
 
-//시간 복잡도: O(n)
+//시간 복잡도: O(n^3logK) (n은 숫자의 개수: 1<=n<=50, k는 숫자의 크기 1<=k<=1000)
 //공간 복잡도: O(n)
-//사용한 알고리즘: 동적 계획법, Bottom-Up
+//사용한 알고리즘: 반복문
 //사용한 자료구조: 1차원 배열
 
-
-// d[i]는 i번재 스티커를 때거나 안땟을때 얻을 수 있는 최대값
-int d[max_int];
-
-int solution(vector<int> sticker)
-{
-    int answer, size;
-    size = (int)sticker.size();
-
-    // 0. 예외처리 1) 길이가 1 - 첫번째 스티커 반환, 2) 길이가 - 두개 중 큰거 반환
-    if(size == 1) return sticker[0];
-    else if(size == 2) return max(sticker[0], sticker[1]);
-    else{
-
-        // 1. 첫번째 스티커를 땠을때
-        d[0] = sticker[0];
-        d[1] = sticker[0];
-
-        for(int i=2; i<size - 1; i++){
-            d[i] = max(d[i-2] + sticker[i], d[i-1]);
+bool isPrime(int num){
+    bool flag = true;
+    for(int i=2; i*i<=num; i++){
+        if(num%i == 0){
+            flag = false;
+            break;
         }
-        answer = d[size - 2];
-
-        // d 배열 초기화
-        for(int i=0; i<size; i++) d[i] = 0;
-
-        // 2. 첫번째 스티커를 안땠을때
-        d[0] = 0;
-        d[1] = sticker[1];
-
-        for(int i=2; i<size; i++){
-            d[i] = max(d[i-2] + sticker[i], d[i-1]);
-        }
-
-        answer = max(answer, d[size - 1]);
-
-        return answer;
     }
+    return flag;
+}
+
+int solution(vector<int> nums) {
+    int answer = 0;
+    int num, size = (int)nums.size();
+
+    for(int i=0; i<size; i++){
+        for(int j=i+1; j<size; j++){
+            for(int k=j+1; k<size; k++){
+                num = nums[i] + nums[j] + nums[k];
+                if(isPrime(num)) answer++;
+            }
+        }
+    }
+
+    return answer;
 }
