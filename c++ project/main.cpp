92a76932631e60e882a4297940f709f24a141ca1
김정윤 <skyepodium@gmail.com>
2019-01-31 20:12:35 +0900

    programmers 예산 by for loop

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,19 +1,18 @@
-#include <iostream>
-#include <stdio.h>
-#include <string>
 #include <vector>
 #include <algorithm>
 
 using namespace std;
 
+int a[10000001];
 int solution(vector<int> d, int budget) {
+
     int answer = 0;
     sort(d.begin(), d.end());
-    for(int  i=0; i<d.size();i++)
-        if(budget-d[i]>=0){
-            budget-=d[i];
+    for(int i=0; i<(int)d.size(); i++){
+        if(budget - d[i] >= 0){
+            budget -= d[i];
             answer++;
         }
-
+    }
     return answer;
 }
