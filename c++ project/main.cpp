88aef1836004cd80d5274cf23192c7a62842d62a
김정윤 <skyepodium@gmail.com>
2019-02-05 19:25:40 +0900

    baekjoon 5567 by bfs

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,18 +1,48 @@
+#include <iostream>
 #include <vector>
-#include <algorithm>
+#include <queue>
 
+#define max_int 501
 using namespace std;
 
-int a[10000001];
-int solution(vector<int> d, int budget) {
+int n, m, a, b, result;
+vector<int> v[max_int];
+int check[max_int];
 
-    int answer = 0;
-    sort(d.begin(), d.end());
-    for(int i=0; i<(int)d.size(); i++){
-        if(budget - d[i] >= 0){
-            budget -= d[i];
-            answer++;
+void bfs(int start){
+    check[start] = 1;
+    queue<int> q;
+    q.push(start);
+    while(!q.empty()){
+        int node = q.front();
+        q.pop();
+
+        for(int i=0; i<v[node].size(); i++){
+            int next = v[node][i];
+            if(check[next] == 0){
+                check[next] = check[node] + 1;
+                q.push(next);
+            }
         }
     }
-    return answer;
+}
+
+int main(){
+    scanf("%d %d", &n, &m);
+    for(int i=0; i<m; i++){
+        scanf("%d %d", &a, &b);
+        v[a].push_back(b);
+        v[b].push_back(a);
+    }
+
+    check[1] = 1;
+    bfs(1);
+    result = 0;
+    for(int i=2; i<=n; i++){
+        if(check[i] == 2 || check[i] == 3){
+            result++;
+        }
+    }
+
+    printf("%d\n", result);
 }
