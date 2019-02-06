dea7aba9b3cae12f1ccbe1fa8039da7cd63f342d
김정윤 <skyepodium@gmail.com>
2019-02-06 18:19:54 +0900

    algospot brave by djsjoint-set

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,49 +1,75 @@
 #include <iostream>
-#include <vector>
+#include <algorithm>
 
-#define max_int 11
+#define max_int 100001
 using namespace std;
 
-int n, m, a, b;
-vector<int> v[max_int];
-bool check[max_int];
+//시간 복잡도: O(nm)
+//공간 복잡도: O(n)
+//사용한 알고리즘: disjoint-set
+//사용한 자료구조: 1차원 배열
 
-void init() {
-    for(int i=1; i<=n; i++){
-        check[i] = false;
-    }
+int t, n, m, a, b, result;
+
+// 정점 a의 부모를 담을 배열 d
+// d[a] = b라면, 정점 a의 부모는 b다.
+int d[max_int];
+
+// 정점 a를 최상위 부모로 가지고 있는 정점들의 개수
+// cnt[a] = 4라면, 정점 a를 최상위 부모로 가지는 정점들의 개수는 4개이다.(자기 자신 포함)
+int cnt[max_int];
+
+
+// 최상위 부모를 찾는 함수
+int find(int node){
+    // 1) 만약, 현재 노드의 부모와 현재 노드가 같다면 최상위 노드이다.
+    if(d[node] == node) return node;
+    // 2) 다르다면, 재귀 호출을 통해 최상위 부모를 찾아서 넣어준다.
+    else return d[node] = find(d[node]);
 }
 
-void dfs(int node){
-    for(int i=0; i<v[node].size(); i++){
-        int next = v[node][i];
-        if(!check[next]){
-            check[next] = true;
-            dfs(next);
-        }
+// 초기화 함수
+void init() {
+    for(int i=1; i<=n; i++){
+        // 정점 i의 부모는 i다.
+        d[i] = i;
+        cnt[i] = 0;
     }
 }
 
 int main(){
-   scanf("%d %d", &n, &m);
+    scanf("%d", &t);
+    for(int test_case = 1; test_case <= t; test_case++){
+        scanf("%d %d", &n, &m);
 
-   for(int i=0; i<m; i++){
-       scanf("%d %d", &a, &b);
-       v[a].push_back(b);
-       v[b].push_back(a);
-   }
+        // 1. 초기화, 1) 부모의 정보를 담는 배열 d, 2) 개수 정보를 담을 배열 i초기화
+        init();
 
-   bool flag = false;
-   for(int i=1; i<=n; i++){
-       init();
-       dfs(i);
-       if(check[i]){
-           flag = true;
-           break;
-       }
-   }
+        // 2. 두 금속을 입력받는다.
+        for(int i=0; i<m; i++){
+            scanf("%d %d", &a, &b);
+            // 1) 각 금속의 부모를 찾는다.
+            a = find(a);
+            b = find(b);
 
-   if(flag) printf("YES\n");
-   else printf("NO\n");
+            // 2) 각 금속의 최상위 부모가 다르다면
+            if(a != b){
+                // a 금속의 최상위 부모의 부모는 b가 된다.
+                d[a] = b;
+            }
+        }
 
+        // 모든 정점의 최상위 부모를 찾고, 개수를 증가시켜준다.
+        for(int i=1; i<=n; i++){
+            cnt[find(i)]++;
+        }
+
+        result = 0;
+        // 최상위 부모로 가장 많이 선택된 정점에 대해 선택된 개수를 갱신해준다.
+        for(int i=1; i<=n; i++){
+            result = max(result, cnt[i]);
+        }
+
+        printf("%d\n", result);
+    }
 }
