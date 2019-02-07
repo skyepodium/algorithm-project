e6643dd457e4927fb6a8a6461c3d669f84bd77a0
김정윤 <skyepodium@gmail.com>
2019-02-07 21:41:14 +0900

    algospot CSBASEBALL by for loop

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,45 +1,23 @@
 #include <iostream>
-#include <algorithm>
 
-#define max_int 101
 using namespace std;
 
-//시간 복잡도: O(mnlogn)
-//공간 복잡도: O(n)
-//사용한 알고리즘: 그리디, STL sort
-//사용한 자료구조: 배열
-
-
-int t, n, m, result;
-int a[max_int];
+int t, n, m, num;
 
 int main(){
     scanf("%d", &t);
-
-    for(int test_case = 1; test_case <=t; test_case++){
-
-        // 1. 변수 초기화
-        result = 0;
-
-        // 2. 정보를 입력받는다.
+    for(int test_case = 1; test_case <= t; test_case++){
         scanf("%d %d", &n, &m);
-        for(int i=0; i<n; i++){
-            scanf("%d", &a[i]);
+
+        if(n > m){
+            printf("0\n");
         }
+        else{
+            num = m - n;
 
-        // 3. m번 만큼 아이템을 사용합니다.
-        while(m--){
-            // 마력 내림차순으로 정렬
-            sort(a, a+n, greater<int>());
-
-            // 제일 마력이 많이 들어있는 아이템을 사용합니다.
-            result += a[0];
-
-            // 마력은 0이 되면 없어집니다.
-            if(a[0] > 0) a[0]--;
+            int base = 4;
+            int result = base + num;
+            printf("%d\n", result);
         }
-
-        // 4. 결과 출력
-        printf("%d\n", result);
     }
 }
