33ae8dff6c8af52e1d6660ec7d83b2ef5f5ab5dd
김정윤 <skyepodium@gmail.com>
2019-02-08 22:37:29 +0900

    구름 에듀 아르바이트

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,22 +1,34 @@
 #include <iostream>
+#include <algorithm>
 
-#define max_int 101
+#define max_int 1001
 using namespace std;
 
-int t, n;
-int mod = 1000000007;
-int d[max_int];
+int n;
+int a[max_int];
+int d[max_int][3];
+
+int max3(int q, int w, int e){
+    return max(max(q, w), e);
+}
 
 int main(){
-    scanf("%d", &t);
-    for(int test_case = 1; test_case <= t; test_case++){
-        scanf("%d", &n);
+    scanf("%d", &n);
 
-        d[0] = d[1] = 1;
-        for(int i=2; i<=n; i++){
-            d[i] = (d[i-1] + d[i-2])%mod;
-        }
-        printf("%d\n", d[n]);
-
+    for(int i=0; i<n; i++){
+        scanf("%d", &a[i]);
     }
+
+    d[0][1] = a[0];
+
+    for(int i=1; i<n; i++){
+        d[i][0] = max3(d[i-1][0], d[i-1][1], d[i-1][2]);
+        d[i][1] = d[i-1][0] + a[i];
+        d[i][2] = d[i-1][1] + a[i];
+    }
+
+
+    printf("%d\n", max3(d[n-1][0], d[n-1][1], d[n-1][2]));
+
+
 }
