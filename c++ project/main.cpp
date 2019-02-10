bfae4fd7d44b635d168d3e5adcb0ef40ffed1629
김정윤 <skyepodium@gmail.com>
2019-02-10 15:41:04 +0900

    구름 미로탈출하기 by BFS

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,40 +1,55 @@
 #include <iostream>
-#include <algorithm>
+#include <queue>
 
 #define max_int 101
-#define max_val 1001
 using namespace std;
 
-int n;
-int a[max_int][max_int];
-int d[max_int][max_int];
+int n, m, start_x, start_y, end_x, end_y;
+char a[max_int][max_int];
+int check[max_int][max_int];
+int dx[] = {0, 0, 1, -1};
+int dy[] = {-1, 1, 0, 0};
 
-void init(){
-    for(int i=0; i<=n; i++){
-        for(int j=0; j<=n; j++){
-            d[i][j] = max_val;
+void bfs(int x, int y){
+    queue<pair<int, int>> q;
+    q.push(make_pair(x, y));
+    check[x][y] = 1;
+    while(!q.empty()){
+        x = q.front().first;
+        y = q.front().second;
+        q.pop();
+
+        for(int i=0; i<4; i++){
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if(nx>=0 && nx<n && ny>=0 && ny<m){
+                if(check[nx][ny] == 0 && a[nx][ny] != '#'){
+                    check[nx][ny] = check[x][y] + 1;
+                    q.push(make_pair(nx, ny));
+                }
+            }
         }
     }
 }
 
 int main(){
-    scanf("%d", &n);
-
-    init();
-
-    for(int i=1; i<=n; i++){
-        for(int j=1; j<=n; j++){
-            scanf("%d", &a[i][j]);
+    scanf("%d %d", &n, &m);
+    for(int i=0; i<n; i++){
+        scanf("%s", a[i]);
+        for(int j=0; j<m; j++){
+            if(a[i][j] == 'S'){
+                start_x = i;
+                start_y = j;
+            }
+            else if(a[i][j] == 'E'){
+                end_x = i;
+                end_y = j;
+            }
         }
     }
 
-    d[1][1] = a[1][1];
-    for(int i=1; i<=n; i++){
-        for(int j=1; j<=n; j++){
-            if(i==1 && j==1) continue;
-            d[i][j] = min(d[i-1][j], d[i][j-1]) + a[i][j];
-        }
-    }
+    bfs(start_x, start_y);
 
-    printf("%d\n", d[n][n]);
+    printf("%d\n", check[end_x][end_y] - 1);
 }
