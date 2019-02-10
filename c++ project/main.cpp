0203657d4204b5535a614ad562f8c81f2a2f51ba
김정윤 <skyepodium@gmail.com>
2019-02-10 17:26:31 +0900

    goorm 그래프의 최단 경로 by BFS

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,27 +1,42 @@
 #include <iostream>
-
-#define max_int 1001
-#define max_val 10001
+#include <vector>
+#include <queue>
+#define max_int 100001
 using namespace std;
 
-int n, m;
-int d[max_val];
-int k[max_int];
-int s[max_int];
+int n, m, start_node, end_node, a, b;
+vector<int> v[max_int];
+int check[max_int];
 
-int main(){
-    scanf("%d %d", &n, &m);
+void bfs(int start){
+    check[start] = 1;
+    queue<int> q;
+    q.push(start);
+    while(!q.empty()){
+        int node = q.front();
+        q.pop();
 
-    for(int i=0; i<n; i++){
-        scanf("%d %d", &k[i], &s[i]);
-    }
-
-    for(int i=0; i<n; i++){
-        for(int j=m; j>=k[i]; j--){
-            if(j-k[i]>=0) d[j] = max(d[j], d[j-k[i]] + s[i]);
-
+        for(int i=0; i<v[node].size(); i++){
+            int next = v[node][i];
+            if(check[next] == 0){
+                check[next] = check[node] + 1;
+                q.push(next);
+            }
         }
     }
+}
 
-    printf("%d\n", d[m]);
+int main(){
+    scanf("%d %d %d %d", &n, &m, &start_node, &end_node);
+
+    for(int i=0; i<m; i++){
+        scanf("%d %d", &a, &b);
+        v[a].push_back(b);
+        v[b].push_back(a);
+    }
+
+    bfs(start_node);
+
+    printf("%d\n", check[end_node] - 1);
+
 }
