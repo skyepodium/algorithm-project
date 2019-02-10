5a3b89da634952cfed81de249bcb606302669982
김정윤 <skyepodium@gmail.com>
2019-02-10 15:45:35 +0900

    goorm LIS by DP

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,55 +1,33 @@
 #include <iostream>
-#include <queue>
 
-#define max_int 101
+#define max_int 1001
 using namespace std;
 
-int n, m, start_x, start_y, end_x, end_y;
-char a[max_int][max_int];
-int check[max_int][max_int];
-int dx[] = {0, 0, 1, -1};
-int dy[] = {-1, 1, 0, 0};
-
-void bfs(int x, int y){
-    queue<pair<int, int>> q;
-    q.push(make_pair(x, y));
-    check[x][y] = 1;
-    while(!q.empty()){
-        x = q.front().first;
-        y = q.front().second;
-        q.pop();
-
-        for(int i=0; i<4; i++){
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-
-            if(nx>=0 && nx<n && ny>=0 && ny<m){
-                if(check[nx][ny] == 0 && a[nx][ny] != '#'){
-                    check[nx][ny] = check[x][y] + 1;
-                    q.push(make_pair(nx, ny));
-                }
-            }
-        }
-    }
-}
+int n;
+int a[max_int];
+int d[max_int];
 
 int main(){
-    scanf("%d %d", &n, &m);
+
+    scanf("%d", &n);
     for(int i=0; i<n; i++){
-        scanf("%s", a[i]);
-        for(int j=0; j<m; j++){
-            if(a[i][j] == 'S'){
-                start_x = i;
-                start_y = j;
-            }
-            else if(a[i][j] == 'E'){
-                end_x = i;
-                end_y = j;
+        scanf("%d", &a[i]);
+        d[i] = 1;
+    }
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<i; j++){
+            if(a[j] < a[i]){
+                d[i] = max(d[i], d[j] + 1);
             }
         }
     }
 
-    bfs(start_x, start_y);
+    int result = 0;
+    for(int i=0; i<n; i++){
+        result = max(result, d[i]);
+    }
 
-    printf("%d\n", check[end_x][end_y] - 1);
+    printf("%d\n", result);
+
 }
