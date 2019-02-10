7e756c7113d3e32a8f924eccd6d1bd86c94ab615
김정윤 <skyepodium@gmail.com>
2019-02-10 15:26:50 +0900

    구름 회선설치하기 by DP

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,34 +1,40 @@
 #include <iostream>
 #include <algorithm>
 
-#define max_int 1001
+#define max_int 101
+#define max_val 1001
 using namespace std;
 
 int n;
-int a[max_int];
-int d[max_int][3];
+int a[max_int][max_int];
+int d[max_int][max_int];
 
-int max3(int q, int w, int e){
-    return max(max(q, w), e);
+void init(){
+    for(int i=0; i<=n; i++){
+        for(int j=0; j<=n; j++){
+            d[i][j] = max_val;
+        }
+    }
 }
 
 int main(){
     scanf("%d", &n);
 
-    for(int i=0; i<n; i++){
-        scanf("%d", &a[i]);
+    init();
+
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n; j++){
+            scanf("%d", &a[i][j]);
+        }
     }
 
-    d[0][1] = a[0];
-
-    for(int i=1; i<n; i++){
-        d[i][0] = max3(d[i-1][0], d[i-1][1], d[i-1][2]);
-        d[i][1] = d[i-1][0] + a[i];
-        d[i][2] = d[i-1][1] + a[i];
+    d[1][1] = a[1][1];
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n; j++){
+            if(i==1 && j==1) continue;
+            d[i][j] = min(d[i-1][j], d[i][j-1]) + a[i][j];
+        }
     }
 
-
-    printf("%d\n", max3(d[n-1][0], d[n-1][1], d[n-1][2]));
-
-
+    printf("%d\n", d[n][n]);
 }
