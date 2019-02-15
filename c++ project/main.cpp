d812e7052c08247b0ea665bfcbb120f9064e102e
김정윤 <skyepodium@gmail.com>
2019-02-15 18:29:17 +0900

    goorm 0/1 knapsack

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,56 +1,35 @@
 #include <iostream>
 
-#define max_int 501
+#define max_int 101
+#define max_val 10001
 using namespace std;
 
-int n, m, x1, y1, x2, y2;
-int a[max_int][max_int];
+int n, m;
+int w[max_int];
+int p[max_int];
+int d[max_int][max_val];
 
 int main(){
     scanf("%d %d", &n, &m);
 
     for(int i=1; i<=n; i++){
-        for(int j=1; j<=n; j++){
-            scanf("%d", &a[i][j]);
+        scanf("%d %d", &w[i], &p[i]);
+    }
+
+    d[1][m] = 0;
+    if(m-w[1] >= 0) d[1][m-w[1]] = p[1];
+
+    for(int i=2; i<=n; i++){
+        for(int j=0; j<=m; j++){
+            d[i][j] = d[i-1][j];
+
+            if(j+w[i] <= m) d[i][j] = max(d[i][j], d[i-1][j+w[i]] + p[i]);
         }
     }
 
-    while(m--){
-        scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
-
-
-
-        int r1 = 0;
-        int r2 = 0;
-        int r3 = 0;
-        int r4 = 0;
-
-        for(int i=1; i<=x1 - 1; i++){
-            for(int j=1; j<=y1 - 1; j++){
-                r1 += a[i][j];
-            }
-        }
-
-        for(int i=1; i<=x1 - 1; i++){
-            for(int j=1; j<=y2; j++){
-                r2 += a[i][j];
-            }
-        }
-
-        for(int i=1; i<=x2; i++){
-            for(int j=1; j<=y1 - 1; j++){
-                r3 += a[i][j];
-            }
-        }
-
-        for(int i=1; i<=x2; i++){
-            for(int j=1; j<=y2; j++){
-                r4 += a[i][j];
-            }
-        }
-
-        printf("%d\n", r4 - r2 - r3 + r1);
+    int result = 0;
+    for(int j=0; j<=m; j++){
+        result = max(result, d[n][j]);
     }
-
-
+    printf("%d\n", result);
 }
