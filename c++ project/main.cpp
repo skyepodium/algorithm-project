be1fa120cda386ef7f5b8217077ff5f90b8caa24
김정윤 <skyepodium@gmail.com>
2019-02-22 19:44:29 +0900

    백준 9466 텀 프로젝트 by 위상정렬

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,93 +1,61 @@
 #include <iostream>
+#include <vector>
 #include <queue>
 
-#define max_int 51
+#define max_int 100001
+
 using namespace std;
 
-int n, m, start_i, start_j, end_i, end_j;
-char a[max_int][max_int];
-int water_day[max_int][max_int];
-int check[max_int][max_int];
-int dx[] = {0, 0, 1, -1};
-int dy[] = {-1, 1, 0, 0};
+int t, n, num, result;
+int ind[max_int];
+vector<int> v[max_int];
 
-queue<pair<int, int>> water;
-
-void water_bfs() {
-    while(!water.empty()){
-        int x = water.front().first;
-        int y = water.front().second;
-        water.pop();
-
-        for(int i=0; i<4; i++){
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            if(nx>=0 && nx<n && ny>=0 && ny<m){
-                if(water_day[nx][ny] == 0 && a[nx][ny] == '.'){
-                    water_day[nx][ny] = water_day[x][y] + 1;
-                    water.push(make_pair(nx, ny));
-                }
-            }
-        }
-    }
-}
-
-void bfs() {
-    queue<pair<int, int>> q;
-    q.push(make_pair(start_i, start_j));
-    while(!q.empty()){
-        int x = q.front().first;
-        int y = q.front().second;
-        q.pop();
-
-        for(int i=0; i<4; i++){
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            if(nx>=0 && nx<n && ny>=0 && ny<m){
-                if(check[nx][ny] == 0 && (a[nx][ny] == '.' || a[nx][ny] == 'D')){
-                    if(water_day[nx][ny] == 0){
-                        check[nx][ny] = check[x][y] + 1;
-                        q.push(make_pair(nx, ny));
-                    }else{
-                        if(water_day[nx][ny] > check[x][y] + 1){
-                            check[nx][ny] = check[x][y] + 1;
-                            q.push(make_pair(nx, ny));
-                        }
-                    }
-                }
-            }
-        }
+void init(){
+    result = 0;
+    for(int i=0; i<=n; i++){
+        ind[i] = 0;
+        v[i].clear();
     }
 }
 
 int main(){
-    scanf("%d %d", &n, &m);
+    scanf("%d", &t);
 
-    for(int i=0; i<n; i++){
-        scanf("%s", a[i]);
-        for(int j=0; j<m; j++){
-            if(a[i][j] == 'S'){
-                start_i = i;
-                start_j = j;
-            }
-            else if(a[i][j] == 'D'){
-                end_i = i;
-                end_j = j;
-            }
-            else if(a[i][j] == '*'){
-                water.push(make_pair(i, j));
+    for(int test_case=1; test_case <= t; test_case++){
+        scanf("%d", &n);
+
+        init();
+
+        for(int i=1; i<=n; i++){
+            scanf("%d", &num);
+            v[i].push_back(num);
+            ind[num]++;
+        }
+
+        queue<int> q;
+        for(int i=1; i<=n; i++){
+            if(ind[i] == 0){
+                q.push(i);
+                result++;
             }
         }
-    }
 
-    water_bfs();
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
 
-    bfs();
+            for(int i=0; i<v[node].size(); i++){
+                int next = v[node][i];
 
-    if(check[end_i][end_j] != 0){
-        printf("%d\n", check[end_i][end_j]);
-    }
-    else{
-        printf("KAKTUS\n");
+                ind[next]--;
+
+                if(ind[next] == 0) {
+                    q.push(next);
+                    result++;
+                }
+            }
+        }
+
+        printf("%d\n", result);
     }
 }
