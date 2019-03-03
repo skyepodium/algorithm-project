d09573f9f349db1a7c1a3930305190417eec43f6
김정윤 <skyepodium@gmail.com>
2019-03-03 17:21:40 +0900

    baekjoon 2563 by for loop

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,37 +1,26 @@
 #include <iostream>
 
-#define max_int 100001
+#define max_int 101
 using namespace std;
 
-int t, n, m, num, start_index, end_index;
-int a[max_int];
-int b[max_int];
-int c[max_int];
+int n, x, y;
+bool check[max_int][max_int];
+int result = 0;
 
 int main(){
-    scanf("%d", &t);
+    scanf("%d", &n);
 
-    for(int test_case=1; test_case<=t; test_case++){
-        scanf("%d %d", &n, &m);
-
-        for(int i=1; i<=n; i++){
-            scanf("%d", &num);
-            a[i] = a[i-1];
-            b[i] = b[i-1];
-            c[i] = c[i-1];
-            if(num == 1) a[i]++;
-            else if(num == 2) b[i]++;
-            else c[i]++;
-        }
-
-        printf("#%d\n", test_case);
-        for(int i=0; i<m; i++){
-            scanf("%d %d", &start_index, &end_index);
-            printf("%d ", a[end_index] - a[start_index-1]);
-            printf("%d ", b[end_index] - b[start_index-1]);
-            printf("%d\n", c[end_index] - c[start_index-1]);
+    for(int k=0; k<n; k++){
+        scanf("%d %d", &x, &y);
+        for(int i=x; i<x+10; i++){
+            for(int j=y; j<y+10; j++){
+                if(!check[i][j]){
+                    check[i][j] = true;
+                    result++;
+                }
+            }
         }
     }
-
+    printf("%d\n", result);
 
 }
