8efdb6350a90ad675e6e899d6b9874c0ec8df686
김정윤 <skyepodium@gmail.com>
2019-03-12 21:57:05 +0900

    test i can do it

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,78 +1,278 @@
 #include <iostream>
-#include <queue>
+#include <vector>
+#include <string>
 #include <cstring>
 
-#define max_val 2147483647
-#define max_k 31
-#define max_int 201
 using namespace std;
 
-int k, n, m;
-int a[max_int][max_int];
-int check[max_int][max_int][max_k];
-int dx[] = {0, 0, 1, -1, 1, 2, 2, 1, -1, -2, -1, -2};
-int dy[] = {-1, 1, 0, 0, 2, 1, -1, -2, 2, 1, -2, -1};
-
-struct info{
-    int x;
-    int y;
-    int remain;
-};
-
-void bfs(){
-    queue<info> q;
-    q.push({0, 0, k});
-    check[0][0][k] = 0;
-    while(!q.empty()){
-        int x = q.front().x;
-        int y = q.front().y;
-        int remain = q.front().remain;
-        q.pop();
-
-        for(int i=0; i<12; i++){
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-
-            if(i < 4){
-                if(nx >=0 && nx<n && ny>=0 && ny<m && a[nx][ny] == 0){
-                    if(check[nx][ny][remain] == -1){
-                        check[nx][ny][remain] = check[x][y][remain] + 1;
-                        q.push({nx, ny, remain});
-                    }
-                }
-            }
-            else{
-                if(remain > 0 && nx>=0 && nx<n && ny>=0 && ny<m && a[nx][ny] == 0){
-                    if(check[nx][ny][remain-1] == -1){
-                        check[nx][ny][remain-1] = check[x][y][remain] + 1;
-                        q.push({nx, ny, remain-1});
-                    }
-                }
-            }
-
+int t, n, q;
+char base[8][8];
+vector<int> result;
+vector<vector<string>> question;
+vector<vector<string>> v;
+void rotate(int idx){
+    char temp[8][8];
+    for(int i=0; i<8; i++){
+        for(int j=0; j<8; j++){
+            temp[j][7-i] = v[idx][i][j];
         }
-
+    }
+    for(int i=0; i<8; i++){
+        for(int j=0; j<8; j++){
+            v[idx][i][j] = temp[i][j];
+        }
     }
 }
 
+
+bool do_test(int q_idx, int f_idx){
+    memset(base, '.', sizeof(base));
+
+    for(int i=0; i<8; i++){
+        for(int j=0; j<8; j++) {
+            if (v[f_idx][i][j] == '.' && base[i][j] != '.') continue;
+            base[i][j] = v[f_idx][i][j];
+        }
+    }
+
+    bool flag = true;
+    for(int i=0; i<8; i++){
+        for(int j=0; j<8; j++){
+            if(base[i][j] == '.') base[i][j] ='W';
+            if(question[q_idx][i][j] != base[i][j]){
+                flag = false;
+                break;
+            }
+        }
+    }
+
+    return flag;
+}
+
+bool do_test2(int q_idx, int f_idx, int s_idx){
+    memset(base, '.', sizeof(base));
+
+    for(int i=0; i<8; i++){
+        for(int j=0; j<8; j++) {
+            if (v[f_idx][i][j] == '.' && base[i][j] != '.') continue;
+            base[i][j] = v[f_idx][i][j];
+        }
+    }
+
+    for(int i=0; i<8; i++){
+        for(int j=0; j<8; j++){
+            if (v[s_idx][i][j] == '.' && base[i][j] != '.') continue;
+            base[i][j] = v[s_idx][i][j];
+        }
+    }
+
+    bool flag = true;
+    for(int i=0; i<8; i++){
+        for(int j=0; j<8; j++){
+            if(base[i][j] == '.') base[i][j] ='W';
+            if(question[q_idx][i][j] != base[i][j]){
+                flag = false;
+                break;
+            }
+        }
+    }
+
+    return flag;
+}
+
+bool do_test3(int q_idx, int f_idx, int s_idx, int t_idx){
+    memset(base, '.', sizeof(base));
+
+    for(int i=0; i<8; i++){
+        for(int j=0; j<8; j++) {
+            if (v[f_idx][i][j] == '.' && base[i][j] != '.') continue;
+            base[i][j] = v[f_idx][i][j];
+        }
+    }
+
+    for(int i=0; i<8; i++){
+        for(int j=0; j<8; j++){
+            if (v[s_idx][i][j] == '.' && base[i][j] != '.') continue;
+            base[i][j] = v[s_idx][i][j];
+        }
+    }
+
+    for(int i=0; i<8; i++){
+        for(int j=0; j<8; j++){
+            if (v[t_idx][i][j] == '.' && base[i][j] != '.') continue;
+            base[i][j] = v[t_idx][i][j];
+        }
+    }
+
+
+
+    bool flag = true;
+    for(int i=0; i<8; i++){
+        for(int j=0; j<8; j++){
+            if(base[i][j] == '.') base[i][j] ='W';
+            if(question[q_idx][i][j] != base[i][j]){
+                flag = false;
+                break;
+            }
+        }
+    }
+
+//    if(flag){
+//        for(int i=0; i<8; i++){
+//            for(int j=0; j<8; j++){
+//                printf("%c", base[i][j]);
+//            }
+//            cout << endl;
+//        }
+//        cout << endl;
+//    }
+
+    return flag;
+}
+
 int main(){
-    scanf("%d %d %d", &k, &m, &n);
-    for(int i=0; i<n; i++){
-        for(int j=0; j<m; j++){
-            scanf("%d", &a[i][j]);
+    scanf("%d", &t);
+    for(int test_case=1; test_case<=t; test_case++){
+        // 1. 도형판 입력
+        scanf("%d", &n);
+        for(int idx=0; idx<n; idx++){
+            vector<string> tmp;
+            for(int i=0; i<8; i++){
+                string word;
+                cin >> word;
+                tmp.push_back(word);
+            }
+            v.push_back(tmp);
         }
+
+        // 2. 문제 입력
+        scanf("%d", &q);
+        for(int idx = 0; idx<q; idx++){
+            vector<string> tmp;
+            for(int i=0; i<8; i++){
+                string word;
+                cin >> word;
+                tmp.push_back(word);
+            }
+            question.push_back(tmp);
+        }
+
+
+        // 3. 문제 하나씩 검토
+        for(int idx=0; idx<question.size(); idx++){
+            bool find = false;
+
+            for(int i=0; i<n; i++){
+                for(int a=0; a<4; a++){
+                    rotate(i);
+                    if(do_test(idx, i)){
+                        if(!find){
+                            find = true;
+                            result.push_back(1);
+                            result.push_back(i+1);
+                        }
+                    }
+                }
+            }
+
+            // 3) 2개 사용 시작
+            if(find) continue;
+            for(int i=0; i<n; i++){
+                for(int j=0; j<n; j++){
+
+                        for(int a=0; a<4; a++){
+                            rotate(i);
+                            for(int b=0; b<4; b++){
+                                rotate(j);
+                                    if(do_test2(idx, i, j)){
+                                        if(!find){
+                                            find = true;
+                                            result.push_back(2);
+                                            result.push_back(i+1);
+                                            result.push_back(j+1);
+                                        }
+                                    }
+                            }
+                        }
+                }
+            }
+            // 3) 2개 사용 끝
+
+            // 3) 3개 사용 시작
+            if(find) continue;
+            for(int i=0; i<n; i++){
+                for(int j=0; j<n; j++){
+                    for(int k=0; k<n; k++){
+
+                        for(int a=0; a<4; a++){
+                            rotate(i);
+                            for(int b=0; b<4; b++){
+                                rotate(j);
+                                for(int c=0; c<4; c++){
+                                    rotate(k);
+                                    if(do_test3(idx, i, j, k)){
+                                        if(!find){
+                                            find = true;
+                                            result.push_back(3);
+                                            result.push_back(i+1);
+                                            result.push_back(j+1);
+                                            result.push_back(k+1);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            // 3) 3개 사용 끝
+
+            // 3) 4개 사용 시작
+            if(find) continue;
+            for(int i=0; i<n; i++){
+                for(int j=0; j<n; j++){
+                    for(int k=0; k<n; k++){
+
+                        for(int a=0; a<4; a++){
+                            rotate(i);
+                            for(int b=0; b<4; b++){
+                                rotate(j);
+                                for(int c=0; c<4; c++){
+                                    rotate(k);
+                                    if(do_test3(idx, i, j, k)){
+                                        if(!find){
+                                            find = true;
+                                            result.push_back(3);
+                                            result.push_back(i+1);
+                                            result.push_back(j+1);
+                                            result.push_back(k+1);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            // 3) 4개 사용 끝
+        }
+    printf("#%d ", test_case);
+    for(int i=0; i<result.size(); i++){
+        printf("%d ", result[i]);
+    }
+    printf("\n");
+
+//        cout << v.size() << endl;
+//        for(int idx=0; idx<v.size(); idx++){
+//            for(int i=0; i<8; i++){
+//                for(int j=0; j<8; j++){
+//                    printf("%c", v[idx][i][j]);
+//                }
+//                cout << endl;
+//            }
+//            cout << endl;
+//        }
     }
 
-    memset(check, -1, sizeof(check));
 
-    bfs();
-
-    int result = max_val;
-    for(int i=0; i<=k; i++){
-        if(check[n-1][m-1][i] != -1){
-            result = min(result, check[n-1][m-1][i]);
-        }
-    }
-    if(result == max_val) printf("%d\n", -1);
-    else printf("%d\n", result);
 }
