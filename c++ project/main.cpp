faa429a6f63561caf80aa8f33e599568df8169ad
skyepodium <skyepodium@gmail.com>
2019-04-20 17:54:00 +0900

    baekjoon 17142 by DFS BFS

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,50 +1,108 @@
 #include <iostream>
-#include <cstring>
-#define max_int 12
+#include <queue>
+#include <vector>
+#include <algorithm>
+
+#define max_int 51
+#define max_val 2147483647
 using namespace std;
 
-int t;
-char a[max_int], b[max_int];
-bool result;
+int n, m, a[max_int][max_int], check[max_int][max_int];
 
-int main(){
-    scanf("%d", &t);
+struct info{
+    int x;
+    int y;
+};
+
+vector<info> virus;
+vector<info> pick;
+queue<info> q;
+int result = max_val;
+int dx[] = {0, 0, 1, -1};
+int dy[] = {-1, 1, 0, 0};
+void bfs() {
     
-    for(int test_case=1; test_case<=t; test_case++){
-        scanf("%s", a);
-        scanf("%s", b);
-
-        result = true;
+    while(!q.empty()){
+        int x = q.front().x;
+        int y = q.front().y;
+        q.pop();
         
-        if(strlen(a) == strlen(b)){
-            for(int i=0; i<strlen(a); i++){
-                if(a[i] == 'B'){
-                    if(b[i] != 'B'){
-                        result = false;
-                        break;
-                    }
-                }else if(a[i] == 'A' || a[i] == 'D' || a[i] == 'O' || a[i] == 'P' || a[i] == 'Q' || a[i] == 'R'){
-                    if(b[i] != 'A' && b[i] != 'D' && b[i] != 'O' && b[i] != 'P' && b[i] != 'Q' && b[i] != 'R'){
-                        result = false;
-                        break;
-                    }
-                }else{
-                    if(b[i] == 'B' || b[i] == 'A' || b[i] == 'D' || b[i] == 'O' || b[i] == 'P' || b[i] == 'Q' || b[i] == 'R'){
-                        result = false;
-                        break;
-                    }
+        for(int i=0; i<4; i++){
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            
+            if(nx>=0 && nx<n && ny>=0 && ny<n){
+                if(a[nx][ny] != 1 && check[nx][ny] == -1){
+                    check[nx][ny] = check[x][y] + 1;
+                    q.push({nx, ny});
                 }
             }
         }
-        else{
-            result = false;
-        }
-        
-        if(result){
-            printf("#%d SAME\n", test_case);
-        }
-        else{
-            printf("#%d DIFF\n", test_case);
+    }
+    
+    bool flag = true;
+    int max_time = 0;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            if(a[i][j] == 0){
+                if(check[i][j] == -1){
+                    flag = false;
+                    break;
+                }else{
+                    max_time = max(max_time, check[i][j]);
+                }
+            }
+
         }
     }
+    if(flag) result = min(result, max_time);
+}
+
+void go(int idx){
+    if(idx == virus.size()){
+        
+        if(pick.size() == m){
+            
+            for(int i=0; i<m; i++){
+                q.push({pick[i].x, pick[i].y});
+            }
+            for(int i=0; i<n; i++){
+                for(int j=0; j<n; j++){
+                    check[i][j] = -1;
+                }
+            }
+            
+            for(int i=0; i<pick.size(); i++){
+                int x = pick[i].x;
+                int y = pick[i].y;
+                check[x][y] = 0;
+            }
+            bfs();
+
+        }
+        return;
+    }
+    
+    
+    pick.push_back({virus[idx].x, virus[idx].y});
+    go(idx+1);
+    pick.pop_back();
+    
+    go(idx+1);
+}
+
+int main(){
+    scanf("%d %d", &n, &m);
+    
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            scanf("%d", &a[i][j]);
+            if(a[i][j] == 2) virus.push_back({i, j});
+        }
+    }
+    go(0);
+    
+    if(result == max_val) result = -1;
+    printf("%d\n", result);
+    
 }
