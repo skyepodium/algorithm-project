eac2d79c28fa0b7743584f7e7c804c71ef579e50
skyepodium <skyepodium@gmail.com>
2019-04-25 09:21:06 +0900

    solving baekjoon 12100...

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,129 +1,81 @@
 #include <iostream>
 #include <queue>
 #include <vector>
+#include <cmath>
 #include <algorithm>
 
-#define max_int 51
-#define max_val 2147483647
+#define max_int 1100000
 using namespace std;
 
-//시간 복잡도: O(2^m*n^2)
-//공간 복잡도: O(n^2)
-//사용한 알고리즘: 백트래킹, BFS
-//사용한 자료구조: 큐
-
-int n, m, a[max_int][max_int], check[max_int][max_int], result = max_val;
+bool check[max_int];
+int n, num, max_size, result;
+int dx[] = {0, 1, 0, -1};
+int dy[] = {1, 0, -1, 0};
 
 struct info{
-    int x;
-    int y;
+    int cnt;
+    int num;
+    vector<vector<int>> v;
 };
 
-vector<info> virus, pick;
 queue<info> q;
-int dx[] = {0, 0, 1, -1};
-int dy[] = {-1, 1, 0, 0};
 
 void bfs() {
-           
     while(!q.empty()){
-        int x = q.front().x;
-        int y = q.front().y;
+        info cur = q.front();
         q.pop();
         
-        for(int i=0; i<4; i++){
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            
-            if(nx>=0 && nx<n && ny>=0 && ny<n){
-                // 1) 벽은 지나갈 수 없습니다.
-                // 2) 비활성화 된 바이러스는 활성화 바이러스를 만나면 활성화 됩니다.
-                if(a[nx][ny] != 1 && check[nx][ny] == -1){
-                    check[nx][ny] = check[x][y] + 1;
-                    q.push({nx, ny});
-                }
-            }
-        }
-    }
-    
-    bool isClear = true;
-    int max_time = 0;
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            if(a[i][j] == 0){
-                // 만약 빈칸인데 바이러스가 퍼지지 않았으면 실패입니다.
-                if(check[i][j] == -1){
-                    isClear = false;
-                    break;
-                }
-                // 바이러스가 퍼졌으면 최대 시간을 갱신해줍니다.
-                else{
-                    max_time = max(max_time, check[i][j]);
-                }
-            }
-
-        }
-    }
-    // 최소시간을 갱신해줍니다.
-    if(isClear) result = min(result, max_time);
-}
-
-// 재귀를 통해 m개의 활성화 바이러스를 선택해줍니다.
-void go(int idx){
-    if(idx == virus.size()){
+        int c_cnt = cur.cnt;
+        int c_num = cur.num;
         
-        if(pick.size() == m){
+        for(int k=0; k<4; k++){
             
-            // 선택한 m개의 바이러스를 큐에 넣어줍니다.
-            for(int i=0; i<m; i++) q.push(pick[i]);
-            
-            // check 배열 초기화
-            for(int i=0; i<n; i++){
-                for(int j=0; j<n; j++){
-                    check[i][j] = -1;
+            if(k==0){
+                
+               for(int i=0; i<n; i++){
+                   
+                   int cur_num = 0;
+                   for(int j=n-1; j>=0; j++){
+                       if(cur.v[i][j]){
+                           
+                       }else{
+                           
+                       }
+                   }
                 }
+                
+                
             }
-            
-            // m개의 바이러스에 대해 시작을 0 으로 설정합니다.
-            for(int i=0; i<pick.size(); i++){
-                int x = pick[i].x;
-                int y = pick[i].y;
-                check[x][y] = 0;
-            }
-            
-            // bfs 탐색을 통해 지도 전체에 바이러스가 퍼지는 최소 시간을 계산해줍니다.
-            bfs();
-
         }
-        return;
     }
-    
-    // 1) idx번째 바이러스 선택
-    pick.push_back({virus[idx].x, virus[idx].y});
-    go(idx+1);
-    pick.pop_back();
-    
-    // 2) idx번째 바이러스를 선택하지 않고 지나감
-    go(idx+1);
 }
 
 int main(){
-    // 1. 문제를 입력받습니다.
-    scanf("%d %d", &n, &m);
     
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            scanf("%d", &a[i][j]);
+    scanf("%d", &n);
+    max_size = n*n-1;
+    
+    int temp_val = 0;
+    int temp_size = max_size;
+    vector<vector<int>> main;
+    for(int i=1; i<=n; i++){
+        vector<int> sub;
+        for(int j=1; j<=n; j++){
+            scanf("%d", &num);
+            sub.push_back(num);
             
-            // 2. 바이러스일 경우 x, y를 따로 저장해줍니다.
-            if(a[i][j] == 2) virus.push_back({i, j});
+            if(num != 0) temp_val += pow(2, temp_size);
+            temp_size--;
+            
+            result = max(result, num);
         }
+        main.push_back(sub);
     }
     
-    // 3. 전체 바이러스에 대해 활성화 시킬 m개만 선택해줍니다.
-    go(0);
+    q.push({0, temp_val, main});
     
-    // 4. 결과를 출력합니다.
-    if(result == max_val) result = -1;
+    bfs();
+    
     printf("%d\n", result);
 }
+
