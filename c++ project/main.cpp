c5fd7741be67e1ebc0bcf5259f47bfcefa90bd87
skyepodium <skyepodium@gmail.com>
2019-05-26 14:27:55 +0900

    baekjoon 17247 by BFS

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,39 +1,66 @@
 #include <iostream>
-#include <algorithm>
+#include <queue>
 #include <vector>
+#define max_int 1001
 using namespace std;
 
-int n, m;
-struct info{
-    int a;
-    int b;
-    char c;
-};
-vector<info> v;//
+int n, m, a[max_int][max_int], check[max_int][max_int];
+int dx[] = {0, 0, 1, -1};
+int dy[] = {-1, 1, 0, 0};
 
-bool cmp(const info &a, const info &b){
-    if(a.b == b.b){
-        return a.a < b.a;
-    }else{
-        return a.b < b.b;
+struct info{
+    int x;
+    int y;
+};
+
+vector<info> v;
+
+void bfs(){
+    
+    queue<info> q;
+    info start_node = v[0];
+    int start_x = start_node.x;
+    int start_y = start_node.y;
+    check[start_x][start_y] = 0;
+    q.push(start_node);
+    while(!q.empty()){
+        info cur = q.front();
+        int x = cur.x;
+        int y = cur.y;
+        q.pop();
+        
+        for(int i=0; i<4; i++){
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            
+            if(nx>=0 && nx<n && ny>=0 && ny<m){
+                if(check[nx][ny] == 0){
+                    check[nx][ny] = check[x][y] + 1;
+                    q.push({nx, ny});
+                }
+            }
+        }
     }
 }
+
 
 int main(){
     scanf("%d %d", &n, &m);
     
-    for(int i=0; i<m; i++){
-        int a, b;
-        char c;
-        cin >> a >> b >> c;
-        v.push_back({a, b, c});
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            scanf("%d", &a[i][j]);
+            if(a[i][j] == 1){
+                v.push_back({i, j});
+            }
+        }
     }
     
-    sort(v.begin(), v.end(), cmp);
+    bfs();
     
-    for(int i=0; i<m; i++){
-        cout << v[i].c;
-    }
-    cout << endl;
+    info end_node = v[1];
+    int end_x = end_node.x;
+    int end_y = end_node.y;
     
+    printf("%d\n", check[end_x][end_y]);
 }
