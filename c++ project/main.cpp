85f7cff1ede8a26019a6ddc545c8b3e5c49ec561
skyepodium <skyepodium@gmail.com>
2019-05-26 14:50:41 +0900

    baekjoon 17249 by for loop

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,66 +1,22 @@
 #include <iostream>
-#include <queue>
-#include <vector>
-#define max_int 1001
+#include <string>
 using namespace std;
 
-int n, m, a[max_int][max_int], check[max_int][max_int];
-int dx[] = {0, 0, 1, -1};
-int dy[] = {-1, 1, 0, 0};
-
-struct info{
-    int x;
-    int y;
-};
-
-vector<info> v;
-
-void bfs(){
-    
-    queue<info> q;
-    info start_node = v[0];
-    int start_x = start_node.x;
-    int start_y = start_node.y;
-    check[start_x][start_y] = 0;
-    q.push(start_node);
-    while(!q.empty()){
-        info cur = q.front();
-        int x = cur.x;
-        int y = cur.y;
-        q.pop();
-        
-        for(int i=0; i<4; i++){
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            
-            if(nx>=0 && nx<n && ny>=0 && ny<m){
-                if(check[nx][ny] == 0){
-                    check[nx][ny] = check[x][y] + 1;
-                    q.push({nx, ny});
-                }
-            }
-        }
-    }
-}//
-
-
 int main(){
-    scanf("%d %d", &n, &m);
+    string word;
+    cin >> word;
     
-    for(int i=0; i<n; i++){
-        for(int j=0; j<m; j++){
-            scanf("%d", &a[i][j]);
-            if(a[i][j] == 1){
-                v.push_back({i, j});
-            }
+    int l_cnt = 0;
+    int r_cnt = 0;
+    bool face = false;
+    for(int i=0; i<(int)word.size(); i++){
+        if(word[i] == '(') face = true;
+        
+        if(word[i] == '@'){
+            if(!face) l_cnt++;
+            else r_cnt++;
         }
     }
     
-    bfs();
-    
-    info end_node = v[1];
-    int end_x = end_node.x;
-    int end_y = end_node.y;
-    
-    printf("%d\n", check[end_x][end_y]);
+    printf("%d %d\n", l_cnt, r_cnt);
 }
