480f618e667aa85519a45bab71114e6acc177ac0
skyepodium <skyepodium@gmail.com>
2019-06-08 20:22:12 +0900

    baekjoon 4641 by for loop

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,45 +1,30 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
-#define max_int 9
+#define max_int 101
 using namespace std;
 
-int a[max_int];
-vector<int> pick;
-bool flag = false;
-void go(int idx){
-    
-    if(idx == max_int){
-        if((int)pick.size() == 7){
-            int result = 0;
-            vector<int> temp;
-            for(int i=0; i<7; i++){
-                result += pick[i];
-                temp.push_back(pick[i]);
-            }
-            if(result == 100 && !flag){
-                flag = true;
-                sort(temp.begin(), temp.end());
-                for(int i=0; i<7; i++){
-                    printf("%d\n", temp[i]);
-                }
+int num, result;
+bool check[max_int];
+int main(){
+    while(true){
+        scanf("%d", &num);
+        if(num == -1) break;
+        for(int i=0; i<max_int; i++){
+            check[i] = false;
+        }
+        check[num] = true;
+        result = 0;
+        
+        while(true){
+            scanf("%d", &num);
+            if(num == 0) break;
+            check[num] = true;
+        }
+        for(int i=1; i<=50; i++){
+            if(check[i] && check[i*2]){
+//                cout << i << endl;
+                result++;
             }
         }
-        return;
+        printf("%d\n", result);
     }
-    
-    pick.push_back(a[idx]);
-    go(idx + 1);
-    pick.pop_back();
-    
-    go(idx + 1);
-}
-
-int main(){
-    for(int i=0; i<max_int; i++){
-        scanf("%d", &a[i]);
-    }
-    
-    go(0);
-    
 }
