1b4eed8f54476ab362c238fa927ae3e4bd4162e0
skyepodium <skyepodium@gmail.com>
2019-06-12 22:00:33 +0900

    goorm 08a by dfs, bfs

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,30 +1,63 @@
 #include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
 #define max_int 101
 using namespace std;
 
-int num, result;
+int n, m, a, b;
 bool check[max_int];
-int main(){
-    while(true){
-        scanf("%d", &num);
-        if(num == -1) break;
-        for(int i=0; i<max_int; i++){
-            check[i] = false;  
+vector<int> v[max_int];
+
+void dfs (int node) {
+    check[node] = true;
+    printf("%d", node);
+    for(int i=0; i<v[node].size(); i++){
+        int next = v[node][i];
+        if(!check[next]){
+            printf("-");
+            dfs(next);
         }
-        check[num] = true;
-        result = 0;
-        
-        while(true){
-            scanf("%d", &num);
-            if(num == 0) break;
-            check[num] = true;
+    }
+}
+
+void bfs (int start) {
+    queue<int> q;
+    q.push(start);
+    while(!q.empty()){
+        int node = q.front();
+        if(check[node]){
+            printf("-%d", node);
+        }else{
+            printf("%d", node);
+            check[node] = true;
         }
-        for(int i=1; i<=50; i++){
-            if(check[i] && check[i*2]){
-//                cout << i << endl;
-                result++;
+        q.pop();
+        for(int i=0; i<v[node].size(); i++){
+            int next = v[node][i];
+            if(!check[next]){
+                check[next] = true;
+                q.push(next);
             }
         }
-        printf("%d\n", result);
     }
+}
+int main(){
+    scanf("%d %d", &n, &m);
+    
+    for(int i=0; i<m; i++){
+        scanf("%d %d", &a, &b);
+        v[a].push_back(b);
+        v[b].push_back(a);
+    }
+    
+    for(int i=1; i<=n; i++){
+        sort(v[i].begin(), v[i].end());
+    }
+    
+    dfs(1);
+    printf("\n");
+    for(int i=0; i<=n; i++) check[i] = false;
+    bfs(1);
+    printf("\n");
 }
