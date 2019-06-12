70f2e28ff577d744f0fb0055d416b51f2ce7e28b
skyepodium <skyepodium@gmail.com>
2019-06-12 22:51:23 +0900

    baekjoon 1162 dijkstra + DP

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -2,62 +2,81 @@
 #include <vector>
 #include <queue>
 #include <algorithm>
-#define max_int 101
+
+#define max_int 10001
+#define max_val 10000000001
+#define lld long long int
 using namespace std;
 
-int n, m, a, b;
-bool check[max_int];
-vector<int> v[max_int];
+int n, m, k, a, b, c;
+lld result = max_val;
+struct info{
+    int cur;
+    lld cost;
+    int cnt;
+};
+vector<info> v[max_int];
+lld d[max_int][21];
 
-void dfs (int node) {
-    check[node] = true;
-    printf("%d", node);
-    for(int i=0; i<v[node].size(); i++){
-        int next = v[node][i];
-        if(!check[next]){
-            printf("-");
-            dfs(next);
+struct cmp{
+    bool operator()(const info &a, const info &b){
+        return a.cost > b.cost;
+    }
+};
+
+void init(){
+    for(int i=1; i<=n; i++){
+        for(int j=0; j<=k; j++){
+            d[i][j] = max_val;
         }
     }
 }
 
-void bfs (int start) {
-    queue<int> q;
-    q.push(start);
-    while(!q.empty()){
-        int node = q.front();
-        if(check[node]){
-            printf("-%d", node);
-        }else{
-            printf("%d", node);
-            check[node] = true;
-        }
-        q.pop();
-        for(int i=0; i<v[node].size(); i++){
-            int next = v[node][i];
-            if(!check[next]){
-                check[next] = true;
-                q.push(next);
+void dijkstra() {
+    priority_queue<info, vector<info>, cmp> pq;
+    d[1][k] = 0;
+    pq.push({1, d[1][k], k});
+    while(!pq.empty()){
+        info cur = pq.top();
+        pq.pop();
+        int c_node = cur.cur;
+        int c_cnt = cur.cnt;
+        for(int i=0; i < v[c_node].size(); i++){
+            info next = v[c_node][i];
+            int n_node = next.cur;
+            lld n_cost = next.cost;
+            
+            if(d[n_node][c_cnt] > d[c_node][c_cnt] + n_cost){
+                d[n_node][c_cnt] = d[c_node][c_cnt] + n_cost;
+                pq.push({n_node, d[n_node][c_cnt], c_cnt});
+            }
+
+            if(c_cnt > 0){
+                if(d[n_node][c_cnt-1] > d[c_node][c_cnt]){
+                    d[n_node][c_cnt-1] = d[c_node][c_cnt];
+                    pq.push({n_node, d[n_node][c_cnt-1], c_cnt-1});
+                }
             }
         }
     }
 }
+
 int main(){
-    scanf("%d %d", &n, &m);
+    scanf("%d %d %d", &n, &m, &k);
+    
+    init();
     
     for(int i=0; i<m; i++){
-        scanf("%d %d", &a, &b);
-        v[a].push_back(b);
-        v[b].push_back(a);
+        scanf("%d %d %d", &a, &b, &c);
+        v[a].push_back({b, c, 0});
+        v[b].push_back({a, c, 0});
     }
     
-    for(int i=1; i<=n; i++){
-        sort(v[i].begin(), v[i].end());
+    dijkstra();
+    
+    for(int j=0; j<=k; j++){
+        result = min(result, d[n][j]);
     }
     
-    dfs(1);
-    printf("\n");
-    for(int i=0; i<=n; i++) check[i] = false;
-    bfs(1);
-    printf("\n");
+    printf("%lld\n", result);
 }
