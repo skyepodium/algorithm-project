953193ac44b86b3f6dbc72cacf65aad14da0687c
skyepodium <skyepodium@gmail.com>
2019-06-23 17:58:35 +0900

    baekjoon 10804 by for loop

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -2,21 +2,28 @@
 
 using namespace std;
 
-int a[10], b[10], a_result, b_result;
+int a[21], s, e;
 
 int main(){
+    for(int i=1; i<=20; i++) a[i] = i;
+    
     for(int i=0; i<10; i++){
-        scanf("%d", &a[i]);
+        scanf("%d %d", &s, &e);
+        
+        int size = e - s + 1;
+        
+        for(int i=0; i<size/2; i++){
+            int l_idx = s + i;
+            int r_idx = s + (size - i - 1);
+            
+            int temp = a[l_idx];
+            a[l_idx] = a[r_idx];
+            a[r_idx] = temp;
+        }
     }
-    for(int i=0; i<10; i++){
-        scanf("%d", &b[i]);
+    
+    for(int i=1; i<=20; i++){
+        printf("%d ", a[i]);
     }
-    for(int i=0; i<10; i++){
-        if(a[i] > b[i]) a_result++;
-        else if(a[i] < b[i]) b_result++;
-    }
-
-    if(a_result > b_result) printf("A\n");
-    else if(a_result == b_result) printf("D\n");
-    else printf("B\n");
+    printf("\n");
 }
