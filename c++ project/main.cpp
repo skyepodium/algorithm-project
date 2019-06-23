f5162232c220d2af0aaf7c84f762ccb8092980c6
skyepodium <skyepodium@gmail.com>
2019-06-23 19:26:30 +0900

    baekjoon 11557 by max

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,26 +1,23 @@
 #include <iostream>
-#include <vector>
-#define max_int 1001
-
+#include <string>
 using namespace std;
 
-int a, b, cnt[1000001];
-vector<int> v;
-int main(){
-    for(int i=1; i<=max_int; i++){
-        for(int j=1; j<=i; j++){
-            v.push_back(i);
-        }
-    }
-    scanf("%d %d", &a, &b);
-    a--;
-    b--;
+int t, n, drink, max_drink;
+string name, max_name;
 
-    int cur = 0;
-    for(int i=0; i<v.size(); i++){
-        cur += v[i];
-        cnt[i] = cur;
-    }
+int main(){
+    scanf("%d", &t);
     
-    printf("%d\n", cnt[b] - cnt[a-1]);
+    for(int test_case=1; test_case<=t; test_case++){
+        scanf("%d", &n);
+        max_drink = 0;
+        for(int i=0; i<n; i++){
+            cin >> name >> drink;
+            if(drink > max_drink){
+                max_drink = drink;
+                max_name = name;
+            }
+        }
+        cout << max_name << "\n";
+    }
 }
