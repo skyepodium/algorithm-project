65e42a35818601843952f88986f31ba3141d6759
skyepodium <skyepodium@gmail.com>
2019-06-23 19:13:36 +0900

    baekjoon 4153 by if else

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -2,20 +2,32 @@
 #define max_int 1001
 using namespace std;
 
-int n, m, a, b;
-int result[max_int];
+int a, b, c;
+
+int max(int a, int b){
+    return a < b ? b : a;
+}
+
+int max3(int a, int b, int c){
+    return max(max(a, b), c);
+}
 
 int main(){
-    scanf("%d %d", &n, &m);
-    
-    for(int i=0; i<m; i++){
-        scanf("%d %d", &a, &b);
-        result[a]++;
-        result[b]++;
+    while(true){
+        scanf("%d %d %d", &a, &b, &c);
+        if(!a && !b && !c) break;
+        
+        a *= a;
+        b *= b;
+        c *= c;
+        
+        if(a > c) swap(a, c);
+        if(b > c) swap(b, c);
+
+        if(c == a+b){
+            printf("right\n");
+        }else{
+            printf("wrong\n");
+        }
     }
-    
-    for(int i=1; i<=n; i++){
-        printf("%d\n", result[i]);
-    }
-    
 }
