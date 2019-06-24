803a7f25ec09724ae12a96d5ce2390b1b257053a
skyepodium <skyepodium@gmail.com>
2019-06-24 22:32:33 +0900

    baekjoon 1159 by string

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,48 +1,44 @@
 #include <iostream>
 #include <string>
-#include <cstring>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 int n, a[26];
-//string word;
+string name, result;
+vector<string> v;
+
+
+bool cmp(const string &a, const string &b){
+    return a[0] < b[0];
+}
 
 int main(){
-    
     scanf("%d", &n);
     
-    cin.ignore();
-    
-    for(int test_case=1; test_case<=n; test_case++){
-        memset(a, 0, sizeof(a));
-        char word[1000];
-        cin.getline(word,1000,'\n');
-        for(int i=0; i<strlen(word); i++){
-            
-            int cur = word[i];
-            
-            // 소문자
-            if(cur >= 97 && cur <=122){
-                a[cur-97]++;
-            }
-            // 대문자
-            else if(cur >= 65 && cur <=90){
-                a[cur-65]++;
-            }
-        }
-        
-        bool p = true;
-        bool d = true;
-        bool t = true;
-        
-        for(int i=0; i<26; i++){
-            if(a[i] < 3) t = false;
-            if(a[i] < 2) d = false;
-            if(a[i] < 1) p = false;
-        }
-        
-        if(t) printf("Case %d: Triple pangram!!!\n", test_case);
-        else if(d) printf("Case %d: Double pangram!!\n", test_case);
-        else if(p) printf("Case %d: Pangram!\n", test_case);
-        else printf("Case %d: Not a pangram\n", test_case);
+    for(int i=0; i<n; i++){
+        cin >> name;
+        v.push_back(name);
     }
+    
+    sort(v.begin(), v.end(), cmp);
+    
+    int cur = v[0][0];
+    int cnt = 1;
+    bool flag = false;
+    for(int i=1; i<(int)v.size(); i++){
+        if(cur != v[i][0]){
+            if(cnt >= 5){
+                result += cur;
+            }
+            cur = v[i][0];
+            cnt = 1;
+        }else{
+            if(i == v.size() - 1) flag = true;
+            cnt++;
+        }
+    }
+    if(flag && cnt >= 5)  result += cur;
+    if(result.size() > 0) cout << result << endl;
+    else cout << "PREDAJA" << endl;
 }
