ced8aac7da4d158899f5ddbbb9cd743db98f2c96
skyepodium <skyepodium@gmail.com>
2019-06-27 22:11:18 +0900

    baekjoon 11049 by DP top-down

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,48 +1,47 @@
 #include <iostream>
-#define max_int 101
+#include <algorithm>
+#define max_int 501
+#define max_val 2147483647
 using namespace std;
 
-int n, m, a[max_int][max_int], result = 0;
-bool check[max_int][max_int];
-int dx[] = {0, 0, 1, -1};
-int dy[] = {-1, 1, 0, 0};
+int n, d[max_int][max_int];
+struct info{
+    int a;
+    int b;
+};
 
-void dfs(int x, int y){
-    check[x][y] = true;
-    for(int i=0; i<4; i++){
-        int nx = x + dx[i];
-        int ny = y + dy[i];
-        if(nx>=0 && nx<n && ny >= 0 && ny<m){
-            if(!check[nx][ny]){
-                dfs(nx, ny);//
-            }
+info a[max_int];
+
+int go(int start, int end){
+    
+    if(d[start][end] != max_val) return d[start][end];
+    if(end == start+1) return d[start][end] = a[start].a*a[start].b*a[end].b;
+    
+    for(int i=start; i<end; i++){
+        d[start][end] = min(d[start][end], go(start, i) + go(i+1, end) + a[start].a*a[i].b*a[end].b);
+    }
+    
+    return d[start][end];
+}
+
+void init(){
+    for(int i=0; i<=n; i++){
+        for(int j=0; j<=n; j++){
+            if(i==j) d[i][j] = 0;
+            else d[i][j] = max_val;
         }
     }
 }
 
 int main(){
-    scanf("%d %d", &n, &m);
+    scanf("%d", &n);
     
-    for(int i=0; i<n; i++){
-        for(int j=0; j<m; j++){
-            scanf("%d", &a[i][j]);
-        }
+    init();
+    
+    for(int i=1; i<=n; i++){
+        scanf("%d %d", &a[i].a, &a[i].b);
     }
     
-    
-    while(true){
-        int flag = 0;
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(a[i][j] == 1){
-                    dfs(i, j);
-                    flag++;
-                }
-            }
-        }
-        if(flag == 0) break;
-        result++;
-    }
-    printf("%d\n", result);
+    printf("%d\n", go(1, n));
     
 }
