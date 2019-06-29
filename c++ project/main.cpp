a583b74c4c3a1beb5d33f7de682783e8027d071b
skyepodium <skyepodium@gmail.com>
2019-06-29 12:59:20 +0900

    baekjoon lcs

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,23 +1,31 @@
 #include <iostream>
+#include <cstring>
 #include <algorithm>
-#define lld long long int
-#define max_int 1000001
+#define max_int 1001
+
 using namespace std;
 
-int t, n, a[max_int];
+int n, m, d[max_int][max_int];
+char a[max_int], b[max_int];
 
 int main(){
-    scanf("%d", &t);
+    scanf("%s", a);
+    n = (int)strlen(a);
+    scanf("%s", b);
+    m = (int)strlen(b);
     
-    for(int test_case=1; test_case<=t; test_case++){
-        scanf("%d", &n);
-        
-        for(int i=1; i<=n; i++){
-            scanf("%d", &a[i]);
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=m; j++){
+            if(a[i-1] == b[j-1]){
+                d[i][j] = d[i-1][j-1] + 1;
+            }
+            else{
+                d[i][j] = max(d[i-1][j], d[i][j-1]);
+            }
         }
-        
-        sort(a + 1, a + 1 + n);
-        
-        printf("#%d %lld\n", test_case, (lld)(a[1] * a[n]));
     }
+    
+    printf("%d\n", d[n][m]);
 }
+
+
