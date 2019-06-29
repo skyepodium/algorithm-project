cc7ba843607a334f9ae23b4e68b8a9f35c1538ad
skyepodium <skyepodium@gmail.com>
2019-06-29 12:38:08 +0900

    swea 7829 by sort

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,47 +1,23 @@
 #include <iostream>
 #include <algorithm>
-#define max_int 501
-#define max_val 2147483647
+#define lld long long int
+#define max_int 1000001
 using namespace std;
 
-int n, d[max_int][max_int];
-struct info{
-    int a;
-    int b;
-};
-
-info a[max_int];
-
-int go(int start, int end){
-    
-    if(d[start][end] != max_val) return d[start][end];
-    if(end == start+1) return d[start][end] = a[start].a*a[start].b*a[end].b;
-    
-    for(int i=start; i<end; i++){
-        d[start][end] = min(d[start][end], go(start, i) + go(i+1, end) + a[start].a*a[i].b*a[end].b);
-    }
-    
-    return d[start][end];
-}
-
-void init(){
-    for(int i=0; i<=n; i++){
-        for(int j=0; j<=n; j++){
-            if(i==j) d[i][j] = 0;
-            else d[i][j] = max_val;
-        }
-    }
-}
+int t, n, a[max_int];
 
 int main(){
-    scanf("%d", &n);
+    scanf("%d", &t);
     
-    init();
-    
-    for(int i=1; i<=n; i++){
-        scanf("%d %d", &a[i].a, &a[i].b);
+    for(int test_case=1; test_case<=t; test_case++){
+        scanf("%d", &n);
+        
+        for(int i=1; i<=n; i++){
+            scanf("%d", &a[i]);
+        }
+        
+        sort(a + 1, a + 1 + n);
+        
+        printf("#%d %lld\n", test_case, (lld)(a[1] * a[n]));
     }
-    
-    printf("%d\n", go(1, n));
-    
 }
