2b09d95d366582776924d03962c75226aeb563cc
skyepodium <skyepodium@gmail.com>
2019-06-29 21:04:47 +0900

    baekjoon 1102 by DP, bit masking

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,27 +1,81 @@
 #include <iostream>
-#define max_int 41
-#define lld long long int
+#define max_int 16
+#define max_val 801
+
 using namespace std;
 
-int n, m, num, a[max_int];
-lld result = 1;
+/*
+ 시간 복잡도: O(2^n * n^2) - 1600만
+ 공간 복잡도: O(2^n)
+ 사용한 알고리즘: 비트 마스킹, DP - bottom up
+ 사용한 자료구조: 비트셋
+ */
+
+int n, p, a[max_int][max_int], first_status, result = max_val;
+int check[1 << max_int];
+char c[max_int];
+
+int min(int a, int b){
+    return a < b ? a : b;
+}
 
 int main(){
-    a[0] = 1;
-    a[1] = 1;
-    a[2] = 2;
-    for(int i=3; i<max_int; i++){
-        a[i] = a[i-1] + a[i-2];
+    // 0. 입력
+    scanf("%d", &n);
+    
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            scanf("%d", &a[i][j]);
+        }
     }
     
-    scanf("%d %d", &n, &m);
+    scanf("%s", c);
     
-    int cur = 1;
-    for(int i=0; i<m; i++){
-        scanf("%d", &num);
-        result *= a[num - cur];
-        cur = num + 1;
+    for(int i=0; i<n; i++){
+        if(c[i] == 'Y'){
+            first_status |= (1<<i);
+        }
     }
-    result *= a[n-cur+1];
-    printf("%lld\n", result);
+    scanf("%d", &p);
+    
+    // 1. 초기화
+    int num = (1 << n);
+    for(int i=0; i<num; i++){
+        check[i] = max_val;
+    }
+    check[first_status] = 0;
+    
+    // 2. DP 수행
+    for(int k=0; k<num; k++){
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(i == j) continue;
+                
+                if((k & (1 << i))){
+                    if(k & (1<<j)) continue;
+                    int next = k | (1 << j);
+                    check[next] = min(check[next], check[k] + a[i][j]);
+                }
+            }
+        }
+    }
+    
+    // 3. 최소값 갱신
+    for(int k=0; k<num; k++){
+        int cnt = 0;
+        for(int q=0; q<n; q++){
+            if(k & (1 << q)){
+                cnt++;
+            }
+        }
+        if(cnt >= p){
+            result = min(result, check[k]);
+        }
+    }
+    
+    // 4. 예외처리
+    if(result == max_val) result = -1;
+    
+    // 5. 출력
+    printf("%d\n", result);
 }
