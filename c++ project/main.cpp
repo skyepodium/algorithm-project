6f9ebf75d38f677aa4be4c8dc0dd5f162431c8e0
skyepodium <skyepodium@gmail.com>
2019-06-29 13:16:09 +0900

    baekjoon 9592 by DP

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <cstring>
 #include <algorithm>
+#include <vector>
 #define max_int 1001
 
 using namespace std;
 
-int n, m, d[max_int][max_int];
+int n, m, d[max_int][max_int], p[max_int][max_int];
 char a[max_int], b[max_int];
 
 int main(){
@@ -14,18 +15,51 @@ int main(){
     scanf("%s", b);
     m = (int)strlen(b);
     
+    // 0 대각선, 1 위, 2 왼쪽
     for(int i=1; i<=n; i++){
         for(int j=1; j<=m; j++){
             if(a[i-1] == b[j-1]){
                 d[i][j] = d[i-1][j-1] + 1;
+                p[i][j] = 0;
             }
             else{
-                d[i][j] = max(d[i-1][j], d[i][j-1]);
+                if(d[i-1][j] > d[i][j-1]){
+                    d[i][j] = d[i-1][j];
+                    p[i][j] = 1;
+                }else{
+                    d[i][j] = d[i][j-1];
+                    p[i][j] = 2;
+                }
             }
         }
     }
+
     
+    vector<char> result;
+    
+
+    
+    int x = n;
+    int y = m;
+    while(true){
+        if(x==0 || y==0) break;
+        int dir = p[x][y];
+        if(dir == 0){
+            result.push_back(a[x-1]);
+            x--;
+            y--;
+        }else if(dir == 1){
+            x--;
+        }else{
+            y--;
+        }
+    }
+
     printf("%d\n", d[n][m]);
+    for(int i=(int)result.size() - 1; i>=0; i--){
+        printf("%c", result[i]);
+    }
+    printf("\n");
 }
 
 
