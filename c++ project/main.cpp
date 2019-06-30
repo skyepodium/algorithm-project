659d17852731272a1b31821912aed1fd9f152b43
skyepodium <skyepodium@gmail.com>
2019-06-30 15:45:17 +0900

    baekjoon 1275 by fenwick tree

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
-#define max_int 1000001
+#define max_int 100001
 #define lld long long int
 using namespace std;
 
-int n, m, a, b, c;
+int n, q, x, y, a, b;
 lld tree[max_int], d[max_int];
 
 void update_tree(int idx, lld delta){
     
     while(idx <= n){
         tree[idx] += delta;
-        idx += idx & -idx;
+        idx += (idx & -idx);
     }
 }
 
@@ -25,25 +25,28 @@ lld get_data(int idx){
     return result;
 }
 
+void swap(int &a, int &b){
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
 int main(){
-    scanf("%d %d", &n, &m);
+    scanf("%d %d", &n, &q);
     
-    for(int i=0; i<m; i++){
-        scanf("%d %d %d", &a, &b, &c);
+    for(int i=1; i<=n; i++){
+        scanf("%lld", &d[i]);
+        update_tree(i, d[i]);
+    }
+ 
+    for(int i=0; i<q; i++){
+        scanf("%d %d %d %d", &x, &y, &a, &b);
         
-        if(a == 1){
-            lld delta = c - d[b];
-            d[b] = c;
-            update_tree(b, delta);
-        }
-        else{
-            if(b > c){
-                int temp = b;
-                b = c;
-                c = temp;
-            }
-            printf("%lld\n", get_data(c) - get_data(b-1));
-        }
+        if(x > y) swap(x, y);
+        printf("%lld\n", get_data(y) - get_data(x - 1));
         
+        lld delta = b - d[a];
+        d[a] = b;
+        update_tree(a, delta);
     }
 }
