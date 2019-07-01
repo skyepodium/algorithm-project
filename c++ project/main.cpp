6f8ae5c5073bfbbfc6b73db2e9d64c7decd83c8e
skyepodium <skyepodium@gmail.com>
2019-07-01 23:02:05 +0900

    baekjoon 11969 by DP

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -2,45 +2,22 @@
 #define max_int 100001
 using namespace std;
 
-int n, a[max_int], d[max_int], top = 1;
-
-int lower_bound(int start, int end, int val){
-    int result = 0;
-    
-    while(start <= end){
-        int mid = (start + end) / 2;
-        
-        if(d[mid] < val){
-            start = mid + 1;
-        }else{
-            result = mid;
-            end = mid - 1;
-        }
-    }
-    return result;
-}
+int n, m, q, w, a[max_int], d[max_int];
 
 int main(){
+    scanf("%d", &n);
+    for(int i=1; i<=n; i++){
+        scanf("%d", &a[i]);
+    }
     
-    while(scanf("%d", &n) > 0){
-        for(int i=1; i<=n; i++){
-            scanf("%d", &a[i]);
-        }
-        
-        top = 1;
-        for(int i=1; i<=n; i++) d[i] = 0;
-        
-        d[1] = a[1];
-        for(int i=2; i<=n; i++){
-            if(a[i] > d[top]){
-                top++;
-                d[top] = a[i];
-            }
-            else{
-                int idx = lower_bound(1, top, a[i]);
-                d[idx] = a[i];
-            }
-        }
-        printf("%d\n",  top);
+    for(int i=1; i<=n; i++){
+        d[i] = d[i-1] + a[i];
+    }
+    
+    scanf("%d", &m);
+    
+    for(int i=0; i<m; i++){
+        scanf("%d %d", &q, &w);
+        printf("%d\n", d[w] - d[q-1]);
     }
 }
