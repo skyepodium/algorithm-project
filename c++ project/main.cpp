605654be4b8039defbafa875555d33cc0189d45c
skyepodium <skyepodium@gmail.com>
2019-07-01 23:07:46 +0900

    baekjoon 11969 by DP

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -2,22 +2,30 @@
 #define max_int 100001
 using namespace std;
 
-int n, m, q, w, a[max_int], d[max_int];
+int n, m, q, w, d[max_int], a[max_int], b[max_int], c[max_int];
 
 int main(){
-    scanf("%d", &n);
+    scanf("%d %d", &n, &m);
     for(int i=1; i<=n; i++){
-        scanf("%d", &a[i]);
+        scanf("%d", &d[i]);
     }
     
     for(int i=1; i<=n; i++){
-        d[i] = d[i-1] + a[i];
+        a[i] = a[i-1];
+        b[i] = b[i-1];
+        c[i] = c[i-1];
+        if(d[i] == 1) a[i]++;
+        else if(d[i] == 2) b[i]++;
+        else if(d[i] == 3) c[i]++;
     }
-    
-    scanf("%d", &m);
     
     for(int i=0; i<m; i++){
         scanf("%d %d", &q, &w);
-        printf("%d\n", d[w] - d[q-1]);
+        
+        int a_num = a[w] - a[q-1];
+        int b_num = b[w] - b[q-1];
+        int c_num = c[w] - c[q-1];
+
+        printf("%d %d %d\n", a_num, b_num, c_num);
     }
 }
