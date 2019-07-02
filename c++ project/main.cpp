97f426e2da85b12947347fba328d5af8b800f21f
skyepodium <skyepodium@gmail.com>
2019-07-02 22:50:39 +0900

    07.02

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,36 +1,34 @@
 #include <iostream>
-#define max_int 1001
-#define max_val 1000001
+#include <string>
+#define max_int 200001
 using namespace std;
 
-int t, n, a[max_int], d[max_int], result;
-
-int max(int a, int b){
-    return a > b ? a : b;
-}
+int n, s, e, d[max_int][26], size;
+char ch;
+string word;
 
 int main(){
-    scanf("%d", &t);
+    ios_base::sync_with_stdio(false);//
     
-    for(int test_case=1; test_case<=t; test_case++){
-        scanf("%d", &n);
-        
-        result = -max_val;
-        
-        for(int i=1; i<=n; i++){
-            scanf("%d", &a[i]);
-            d[i] = 0;
-        }
-        
-        for(int i=1; i<=n; i++){
-            d[i] = d[i-1] + a[i];
-        }
-        
-        for(int i=1; i<=n; i++){
-            for(int j=i; j<=n; j++){
-                result = max(result, d[j] - d[i-1]);
+    cin >> word;
+    for(int k=0; k<26; k++){
+        for(int i=0; i<(int)word.size(); i++){
+            int j = word[i] - 'a';
+            d[i+1][k] = d[i][k];
+            if(k == j){
+                d[i+1][k]++;
             }
         }
-        printf("%d\n", result);
     }
+    
+    scanf("%d", &n);
+    for(int i=0; i<n; i++){
+        cin >> ch >> s >> e;
+        s++;
+        e++;
+        cin.ignore();
+        
+        printf("%d\n", d[e][ch - 'a'] - d[s-1][ch - 'a']);
+    }
+    
 }
