dabf7c4862d64293e3e2c9be4a0c3fef9e96183c
skyepodium <skyepodium@gmail.com>
2019-07-05 20:15:49 +0900

    baekjoon 2491 by dp

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,87 +1,37 @@
 #include <iostream>
-#include <queue>
-#include <vector>
-#define max_int 10000
+#include <algorithm>
+#define max_int 100001
 using namespace std;
 
-int t, n, mid_num, a[max_int];
-vector<int> result;
-
-struct left_cmp{
-    bool operator()(const int &a, const int &b){
-        return a < b;
-    }
-};
-
-struct right_cmp{
-    bool operator()(const int &a, const int &b){
-        return a > b;
-    }
-};
-
-void init(){
-    result.clear();
-}
+int n, a[max_int], in_d[max_int], de_d[max_int], result1 = 1, result2 = 1;
 
 int main(){
-    scanf("%d", &t);
+    scanf("%d", &n);
     
-    for(int test_case=1; test_case<=t; test_case++){
-        scanf("%d", &n);
-        
-        init();
-        
-        for(int i=1; i<=n; i++){
-            scanf("%d", &a[i]);
-        }
-        
-        mid_num = a[1];
-        result.push_back(a[1]);
-        
-        priority_queue<int, vector<int>, left_cmp> left_q;
-        priority_queue<int, vector<int>, right_cmp> right_q;
-        
-        for(int i=2; i<=n; i++){
-            if(a[i] > mid_num){
-                right_q.push(a[i]);
-            }else if(a[i] < mid_num){
-                left_q.push(a[i]);
-            }else{
-                if(a[i] == a[i+1]){
-                    left_q.push(a[i]);
-                    right_q.push(a[i+1]);
-                    i = i +1;
-                }else{
-                    left_q.push(a[i]);
-                }
-            }
-            if(i % 2 == 1){
-                int left_size = (int)left_q.size();
-                int right_size = (int)right_q.size();
-               if(left_size  < right_size){
-                    left_q.push(mid_num);
-                    mid_num = right_q.top();
-                    right_q.pop();
-                }else if(left_size > right_size){
-                    right_q.push(mid_num);
-                    mid_num = left_q.top();
-                    left_q.pop();
-                }
-                result.push_back(mid_num);
-            }
-        }
-        
-        printf("%d\n", n/2 + 1);
-        int cnt = 0;
-        for(int i=0; i<(int)result.size(); i++){
-            printf("%d ", result[i]);
-            cnt++;
-            if(cnt == 10){
-                printf("\n");
-                cnt = 0;
-            }
-        }
-        printf("\n");
-
+    for(int i=1; i<=n; i++){
+        scanf("%d", &a[i]);
     }
+    
+    in_d[1] = 1;
+    for(int i=2; i<=n; i++){
+        if(a[i] >= a[i-1]){
+            in_d[i] = in_d[i-1] + 1;
+        }else{
+            in_d[i] = 1;
+        }
+        result1 = max(result1, in_d[i]);
+    }
+    
+    de_d[1] = 1;
+    for(int i=2; i<=n; i++){
+        if(a[i] <= a[i-1]){
+            de_d[i] = de_d[i-1] + 1;
+        }else{
+            de_d[i] = 1;
+        }
+        result2 = max(result2, de_d[i]);
+    }
+    
+    printf("%d\n", max(result1, result2));
+
 }
