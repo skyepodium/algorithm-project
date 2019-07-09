4148aaaf1f38709a1a0049cd497155accf0df906
skyepodium <skyepodium@gmail.com>
2019-07-09 23:22:56 +0900

    baekjoon 2162 by ccw, disjoint-set

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,102 +1,107 @@
 #include <iostream>
-#include <queue>
-#include <vector>
-#define max_int 1001
-#define max_val 10000001
+#include <algorithm>
+#define max_int 3001
 #define lld long long int
 using namespace std;
 
-int n, m, k, a, b;
-lld c, d[max_int][200];
+int n, d[max_int], cnt[max_int], group_cnt, max_cnt;
 struct info{
-    int cur;
-    lld cost;
+    lld x, y;
 };
 
-vector<info> v[max_int];
-
-struct cmp{
-    bool operator()(const info &a, const info &b){
-        return a.cost > b.cost;
-    }
+struct edge{
+    info first, second;
 };
 
-int update_cost(int node, lld val){
-    int start = 1;
-    int end = 100;
-    int mid = 0;
-    int ret = 0;
-    while(start <= end){
-        mid = (start + end) / 2;
-        if(d[node][mid] < val){
-            start = mid + 1;
-        }
-        else{
-            ret = mid;
-            end = mid - 1;
-        }
-    }
-    
-    if(ret != 0){
-        for(int i=100; i>=ret; i--){
-            d[node][i+1] = d[node][i];
-        }
-        d[node][ret] = val;
-    }
-    return ret;
+edge line[max_int];
+
+bool operator > (info &a, info &b){
+    if(a.x == b.x) return a.y > b.y;
+    else return a.x > b.x;
 }
 
-void dijkstra(){
-    d[1][1] = 0;
-    priority_queue<info, vector<info>, cmp> pq;
-    pq.push({1, d[1][1]});
+bool operator <= (info &a, info &b){
+    if(a.x == b.x) return a.y <= b.y;
+    else return a.x < b.x;
+}
+
+int ccw(info r, info p, info q){
+    lld first = (p.x - r.x) * (q.y - r.y);
+    lld second = (p.y - r.y) * (q.x - r.x);
+    lld result = first - second;
+    if(result > 0) return 1;
+    else if(result == 0) return 0;
+    else return -1;
+}
+
+bool check_cross(edge first_line, edge second_line){
+    info a = first_line.first;
+    info b = first_line.second;
+    info c = second_line.first;
+    info d = second_line.second;
     
-    while(!pq.empty()){
-        info cur = pq.top();
-        pq.pop();
-        
-        int c_node = cur.cur;
-        
-        for(int i=0; i<v[c_node].size(); i++){
-            info next = v[c_node][i];
-            int n_node = next.cur;
-            lld n_cost = next.cost;
+    int first = ccw(a, b, c) * ccw(a, b, d);
+    int second = ccw(c, d, a) * ccw(c, d, b);
+    
+    if(first == 0 && second == 0){
+        if(a > b) swap(a, b);
+        if(c > d) swap(c, d);
+        return a <= d && c <= b;
+    }else{
+        return first <=0 && second <= 0;
+    }
+}
+
+int find(int node){
+    if(node == d[node]) return node;
+    else return d[node] = find(d[node]);
+}
+
+void init(){
+    for(int i=1; i<=n; i++){
+        d[i] = i;
+        cnt[i] = 1;
+    }
+}
+
+
+int main(){
+    scanf("%d", &n);
+    
+    init();
+    
+    for(int i=1; i<=n; i++){
+        scanf("%lld %lld", &line[i].first.x, &line[i].first.y);
+        scanf("%lld %lld", &line[i].second.x, &line[i].second.y);
+    }
+    
+    for(int i=1; i<n; i++){
+        for(int j=i+1; j<=n; j++){
+            if(i == j) continue;
+
+            int first_num = find(i);
+            int second_num = find(j);
             
-            lld new_cost = d[c_node][1] + n_cost;
-            if(new_cost < d[n_node][1]){
-                update_cost(n_node, new_cost);
-                pq.push({n_node, d[n_node][1]});
-            }else{
-                if(new_cost < d[n_node][k]){
-                    update_cost(n_node, new_cost);
+            if(first_num != second_num){
+                edge first_line = line[i];
+                edge second_line = line[j];
+
+                bool check_result = check_cross(first_line, second_line);
+                
+                if(check_result){
+                    if(first_num > second_num) swap(first_num, second_num);
+                    d[second_num] = first_num;
+                    cnt[first_num] += cnt[second_num];
+                    group_cnt++;
                 }
             }
         }
     }
-}
-
-void init(){
-    for(int i=0; i<=n; i++){
-        for(int j=0; j<=100; j++){
-            d[i][j] = max_val;
-        }
-    }
-}
-
-int main(){
-    scanf("%d %d %d", &n, &m, &k);
-    
-    init();
-    
-    for(int i=0; i<m; i++){
-        scanf("%d %d %lld", &a, &b, &c);
-        v[a].push_back({b, c});
-    }
-    
-    dijkstra();
     
     for(int i=1; i<=n; i++){
-        if(d[i][k] == max_val) d[i][k] = -1;
-        printf("%lld\n", d[i][k]);
+        max_cnt = max(max_cnt, cnt[i]);
     }
+    
+    printf("%d\n", n-group_cnt);
+    printf("%d\n", max_cnt);
 }
