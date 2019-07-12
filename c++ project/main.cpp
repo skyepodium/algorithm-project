0be0e436bc41a1a7fd329410b8ad651b45406a13
skyepodium <skyepodium@gmail.com>
2019-07-12 21:04:18 +0900

    baekjoon 1688 by ray-casting

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,26 +1,24 @@
 #include <iostream>
-#include <algorithm>
-#define max_int 3001
+#define max_int 10001
+#define max_val 1000000001
 #define lld long long int
+
 using namespace std;
 
-int n, d[max_int], cnt[max_int], group_cnt, max_cnt;
+int n;
+
 struct info{
     lld x, y;
 };
 
-struct edge{
-    info first, second;
-};
+info point[max_int], terror[4], start_point, end_point;
 
-edge line[max_int];
-
-bool operator > (info &a, info &b){
+bool operator > (info a, info b){
     if(a.x == b.x) return a.y > b.y;
     else return a.x > b.x;
 }
 
-bool operator <= (info &a, info &b){
+bool operator <= (info a, info b){
     if(a.x == b.x) return a.y <= b.y;
     else return a.x < b.x;
 }
@@ -34,11 +32,7 @@ int ccw(info r, info p, info q){
     else return -1;
 }
 
-bool check_cross(edge first_line, edge second_line){
-    info a = first_line.first;
-    info b = first_line.second;
-    info c = second_line.first;
-    info d = second_line.second;
+bool check_cross(info a, info b, info c, info d){
     
     int first = ccw(a, b, c) * ccw(a, b, d);
     int second = ccw(c, d, a) * ccw(c, d, b);
@@ -46,62 +40,61 @@ bool check_cross(edge first_line, edge second_line){
     if(first == 0 && second == 0){
         if(a > b) swap(a, b);
         if(c > d) swap(c, d);
+        
         return a <= d && c <= b;
-    }else{
-        return first <=0 && second <= 0;
+    }
+    else{
+        return first <= 0 && second <= 0;
     }
 }
-
-int find(int node){
-    if(node == d[node]) return node;
-    else return d[node] = find(d[node]);
-}
-
-void init(){
-    for(int i=1; i<=n; i++){
-        d[i] = i;
-        cnt[i] = 1;
-    }
-}
-
 
 int main(){
     scanf("%d", &n);
     
-    init();
-    
     for(int i=1; i<=n; i++){
-        scanf("%lld %lld", &line[i].first.x, &line[i].first.y);
-        scanf("%lld %lld", &line[i].second.x, &line[i].second.y);
+        scanf("%lld %lld", &point[i].x, &point[i].y);
     }
     
-    for(int i=1; i<n; i++){
-        for(int j=i+1; j<=n; j++){
-            if(i == j) continue;
-
-            int first_num = find(i);
-            int second_num = find(j);
+    for(int i=1; i<=3; i++){
+        scanf("%lld %lld", &terror[i].x, &terror[i].y);
+    }
+    
+    if (n >= 3) {
+        for(int i=1; i<=3; i++){
+            start_point = terror[i];
+            end_point.x = max_val;
+            end_point.y = terror[i].y + 1;
             
-            if(first_num != second_num){
-                edge first_line = line[i];
-                edge second_line = line[j];
-
-                bool check_result = check_cross(first_line, second_line);
+            bool find = false;
+            int cnt = 0;
+            for(int j=1; j<=n; j++){
+                info first = point[j];
+                info second = point[j+1];
+                if(j == n) second = point[1];
                 
+                if(start_point.x == first.x && start_point.y == first.y){
+                    printf("1\n");
+                    find = true;
+                    break;
+                }else if(start_point.x == second.x && start_point.y == second.y){
+                    printf("1\n");
+                    find = true;
+                    break;
+                }
+                
+                bool check_result = check_cross(start_point, end_point, first, second);
                 if(check_result){
-                    if(first_num > second_num) swap(first_num, second_num);
-                    d[second_num] = first_num;
-                    cnt[first_num] += cnt[second_num];
-                    group_cnt++;
+                    cnt++;
                 }
             }
+            if(find) continue;
+            if(cnt % 2 == 0) printf("%d\n", 0);
+            else printf("%d\n", 1);
         }
     }
-    
-    for(int i=1; i<=n; i++){
-        max_cnt = max(max_cnt, cnt[i]);
+    else{
+        for(int i=1; i<=n; i++){
+            printf("0\n");
+        }
     }
-    
-    printf("%d\n", n-group_cnt);
-    printf("%d\n", max_cnt);
 }
