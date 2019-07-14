8f5e225d84b18464b698ec08b5bc927419c84d30
skyepodium <skyepodium@gmail.com>
2019-07-14 13:52:51 +0900

    baekjoon 2166 by ccw

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,50 +1,69 @@
 #include <iostream>
 #define lld long long int
+#define max_int 10001
+
 using namespace std;
 
 /*
- 시간 복잡도: O(1)
- 공간 복잡도: O(1)
+ 시간 복잡도: O(n)
+ 공간 복잡도: O(n)
  사용한 알고리즘: CCW
  사용한 자료구조: 배열
 */
 
-int ccw_result;
+int n;
+lld result;
 
-// 점을 입력받을 구조체를 정의 합니다.
+// 점의 좌표 x, y를 받을 구조체 정의
+// 좌표는 int범위이지만, 계산 과정중 int범위를 넘어갈 수 있기 때문에 long long int를 사용했습니다.
 struct info{
     lld x, y;
 };
 
-// 정의한 구조체를 기반으로 배열을 생성합니다.(3개의 점이 주어집니다.)
-info point[4];
+// point 배열에는 다각형의 좌표를 저장합니다.
+// origin에는 다각형의 가장 첫 좌표를 저장합니다.
+info point[max_int], origin;
 
-// CCW
-// 세점의 좌표정보를 순서대로 받습니다.
-int ccw(info r, info p, info q){
-    // 벡터의 외적을 수행합니다.
-    // 점 p, q에대해 점 r의 x, y를 빼줘서 그래프를 원점에 맞춥니다.
+// CCW 계산
+// ccw(즉, 벡터의 외적)의 결과는 두 벡터가 이루는 평행사변형의 면적입니다.
+lld ccw(info r, info p, info q){
     lld first = (p.x - r.x) * (q.y - r.y);
     lld second = (p.y - r.y) * (q.x - r.x);
     lld result = first - second;
     
-    // 1) 1 이면 벡터rp 기준 점 q는 왼쪽에 존재합니다.
-    if(result > 0) return 1;
-    // 2) 0 이면 일직선에 존재
-    else if(result == 0) return 0;
-    // 3) -1 이면 벡터rp 기준 점 q는 오른쪽에 존재합니다.
-    else return -1;
+    return result;
 }
 
 int main(){
-    //1. 3개의 점을 순서대로 배열에 입력받습니다.
-    for(int i=1; i<=3; i++){
+    // 1. 입력
+    scanf("%d", &n);
+    
+    // 배열에 좌표를 순서대로 입력받습니다.
+    for(int i=1; i<=n; i++){
         scanf("%lld %lld", &point[i].x, &point[i].y);
     }
     
-    // 2. CCW를 수행합니다.
-    ccw_result = ccw(point[1], point[2], point[3]);
+    // 가장 첫번째 입력받은 점이 기준입니다.
+    // 이 점을 기준으로 벡터의 외적을 수행합니다.
+    origin = point[1];
     
-    // 3. 결과 출력
-    printf("%d\n", ccw_result);
+    // 2. ccw 계산
+    // 점을 순서대로 순회하면서 ccw 결과를 더해줍니다.
+    for(int i=2; i<=n-1; i++){
+        lld ccw_result = ccw(origin, point[i], point[i+1]);
+        result += ccw_result;
+    }
+    
+    // 3. 결과
+    // ccw 결과가 음수일 수도 있습니다. 이때는 양수로 만들어줍니다.
+    if(result < 0) result *= -1;
+    
+    // ccw의 결과는 평행사변형의 면적이기 때문에 절반으로 나눠서 삼각형의 면적으로 바꿔주었습니다.
+    printf("%lld.", result / 2);
+    
+    // 제일 힘들었던 부분
+    // 2로 나누었을때 나머지가 있다는 것은 무조건 소수점 첫번째 자리가 5입니다. 두번째 자리는 없습니다.
+    if(result % 2 == 0) printf("0\n");
+    else printf("5\n");
 }
+
