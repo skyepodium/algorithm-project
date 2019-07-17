bfa0f78d8941b90c282e3e626ae27ebcda605a24
skyepodium <skyepodium@gmail.com>
2019-07-17 20:02:21 +0900

    swea 1795 by dijkstra

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,57 +1,106 @@
 #include <iostream>
-#include <cmath>
-#define max_val 10001
-#define EPSILON 1e-9
+#include <vector>
+#include <queue>
+#define max_val 2147483647
+#define max_int 1001
 using namespace std;
 
-int n;
-struct info {
-    double x, y;
+int t, n, m, start_node, a, b, c, d[max_int], e[max_int], result;
+
+struct info{
+    int cur, cost;
 };
-double cross(info a, info b){
-    return a.x * b.y - a.y * b.x;
+
+int max(int a, int b){
+    return a > b ? a : b;
 }
 
-info operator * (info a, double val){
-    return {a.x * val, a.y * val};
-}
+vector<info> v[max_int], r[max_int];
 
-info operator + (info a, info b) {
-    return {b.x + a.x, b.y + a.y};
-}
+struct cmp{
+    bool operator()(const info &a, const info &b){
+        return a.cost > b.cost;
+    }
+};
 
-info operator - (info a, info b) {
-    return {a.x - b.x, a.y - b.y};
-}
-
-info a, b, c, d, result;
-double x, y;
-
-double ccw(const info&r, const info&p, const info&q) {
-    return cross(p - r, q - r);
-}
-
-bool lineIntersection(info a, info b, info c, info d, info&x) {
-    double det = cross(b - a, d - c);
-    if (fabs(det) < EPSILON)return false;
-    x = a + (b-a)*(cross(c - a, d - c) / det);
-    return true;
-}
-int main(){
-    scanf("%d", &n);
+void dijkstra(){
+    priority_queue<info, vector<info>, cmp> pq;
+    d[start_node] = 0;
+    pq.push({start_node, d[start_node]});
     
-    for(int test_case=1; test_case<=n; test_case++){
-        scanf("%lf %lf %lf %lf %lf %lf %lf %lf", &a.x, &a.y, &b.x, &b.y, &c.x, &c.y, &d.x, &d.y);
-        if (lineIntersection(a, b, c, d, result)) {
-            printf("POINT %.2lf %.2lf\n", result.x, result.y);
+    while(!pq.empty()){
+        info cur = pq.top();
+        pq.pop();
+        int c_node = cur.cur;
+        
+        for(int i=0; i<v[c_node].size(); i++){
+            info next = v[c_node][i];
+            int n_node = next.cur;
+            int n_cost = next.cost;
+            
+            if(d[n_node] > d[c_node] + n_cost){
+                d[n_node] = d[c_node] + n_cost;
+                pq.push({n_node, d[n_node]});
+            }
         }
-        else {
-            double ab = ccw(a, b, c)*ccw(a, b, d);
-            double cd = ccw(c, d, a)*ccw(c, d, b);
-            if (fabs(ab) < EPSILON && fabs(cd) < EPSILON)
-                printf("LINE\n");
-            else
-                printf("NONE\n");
+    }
+}
+
+void r_dijkstra(){
+    priority_queue<info, vector<info>, cmp> pq;
+    e[start_node] = 0;
+    pq.push({start_node, e[start_node]});
+    
+    while(!pq.empty()){
+        info cur = pq.top();
+        pq.pop();
+        int c_node = cur.cur;
+        
+        for(int i=0; i<r[c_node].size(); i++){
+            info next = r[c_node][i];
+            int n_node = next.cur;
+            int n_cost = next.cost;
+            
+            if(e[n_node] > e[c_node] + n_cost){
+                e[n_node] = e[c_node] + n_cost;
+                pq.push({n_node, e[n_node]});
+            }
         }
+    }
+}
+
+void init(){
+    result = 0;
+    for(int i=0; i<=n; i++){
+        d[i] = max_val;
+        e[i] = max_val;
+        v[i].clear();
+        r[i].clear();
+    }
+}
+
+int main(){
+    scanf("%d", &t);
+    
+    for(int test_case=1; test_case<=t; test_case++){
+        scanf("%d %d %d", &n, &m, &start_node);
+        
+        init();
+        
+        for(int i=0; i<m; i++){
+            scanf("%d %d %d", &a, &b, &c);
+            v[a].push_back({b, c});
+            r[b].push_back({a, c});
+        }
+        
+        dijkstra();
+        r_dijkstra();
+        
+        for(int i=1; i<=n; i++){
+            result = max(result, d[i] + e[i]);
+        }
+        
+        printf("#%d %d\n", test_case, result);
+        
     }
 }
