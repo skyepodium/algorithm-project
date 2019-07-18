eae3303b589e1778475b845dd3ba24abc0cede74
skyepodium <skyepodium@gmail.com>
2019-07-18 21:12:57 +0900

    baekjoon 1485

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,106 +1,48 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
-#include <queue>
-#define max_val 2147483647
-#define max_int 1001
+#define lld long long int
 using namespace std;
 
-int t, n, m, start_node, a, b, c, d[max_int], e[max_int], result;
-
+int t;
 struct info{
-    int cur, cost;
+    lld x, y;
 };
 
-int max(int a, int b){
-    return a > b ? a : b;
+info point[5];
+
+lld cal_dist(info a, info b){
+    lld first = (b.x - a.x) * (b.x - a.x);
+    lld second = (b.y - a.y) * (b.y - a.y);
+    return first + second;
 }
-
-vector<info> v[max_int], r[max_int];
-
-struct cmp{
-    bool operator()(const info &a, const info &b){
-        return a.cost > b.cost;
-    }
-};
-
-void dijkstra(){
-    priority_queue<info, vector<info>, cmp> pq;
-    d[start_node] = 0;
-    pq.push({start_node, d[start_node]});
-    
-    while(!pq.empty()){
-        info cur = pq.top();
-        pq.pop();
-        int c_node = cur.cur;
-        
-        for(int i=0; i<v[c_node].size(); i++){
-            info next = v[c_node][i];
-            int n_node = next.cur;
-            int n_cost = next.cost;
-            
-            if(d[n_node] > d[c_node] + n_cost){
-                d[n_node] = d[c_node] + n_cost;
-                pq.push({n_node, d[n_node]});
-            }
-        }
-    }
-}
-
-void r_dijkstra(){
-    priority_queue<info, vector<info>, cmp> pq;
-    e[start_node] = 0;
-    pq.push({start_node, e[start_node]});
-    
-    while(!pq.empty()){
-        info cur = pq.top();
-        pq.pop();
-        int c_node = cur.cur;
-        
-        for(int i=0; i<r[c_node].size(); i++){
-            info next = r[c_node][i];
-            int n_node = next.cur;
-            int n_cost = next.cost;
-            
-            if(e[n_node] > e[c_node] + n_cost){
-                e[n_node] = e[c_node] + n_cost;
-                pq.push({n_node, e[n_node]});
-            }
-        }
-    }
-}
-
-void init(){
-    result = 0;
-    for(int i=0; i<=n; i++){
-        d[i] = max_val;
-        e[i] = max_val;
-        v[i].clear();
-        r[i].clear();
-    }
-}
+vector<lld> v;
 
 int main(){
     scanf("%d", &t);
     
     for(int test_case=1; test_case<=t; test_case++){
-        scanf("%d %d %d", &n, &m, &start_node);
+        v.clear();
         
-        init();
-        
-        for(int i=0; i<m; i++){
-            scanf("%d %d %d", &a, &b, &c);
-            v[a].push_back({b, c});
-            r[b].push_back({a, c});
+        for(int i=1; i<=4; i++){
+            scanf("%lld %lld", &point[i].x, &point[i].y);
         }
         
-        dijkstra();
-        r_dijkstra();
-        
-        for(int i=1; i<=n; i++){
-            result = max(result, d[i] + e[i]);
+        for(int i=1; i<=4; i++){
+            for(int j=i+1; j<=4; j++){
+                if(i==j) continue;
+                
+                lld dist = cal_dist(point[i], point[j]);
+                v.push_back(dist);
+            }
         }
-        
-        printf("#%d %d\n", test_case, result);
-        
+        sort(v.begin(), v.end());
+        if(v[0] == v[1] && v[1] == v[2] && v[2] == v[3] && v[4] == v[5]){
+            printf("1\n");
+        }
+        else{
+            printf("0\n");
+        }
     }
+    
 }
