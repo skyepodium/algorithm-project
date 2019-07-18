941dcb962e61f5966c743fee53a4ba48dabba0df
skyepodium <skyepodium@gmail.com>
2019-07-18 21:53:56 +0900

    baekjoon 6439 by ccw

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,48 +1,92 @@
 #include <iostream>
-#include <algorithm>
-#include <vector>
 #define lld long long int
 using namespace std;
 
 int t;
+bool result;
 struct info{
     lld x, y;
 };
 
-info point[5];
+lld min_x, min_y, max_x, max_y;
+info point[5], result_node, start_node, end_node, a, b, c, d;
 
-lld cal_dist(info a, info b){
-    lld first = (b.x - a.x) * (b.x - a.x);
-    lld second = (b.y - a.y) * (b.y - a.y);
-    return first + second;
+bool operator <= (info a, info b){
+    if(a.y == b.y) return a.x <= b.x;
+    else return a.y < b.y;
 }
-vector<lld> v;
+
+bool operator > (info a, info b){
+    if(a.y == b.y) return a.x > b.x;
+    else return a.y > b.y;
+}
+
+int ccw(info r, info p, info q){
+    lld first =  (p.x - r.x) * (q.y - r.y);
+    lld second = (p.y - r.y) * (q.x - r.x);
+    lld result = first - second;
+    
+    if(result > 0) return 1;
+    else if(result == 0) return 0;
+    else return -1;
+}
+
+bool LineInterSection(info a, info b, info c, info d){
+    int first = ccw(a, b, c) * ccw(a, b, d);
+    int second = ccw(c, d, a) * ccw(c, d, b);
+    
+    if(first == 0 && second == 0){
+        if(a > b) swap(a, b);
+        if(c > d) swap(c, d);
+        
+        return a <= d && c <= b;
+    }
+    else{
+        return first <= 0 && second <= 0;
+    }
+}
 
 int main(){
     scanf("%d", &t);
     
     for(int test_case=1; test_case<=t; test_case++){
-        v.clear();
         
-        for(int i=1; i<=4; i++){
-            scanf("%lld %lld", &point[i].x, &point[i].y);
-        }
+        result = false;
         
+        scanf("%lld %lld", &start_node.x, &start_node.y);
+        scanf("%lld %lld", &end_node.x, &end_node.y);
+        
+        scanf("%lld %lld %lld %lld", &min_x, &min_y, &max_x, &max_y);
+
+        if(min_x > max_x) swap(min_x, max_x);
+        if(min_y > max_y) swap(min_y, max_y);
+
+        point[1] = {min_x, min_y};
+        point[2] = {max_x, min_y};
+        point[3] = {max_x, max_y};
+        point[4] = {min_x, max_y};
+
         for(int i=1; i<=4; i++){
-            for(int j=i+1; j<=4; j++){
-                if(i==j) continue;
-                
-                lld dist = cal_dist(point[i], point[j]);
-                v.push_back(dist);
+            info first, second;
+            first = point[i];
+            if(i==4) second = point[1];
+            else second = point[i+1];
+            
+            bool check_intersection = LineInterSection(start_node, end_node, first, second);
+            if(check_intersection){
+                result = true;
             }
         }
-        sort(v.begin(), v.end());
-        if(v[0] == v[1] && v[1] == v[2] && v[2] == v[3] && v[4] == v[5]){
-            printf("1\n");
+        
+        if(!result){
+            if(min_x <= start_node.x && start_node.x <= max_x && min_y <= start_node.y && start_node.y <= max_y){
+                if(min_x <= end_node.x && end_node.x <= max_x && min_y <= end_node.y && end_node.y <= max_y){
+                    result = true;
+                }
+            }
         }
-        else{
-            printf("0\n");
-        }
+        
+        if(result) printf("T\n");
+        else printf("F\n");
     }
-    
 }
