385dea88343d2e670b63d2701801f80b1672e9b4
skyepodium <skyepodium@gmail.com>
2019-07-19 16:16:16 +0900

    baekjoon 11286 by heap

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,60 +1,41 @@
 #include <iostream>
-#define max_int 1025
+#include <queue>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-int n, m, a[max_int][max_int], d[max_int][max_int], tree[max_int][max_int], cmd, x, y, value;
-int start_x, start_y, end_x, end_y;
+int n, num;
 
-void update(int x, int y, int delta){
-    int ty;
-    while(x <= n){
-        ty = y;
-        while(ty <= n){
-            tree[x][ty] += delta;
-            ty += (ty & -ty);
-        }
-        x += (x & -x);
-    }
-}
-
-int get_data(int x, int y){
-    int ret = 0;
-    int ty;
-    while(x >= 1){
-        ty = y;
-        while(ty >= 1){
-            ret += tree[x][ty];
-            ty -= (ty & -ty);
-        }
-        x -= (x & -x);
-    }
-    return ret;
-}
-
-int main(){
-    scanf("%d %d", &n, &m);
-    
-    for(int i=1; i<=n; i++){
-        for(int j=1; j<=n; j++){
-            scanf("%d", &a[i][j]);
-            update(i, j, a[i][j]);
-        }
-    }
-    
-    for(int i=0; i<m; i++){
-        scanf("%d", &cmd);
-        
-        if(cmd == 0){
-            scanf("%d %d %d", &x, &y, &value);
-            int delta = value - a[x][y];
-            a[x][y] = value;
-            update(x, y, delta);
+struct cmp{
+    bool operator()(const int &a, const int &b){
+        if(abs(a) == abs(b)){
+            return a > b;
         }
         else{
-            scanf("%d %d %d %d", &start_x, &start_y, &end_x, &end_y);
-            
-            int result = get_data(end_x, end_y) - get_data(start_x - 1, end_y) - get_data(end_x, start_y - 1) + get_data(start_x - 1, start_y - 1);
-            printf("%d\n", result);
+            return abs(a) > abs(b);
+        }
+    }
+};
+
+int main(){
+    scanf("%d", &n);
+    
+    priority_queue<int, vector<int>, cmp> pq;
+    
+    for(int i=0; i<n; i++){
+        scanf("%d", &num);
+        
+        if(num == 0){
+            if(pq.size() == 0){
+                printf("0\n");
+            }
+            else{
+                printf("%d\n", pq.top());
+                pq.pop();
+            }
+        }
+        else{
+            pq.push(num);
         }
     }
 }
