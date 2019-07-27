12b604525ce2e36a2a42fd42dd11f4f4ca25dc4a
skyepodium <skyepodium@gmail.com>
2019-07-27 14:45:58 +0900

    codeforces 20c by dijkstra

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,40 +1,83 @@
 #include <iostream>
-#include <algorithm>
-#define max_int 101
+#include <vector>
+#include <queue>
+#define max_int 100001
+#define max_val 100000000001
 using namespace std;
 
-int n, a[max_int], temp[max_int], result;
+int n, m, a, b, c, p[max_int];
+long long int d[max_int];
 
-void cal(int start, int end){
-    int ret = 0;
-    for(int i=1; i<=n; i++){
-        temp[i] = a[i];
+struct info{
+    int cur;
+    long long int cost;
+};
+vector<info> v[max_int];
+
+struct cmp{
+    bool operator()(const info &a, const info &b){
+        return a.cost > b.cost;
     }
-    for(int i=start; i<=end; i++){
-        if(temp[i] == 0) temp[i] = 1;
-        else temp[i] = 0;
+};
+
+void dijkstra(){
+    priority_queue<info, vector<info>, cmp> pq;
+    p[1] = 0;
+    d[1] = 0;
+    pq.push({1, d[1]});
+    
+    while(!pq.empty()){
+        info cur = pq.top();
+        pq.pop();
+        int c_node = cur.cur;
+        
+        for(int i=0; i<v[c_node].size(); i++){
+            info next = v[c_node][i];
+            int n_node = next.cur;
+            long long int n_cost = next.cost;
+            
+            if(d[n_node] > d[c_node] + n_cost){
+                d[n_node] = d[c_node] + n_cost;
+                p[n_node] = c_node;
+                pq.push({n_node, d[n_node]});
+            }
+        }
     }
-    for(int i=1; i<=n; i++){
-        if(temp[i] == 1) ret++;
+}
+
+void init(){
+    for(int i=0; i<=n; i++){
+        d[i] = max_val;
     }
-    result = max(result, ret);
+}
+
+void go(int c_node){
+    if(c_node == 0) return;
+    else{
+        go(p[c_node]);
+        printf("%d ", c_node);
+    }
+        
 }
 
 int main(){
-    scanf("%d", &n);
+    scanf("%d %d", &n, &m);
+   
+    init();
     
-//    int t = 0;
-    for(int i=1; i<=n; i++){
-        scanf("%d", &a[i]);
-       // if(a[i] == 1) t++;
-    }
-    //result = max(result, t);
-    
-    for(int i=1; i<=n; i++){
-        for(int j=i; j<=n; j++){
-            cal(i, j);
-        }
+    for(int i=0; i<m; i++){
+        scanf("%d %d %d", &a, &b, &c);
+        v[a].push_back({b, (long long int)c});
+        v[b].push_back({a, (long long int)c});
     }
     
-    printf("%d\n", result);
+    dijkstra();
+    
+    if(d[n] != max_val){
+        go(n);
+        printf("\n");
+    }
+    else{
+        printf("-1\n");
+    }
 }
