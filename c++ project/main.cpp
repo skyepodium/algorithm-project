194fd5e7c89610ab4fef21ac4a57b1ab362bdda9
skyepodium <skyepodium@gmail.com>
2019-08-04 14:39:22 +0900

    comment

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 #include <queue>
-#define max_int 3001
+#define max_int 2001
+#define max_val 2147483647
 
 using namespace std;
 
-int n, m, start_x, start_y, left_cnt, right_cnt, result;
-bool check[max_int][max_int];
+int n, m, start_x, start_y, left_cnt, right_cnt, result, d[max_int][max_int];
 char a[max_int][max_int];
 
 int dx[] = {-1, 1, 0, 0};
-int dy[] = {0, 0, -1, 1};
- 
+int dy[] = {0, 0, -1, 1}; 
+
 struct info{
     int x, y, l_cnt, r_cnt;
 };
@@ -26,9 +26,9 @@ struct cmp{
     }
 };
 
-void bfs(){
+void dijkstra(){
     priority_queue<info, vector<info>, cmp> pq;
-    check[start_x][start_y] = true;
+    d[start_x][start_y] = 0;
     result++;
     pq.push({start_x, start_y, left_cnt, right_cnt});
     
@@ -46,25 +46,39 @@ void bfs(){
             int ny = y + dy[i];
             
             if(nx > n || nx < 1 || ny > m || ny < 1) continue;
-            if(check[nx][ny] || a[nx][ny] == '*')  continue;
+            if(a[nx][ny] == '*')  continue;
             
             if(i == 2){
                 if(l_cnt == 0) continue;
-                check[nx][ny] = true;
-                result++;
-                pq.push({nx, ny, l_cnt - 1, r_cnt});
+                if(d[nx][ny] > d[x][y] + 1){
+                    result++;
+                    d[nx][ny] = d[x][y] + 1;
+                    pq.push({nx, ny, l_cnt - 1, r_cnt});
+                }
             }
             else if(i == 3){
                 if(r_cnt == 0) continue;
-                check[nx][ny] = true;
-                result++;
-                pq.push({nx, ny, l_cnt, r_cnt - 1});
+                if(d[nx][ny] > d[x][y] + 1){
+                    result++;
+                    d[nx][ny] = d[x][y] + 1;
+                    pq.push({nx, ny, l_cnt, r_cnt - 1});
+                }
             }
             else{
-                check[nx][ny] = true;
-                result++;
-                pq.push({nx, ny, l_cnt, r_cnt});
+                if(d[nx][ny] > d[x][y]){
+                    result++;
+                    d[nx][ny] = d[x][y];
+                    pq.push({nx, ny, l_cnt, r_cnt});
+                }
             }
+        }
+    }
+}
+
+void init(){
+    for(int i=0; i<=n; i++){
+        for(int j=0; j<=m; j++){
+            d[i][j] = max_val;
         }
     }
 }
@@ -72,15 +86,15 @@ void bfs(){
 int main(){
     scanf("%d %d", &n, &m);
     
+    init();
+    
     scanf("%d %d", &start_x, &start_y);
     
     scanf("%d %d", &left_cnt, &right_cnt);
     
-    for(int i=1; i<=n; i++){
-        scanf("%s", a[i] + 1);
-    }
+    for(int i=1; i<=n; i++) scanf("%s", a[i] + 1);
     
-    bfs();
+    dijkstra();
     
     printf("%d\n", result);
 }
