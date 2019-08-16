b00ad286ea5e7de8edab9bd4b03a30f04da1e35c
skyepodium <skyepodium@gmail.com>
2019-08-16 20:37:48 +0900

    08.16

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,23 +1,62 @@
 #include <iostream>
-#define max_int 10001
-
+#include <queue>
+#define max_int 301
+#define max_val 2147483647
 using namespace std;
 
-int n, result, bonus;
-char a[max_int];
+int n, m, a[max_int][max_int], d[max_int][max_int];
+int dx[] = {0, 0, 1, -1};
+int dy[] = {-1, 1, 0, 0};
 
-int main(){
-    scanf("%d", &n);
-    scanf("%s", a);
+struct info{
+    int x, y, cost;
+};
+
+struct cmp{
+    bool operator()(const info &a, const info &b){
+        return a.cost > b.cost;
+    }
+};
+
+
+void dijkstra(){
+    priority_queue<info, vector<info>, cmp> pq;
+    d[1][1] = 0;
+    pq.push({1, 1, d[1][1]});
     
-    for(int i=0; i<n; i++){
-        if(a[i] == 'O'){
-            result += i + 1 + bonus;
-            bonus++;
-        }
-        else{
-            bonus = 0;
+    while(!pq.empty()){
+        info cur = pq.top();
+        pq.pop();
+        
+        int x = cur.x;
+        int y = cur.y;
+        int cost = cur.cost;
+        
+        for(int i=0; i<2; i++){
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            
+            if(nx < 1 || nx > n || ny < 1 || ny> m) continue;
+            
+            if(d[nx][ny] > d[x][y] + cost){
+                d[nx][ny] = d[x][y] + cost;
+                pq.push({nx, ny, d[nx][ny]});
+            }
         }
     }
-    printf("%d\n", result);
+}
+
+int main(){
+    scanf("%d %d", &n, &m);
+    
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=m; j++){
+            scanf("%d", &a[i][j]);
+            d[i][j] = max_val;
+        }
+    }
+    
+    dijkstra();
+    
+    printf("%d\n", d[n][m]);
 }
