8f3af04d6c21c1b364826fb3c60fdc4ee5af49d0
skyepodium <skyepodium@gmail.com>
2019-08-18 12:38:10 +0900

    baekjoon 17391 by DP(bottom-up)

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,50 +1,11 @@
 #include <iostream>
-#include <queue>
+#include <algorithm>
 #define max_int 301
 #define max_val 2147483647
 using namespace std;
 
 int n, m, a[max_int][max_int], d[max_int][max_int];
-int dx[] = {0, 0, 1, -1};
-int dy[] = {-1, 1, 0, 0};
 
-struct info{
-    int x, y, cost;
-};
-
-struct cmp{
-    bool operator()(const info &a, const info &b){
-        return a.cost > b.cost;
-    }
-};
-
-
-void dijkstra(){
-    priority_queue<info, vector<info>, cmp> pq;
-    d[1][1] = 0;
-    pq.push({1, 1, d[1][1]});
-    
-    while(!pq.empty()){
-        info cur = pq.top();
-        pq.pop();
-        
-        int x = cur.x;
-        int y = cur.y;
-        int cost = cur.cost;
-        
-        for(int i=0; i<2; i++){
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            
-            if(nx < 1 || nx > n || ny < 1 || ny> m) continue;
-            
-            if(d[nx][ny] > d[x][y] + cost){
-                d[nx][ny] = d[x][y] + cost;
-                pq.push({nx, ny, d[nx][ny]});
-            }
-        }
-    }
-}
 
 int main(){
     scanf("%d %d", &n, &m);
@@ -53,10 +14,22 @@ int main(){
         for(int j=1; j<=m; j++){
             scanf("%d", &a[i][j]);
             d[i][j] = max_val;
-        }
+         }
     }
     
-    dijkstra();
+    d[1][1] = 0;
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=m; j++){
+            int x = a[i][j];
+            
+            for(int k=1; k<=x; k++){
+                
+                if(i+k <= n) d[i+k][j] = min(d[i][j] + 1, d[i+k][j]);
+                if(j+k <= m) d[i][j+k] = min(d[i][j] + 1, d[i][j+k]);
+
+            }
+        }
+    }
     
     printf("%d\n", d[n][m]);
 }
