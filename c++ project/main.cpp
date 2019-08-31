6b739ba8cc4fc64c31e612830f81dfad16c1f045
skyepodium <skyepodium@gmail.com>
2019-08-31 15:44:50 +0900

    kakao blind recruitment 실패율

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,100 +1,51 @@
+#include <iostream>
 #include <string>
 #include <vector>
-#include <map>
-#include <iostream>
+#include <algorithm>
+#define max_int 502
 
 using namespace std;
 
-struct info{
-    int type;
-    string userId;
+int check[max_int];
+int cnt = 0;
+
+struct info {
+    int num;
+    double ratio;
 };
 
-vector<info> event;
-map<string, string> id;
+vector<info> result;
 
-void slice(string word, int type){
-    
-    int startIdx = 0;
-    
-    // change
-    if(type == 1) startIdx = 7;
-    // enter
-    else if(type == 2) startIdx = 6;
-    // leave
-    else startIdx = 6;
-    
-    int size = (int)word.size();
-    
-    string temp = "";
-    string userId = "";
-    string nickName = "";
-    
-    for(int i=startIdx; i<size; i++){
-        if(word[i] == ' '){
-            userId = temp;
-            temp = "";
-        }
-        else{
-            temp += word[i];
-        }
-    }
-    if(type == 3) userId = temp;
-    else nickName = temp;
-
-    
-    // 1. change
-    if(type == 1){
-        id[userId] = nickName;
-    }
-    // 2. enter
-    else if(type == 2){
-        if(id.find(userId) == id.end()){
-            id.insert(pair<string, string>(userId, nickName));
-        }
-        else{
-            id[userId] = nickName;
-        }
-        event.push_back({type, userId});
-    }
-    // 3. leave
-    else{
-        event.push_back({type, userId});
-    }
+bool cmp(const info &a, const info &b){
+    if(a.ratio == b.ratio) return a.num < b.num;
+    else return a.ratio > b.ratio;
 }
 
-vector<string> solution(vector<string> record) {
-    vector<string> answer;
+vector<int> solution(int N, vector<int> stages) {
+    vector<int> answer;
     
-    for(int i=0; i<(int)record.size(); i++){
-        string cmd = record[i];
-        
-        // 1. 변경
-        if(cmd[0] == 'C'){
-            slice(cmd, 1);
-        }
-        // 2. 입장
-        else if(cmd[0] == 'E'){
-            slice(cmd, 2);
-        }
-        // 3. 퇴장
-        else{
-            slice(cmd, 3);
-        }
+    cnt = (int)stages.size();
+    
+    for(int i=0; i<cnt; i++){
+        int num = stages[i];
+        check[num]++;
     }
     
-    for(int i=0; i<(int)event.size(); i++){
-        int type = event[i].type;
-        string userId = event[i].userId;
+    for(int i=1; i<=N; i++){
+        double ratio;
+        if(check[i] != 0) ratio = (double)((double)check[i]/(double)cnt);
+        else ratio = 0;
         
-        if(type == 2){
-            answer.push_back(id[userId] + "님이 들어왔습니다.");
-        }
-        else{
-            answer.push_back(id[userId] + "님이 나갔습니다.");
-        }
+        result.push_back({i, ratio});
+        cnt -= check[i];
     }
-
+    
+    sort(result.begin(), result.end(), cmp);
+    
+    for(int i=0; i<result.size(); i++){
+        cout << result[i].num << " " << result[i].ratio << endl;
+        answer.push_back(result[i].num);
+    }
     
     return answer;
 }
