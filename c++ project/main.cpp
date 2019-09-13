668a09bad02dc6d0fc17c473b4fc5e1550f20e13
skyepodium <skyepodium@gmail.com>
2019-09-13 19:01:48 +0900

    baekjoon 2606 by floyd

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,61 +1,37 @@
 #include <iostream>
-#include <vector>
-#define max_int 1001
+#define max_int 101
+#define max_val 10001
 using namespace std;
 
-int t, n, a, b, result, check[max_int], start_node;
-vector<int> v[max_int];
-
-int max(int a, int b){
-    return a > b ? a : b;
-}
-
-void dfs(int node, int prev){
-    check[node] = check[prev] + 1;
-
-    for(int i=0; i<v[node].size(); i++){
-        int next = v[node][i];
-        if(check[next] == -1){
-            dfs(next, node);
-        }
-        else if(next == start_node){
-            result = max(result, check[prev] + 1);
-        }
-    }
-}
-
-void clear(){
-    check[0] = 0;
-    for(int i=1; i<=n; i++){
-        check[i] = -1;
-    }
-}
-
-void init(){
-    result = n;
-    for(int i=0; i<=n; i++){
-        v[i].clear();
-    }
-}
+int n, m, a, b, d[max_int][max_int], result;
 
 int main(){
-    scanf("%d", &t);
+    scanf("%d %d", &n, &m);
     
-    for(int test_case=1; test_case<=t; test_case++){
-        scanf("%d", &n);
-        
-        for(int i=1; i<=n; i++){
-            scanf("%d %d", &a, &b);
-            v[a].push_back(b);
-            v[b].push_back(a);
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n; j++){
+            d[i][j] = max_val;
         }
-        
-        for(int i=1; i<=n; i++){
-            clear();
-            start_node = i;
-            dfs(i, 0);     
-        }
-        
-        printf("#%d %d\n", test_case, result);
     }
+    
+    for(int i=0; i<m; i++){
+        scanf("%d %d", &a, &b);
+        d[a][b] = d[b][a] = 1;
+    }
+    
+    for(int k=1; k<=n; k++){
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                if(d[i][j] > d[i][k] + d[k][j]){
+                    d[i][j] = d[i][k] + d[k][j];
+                }
+            }
+        }
+    }
+    
+    for(int j=2; j<=n; j++){
+        if(d[1][j] != max_val) result++;
+    }
+    
+    printf("%d\n", result);
 }
