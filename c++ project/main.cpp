22535d22275f31b5ab30247f6f90c83662342c52
skyepodium <skyepodium@gmail.com>
2019-09-14 14:59:46 +0900

    baekjoon 2617 by dfs

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,36 +1,68 @@
 #include <iostream>
+#include <vector>
 #define max_int 101
-#define max_val 10001
 using namespace std;
 
-int n, m, a, b, d[max_int][max_int], result;
+int n, m, a, b, cnt, rcnt, result, num;
+bool check[max_int], rcheck[max_int];
+
+vector<int> v[max_int];
+vector<int> r[max_int];
+
+
+void clear(){
+    cnt = 0;
+    rcnt = 0;
+    for(int i=1; i<=n; i++){
+        check[i] = false;
+        rcheck[i] = false;
+    }
+}
+
+void dfs(int node){
+    check[node] = true;
+    
+    for(int i=0; i<v[node].size(); i++){
+        int next = v[node][i];
+        if(!check[next]){
+            cnt++;
+            dfs(next);
+        }
+    }
+}
+
+void rdfs(int node){
+    rcheck[node] = true;
+    
+    for(int i=0; i<r[node].size(); i++){
+        int next = r[node][i];
+        if(!rcheck[next]){
+            rcnt++;
+            rdfs(next);
+        }
+    }
+}
 
 int main(){
     scanf("%d %d", &n, &m);
     
-    for(int i=1; i<=n; i++){
-        for(int j=1; j<=n; j++){
-            d[i][j] = max_val;
-        }
-    }
+    num = (n+1)/2;
     
     for(int i=0; i<m; i++){
         scanf("%d %d", &a, &b);
-        d[a][b] = d[b][a] = 1;
+        v[a].push_back(b);
+        r[b].push_back(a);
     }
     
-    for(int k=1; k<=n; k++){
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=n; j++){
-                if(d[i][j] > d[i][k] + d[k][j]){
-                    d[i][j] = d[i][k] + d[k][j];
-                }
-            }
+    for(int i=1; i<=n; i++){
+        clear();
+        dfs(i);
+        rdfs(i);
+        
+        if(cnt >= num || rcnt >= num){
+            result++;
         }
-    }
-    
-    for(int j=2; j<=n; j++){
-        if(d[1][j] != max_val) result++;
+        
     }
     
     printf("%d\n", result);
