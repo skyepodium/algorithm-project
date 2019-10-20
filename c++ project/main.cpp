dc50c03bfe3731773a2650af524998c9dbd95c5d
skyepodium <skyepodium@gmail.com>
2019-10-20 17:08:20 +0900

    swea 8658

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,90 +1,37 @@
 #include <iostream>
-#include <queue>
-#include <vector>
 #include <algorithm>
-#define max_int 101
-#define max_val 100
+#define max_val 1000001
 using namespace std;
 
-int n, cnt[max_int], num, node, result=max_val;
-bool check[max_int];
-vector<int> v[max_int], first, second;
+int t, n, max_result, min_result;
 
-void bfs(int start){
-    queue<int> q;
-    q.push(start);
-    while(!q.empty()){
-        int node = q.front();
-        q.pop();
-        
-        for(int i=0; i<v[node].size(); i++){
-            int next = v[node][i];
-            if(!check[next]){
-                check[next] = true;
-                q.push(next);
-            }
-        }
+int calc(int num){
+    int ret = 0;
+    while(num > 0){
+        ret += num % 10;
+        num /= 10;
     }
+    return ret;
 }
 
-void go(int idx, int first_cnt, int second_cnt){
-    if(idx == n+1){
-        if((int)first.size() != 0 && (int)second.size() != 0) {
-
-            for(int i=1; i<=n; i++) check[i] = false;
-            
-            bfs(first[0]);
-            bool flag = true;
-            for(int i=0; i<(int)first.size(); i++){
-                int c_node = first[i];
-                if(!check[c_node]){
-                    flag = false;
-                    break;
-                }
-            }
-            
-            bfs(second[0]);
-            for(int i=0; i<(int)second.size(); i++){
-                int c_node = second[i];
-                if(!check[c_node]){
-                    flag = false;
-                    break;
-                }
-            }
-              
-            if(flag){
-                result = min(result, abs(first_cnt - second_cnt));
-            }
-        }
-        return;
-    }
-    
-    first.push_back(idx);
-    go(idx + 1, first_cnt + cnt[idx], second_cnt);
-    first.pop_back();
-    
-    second.push_back(idx);
-    go(idx + 1, first_cnt, second_cnt + cnt[idx]);
-    second.pop_back();
+void init(){
+    max_result = 0;
+    min_result = max_val;
 }
 
 int main(){
-    scanf("%d", &n);
+    scanf("%d", &t);
     
-    for(int i=1; i<=n; i++){
-        scanf("%d", &cnt[i]);
-    }
-    
-    for(int i=1; i<=n; i++){
-        scanf("%d", &num);
-        for(int j=0; j<num; j++){
-            scanf("%d", &node);
-            v[i].push_back(node);
+    for(int test_case=1; test_case<=t; test_case++){
+        
+        init();
+        
+        for(int i=1; i<=10; i++){
+            scanf("%d", &n);
+            int temp = calc(n);
+            min_result = min(min_result, temp);
+            max_result = max(max_result, temp);
         }
+        printf("#%d %d %d\n", test_case, max_result, min_result);
     }
-    
-    go(1, 0, 0);
-    
-    if(result == max_val) result = -1;
-    printf("%d\n", result);
 }
