a0c71c0081a526044019f2eebba030dee186fe45
skyepodium <skyepodium@gmail.com>
2019-10-20 17:56:10 +0900

    swea 8673

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,37 +1,41 @@
 #include <iostream>
 #include <algorithm>
-#define max_val 1000001
+#include <cmath>
+#define max_int 2048
 using namespace std;
 
-int t, n, max_result, min_result;
-
-int calc(int num){
-    int ret = 0;
-    while(num > 0){
-        ret += num % 10;
-        num /= 10;
-    }
-    return ret;
-}
-
-void init(){
-    max_result = 0;
-    min_result = max_val;
-}
+int t, n, size, a[max_int], b[max_int], result;
 
 int main(){
     scanf("%d", &t);
     
     for(int test_case=1; test_case<=t; test_case++){
+        scanf("%d", &n);
         
-        init();
+        result = 0;
         
-        for(int i=1; i<=10; i++){
-            scanf("%d", &n);
-            int temp = calc(n);
-            min_result = min(min_result, temp);
-            max_result = max(max_result, temp);
+        size = pow(2, n);
+        
+        for(int i=1; i<=size; i++){
+            scanf("%d", &a[i]);
         }
-        printf("#%d %d %d\n", test_case, max_result, min_result);
+        
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=size; j=j+2){
+                if(i%2 == 1) result += abs(a[j] - a[j+1]);
+                else result += abs(b[j] - b[j+1]);
+                if(i%2 == 1){
+                    if(a[j] > a[j+1]) b[(j+1)/2] = a[j];
+                    else b[(j+1)/2] = a[j+1];
+                }
+                else{
+                    if(b[j] > b[j+1]) a[(j+1)/2] = b[j];
+                    else a[(j+1)/2] = b[j+1];
+                }
+            }
+            size /= 2;
+        }
+        
+        printf("#%d %d\n", test_case, result);
     }
 }
