e2eb4725e3f929b52bb3d867d6b1de3cc7ea8d0a
skyepodium <skyepodium@gmail.com>
2019-10-26 18:38:28 +0900

    baekjoon 2178 by bfs

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,41 +1,48 @@
 #include <iostream>
-#include <algorithm>
-#include <cmath>
-#define max_int 2048
+#include <queue>
+#define max_int 101
 using namespace std;
 
-int t, n, size, a[max_int], b[max_int], result;
+int n, m, check[max_int][max_int], dx[] = {0, 0, 1, -1}, dy[] = {-1, 1, 0, 0};
+char a[max_int][max_int];
+
+struct info{
+    int x, y;
+};
+
+void bfs(){
+    queue<info> q;
+    check[1][1] = 1;
+    q.push({1, 1});
+    
+    while(!q.empty()){
+        info cur = q.front();
+        q.pop();
+        
+        int x = cur.x;
+        int y = cur.y;
+        
+        for(int i=0; i<4; i++){
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            
+            if(nx < 1 || nx > n || ny < 1 || ny > m) continue;
+            
+            if(a[nx][ny] == '1' && check[nx][ny] == 0){
+                check[nx][ny] = check[x][y] + 1;
+                q.push({nx, ny});
+            }
+        }
+    }
+}
 
 int main(){
-    scanf("%d", &t);
-    
-    for(int test_case=1; test_case<=t; test_case++){
-        scanf("%d", &n);
-        
-        result = 0;
-        
-        size = pow(2, n);
-        
-        for(int i=1; i<=size; i++){
-            scanf("%d", &a[i]);
-        }
-        
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=size; j=j+2){
-                if(i%2 == 1) result += abs(a[j] - a[j+1]);
-                else result += abs(b[j] - b[j+1]);
-                if(i%2 == 1){
-                    if(a[j] > a[j+1]) b[(j+1)/2] = a[j];
-                    else b[(j+1)/2] = a[j+1];
-                }
-                else{
-                    if(b[j] > b[j+1]) a[(j+1)/2] = b[j];
-                    else a[(j+1)/2] = b[j+1];
-                }
-            }
-            size /= 2;
-        }
-        
-        printf("#%d %d\n", test_case, result);
+    scanf("%d %d", &n, &m);
+    for(int i=1; i<=n; i++){
+        scanf("%s", a[i] + 1);
     }
+    
+    bfs();
+    
+    printf("%d\n", check[n][m]);
 }
