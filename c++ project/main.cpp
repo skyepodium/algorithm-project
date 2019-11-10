2faf9c2db46be98df8a4661d8850dc3dc0413769
skyepodium <skyepodium@gmail.com>
2019-11-10 10:05:25 +0900

    baekjoon 14501

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,216 +1,50 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <set>
-#define lld long long int
-using namespace std;
+#include <stdio.h>
 
-const int max_int = 1001;
+/*
+시간 복잡도: O(n)
+공간 복잡도: O(n)
+사용한 알고리즘: 동적 계획법(bottom-up, tabulation)
+사용한 자료구조: 1차원 배열
+*/
 
-int t, n;
-char hex_map[max_int][max_int], bit_map[max_int][max_int], temp_map[max_int][max_int];
-set<lld> s;
-vector<lld> v;
+const int max_int = 17;
 
-string group[2][10] = {
-    {"0001101", "0011001", "0010011", "0111101", "0100011", "0110001", "0101111", "0111011", "0110111", "0001011"},
-    {"1110010", "1100110", "1101100", "1000010", "1011100", "1001110", "1010000", "1000100", "1001000", "1110100"}
-};
+/*
+ d[i]는 i번째 날에 받을 수 있는 최대 금액
+ 1) i번째 날에 일을 했을 경우
+ d[i+t[i]] = max(d[i+t[i]], d[i] + p[i]);
+ 
+ 2) i번째 날에 일을 하지 않았을 경우
+ d[i+1] = max(d[i+1], d[i]);
+ */
+int n, t[max_int], p[max_int], d[max_int], result;
 
-string hex_to_bit(int num);
-
-void set_bit_map();
-
-void rotate_map();
-
-void check_map(int m);
-
-void init();
-
-void find_num(int idx, int a_idx, int b_idx, int m);
+int max(int a, int b){
+    return a > b ? a : b;
+}
 
 int main(){
-    scanf("%d", &t);
-    
-    for(int test_case=1; test_case<=t; test_case++){
-        scanf("%d", &n);
-        
-        init();
-        
-        for(int i=0; i<n; i++){
-            scanf("%s", hex_map[i]);
-        }
-        set_bit_map();
-
-        for(int i=0; i<n; i++){
-            printf("%s", bit_map[i]);
-        }
-        
-        for(int rotate_cnt = 0; rotate_cnt < 4; rotate_cnt++){
-            rotate_map();
-            for(int m = 1; m <= 3; m++){
-                check_map(m);
-            }
-        }
-
-        printf("#%d %d ", test_case, (int)v.size());
-        sort(v.begin(), v.end());
-        for(int i=0; i<(int)v.size(); i++){
-            printf("%012lld ", v[i]);
-        }
-        printf("\n");
-    }
-}
-
-void init(){
-    s.clear();
-    v.clear();
-}
-
-void rotate_map() {
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            temp_map[i][j] = bit_map[n-1-j][i];
-        }
+    //1. 문제 입력
+    scanf("%d", &n);
+    for(int i=1; i<=n; i++){
+        scanf("%d %d", &t[i], &p[i]);
     }
     
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            bit_map[i][j] = temp_map[i][j];
+    // 2. DP 수행
+    for(int i=1; i<=n; i++){
+        // 1) i번째 날에 일을 했을 경우
+        if(i+t[i] <=n+1){
+            d[i+t[i]] = max(d[i+t[i]], d[i] + p[i]);
+            // 최대값 갱신
+            result = max(result, d[i+t[i]]);
         }
-    }
-}
-
-string hex_to_bit(int num){
-    string ret = "0000";
-    int idx = 3;
-    
-    while(num > 0){
-        ret[idx] = (num%2) + 48;
-        num /= 2;
-        idx--;
-    }
-    return ret;
-}
-
-void set_bit_map(){
-    for(int i=0; i<n; i++){
-        int k_idx = 0;
-        for(int j=0; j<n/4; j++){
-            char curNum = hex_map[i][j];
-            
-            if(curNum <= 96) curNum -= 48;
-            else curNum -= 87;
-            
-            string ret = hex_to_bit(curNum);
-            for(int k=0; k<4; k++){
-                bit_map[i][k_idx + k] = ret[k];
-            }
-            k_idx += 4;
-        }
-    }
-}
-
-void check_map(int m){
-    for(int i=0; i<n; i++){
         
-        for(int j=0; j<= n - 97*m; j++){
-
-            int is_exist = 0;
-            for(int k=0; k<m; k++){
-                char s_1, s_2, s_3, s_4, c_1, c_2, c_3, c_4, c_5, e_1, e_2, e_3, e_4;
-                s_1 = bit_map[i][j+0*m+k];
-                s_2 = bit_map[i][j+1*m+k];
-                s_3 = bit_map[i][j+2*m+k];
-                s_4 = bit_map[i][j+3*m+k];
-                
-                c_1 = bit_map[i][j+46*m+k];
-                c_2 = bit_map[i][j+47*m+k];
-                c_3 = bit_map[i][j+48*m+k];
-                c_4 = bit_map[i][j+49*m+k];
-                c_5 = bit_map[i][j+50*m+k];
-
-                e_1 = bit_map[i][j+93*m+k];
-                e_2 = bit_map[i][j+94*m+k];
-                e_3 = bit_map[i][j+95*m+k];
-                e_4 = bit_map[i][j+96*m+k];
-                
-                if(s_1 == '0' && s_2 == '1' && s_3 == '0' && s_4 == '1' && c_1 == '0' && c_2 == '1' && c_3 == '0' && c_4 == '1' && c_5 == '0' && e_1 == '1' && e_2 == '0' && e_3 == '1' && e_4 == '0'){
-                    is_exist++;
-                }
-            }
-            if(is_exist == m){
-                find_num(i, j+4*m, j+51*m, m);
-            }
-        }
-    }
-}
-
-void find_num(int idx, int a_idx, int b_idx, int m){
-    lld ret = 0;
-    bool a_find = true, b_find = true;
-    
-        for(int i=0; i<6; i++){
-            string curNum = "aaaaaaa";
-            
-            int is_exist = 0;
-            int num = 0;
-            
-            for(int k=0; k<m; k++){
-                curNum[0] = bit_map[idx][a_idx+0*m+k];
-                curNum[1] = bit_map[idx][a_idx+1*m+k];
-                curNum[2] = bit_map[idx][a_idx+2*m+k];
-                curNum[3] = bit_map[idx][a_idx+3*m+k];
-                curNum[4] = bit_map[idx][a_idx+4*m+k];
-                curNum[5] = bit_map[idx][a_idx+5*m+k];
-                curNum[6] = bit_map[idx][a_idx+6*m+k];
-
-                for(int j=0; j<10; j++){
-                    if(curNum == group[0][j]){
-                        num = j;
-                        is_exist++;
-                    }
-                }
-            }
-            if(is_exist != m) a_find = false;
-            else ret = ret * 10 + num;
-            a_idx = a_idx + 7*m;
-        }
-    
-    for(int i=0; i<6; i++){
-        string curNum = "aaaaaaa";
-        
-        int is_exist = 0;
-        int num = 0;
-        
-        for(int k=0; k<m; k++){
-            curNum[0] = bit_map[idx][b_idx+0*m+k];
-            curNum[1] = bit_map[idx][b_idx+1*m+k];
-            curNum[2] = bit_map[idx][b_idx+2*m+k];
-            curNum[3] = bit_map[idx][b_idx+3*m+k];
-            curNum[4] = bit_map[idx][b_idx+4*m+k];
-            curNum[5] = bit_map[idx][b_idx+5*m+k];
-            curNum[6] = bit_map[idx][b_idx+6*m+k];
-
-            for(int j=0; j<10; j++){
-                if(curNum == group[1][j]){
-                    num = j;
-                    is_exist++;
-                }
-            }
-        }
-        if(is_exist != m) b_find = false;
-        else ret = ret * 10 + num;
-        b_idx = b_idx + 7*m;
-    }
-    
-    if(a_find && b_find){
-//        auto iter = s.find(ret);
-//        if(iter == s.end()){
-//            s.insert(ret);
-            v.push_back(ret);
-//        }
+        // 2) i번째 날에 일을 하지 않았을 경우
+        d[i+1] = max(d[i+1], d[i]);
+        // 최대값 갱신
+        result = max(result, d[i+1]);
     }
 
+    // 3. 출력
+    printf("%d\n", result);
 }
