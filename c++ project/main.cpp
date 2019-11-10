538a4e4d233a2cd2e6413e0cd721f5c76e1f7d78
skyepodium <skyepodium@gmail.com>
2019-11-10 13:29:58 +0900

    baekjoon 2805 by binary_search

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,50 +1,49 @@
 #include <stdio.h>
 
-/*
-시간 복잡도: O(n)
-공간 복잡도: O(n)
-사용한 알고리즘: 동적 계획법(bottom-up, tabulation)
-사용한 자료구조: 1차원 배열
-*/
+const int max_cnt = 1000001;
+typedef long long lld;
 
-const int max_int = 17;
-
-/*
- d[i]는 i번째 날에 받을 수 있는 최대 금액
- 1) i번째 날에 일을 했을 경우
- d[i+t[i]] = max(d[i+t[i]], d[i] + p[i]);
- 
- 2) i번째 날에 일을 하지 않았을 경우
- d[i+1] = max(d[i+1], d[i]);
- */
-int n, t[max_int], p[max_int], d[max_int], result;
+int n, tree[max_cnt], max_height, result;
+lld m;
 
 int max(int a, int b){
     return a > b ? a : b;
 }
 
-int main(){
-    //1. 문제 입력
-    scanf("%d", &n);
+lld get_tree(int val){
+    lld ret = 0;
+    
     for(int i=1; i<=n; i++){
-        scanf("%d %d", &t[i], &p[i]);
+        if(tree[i] > val) ret += tree[i] - val;
+    }
+    return ret;
+}
+
+void binary_search(int start, int end){
+    
+    int mid = 0;
+    while(start < end){
+        mid = (start + end) / 2;
+        lld tree_result = get_tree(mid);
+        
+        if(tree_result < m){
+            end = mid;
+        }else{
+            start = mid+1;
+            result = max(result, mid);
+        }
+    }
+}
+
+int main(){
+    scanf("%d %lld", &n, &m);
+    
+    for(int i=1; i<=n; i++) {
+        scanf("%d", &tree[i]);
+        max_height = max(max_height, tree[i]);
     }
     
-    // 2. DP 수행
-    for(int i=1; i<=n; i++){
-        // 1) i번째 날에 일을 했을 경우
-        if(i+t[i] <=n+1){
-            d[i+t[i]] = max(d[i+t[i]], d[i] + p[i]);
-            // 최대값 갱신
-            result = max(result, d[i+t[i]]);
-        }
-        
-        // 2) i번째 날에 일을 하지 않았을 경우
-        d[i+1] = max(d[i+1], d[i]);
-        // 최대값 갱신
-        result = max(result, d[i+1]);
-    }
-
-    // 3. 출력
+    binary_search(0, max_height);
+    
     printf("%d\n", result);
 }
