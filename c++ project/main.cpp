b21137523816210e3eb21c6a6a0f21913e7085f8
skyepodium <skyepodium@gmail.com>
2020-01-04 16:40:50 +0900

    01.04

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,27 +1,11 @@
 #include <iostream>
-#define max_int 101
+#define max_int 1001
 using namespace std;
 
-int n, m, a[max_int], result, sum_result;
-
-int max(int a, int b){
-    return a > b ? a : b;
-}
+char a[max_int];
 
 int main(){
-    scanf("%d %d", &n, &m);
-    
-    for(int i=1; i<=n; i++) scanf("%d", &a[i]);
-    
-    for(int i=1; i<=n-2; i++){
-        for(int j=i+1; j<=n-1; j++){
-            for(int k=j+1; k<=n; k++){
-                sum_result = a[i] + a[j] + a[k];
-                if(sum_result <= m){
-                    result = max(result, sum_result);
-                }
-            }
-        }
-    }
-    printf("%d\n", result);
+    scanf("%s", a);
+    if(a[0] >= 97) a[0] -= 32;
+    printf("%s\n", a);   
 }
