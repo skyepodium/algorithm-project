460ed3ac236376fac132217e4022b90d59484e5f
skyepodium <skyepodium@gmail.com>
2020-04-17 22:00:30 +0900

    baekjoon 1260

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,101 +1,67 @@
 #include <iostream>
 #include <vector>
-#define max_int 101
+#include <queue>
+#include <algorithm>
+
+#define max_int 1001
 using namespace std;
 
-//시간 복잡도: O(n*2^g + xy)
-//공간 복잡도: O(xy + n)
-//사용한 알고리즘: 반복문
-//사용한 자료구조: 스택(벡터), 2차원 배열
+int n, m, start_node, a, b;
 
-int n, x, y, d, g, result;
+vector<int> v[max_int];
+bool check[max_int];
 
-//끝점의 정보
-int end_x, end_y;
-
-bool map[max_int][max_int];
-
-//왼쪽, 위쪽, 오른쪽, 아래쪽
-int dx[] = {0, -1, 0, 1}, dy[] = {1, 0, -1, 0};
-
-     
-//이전 세대의 방향정보를 저장하는 스택
-//stl 스택쓰면 귀찮으니까 인덱스로 접근 할 수 있는 벡터를 사용한다.
-vector<int> dragon;
-
-//스택을 조사하면서 드래곤 커브를 만드는 함수       
-void make_generation(vector<int> &dragon){
+void dfs(int node) {
+    check[node] = true;
+    printf("%d ", node);
     
-    //현재 스택의 크기를 먼저 계산해 놓는다.
-    int size = (int)dragon.size();
-        
-    //스택의 뒤에서 부터 꺼내면서
-    //다음세대의 방향정보를 dir = (dragon[i] + 1)%4; 규칙에 따라 생성한다.
-    for(int i=size-1; i>=0; i--){
-        int dir = (dragon[i] + 1)%4;
-        
-        //다음 세대의 방향정보를 바탕으로 다음 x,y를 찾고 이를 갱신한다.           
-        end_x = end_x + dx[dir];
-        end_y = end_y + dy[dir];
-             
-        //만들어진 드래곤 커브를 지도에 놓아준다.           
-        map[end_x][end_y] = true;    
-             
-        //다음세대를 위하 스택에 방향정보를 넣어준다.    
-        dragon.push_back(dir);            
-    }        
-}
-    
-int main(){
-    
-    scanf("%d", &n);
-               
-    for(int i=0; i<n; i++){
-        //x, y의 순서를 바꿔서 입력받는다.
-        int y, x, d, g;
-        scanf("%d %d %d %d", &y, &x, &d, &g);    
-        
-        //기존 드래곤 커브의 스택을 비워준다.
-        dragon.clear();
-        
-        //시작점에에 드래곤 커브가 놓여있으므로 지도에 표시해.준다.
-        map[x][y] = true;    
-        
-        //0세대는 미리 만들어 놓는다.
-        end_x = x + dx[d];
-        end_y = y + dy[d];
-        
-        //0세대를 만든 이후에도 지도에 표시해준다.
-        map[end_x][end_y] = true;
-        
-        //0세대의 방향정보를 스택에 넣어준다.
-        dragon.push_back(d);
-        
-        //반복문을 통해 0부터 차례차례 드래곤 커브를 만들면서 g세대까지 만든다.
-        for(int i=0; i<g; i++){
-            
-            //드래곤 커브를 만들자
-            make_generation(dragon);
+    for(int i=0; i<v[node].size(); i++){
+        int next = v[node][i];
+        if(!check[next]) {
+            dfs(next);
         }
-        
     }
+}
+
+void bfs() {
+    queue<int> q;
+    q.push(start_node);
+    check[start_node] = true;
     
-    //100*100 2차원 행렬을 2중 for문 사용한 단순한 탐색
-    //인접한 4칸이 모두 true이면, 4칸이 모두 드래곤 커브의 일부인것
-    //갯수를 1증가시킨다.
-    for(int i=0; i<=max_int-2; i++){
-        for(int j=0; j<=max_int-2; j++){
-            
-            //인접한 4칸의 정사각형이 모두 드래곤의 일부이면
-            if(map[i][j] == true && map[i][j+1] == true && map[i+1][j] == true && map[i+1][j+1] == true){
-                
-                //갯수를 1 증가시킨다.
-                result++;
+    while(!q.empty()){
+        int node = q.front();
+        printf("%d ", node);
+        q.pop();
+        
+        for(int i=0; i<v[node].size(); i++){
+            int next = v[node][i];
+            if(!check[next]){
+                check[next] = true;
+                q.push(next);
             }
         }
     }
+}
+
+int main() {
+    scanf("%d %d %d", &n, &m, &start_node);
     
-    //갯수 출력
-    printf("%d\n", result);
+    for(int i=1; i<=m; i++){
+        scanf("%d %d", &a, &b);
+        v[a].push_back(b);
+        v[b].push_back(a);
+    }
     
+    for(int i=1; i<=n; i++){
+        sort(v[i].begin(), v[i].end());
+    }
+    
+    dfs(start_node);
+    
+    printf("\n");
+    for(int i=0; i<=n; i++) check[i] = false;
+    
+    bfs();
+    
+    printf("\n");
 }
