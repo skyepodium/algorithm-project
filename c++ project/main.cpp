0665254ec167bed383d55d355460a98769088f17
skyepodium <skyepodium@gmail.com>
2020-04-30 19:08:31 +0900

    baekjoon 3190 test

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,56 +1,77 @@
 #include <iostream>
-#include <algorithm>
+#include <vector>
+#define max_int 101
 using namespace  std;
 
-int n, v[4], cnt[4][4], num, max_idx=0, max_val=0;
+int n, k, l, x, y, X, a[max_int][max_int], result = 0, len = 1, cx = 1, cy = 1, cdir = 0;
+bool apple[max_int][max_int];
+int dx[] = {0, 1, 0, -1};
+int dy[] = {1, 0, -1, 0};
 
-int max3(int a, int b, int c){
-    return max(max(a, b), c);
-}
+char C, cmd[max_int];
 
 int main(){
+
+    // 1. 입력
     scanf("%d", &n);
 
-    for(int i=1; i<=n; i++){
-        for(int j=1; j<=3; j++){
-            scanf("%d", &num);
+    scanf("%d", &k);
 
-            v[j] += num;
-            cnt[j][num]++;
+    for(int i=1; i<=k; i++){
+        scanf("%d %d", &x, &y);
+        apple[x][y] = true;
+    }
+
+     
+    for(int i=0; i<=n; i++){
+        for(int j=0; j<=n; j++){
+            a[i][j] = -1;
         }
     }
 
-    if(v[1] == v[2] == v[3]){
-        for(int i=1; i<=4; i++){
-            if(i==4) {.      
-                cout << "Sfdkkkk" << endl;
-            }
-            if(cnt[1][i] == cnt[2][i] == cnt[3][i]){
-                continue;
-            }
-            else{
-                max_val = max3(cnt[1][i], cnt[2][i], cnt[3][i]);
-                max_idx = 0;
+    // 2. 뱀 초기화
+    a[1][1] = 0;
 
-                for(int i=1; i<=3; i++){
-                    if(max_val == cnt[max_idx][i]){
-                        max_idx = i;
-                        break;    
-                    }
-                }
+    cin >> l;
+    for(int i=0; i<=l; i++){
+        x = n*n+1;
+        if (i < l){
+            cin >> X >> C;
+        }
+
+        // 3. 반복
+        while(result < X){
+            result++;
+            cx = cx + dx[cdir];
+            cy = cy + dy[cdir];
+
+            // 벽에 부딫친경우 종료
+            if(cx > n || cx < 1 || cy > n || cy < 1 ) {
+                cout << result << endl;
+                return 0;
             }
+
+            // 사과 검사
+            if(apple[cx][cy]) {
+                apple[cx][cy] = false;
+                len++;
+            }
+
+            // 몸 부딫치는 경우 검사
+            if(a[cx][cy] != -1 && result - a[cx][cy] <= len){
+                cout << result << endl;
+                return 0;
+            }
+
+            a[cx][cy] = result;
+        }
+        // 방향 전환
+        if(C == 'L') {
+            cdir = (cdir + 3 ) % 4;
+        }
+        else {
+            cdir = (cdir + 1 ) % 4;
         }
     }
-    else{
-        max_val = max3(v[1], v[2], v[3]);
-        max_idx = 0;   
-
-        for(int i=1; i<=3; i++){
-            if(max_val == v[i]){
-                max_idx = i;
-                break;
-            }
-        }
-    }
-    printf("%d %d\n", max_idx, max_val);
+    return 0;
 }
