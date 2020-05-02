c381393873143a9be9ad2bb9169d45726dce3039
skyepodium <skyepodium@gmail.com>
2020-05-02 15:56:45 +0900

    카카오 비밀지도 주석 추가

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -14,13 +14,22 @@ using namespace std;
 vector<string> solution(int n, vector<int> arr1, vector<int> arr2) {
     vector<string> answer;
 
+    // n개의 숫자에 대해 n번 반복합니다.
     for(int i=0; i<n; i++){
+        // 결과를 문자열로 저장할 변수 num
         string num = "";
+
+        // 배열 요소의 최대 크기는 2^n - 1, 최대 n개의 비트를 가지고 있습니다.
         for(int j=n-1; j>=0; j--){
-            if(((arr1[i] & (1 << j)) == 0) && ((arr2[i] & (1 << j)) == 0)){
-                num += " ";
-            }else{
+
+            // 1. arr1, arr2의 n 번째 비트가 하나라도 1인 경우
+            if((arr1[i] & (1 << j)) | (arr2[i] & (1 << j))){
                 num += "#";
+            }
+            // 2. arr1, arr2의 n 번째 비트가 모두 0인 경우
+            // (if문의 조건에 0이 들어오면 false로 인식합니다.)
+            else{
+                num += " ";
             }
         }
         answer.push_back(num);
