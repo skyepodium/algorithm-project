5954dd66ac848b5df8edd3b00d47dacd5087e744
skyepodium <skyepodium@gmail.com>
2020-06-07 23:55:15 +0900

    06.07

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,13 +1,7 @@
 #include <iostream>
 
 using namespace std;
-        
-void go(int a, int b){   
-    cout << "a " << a << endl;
-    cout << "b " << b << endl;
-}
 
-int main(){   
-    go(1, 2);
+int main(){
+           
 }
-    
