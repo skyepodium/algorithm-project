d43d681da6d93a177e6cd14a6c3589b1a90b4076
skyepodium <skyepodium@gmail.com>
2020-11-22 13:55:06 +0900

    baekjoon 1712

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,58 +1,16 @@
 #include <iostream>
-#include <algorithm>
-#include <string>
-#include <vector>
-
 using namespace std;
 
-int n;
-string word, first_value, original_value, result;
+int a, b, c, min_value, result = -1;
 
-vector<string> words, a;
+int main () {
+    scanf("%d %d %d", &a, &b, &c);
 
-string lower_string(string value) {
-    string result = "";
-    
-    for(int i=0; i<value.size(); i++) {
-        char cur = value[i];
-        if(cur < 97) {
-            cur = cur + 32;
-        }
-        result += cur;
+    if (c-b > 0) {
+        min_value = a / (c-b);
+        
+        result = min_value + 1;
     }
     
-    return result;
-}
-
-int main() {
-    
-    while (true) {
-        scanf("%d", &n);
-        
-        if (n == 0) break;
-        
-        words.clear();
-        
-        for(int i=0; i<n; i++){
-            cin >> word;
-
-            a.push_back(word);
-            words.push_back(lower_string(word));
-        }
-        
-        sort(words.begin(), words.end());
-        
-        first_value = words[0];
-        
-        for(int i=0; i<(int)a.size(); i++) {
-            original_value = a[i];
-            
-            if (first_value == lower_string(original_value)) {
-                result = original_value;
-                break;
-            }
-        }
-        
-        cout << result << endl;
-    }
+    printf("%d\n", result);
 }
