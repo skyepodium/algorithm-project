f3665c60dd74064d9502911cd99152472e9ab202
skyepodium <skyepodium@gmail.com>
2020-11-22 13:12:02 +0900

    beakjoon 2204 by string

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,34 +1,58 @@
 #include <iostream>
-#include <stack>
+#include <algorithm>
+#include <string>
+#include <vector>
+
 using namespace std;
 
-int t, n, num, result;
-stack<int> s;
+int n;
+string word, first_value, original_value, result;
 
-int main(){
-    scanf("%d", &t);
+vector<string> words, a;
+
+string lower_string(string value) {
+    string result = "";
     
-    for(int test_case=1; test_case<=t; test_case++){
+    for(int i=0; i<value.size(); i++) {
+        char cur = value[i];
+        if(cur < 97) {
+            cur = cur + 32;
+        }
+        result += cur;
+    }
+    
+    return result;
+}
+
+int main() {
+    
+    while (true) {
         scanf("%d", &n);
         
-        for(int i=1; i<=n; i++){
-            scanf("%d", &num);
+        if (n == 0) break;
+        
+        words.clear();
+        
+        for(int i=0; i<n; i++){
+            cin >> word;
+
+            a.push_back(word);
+            words.push_back(lower_string(word));
+        }
+        
+        sort(words.begin(), words.end());
+        
+        first_value = words[0];
+        
+        for(int i=0; i<(int)a.size(); i++) {
+            original_value = a[i];
             
-            if(num != 0) {
-                s.push(num);
-            }
-            else{
-                s.pop();
+            if (first_value == lower_string(original_value)) {
+                result = original_value;
+                break;
             }
         }
         
-        result = 0;
-        
-        while (!s.empty()) {
-            result += s.top();
-            s.pop();
-        }
-        
-        printf("#%d %d\n", test_case, result);
+        cout << result << endl;
     }
 }
