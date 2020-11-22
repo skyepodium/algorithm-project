25d8e7e07239be695c67018386c5fd2114da9d27
skyepodium <skyepodium@gmail.com>
2020-11-22 14:10:22 +0900

    baekjoon 16948 by bfs

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,16 +1,59 @@
 #include <iostream>
+#include <queue>
+#define max_int 201
 using namespace std;
 
-int a, b, c, min_value, result = -1;
+int n, r1, c1, r2, c2, check[max_int][max_int];
+int dx[] = {-2, -2, 0, 0, 2, 2};
+int dy[] = {-1, 1, -2, 2, -1, 1};
 
-int main () {
-    scanf("%d %d %d", &a, &b, &c);
+struct info {
+    int x;
+    int y;
+};
 
-    if (c-b > 0) {
-        min_value = a / (c-b);
-        
-        result = min_value + 1;
-    }
+void bfs(int x, int y) {
+    queue<info> q;
+    check[x][y] = 0;
+    q.push({x, y});
     
-    printf("%d\n", result);
+    while(!q.empty()) {
+        info cur = q.front();
+        q.pop();
+        
+        x = cur.x;
+        y = cur.y;
+        
+        for(int i=0; i<6; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            
+            if(nx >= 0 and nx < n and ny >= 0 and ny < n) {
+                if(check[nx][ny] == -1) {
+                    check[nx][ny] = check[x][y] + 1;
+                    q.push({nx, ny});
+                }
+            }
+        }
+    }
+}
+
+void init () {
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
+            check[i][j] = -1;
+        }
+    }
+}
+
+int main() {
+    scanf("%d", &n);
+    
+    init();
+    
+    scanf("%d %d %d %d", &r1, &c1, &r2, &c2);
+    
+    bfs(r1, c1);
+    
+    printf("%d\n", check[r2][c2]);
 }
