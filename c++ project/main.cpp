82090e9897f654f397a07eb5f7aa093787275146
skyepodium <skyepodium@gmail.com>
2020-11-23 22:56:52 +0900

    baekjoon 2210 by dfs

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,103 +1,67 @@
 #include <iostream>
-#include <queue>
-#include <algorithm>
-#define max_int 1501
-
+#include <vector>
+#include <string>
+#define max_int 5
 using namespace std;
 
-int a, b, c, d, nx, ny, nz, result = 0, arr[3];
-bool check[max_int][max_int];
+int a[max_int][max_int], result = 0;
+int dx[] = {0, 0, 1, -1}, dy[] = {-1, 1, 0, 0};
+vector<string> number_list;
 
-struct info{
-    int x, y, z;
-};
-
-void sort_val(int &a, int &b, int &c) {
-    arr[0] = a;
-    arr[1] = b;
-    arr[2] = c;
+string number_to_string(int value) {
+    string ret = "";
     
-    sort(arr, arr + 3);
+    char string_value = (char)value + 48;
     
-    a = arr[0];
-    b = arr[1];
-    c = arr[2];
+    return ret + string_value;
 }
 
-void bfs(int x, int y, int z) {
-    queue<info> q;
-    check[x][y] = true;
-    q.push({x, y, d-(x+y)});
+
+void go(int x, int y, string s) {
     
-    while(!q.empty()) {
-        info cur = q.front();
-        q.pop();
+    if((int)s.size() >= 6) {
         
-        x = cur.x;
-        y = cur.y;
-        z = cur.z;
+        bool is_exist = false;
         
-        if(x == y && y == z) {
-            result = 1;
-            break;
-        }
-        
-        sort_val(x, y, z);
-        
-        if (y - x > 0) {
-            nx = x * 2;
-            ny = y - x;
-            nz = z;
-            
-            if (nx < max_int && ny > 0) {
-                sort_val(nx, ny, nz);
-                
-                if(!check[nx][ny]) {
-                    check[nx][ny] = true;
-                    q.push({nx, ny, nz});
-                }
+        for(int i=0; i<(int)number_list.size(); i++) {
+            if(number_list[i] == s) {
+                is_exist = true;
+                break;
             }
         }
         
-        if (z - x > 0) {
-            nx = x * 2;
-            ny = z - x;
-            nz = y;
-
-            if (nx < max_int && ny > 0) {
-                sort_val(nx, ny, nz);
-                
-                if(!check[nx][ny]) {
-                    check[nx][ny] = true;
-                    q.push({nx, ny, nz});
-                }
-            }
+        if(!is_exist) {
+            number_list.push_back(s);
+            result++;
         }
         
-        if (z - y > 0) {
-            nx = y * 2;
-            ny = z - y;
-            nz = x;
-
-            if (nx < max_int && ny > 0) {
-                sort_val(nx, ny, nz);
-                
-                if(!check[nx][ny]) {
-                    check[nx][ny] = true;
-                    q.push({nx, ny, nz});
-                }
-            }
-        }
+        return;
+    }
+    
+    for(int i=0; i<4; i++) {
+        int nx = x + dx[i];
+        int ny = y + dy[i];
+        
+        if(nx < 0 || nx >= max_int || ny < 0 || ny >= max_int) continue;
+        
+        go(nx, ny, s + number_to_string(a[nx][ny]));
+        
     }
 }
 
-int main () {
-    scanf("%d %d %d", &a, &b, &c);
-    d = a + b + c;
+
+int main(){
+    for(int i=0; i<max_int; i++) {
+        for(int j=0; j<max_int; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
     
-    sort_val(a, b, c);
-    
-    bfs(a, b, c);
+    for(int i=0; i<max_int; i++) {
+        for(int j=0; j<max_int; j++) {
+            go(i, j, number_to_string(a[i][j]));
+        }
+    }
     
     printf("%d\n", result);
 }
