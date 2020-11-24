47c314d84d12da59912d1746a5f92f8f8a717405
skyepodium <skyepodium@gmail.com>
2020-11-24 21:11:50 +0900

    baekjoon 14938 by dijkstra

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,59 +1,88 @@
 #include <iostream>
 #include <queue>
 #include <vector>
-#define max_int 300001
+#define max_int 101
+#define max_val 1501
 using namespace std;
 
-int n, m, k, x, check[max_int], start_point, end_point, cnt = 0;
-vector<int> v[max_int];
+int n, m, r, item[max_int], a, b, c, d[max_int], result = 0;
+struct info {
+    int cur, cost;
+};
 
-void bfs(int start_node) {
-    queue<int> q;
-    check[start_node] = 0;
-    q.push(start_node);
+vector<info> v[max_int];
+
+int max(int a, int b) {
+    return a > b ? a : b;
+}
+
+void cnt_check() {
+    int ret = 0;
+    for(int i=1; i<=n; i++) {
+        if(d[i] <= m) {
+            ret += item[i];
+        }
+    }
+    result = max(result, ret);
+}
+
+struct cmp {
+    bool operator()(const info &a, const info &b) {
+        return a.cost > b.cost;
+    }
+};
+
+void dijkstra(int start_node) {
+    priority_queue<info, vector<info>, cmp> pq;
+    d[start_node] = 0;
+    pq.push({start_node, d[start_node]});
     
-    while(!q.empty()) {
-        int node = q.front();
-        q.pop();
+    while(!pq.empty()) {
+        info cur = pq.top();
+        pq.pop();
+        
+        int node = cur.cur;
         
         for(int i=0; i<v[node].size(); i++) {
-            int next = v[node][i];
+            info next = v[node][i];
             
-            if(check[next] == -1) {
-                check[next] = check[node] + 1;
-                q.push(next);
+            int n_node = next.cur;
+            int n_cost = next.cost;
+            
+            if(d[n_node] > d[node] + n_cost) {
+                d[n_node] = d[node] + n_cost;
+                pq.push({n_node, d[n_node]});
             }
         }
     }
 }
 
 void init () {
-    for(int i=0; i<=n; i++) {
-        check[i] = -1;
+    for(int i=1; i<=n; i++) {
+        d[i] = max_val;
     }
 }
 
-
 int main () {
-    scanf("%d %d %d %d", &n, &m, &k, &x);
+    scanf("%d %d %d", &n, &m, &r);
     
-    init();
+    init() ;
     
-    for(int i=0; i<m; i++) {
-        scanf("%d %d", &start_point, &end_point);
-        v[start_point].push_back(end_point);
-    }
-    
-    bfs(x);
-
     for(int i=1; i<=n; i++) {
-        if(k == check[i]) {
-            printf("%d\n", i);
-            cnt++;
-        }
+        scanf("%d", &item[i]);
     }
     
-    if(cnt == 0) {
-        printf("-1\n");
+    for(int i=0; i<r; i++) {
+        scanf("%d %d %d", &a, &b, &c);
+        v[a].push_back({b, c});
+        v[b].push_back({a, c});
     }
+    
+    for(int i=1; i<=n; i++) {
+        init();
+        dijkstra(i);
+        cnt_check();
+    }
+    
+    printf("%d\n", result);
 }
