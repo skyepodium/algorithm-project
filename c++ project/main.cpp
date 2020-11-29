3822b555e72bb5574a5c5aa90a598ede3b7f65b1
skyepodium <skyepodium@gmail.com>
2020-11-29 12:23:01 +0900

    baekjoon 1303 by dfs

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,86 +1,50 @@
 #include <iostream>
-#include <queue>
-#define max_int 201
+#define max_int 101
 using namespace std;
 
-int n, k, a[max_int][max_int], s, fx, fy, check[max_int][max_int], result = 0;
-int dx[] = { 0, 0, 1, -1 };
-int dy[] = { -1, 1, 0, 0 };
+int n, m, dx[] = {0, 0, 1, -1}, dy[] = {-1, 1, 0, 0}, w_result, b_result, cnt;
+bool check[max_int][max_int];
+char a[max_int][max_int];
 
-struct info {
-    int x, y;
-};
-queue<info> q;
-
-void bfs() {
-    while (!q.empty()) {
-        info cur = q.front();
-        q.pop();
-
-        int x = cur.x;
-        int y = cur.y;
-        int virus_num = a[x][y];
-
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-
-            if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
-
-            if (check[nx][ny] == -1) {
-                check[nx][ny] = check[x][y] + 1;
-                a[nx][ny] = virus_num;
-                q.push({ nx, ny });
-            }
-            else {
-                if (check[nx][ny] > check[x][y] + 1) {
-                    check[nx][ny] = check[x][y] + 1;
-                    a[nx][ny] = virus_num;
-                    q.push({ nx, ny });
-                }
-                else if (check[nx][ny] == check[x][y] + 1) {
-                    if (a[nx][ny] > virus_num) {
-                        check[nx][ny] = check[x][y] + 1;
-                        a[nx][ny] = virus_num;
-                        q.push({ nx, ny });
-                    }
-                }
-            }
+void dfs(int x, int y, char soldier) {
+    check[x][y] = true;
+    cnt++;
+    
+    for(int i=0; i<4; i++) {
+        int nx = x + dx[i];
+        int ny = y + dy[i];
+        
+        if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+        
+        if (!check[nx][ny] && a[nx][ny] == soldier) {
+            dfs(nx, ny, a[nx][ny]);
         }
     }
 }
 
-void init() {
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            check[i][j] = -1;
-        }
+int main () {
+    scanf("%d %d", &m, &n);
+    
+    for(int i=0; i<n; i++) {
+        scanf("%s", a[i]);
     }
-}
+    
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<m; j++) {
+            if (!check[i][j]) {
+                cnt = 0;
 
-int main() {
-    scanf("%d %d", &n, &k);
+                dfs(i, j, a[i][j]);
 
-    init();
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            scanf("%d", &a[i][j]);
-
-            if (a[i][j] != 0) {
-                check[i][j] = 0;
-                q.push({ i, j });
+                if(a[i][j] == 'W') {
+                    w_result += cnt * cnt;
+                }
+                else{
+                    b_result += cnt * cnt;
+                }
             }
         }
     }
-
-    scanf("%d %d %d", &s, &fx, &fy);
-
-    bfs();
-
-    if (check[fx - 1][fy - 1] <= s) {
-        result = a[fx-1][fy-1];
-    }
-
-    printf("%d\n", result);
+    
+    printf("%d %d\n", w_result, b_result);
 }
