c3ef5dc8c7f896a2cd4c9037a0022525ab7bf855
skyepodium <skyepodium@gmail.com>
2020-11-29 12:59:56 +0900

    baekjoon 10829 by string

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,50 +1,26 @@
 #include <iostream>
-#define max_int 101
+#include <string>
+#define lld long long int
 using namespace std;
 
-int n, m, dx[] = {0, 0, 1, -1}, dy[] = {-1, 1, 0, 0}, w_result, b_result, cnt;
-bool check[max_int][max_int];
-char a[max_int][max_int];
-
-void dfs(int x, int y, char soldier) {
-    check[x][y] = true;
-    cnt++;
-    
-    for(int i=0; i<4; i++) {
-        int nx = x + dx[i];
-        int ny = y + dy[i];
-        
-        if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-        
-        if (!check[nx][ny] && a[nx][ny] == soldier) {
-            dfs(nx, ny, a[nx][ny]);
-        }
-    }
-}
+lld n;
+int num, word_size;
+string result;
 
 int main () {
-    scanf("%d %d", &m, &n);
+    scanf("%lld", &n);
     
-    for(int i=0; i<n; i++) {
-        scanf("%s", a[i]);
+    while(n>0) {
+        num = n % 2;
+        if(num == 0 ) result += "0";
+        else result += "1";
+        
+        n = n / 2;
     }
     
-    for(int i=0; i<n; i++) {
-        for(int j=0; j<m; j++) {
-            if (!check[i][j]) {
-                cnt = 0;
-
-                dfs(i, j, a[i][j]);
-
-                if(a[i][j] == 'W') {
-                    w_result += cnt * cnt;
-                }
-                else{
-                    b_result += cnt * cnt;
-                }
-            }
-        }
+    word_size = (int)result. size();
+    for(int i= word_size - 1; i>=0; i--) {
+        printf("%c", result[i]);
     }
-    
-    printf("%d %d\n", w_result, b_result);
+    printf("\n");
 }
