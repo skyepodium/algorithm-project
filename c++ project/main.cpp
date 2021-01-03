70c459627f091b6a59839f83c095aafc6ff6a174
skyepodium <skyepodium@gmail.com>
2021-01-03 22:27:54 +0900

    프로그래머스 두개 뽑아서 더하기

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,59 +1,29 @@
-#include <iostream>
-#include <queue>
-#define max_int 10001
-
+#include <string>
+#include <vector>
+#include <algorithm>
+#define max_int 201
 using namespace std;
 
-int n, a[max_int], start_point, end_point, check[max_int];
-
-void bfs(int start_node) {
-    queue<int> q;
-    check[start_node] = 0;
-    q.push(start_node);
+vector<int> solution(vector<int> numbers) {
+    vector<int> answer;
     
-    while(!q.empty()) {
-        int node = q.front();
-        q.pop();
-        
-        for(int i=1; node + a[node] * i <= n; i++) {
-            int next_node = node + a[node] * i;
+    bool check[max_int];
+    for(int i=0; i<max_int; i++) check[i] = false;
+    
+    for(int i=0; i<numbers.size(); i++) {
+        for(int j=0; j<numbers.size(); j++) {
+            if(i == j) continue;
             
-            if(check[next_node] == -1) {
-                check[next_node] = check[node] + 1;
-                q.push(next_node);
-            }
-        }
-        
-        for(int i=1; node - a[node] * i >= 1; i++) {
-            int next_node = node - a[node] * i;
+            int sum_value = numbers[i] + numbers[j];
             
-            if(next_node <= n && check[next_node] == -1) {
-                check[next_node] = check[node] + 1;
-                q.push(next_node);
+            if(!check[sum_value]) {
+                check[sum_value] = true;
+                answer.push_back(sum_value);
             }
         }
     }
+    
+    sort(answer.begin(), answer.end());
+    
+    return answer;
 }
-
-void init () {
-    for(int i=0; i<=n; i++) {
-        check[i] = -1;
-    }
-}
-
-int main () {
-    scanf("%d", &n);
-    
-    init();
-    
-    for(int i=1; i<=n; i++) {
-        scanf("%d", &a[i]);
-    }
-    
-    scanf("%d %d", &start_point, &end_point);
-    
-    bfs(start_point);
-    
-    printf("%d\n", check[end_point]);
-}
-
