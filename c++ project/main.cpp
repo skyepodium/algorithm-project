016a309052dd1995ca4004bb95b6c5d7f734eca8
skyepodium <skyepodium@gmail.com>
2021-01-21 23:43:19 +0900

    dfs algorithm

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
+#include <queue>
+#include <vector>
 
-using namespace std;
+#define max_int 10001
+using namespace  std;
+
+int n, m, k, a, b;
+bool check[max_int];
+vector<int> v[max_int];
+
+void dfs(int node) {
+    check[node] = true;
+    for(auto next: v[node]) {
+        if(!check[next]) {
+            dfs(next);
+        }
+    }
+}
 
 int main() {
-    puts("0xL4ugh&amp;&num;123&semi;Ourfirsteventenjoy&amp;&num;125&semi;");
+    scanf("%d %d %d", &n, &m, &k);
+    
+    for(int i=0; i<m; i++) {
+        scanf("%d %d", &a, &b);
+        v[a].push_back(b);
+        v[b].push_back(a);
+    }
+    
+    dfs(1);
+    
+    if(check[k]) printf("Yes\n");
+    else printf("No\n");
 }
