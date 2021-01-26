4d69d72ddb07258d9226d2a0c65e3f72eed01ce3
skyepodium <skyepodium@gmail.com>
2021-01-26 23:39:14 +0900

    ctf

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,33 +1,91 @@
 #include <iostream>
-#include <queue>
-#include <vector>
-#include <algorithm>
+#define lld unsigned long long int
 using namespace std;
 
-void solve() {
-    int n;
-    float m;
-    
-    cin >> n >> m;
-    
-    float cur = 0;
-    int result = -1;
-    for(int i=1; i<=n; i++) {
-        float a, b;
-        cin >> a >> b;
-        float num = (a * b) / 100;
-        cur += num;
-           
-        if(cur > m) {
-            result = i;
-            break;
-        }
-    }
-    
-    cout << result << endl;
-}
+int iVar1;
+lld local_b6;
+lld local_b4;
+lld local_ac;
+lld local_aa;
+lld local_a9;
+lld local_a1;
+lld local_9f;
+lld local_9b;
+lld local_9a;
+lld local_98;
+lld local_90;
+lld local_88;
+lld local_80;
+lld local_78;
+lld local_70;
+lld local_68;
+lld local_5f;
+lld local_5b;
+lld local_59;
+char local_58 [32];
+lld local_38;
+lld local_30;
+lld local_28;
+lld local_1b;
+lld local_13;
+lld local_f;
+lld local_d;
+int local_c;
+
 
 int main() {
-    ios_base::sync_with_stdio(0);
-    solve();
+    local_1b = 0x5f7372656b636168;
+    local_13 = 0x65636361;
+    local_f = 0x7373;
+    local_d = 0;
+    local_c = 10;
+    while (local_c < 0x14) {
+      local_c = local_c + 1;
+    }
+    local_38 = 0x5f687361632d6f6e;
+    local_30 = 0x64726f7773736170;
+    local_28 = 0;
+    local_5f = 0x64406873;
+    local_5b = 0x7730;
+    local_59 = 0;
+    puts("     .--------.");
+    printf("    / .------. \\n");
+    printf("   / /         \\n");
+    puts("   | |        | |");
+    puts("   | |        | |");
+    puts("  _| |________| |_");
+    puts(".\' |_|        |_| \'.");
+    puts("\'._____ ____ _____.\'");
+    puts("|     .\'____\'.     |");
+    puts("\'.__.\'.\'    \'.\'.__.\'");
+    puts("\'.__  Shad0w |  __.\'");
+    puts("|   \'.\'.____.\'.\'   |");
+    puts("\'.____\'.____.\'____.\'");
+    printf("\'.________________.\'");
+    local_98 = 0x73746172676e6f43;
+    local_90 = 0x662072756f79202c;
+    local_88 = 0x203a73692067616c;
+    local_80 = 0x653572657633527b;
+    local_78 = 0x6e65316c6168635f;
+    local_70 = 0x64656e77705f6567;
+    local_68 = 0x7d;
+    local_9a = 0x7b;
+    local_9f = 0x73363125;
+    local_9b = 0;
+    local_a9 = 0x6e69737265766572;
+    local_a1 = 0x67;
+    local_b4 = 0x6c69614620756f59;
+    local_ac = 0x6465;
+    local_aa = 0;
+    local_b6 = 0x7d;
+    puts("secure vault");
+    puts("Enter our password:");
+    puts("You Win");
+    printf((char *)&local_5f);
+    printf((char *)&local_9a);
+    printf((char *)&local_5f);
+    putchar(0x5f);
+    printf((char *)&local_a9);
+    printf("_rul3s");
+    printf((char *)&local_b6);
 }
