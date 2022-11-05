7396cf846b1ae36a2725e2bc7fc519887bf3a025
skyepodium <skyepodium@gmail.com>
2022-11-05 19:19:29 +0900

    leetcode - most-frequent-even-element

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,51 +1,45 @@
+#include <algorithm>
 #include <iostream>
-#include <queue>
+#include <string>
+#include <unordered_map>
+#include <vector>
 
 using namespace std;
 
 struct cmp {
-    bool operator()(int a, int b) {
-        return a > b;
+    bool operator()(const pair<int, int>& a, const pair<int, int>& b) {
+        if (a.second == b.second) {
+            return a.first < b.first;
+        }
+
+        return a.second > b.second;
     }
 };
 
-int x, s, res;
-priority_queue<int, vector<int>, cmp> pq;
+class Solution {
+   public:
+    int mostFrequentEven(vector<int>& nums) {
+        // 1. init
+        unordered_map<int, int> map;
+
+        // 2. count
+        for (auto num : nums) {
+            if (num & 1 == 1) continue;
+            map[num]++;
+        }
+
+        // 3. sort
+        vector<pair<int, int>> v(map.begin(), map.end());
+        sort(v.begin(), v.end(), cmp());
+
+        // 4. return
+        return v.size() > 0 ? v[0].first : -1;
+    }
+};
 
 int main() {
-    // 1. input
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-
-    cin >> x;
-
-    // 2. loop
-    s = 64;
-    pq.push(s);
-
-    while (!pq.empty() && s > x) {
-        int cur = pq.top();
-        s -= cur;
-        int half = cur / 2;
-        pq.pop();
-
-        if (half == 0) continue;
-
-        if (s + half > x) {
-            pq.push(half);
-            s += half;
-        } else {
-            pq.push(half);
-            pq.push(half);
-            s += cur;
-        }
-    }
-
-    while (!pq.empty()) {
-        if (pq.top() != 0) res++;
-        pq.pop();
-    }
-
-    cout << res << endl;
+    Solution s;
+    vector<int> nums = {4, 4, 4, 9, 2, 4};
+    cout << s.mostFrequentEven(nums) << endl;
+    return 0;
 }
