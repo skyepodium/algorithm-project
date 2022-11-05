441b2eff15c305de1cc1733c76ec43e54766c6ba
skyepodium <skyepodium@gmail.com>
2022-11-05 09:40:51 +0900

    baekjoon 1145

--- c++ project/main.cpp
+++ c++ project/main.cpp
@@ -1,66 +1,35 @@
+#include <algorithm>
 #include <iostream>
-#include <queue>
-#include <vector>
-#define max_int 1001
+#define MAX_INT 5
+#define MAX_VAL 1000000
 using namespace std;
 
-int max(int a, int b) {
-    return a > b ? a : b;
+int a[MAX_INT], res = MAX_VAL;
+
+int min(int a, int b) {
+    return a < b ? a : b;
 }
 
-struct Point {
-    int x, y;
-};
-
-int n, m, t, v[max_int][max_int], check[max_int][max_int], tomatoCnt, dx[] = {0, 0, 1, -1}, dy[] = {-1, 1, 0, 0}, res = 0;
-queue<Point> q;
-
-void bfs() {
-    while (!q.empty()) {
-        Point cur = q.front();
-        int x = cur.x;
-        int y = cur.y;
-        q.pop();
-
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-
-            if (nx < 0 || nx >= n || ny < 0 || ny >= m) {
-                continue;
-            }
-
-            if (check[nx][ny] != 0 || v[nx][ny] != 0) {
-                continue;
-            }
-
-            check[nx][ny] = check[x][y] + 1;
-            res = max(res, check[nx][ny]);
-            tomatoCnt--;
-            q.push({nx, ny});
-        }
-    }
+int gcd(int a, int b) {
+    return b == 0 ? a : gcd(b, a % b);
 }
 
 int main() {
-    scanf("%d %d", &m, &n);
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            scanf("%d", &v[i][j]);
-            t = v[i][j];
-            if (t == 1) {
-                q.push({i, j});
-            } else if (t == 0) {
-                tomatoCnt++;
-            }
-        }
+    for (int i = 0; i < MAX_INT; i++) {
+        scanf("%d", &a[i]);
     }
 
-    bfs();
+    sort(a, a + MAX_INT);
 
-    if (tomatoCnt != 0) {
-        res = -1;
+    for (int i = 0; i < MAX_INT; i++) {
+        for (int j = i + 1; j < MAX_INT; j++) {
+            for (int k = j + 1; k < MAX_INT; k++) {
+                int t = a[j] * a[k] / gcd(a[j], a[k]);
+                int r = a[i] * t / gcd(a[i], t);
+
+                res = min(res, r);
+            }
+        }
     }
 
     printf("%d\n", res);
